d95db24fe2d9c665393368789571838fcccf0ba8
hengfeiyang <hengfei.yang@gmail.com>
2023-02-13 10:46:51 +0800

    style: add License Header

--- src/handler/http/request/organization/mod.rs
+++ src/handler/http/request/organization/mod.rs
@@ -1,11 +1,24 @@
-use crate::{
-    handler::http::auth::is_admin_user,
-    infra::config::USERS,
-    service::organization::{self},
-};
+// Copyright 2022 Zinc Labs Inc. and Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use actix_web::{get, web, HttpResponse, Result};
 use serde::Serialize;
 use std::io::Error;
+
+use crate::handler::http::auth::is_admin_user;
+use crate::infra::config::USERS;
+use crate::service::organization;
 
 #[derive(Serialize)]
 struct Organization {
