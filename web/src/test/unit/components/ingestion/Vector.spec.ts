34147de7fee18313deebe7e2eec194c92ce6120f
Bhargav <BJP232004@GMAIL.COM>
2023-02-24 12:15:21 +0530

    feat: unit testing (#159)
    * feat: unit testing
    
    * Update HighLight.spec.ts

---
+++ web/src/test/unit/components/ingestion/Vector.spec.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it, vi } from "vitest";
+import { mount } from "@vue/test-utils";
+import { installQuasar } from "../../helpers/install-quasar-plugin";
+
+import Vector from "../../../../components/ingestion/Vector.vue";
+
+installQuasar();
+
+describe('Vector', async () => {
+    it("should mount Vector", async () => {
+        const wrapper = mount(Vector, {
+            shallow: false,
+            props: {
+                currOrgIdentifier: 'zinc_next',
+                currUserEmail: 'tulsiraval2828@gmail.com',
+                orgAPIKey: 'L"4\R{8f~56e72`0319V'
+            },
+        });
+
+        expect(wrapper.vm.currOrgIdentifier).not.toBe('')
+        expect(wrapper.vm.currUserEmail).not.toBe('')
+        expect(wrapper.vm.orgAPIKey).not.toBe('')
+    });
+})
