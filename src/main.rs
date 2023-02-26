da30677f3567996f10808e285a7d2a50c149b294
hengfeiyang <hengfei.yang@gmail.com>
2023-02-26 11:38:10 +0800

    feat: add content-length for access log

--- src/main.rs
+++ src/main.rs
@@ -163,7 +163,7 @@ async fn main() -> Result<(), anyhow::Error> {
                         .finish(),
                 ))
                 .wrap(middleware::Compress::default())
-                .wrap(middleware::Logger::default())
+                .wrap(middleware::Logger::new(r##"%a "%r" %s %b "%{Content-Length}i" "%{Referer}i" "%{User-Agent}i" %T"##))
                 .wrap(RequestTracing::new())
         })
         .bind(haddr)?
@@ -190,7 +190,7 @@ async fn main() -> Result<(), anyhow::Error> {
                 .app_data(web::Data::new(stats.clone()))
                 .app_data(web::Data::new(local_id))
                 .wrap(middleware::Compress::default())
-                .wrap(middleware::Logger::default())
+                .wrap(middleware::Logger::new(r##"%a "%r" %s %b "%{Content-Length}i" "%{Referer}i" "%{User-Agent}i" %T"##))
                 .wrap(RequestTracing::new())
         })
         .bind(haddr)?
