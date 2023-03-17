b9480e590d9d8797c6fb170dcffdeb3397e34925
hengfeiyang <hengfei.yang@gmail.com>
2023-03-18 01:43:53 +0800

    fix: unit test

--- src/meta/http.rs
+++ src/meta/http.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use actix_web::http;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
@@ -20,15 +19,14 @@ use crate::infra::errors;
 
 #[derive(Clone, Debug, Serialize, Deserialize, ToSchema)]
 pub struct HttpResponse {
-    pub code: u16,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub code: Option<u16>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub message: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub error: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub error_code: Option<u16>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub error_msg: Option<String>,
+    pub error_message: Option<String>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -39,43 +37,30 @@ pub struct ESResponse {
 }
 
 impl HttpResponse {
-    pub fn new(code: u16, message: Option<String>, error: Option<String>) -> Self {
-        HttpResponse {
-            code,
-            message,
-            error,
-            error_code: None,
-            error_msg: None,
-        }
-    }
-
     pub fn message(code: u16, message: String) -> Self {
         HttpResponse {
-            code,
+            code: Some(code),
             message: Some(message),
-            error: None,
             error_code: None,
-            error_msg: None,
+            error_message: None,
         }
     }
 
     pub fn error(code: u16, error: String) -> Self {
         HttpResponse {
-            code,
+            code: None,
             message: None,
-            error: Some(error),
-            error_code: None,
-            error_msg: None,
+            error_code: Some(code),
+            error_message: Some(error),
         }
     }
 
     pub fn error_code(err: errors::ErrorCodes) -> Self {
         HttpResponse {
-            code: http::StatusCode::INTERNAL_SERVER_ERROR.into(),
+            code: None,
             message: None,
-            error: None,
             error_code: Some(err.get_code()),
-            error_msg: Some(err.get_message()),
+            error_message: Some(err.get_message()),
         }
     }
 }
@@ -88,32 +73,33 @@ mod test {
     fn test_err_response() {
         let msg = "This is an error response";
         let err = HttpResponse::message(http::StatusCode::OK.into(), msg.to_string());
-        assert_eq!(err.code, http::StatusCode::OK);
+        assert_eq!(err.code.unwrap(), http::StatusCode::OK);
         assert_eq!(err.message.unwrap(), msg);
-        assert_eq!(err.error, None);
+        assert_eq!(err.error_code, None);
+        assert_eq!(err.error_message, None);
 
         let err = HttpResponse::error(
             http::StatusCode::INTERNAL_SERVER_ERROR.into(),
             msg.to_string(),
         );
-        assert_eq!(err.code, http::StatusCode::INTERNAL_SERVER_ERROR);
+        assert_eq!(err.code, None);
         assert_eq!(err.message, None);
-        assert_eq!(err.error.unwrap(), msg);
+        assert_eq!(
+            err.error_code.unwrap(),
+            http::StatusCode::INTERNAL_SERVER_ERROR
+        );
+        assert_eq!(err.error_message.unwrap(), msg);
 
         let err =
             HttpResponse::error_code(errors::ErrorCodes::ServerInternalError(msg.to_string()));
-        assert_eq!(
-            err.code,
-            errors::ErrorCodes::ServerInternalError(msg.to_string()).get_code()
-        );
+        assert_eq!(err.code, None);
         assert_eq!(err.message, None);
-        assert_eq!(err.error, None);
         assert_eq!(
             err.error_code.unwrap(),
             errors::ErrorCodes::ServerInternalError(msg.to_string()).get_code()
         );
         assert_eq!(
-            err.error_msg.unwrap(),
+            err.error_message.unwrap(),
             errors::ErrorCodes::ServerInternalError(msg.to_string()).get_message()
         );
     }
