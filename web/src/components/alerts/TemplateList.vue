8ae3e682daba0782ccf8c6b92bd76f86e2faa104
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-03-17 15:02:44 +0530

    add : UI changes and added AddTemplate component

--- web/src/components/alerts/TemplateList.vue
+++ web/src/components/alerts/TemplateList.vue
@@ -1,77 +1,82 @@
 <template>
   <q-page class="q-pa-none" style="min-height: inherit">
-    <q-table
-      ref="q-table"
-      :rows="destinations"
-      :columns="columns"
-      row-key="id"
-      style="width: 100%"
-    >
-      <template #no-data>
-        <NoData />
-      </template>
-      <template v-slot:body-cell-actions="props">
-        <q-td :props="props">
+    <div v-if="!showTemplateEditor">
+      <q-table
+        ref="q-table"
+        :rows="destinations"
+        :columns="columns"
+        row-key="id"
+        style="width: 100%"
+      >
+        <template #no-data>
+          <NoData />
+        </template>
+        <template v-slot:body-cell-actions="props">
+          <q-td :props="props">
+            <q-btn
+              icon="edit"
+              class="q-ml-xs iconHoverBtn"
+              padding="sm"
+              unelevated
+              size="sm"
+              round
+              flat
+              :title="t('alert_templates.edit')"
+              @click="editTemplate(props)"
+            ></q-btn>
+            <q-btn
+              :icon="'img:' + getImageURL('images/common/delete_icon.svg')"
+              class="q-ml-xs iconHoverBtn"
+              padding="sm"
+              unelevated
+              size="sm"
+              round
+              flat
+              :title="t('alert_templates.delete')"
+              @click="deleteTemplate(props)"
+            ></q-btn>
+          </q-td>
+        </template>
+        <template #top>
+          <div class="q-table__title">
+            {{ t("alert_templates.header") }}
+          </div>
+          <q-input
+            v-model="destinationSearchKey"
+            borderless
+            filled
+            dense
+            class="q-ml-auto q-mb-xs no-border"
+            :placeholder="t('alert_templates.search')"
+          >
+            <template #prepend>
+              <q-icon name="search" class="cursor-pointer" />
+            </template>
+          </q-input>
           <q-btn
-            icon="edit"
-            class="q-ml-xs iconHoverBtn"
-            padding="sm"
-            unelevated
-            size="sm"
-            round
-            flat
-            :title="t('alerts.edit')"
-            @click="editDestination(props)"
-          ></q-btn>
-          <q-btn
-            :icon="'img:' + getImageURL('images/common/delete_icon.svg')"
-            class="q-ml-xs iconHoverBtn"
-            padding="sm"
-            unelevated
-            size="sm"
-            round
-            flat
-            :title="t('alerts.delete')"
-            @click="deleteDestination(props)"
-          ></q-btn>
-        </q-td>
-      </template>
-      <template #top>
-        <div class="q-table__title">
-          {{ t("destinations.header") }}
-        </div>
-        <q-input
-          v-model="destinationSearchKey"
-          borderless
-          filled
-          dense
-          class="q-ml-auto q-mb-xs no-border"
-          :placeholder="t('destinations.search')"
-        >
-          <template #prepend>
-            <q-icon name="search" class="cursor-pointer" />
-          </template>
-        </q-input>
-        <q-btn
-          class="q-ml-md q-mb-xs text-bold no-border"
-          padding="sm lg"
-          color="secondary"
-          no-caps
-          :label="t(`destinations.add`)"
-          @click="addNewDestination({})"
-        />
-      </template>
+            class="q-ml-md q-mb-xs text-bold no-border"
+            padding="sm lg"
+            color="secondary"
+            no-caps
+            :label="t(`alert_templates.add`)"
+            @click="toggleTemplateEditor"
+          />
+        </template>
 
-      <template #bottom="scope">
-        <QTablePagination
-          :scope="scope"
-          :position="'bottom'"
-          :resultTotal="resultTotal"
-          :perPageOptions="perPageOptions"
-          @update:changeRecordPerPage="changePagination"
-        />
-      </template>
-    </q-table>
+        <template #bottom="scope">
+          <QTablePagination
+            :scope="scope"
+            :position="'bottom'"
+            :resultTotal="resultTotal"
+            :perPageOptions="perPageOptions"
+            @update:changeRecordPerPage="changePagination"
+          />
+        </template>
+      </q-table>
+    </div>
+    <div v-else>
+      <AddTemplate />
+    </div>
   </q-page>
 </template>
 <script lang="ts" setup>
@@ -80,6 +85,7 @@ import { useI18n } from "vue-i18n";
 import type { QTableProps } from "quasar";
 import NoData from "../shared/grid/NoData.vue";
 import { getImageURL } from "@/utils/zincutils";
+import { AddTemplate } from "./index";
 
 const destinations = ref([]);
 const { t } = useI18n();
@@ -93,33 +99,20 @@ const columns: any = ref<QTableProps["columns"]>([
   {
     name: "name",
     field: "name",
-    label: t("destionations.name"),
-    align: "left",
-    sortable: true,
-  },
-  {
-    name: "url",
-    field: "url",
-    label: t("destionations.url"),
-    align: "left",
-    sortable: false,
-  },
-  {
-    name: "method",
-    field: "method",
-    label: t("destionations.method"),
+    label: t("alert_templates.name"),
     align: "left",
     sortable: true,
   },
   {
     name: "actions",
     field: "actions",
-    label: t("alerts.actions"),
+    label: t("alert_templates.actions"),
     align: "center",
     sortable: false,
   },
 ]);
 const destinationSearchKey = ref("");
+const showTemplateEditor = ref(false);
 const perPageOptions: any = [
   { label: "5", value: 5 },
   { label: "10", value: 10 },
@@ -129,9 +122,15 @@ const perPageOptions: any = [
   { label: "All", value: 0 },
 ];
 const resultTotal = ref<number>(0);
-const editDestination = (props) => {};
-const deleteDestination = (props) => {};
-const addNewDestination = (props) => {};
+const editTemplate = (props: any) => {
+  console.log(props);
+};
+const deleteTemplate = (props: any) => {
+  console.log(props);
+};
 const changePagination = () => {};
+const toggleTemplateEditor = () => {
+  showTemplateEditor.value = !showTemplateEditor.value;
+};
 </script>
 <style lang=""></style>
