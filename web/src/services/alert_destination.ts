629a20265073656ab1e7fa1f6127083bcefc9f2d
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-03-20 20:05:06 +0530

    add: pagination in destinations

--- web/src/services/alert_destination.ts
+++ web/src/services/alert_destination.ts
@@ -7,8 +7,10 @@ const destination = {
       data
     );
   },
-  list: ({ org_identifier }: any) => {
-    return http().get(`/api/${org_identifier}/alerts/destinations`);
+  list: ({ org_identifier, page_num, page_size, desc, sort_by }: any) => {
+    return http().get(
+      `/api/${org_identifier}/alerts/destinations?page_num=${page_num}&page_size=${page_size}&sort_by=${sort_by}&desc=${desc}`
+    );
   },
   get_by_name: ({ org_identifier, destination_name }: any) => {
     return http().get(
