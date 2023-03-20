762b8e3cc5e823cf14526232c56ea4e3367de9c8
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-03-20 20:05:06 +0530

    add: pagination in destinations

--- web/src/components/alerts/DestinationList.vue
+++ web/src/components/alerts/DestinationList.vue
@@ -2,12 +2,12 @@
   <q-page class="q-pa-none" style="min-height: inherit">
     <div v-if="!showDestinationEditor">
       <q-table
-        ref="q-table"
+        ref="qTable"
         :rows="destinations"
         :columns="columns"
         row-key="id"
         style="width: 100%"
-        hide-bottom
+        :pagination="pagination"
       >
         <template #no-data>
           <NoData />
@@ -38,7 +38,7 @@
             ></q-btn>
           </q-td>
         </template>
-        <template #top>
+        <template #top="scope">
           <div class="q-table__title">
             {{ t("alert_destinations.header") }}
           </div>
@@ -62,6 +62,25 @@
             :label="t(`alert_destinations.add`)"
             @click="editDestination(null)"
           />
+
+          <QTablePagination
+            :scope="scope"
+            :pageTitle="t('alert_destinations.header')"
+            :position="'top'"
+            :resultTotal="resultTotal"
+            :perPageOptions="perPageOptions"
+            @update:changeRecordPerPage="changePagination"
+          />
+        </template>
+
+        <template #bottom="scope">
+          <QTablePagination
+            :scope="scope"
+            :position="'bottom'"
+            :resultTotal="resultTotal"
+            :perPageOptions="perPageOptions"
+            @update:changeRecordPerPage="changePagination"
+          />
         </template>
       </q-table>
     </div>
@@ -83,8 +102,14 @@
     />
   </q-page>
 </template>
-<script lang="ts" setup>
-import { ref, onBeforeMount, onActivated, defineEmits } from "vue";
+<script lang="ts">
+import {
+  ref,
+  onBeforeMount,
+  onActivated,
+  onMounted,
+  defineComponent,
+} from "vue";
 import { useI18n } from "vue-i18n";
 import type { QTableProps } from "quasar";
 import NoData from "../shared/grid/NoData.vue";
@@ -95,155 +120,215 @@ import templateService from "@/services/alert_templates";
 import { useStore } from "vuex";
 import ConfirmDialog from "../ConfirmDialog.vue";
 import { useRouter } from "vue-router";
+import QTablePagination from "@/components/shared/grid/Pagination.vue";
 
-const emit = defineEmits(["get:destinations"]);
+export default defineComponent({
+  name: "PageAlerts",
+  components: { AddDestination, NoData, ConfirmDialog, QTablePagination },
+  setup(props) {
+    const qTable = ref();
+    const store = useStore();
+    const editingDestination = ref(null);
+    const { t } = useI18n();
+    const columns: any = ref<QTableProps["columns"]>([
+      {
+        name: "#",
+        label: "#",
+        field: "#",
+        align: "left",
+      },
+      {
+        name: "name",
+        field: "name",
+        label: t("alert_destinations.name"),
+        align: "left",
+        sortable: true,
+      },
+      {
+        name: "url",
+        field: "url",
+        label: t("alert_destinations.url"),
+        align: "left",
+        sortable: false,
+      },
+      {
+        name: "method",
+        field: "method",
+        label: t("alert_destinations.method"),
+        align: "left",
+        sortable: true,
+      },
+      {
+        name: "actions",
+        field: "actions",
+        label: t("alert_destinations.actions"),
+        align: "center",
+        sortable: false,
+        style: "width: 110px",
+      },
+    ]);
+    const destinations = ref([]);
+    const templates = ref([]);
+    const confirmDelete = ref({ visible: false, data: null });
+    const destinationSearchKey = ref("");
+    const showDestinationEditor = ref(false);
+    const router = useRouter();
 
-const store = useStore();
-const editingDestination = ref(null);
-const { t } = useI18n();
-const columns: any = ref<QTableProps["columns"]>([
-  {
-    name: "#",
-    label: "#",
-    field: "#",
-    align: "left",
-  },
-  {
-    name: "name",
-    field: "name",
-    label: t("alert_destinations.name"),
-    align: "left",
-    sortable: true,
-  },
-  {
-    name: "url",
-    field: "url",
-    label: t("alert_destinations.url"),
-    align: "left",
-    sortable: false,
-  },
-  {
-    name: "method",
-    field: "method",
-    label: t("alert_destinations.method"),
-    align: "left",
-    sortable: true,
-  },
-  {
-    name: "actions",
-    field: "actions",
-    label: t("alert_destinations.actions"),
-    align: "center",
-    sortable: false,
-    style: "width: 110px",
-  },
-]);
-const destinations = ref([]);
-const templates = ref([]);
-const confirmDelete = ref({ visible: false, data: null });
-const destinationSearchKey = ref("");
-const showDestinationEditor = ref(false);
-const router = useRouter();
+    const perPageOptions: any = [
+      { label: "5", value: 5 },
+      { label: "10", value: 10 },
+      { label: "20", value: 20 },
+      { label: "50", value: 50 },
+      { label: "100", value: 100 },
+      { label: "All", value: 0 },
+    ];
+    const resultTotal = ref(0);
+    const selectedPerPage = ref(20);
+    const pagination: any = ref({
+      rowsPerPage: 20,
+    });
 
-onActivated(() => {
-  getTemplates();
-  if (!destinations.value.length) getDestinations();
+    onActivated(() => {
+      getTemplates();
+      if (!destinations.value.length) getDestinations();
+    });
+    onBeforeMount(() => {
+      getDestinations();
+      getTemplates();
+    });
+
+    onMounted(() => console.log(qTable));
+
+    const getDestinations = () => {
+      destinationService
+        .list({
+          page_num: 1,
+          page_size: 100000,
+          sort_by: "name",
+          desc: false,
+          org_identifier: store.state.selectedOrganization.identifier,
+        })
+        .then((res) => {
+          resultTotal.value = res.data.length;
+          destinations.value = res.data.map((data: any, index: number) => ({
+            ...data,
+            "#": index + 1 <= 9 ? `0${index + 1}` : index + 1,
+          }));
+          updateRoute();
+        })
+        .catch(() => console.log("Error while fetching destinations"));
+    };
+
+    const getTemplates = () => {
+      templateService
+        .list({
+          org_identifier: store.state.selectedOrganization.identifier,
+        })
+        .then((res) => (templates.value = res.data));
+    };
+
+    const updateRoute = () => {
+      if (router.currentRoute.value.query.action === "add")
+        editDestination(null);
+      if (router.currentRoute.value.query.action === "update")
+        editDestination(
+          getDestinationByName(router.currentRoute.value.query.name)
+        );
+    };
+
+    const getDestinationByName = (name) => {
+      return destinations.value.find(
+        (destination) => destination.name === name
+      );
+    };
+
+    const editDestination = (destination: any) => {
+      toggleDestionationEditor();
+      resetEditingDestination();
+      if (!destination) {
+        router.push({
+          name: "alertDestinations",
+          query: {
+            action: "add",
+            org_identifier: store.state.selectedOrganization.identifier,
+          },
+        });
+      } else {
+        editingDestination.value = { ...destination };
+        router.push({
+          name: "alertDestinations",
+          query: {
+            action: "update",
+            name: destination.name,
+            org_identifier: store.state.selectedOrganization.identifier,
+          },
+        });
+      }
+    };
+
+    const resetEditingDestination = () => {
+      editingDestination.value = null;
+    };
+
+    const deleteDestination = () => {
+      if (confirmDelete.value?.data?.name) {
+        destinationService
+          .delete({
+            org_identifier: store.state.selectedOrganization.identifier,
+            destination_name: confirmDelete.value.data.name,
+          })
+          .then(() => getDestinations());
+      }
+    };
+
+    const conformDeleteDestination = (destination: any) => {
+      confirmDelete.value.visible = true;
+      confirmDelete.value.data = destination;
+    };
+
+    const cancelDeleteDestination = () => {
+      confirmDelete.value.visible = false;
+      confirmDelete.value.data = null;
+    };
+
+    const toggleDestionationEditor = () => {
+      showDestinationEditor.value = !showDestinationEditor.value;
+      if (!showDestinationEditor.value)
+        router.push({
+          name: "alertDestinations",
+          query: {
+            org_identifier: store.state.selectedOrganization.identifier,
+          },
+        });
+    };
+    const changePagination = (val: { label: string; value: any }) => {
+      selectedPerPage.value = val.value;
+      pagination.value.rowsPerPage = val.value;
+      qTable.value.setPagination(pagination.value);
+    };
+    return {
+      t,
+      qTable,
+      showDestinationEditor,
+      destinations,
+      columns,
+      editDestination,
+      getImageURL,
+      conformDeleteDestination,
+      destinationSearchKey,
+      editingDestination,
+      templates,
+      toggleDestionationEditor,
+      getDestinations,
+      deleteDestination,
+      cancelDeleteDestination,
+      confirmDelete,
+      changePagination,
+      perPageOptions,
+      resultTotal,
+      pagination,
+    };
+  },
 });
-onBeforeMount(() => {
-  getDestinations();
-  getTemplates();
-});
-
-const getDestinations = () => {
-  destinationService
-    .list({
-      org_identifier: store.state.selectedOrganization.identifier,
-    })
-    .then((res) => {
-      destinations.value = res.data.map((data: any, index: number) => ({
-        ...data,
-        "#": index + 1 <= 9 ? `0${index + 1}` : index + 1,
-      }));
-      updateRoute();
-    });
-};
-
-const getTemplates = () => {
-  templateService
-    .list({
-      org_identifier: store.state.selectedOrganization.identifier,
-    })
-    .then((res) => (templates.value = res.data));
-};
-
-const updateRoute = () => {
-  if (router.currentRoute.value.query.action === "add") editDestination(null);
-  if (router.currentRoute.value.query.action === "update")
-    editDestination(getDestinationByName(router.currentRoute.value.query.name));
-};
-
-const getDestinationByName = (name) => {
-  return destinations.value.find((destination) => destination.name === name);
-};
-
-const editDestination = (destination: any) => {
-  toggleDestionationEditor();
-  resetEditingDestination();
-  if (!destination) {
-    router.push({
-      name: "alertDestinations",
-      query: {
-        action: "add",
-        org_identifier: store.state.selectedOrganization.identifier,
-      },
-    });
-  } else {
-    editingDestination.value = { ...destination };
-    router.push({
-      name: "alertDestinations",
-      query: {
-        action: "update",
-        name: destination.name,
-        org_identifier: store.state.selectedOrganization.identifier,
-      },
-    });
-  }
-};
-
-const resetEditingDestination = () => {
-  editingDestination.value = null;
-};
-
-const deleteDestination = () => {
-  if (confirmDelete.value?.data?.name) {
-    destinationService
-      .delete({
-        org_identifier: store.state.selectedOrganization.identifier,
-        destination_name: confirmDelete.value.data.name,
-      })
-      .then(() => getDestinations());
-  }
-};
-
-const conformDeleteDestination = (destination: any) => {
-  confirmDelete.value.visible = true;
-  confirmDelete.value.data = destination;
-};
-
-const cancelDeleteDestination = () => {
-  confirmDelete.value.visible = false;
-  confirmDelete.value.data = null;
-};
-
-const toggleDestionationEditor = () => {
-  showDestinationEditor.value = !showDestinationEditor.value;
-  if (!showDestinationEditor.value)
-    router.push({
-      name: "alertDestinations",
-      query: {
-        org_identifier: store.state.selectedOrganization.identifier,
-      },
-    });
-};
 </script>
 <style lang=""></style>
