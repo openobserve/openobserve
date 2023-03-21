255ee43a26e982b380658bde14aff4a890055158
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-03-21 18:15:50 +0530

    fix : ingestion module type errors

--- web/src/components/ingestion/Fluentd.vue
+++ web/src/components/ingestion/Fluentd.vue
@@ -51,10 +51,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, type Ref } from "vue";
 import config from "../../aws-exports";
 import { useStore } from "vuex";
 import { getImageURL } from "../../utils/zincutils";
+import type { Endpoint } from "@/ts/interfaces";
 
 export default defineComponent({
   name: "fluentd-mechanism",
@@ -68,7 +69,13 @@ export default defineComponent({
   },
   setup() {
     const store = useStore();
-    const endpoint = ref("");
+    const endpoint: Ref<Endpoint> = ref({
+      url: "",
+      host: "",
+      port: "",
+      protocol: "",
+      tls: "",
+    });
 
     const url = new URL(store.state.API_ENDPOINT);
     endpoint.value = {
