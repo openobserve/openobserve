b217af159f8738b700899fca1f8b2c53a2aeed9f
oasisk <ashish.j.kolhe@gmail.com>
2023-03-27 19:21:36 +0530

    added disclaimer

--- src/handler/http/request/functions/mod.rs
+++ src/handler/http/request/functions/mod.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(feature = "zo_functions")]
-use crate::meta;
 use actix_web::{delete, get, post, web, HttpResponse};
 #[cfg(feature = "zo_functions")]
 use actix_web::{http, HttpRequest};
