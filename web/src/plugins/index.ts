5e2027f71d7636ea8c54c2459afaa664c5f46c2a
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-04-03 02:08:12 +0530

    eslint formatting

--- web/src/plugins/index.ts
+++ web/src/plugins/index.ts
@@ -10,11 +10,11 @@
 //  distributed under the License is distributed on an "AS IS" BASIS,
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
-//  limitations under the License. 
+//  limitations under the License.
 
 import Logs from "./logs/Index.vue";
 export default {
-    install: (app:any, options:any) => {
-        app.component("zinc-logs", Logs);
-    }
-}
+  install: (app: any, options: any) => {
+    app.component("zinc-logs", Logs);
+  },
+};
