d0fd07dd02c8a493cb828ea6b992b96bfcfaf412
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2023-04-05 13:56:31 +0300

    refactor: reduce visibility of `service::db::dashboard::*`
    - Reduce visibility of `service::db::dashboard::*` functions ---
      make them visible within the current crate.
    - Remove verbose `db` identifiers.

--- src/service/db/dashboard.rs
+++ src/service/db/dashboard.rs
@@ -15,33 +15,33 @@
 use anyhow::Context as _;
 use tracing::instrument;
 
-use crate::{common::json, meta::dashboards::Dashboard};
+use crate::{common::json, infra::db, meta::dashboards::Dashboard};
 
 /// # Panics
 ///
 /// Panics if [`Dashboard::dashboard_id`] stored in the database is not equal to
 /// `dashboard_id` argument.
 #[instrument(err)]
-pub async fn get(org_id: &str, dashboard_id: &str) -> Result<Dashboard, anyhow::Error> {
-    let db = &crate::infra::db::DEFAULT;
+pub(crate) async fn get(org_id: &str, dashboard_id: &str) -> Result<Dashboard, anyhow::Error> {
     let key = format!("/dashboard/{org_id}/{dashboard_id}");
-    let bytes = db.get(&key).await?;
+    let bytes = db::DEFAULT.get(&key).await?;
     json::from_slice(&bytes)
         .with_context(|| format!("Failed to deserialize the value for key {key:?} as `Dashboard`"))
 }
 
 #[instrument(err, skip(dashboard))]
-pub async fn put(org_id: &str, dashboard: &Dashboard) -> Result<(), anyhow::Error> {
-    let db = &crate::infra::db::DEFAULT;
+pub(crate) async fn put(org_id: &str, dashboard: &Dashboard) -> Result<(), anyhow::Error> {
     let key = format!("/dashboard/{org_id}/{}", dashboard.dashboard_id);
-    Ok(db.put(&key, json::to_vec(dashboard)?.into()).await?)
+    Ok(db::DEFAULT
+        .put(&key, json::to_vec(dashboard)?.into())
+        .await?)
 }
 
 #[instrument(err)]
-pub async fn list(org_id: &str) -> Result<Vec<Dashboard>, anyhow::Error> {
-    let db = &crate::infra::db::DEFAULT;
+pub(crate) async fn list(org_id: &str) -> Result<Vec<Dashboard>, anyhow::Error> {
     let db_key = format!("/dashboard/{org_id}/");
-    db.list(&db_key)
+    db::DEFAULT
+        .list(&db_key)
         .await?
         .into_values()
         .map(|val| {
@@ -53,15 +53,13 @@ pub async fn list(org_id: &str) -> Result<Vec<Dashboard>, anyhow::Error> {
 }
 
 #[instrument(err)]
-pub async fn delete(org_id: &str, dashboard_id: &str) -> Result<(), anyhow::Error> {
-    let db = &crate::infra::db::DEFAULT;
+pub(crate) async fn delete(org_id: &str, dashboard_id: &str) -> Result<(), anyhow::Error> {
     let key = format!("/dashboard/{org_id}/{dashboard_id}");
-    Ok(db.delete(&key, false).await?)
+    Ok(db::DEFAULT.delete(&key, false).await?)
 }
 
 #[instrument(err)]
 pub async fn reset() -> Result<(), anyhow::Error> {
-    let db = &crate::infra::db::DEFAULT;
     let key = "/dashboard/";
-    Ok(db.delete(key, true).await?)
+    Ok(db::DEFAULT.delete(key, true).await?)
 }
