9a2ffa2b9d4be3c9068826cbfb34e80adc751a93
ktx-keshavi <124258826+ktx-keshavi@users.noreply.github.com>
2023-04-12 17:56:03 +0530

    feat: dashboard import export (#514)
    * Import and export (in progress)
    
    * complete import dashboard
    
    * remove export dialogue and validate json file while import json file
    
    * fix import errors
    
    * fix redirection issue while import file and delete dashboard

---
+++ web/src/components/dashboards/ExportDashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-btn class="q-ml-sm" outline padding="xs" color="primary" text-color="black" no-caps icon="download" @click="downloadDashboard()">
+  </q-btn>
+</template>
+<script lang="ts">
+// @ts-nocheck
+import { defineComponent, ref } from "vue";
+import { useI18n } from "vue-i18n";
+import { getDashboard } from "../../utils/commons.ts";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  name: "ExportDashboard",
+  props: ["dashboardId"],
+  setup(props, { emit }) {
+    const { t } = useI18n();
+    const store = useStore()
+    const downloadDashboard = async () => {
+      // get the dashboard
+      const dashboard = await getDashboard(store, props.dashboardId)
+
+      // prepare json and download via a click
+      const data = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(dashboard));
+      const htmlA = document.createElement('a');
+      htmlA.setAttribute("href", data);
+      const fileName = dashboard.title || "dashboard"
+      htmlA.setAttribute("download", fileName + ".dashboard.json");
+      htmlA.click();
+    }
+
+    return {
+      downloadDashboard
+    }
+  }
+})
+</script>
