1af2a7bea2f1eea2ade9d51f45ea9842e23a3817
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-05-01 19:01:49 +0530

    fix: reverted vite plugin installation in vite config plugins (#674)

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -103,7 +103,6 @@ export default defineConfig({
     }),
     enterpriseResolverPlugin,
     vueJsx(),
-    monacoEditorPlugin({}),
     isTesting && monacoEditorTestResolver(),
   ].filter(Boolean),
   resolve: {
