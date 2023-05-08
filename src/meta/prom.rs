d55110cc3dc0b8722576ec42d00910c08d6aa7bf
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2023-05-08 22:15:47 +0300

    refactor: convert `MetricType` directly from the proto enum
    We don't need any intermediate representation (`&str`).

--- src/meta/prom.rs
+++ src/meta/prom.rs
@@ -13,6 +13,9 @@
 // limitations under the License.
 
 use serde::{Deserialize, Serialize};
+use strum::Display;
+
+use crate::service::metrics::prom::prometheus as proto;
 
 pub const NAME_LABEL: &str = "__name__";
 pub const TYPE_LABEL: &str = "__type__";
@@ -28,62 +31,51 @@ pub const METADATA_LABEL: &str = "prom_metadata";
 pub type FxIndexMap<K, V> =
     indexmap::IndexMap<K, V, std::hash::BuildHasherDefault<rustc_hash::FxHasher>>;
 
-#[derive(Clone, Debug, Serialize)]
+#[derive(Debug, Clone, Serialize)]
 pub struct Metric {
     #[serde(flatten)]
     pub labels: FxIndexMap<String, String>,
     pub value: f64,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ClusterLeader {
     pub name: String,
     pub last_received: i64,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+// cf. https://github.com/prometheus/prometheus/blob/f5fcaa3872ce03808567fabc56afc9cf61c732cb/model/textparse/interface.go#L106-L119
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, Display)]
+#[strum(serialize_all = "lowercase")]
 pub enum MetricType {
-    UNKNOWN,
-    COUNTER,
-    GAUGE,
-    HISTOGRAM,
-    GAUGEHISTOGRAM,
-    SUMMARY,
-    INFO,
-    STATESET,
+    Unknown,
+    Counter,
+    Gauge,
+    Histogram,
+    GaugeHistogram,
+    Summary,
+    Info,
+    StateSet,
 }
 
-impl From<&str> for MetricType {
-    fn from(s: &str) -> Self {
-        match s.to_uppercase().as_str() {
-            "COUNTER" => MetricType::COUNTER,
-            "GAUGE" => MetricType::GAUGE,
-            "HISTOGRAM" => MetricType::HISTOGRAM,
-            "GAUGEHISTOGRAM" => MetricType::GAUGEHISTOGRAM,
-            "SUMMARY" => MetricType::SUMMARY,
-            "INFO" => MetricType::INFO,
-            "STATESET" => MetricType::STATESET,
-            _ => MetricType::UNKNOWN,
+impl From<proto::metric_metadata::MetricType> for MetricType {
+    fn from(mt: proto::metric_metadata::MetricType) -> Self {
+        use proto::metric_metadata::MetricType as ProtoMetricType;
+
+        match mt {
+            ProtoMetricType::Unknown => Self::Unknown,
+            ProtoMetricType::Counter => Self::Counter,
+            ProtoMetricType::Gauge => Self::Gauge,
+            ProtoMetricType::Histogram => Self::Histogram,
+            ProtoMetricType::Gaugehistogram => Self::GaugeHistogram,
+            ProtoMetricType::Summary => Self::Summary,
+            ProtoMetricType::Info => Self::Info,
+            ProtoMetricType::Stateset => Self::StateSet,
         }
     }
 }
 
-impl MetricType {
-    pub fn as_str_name(&self) -> &'static str {
-        match self {
-            MetricType::UNKNOWN => "UNKNOWN",
-            MetricType::COUNTER => "COUNTER",
-            MetricType::GAUGE => "GAUGE",
-            MetricType::HISTOGRAM => "HISTOGRAM",
-            MetricType::GAUGEHISTOGRAM => "GAUGEHISTOGRAM",
-            MetricType::SUMMARY => "SUMMARY",
-            MetricType::INFO => "INFO",
-            MetricType::STATESET => "STATESET",
-        }
-    }
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Metadata {
     pub metric_type: MetricType,
     pub metric_family_name: String,
@@ -135,4 +127,22 @@ pub struct RequestValues {
     pub matches: Option<Vec<String>>,
     pub start: Option<String>,
     pub end: Option<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_metric_type_display() {
+        assert_eq!(MetricType::Counter.to_string(), "counter");
+        assert_eq!(MetricType::Gauge.to_string(), "gauge");
+        assert_eq!(MetricType::Histogram.to_string(), "histogram");
+        assert_eq!(MetricType::GaugeHistogram.to_string(), "gaugehistogram");
+        assert_eq!(MetricType::Summary.to_string(), "summary");
+        assert_eq!(MetricType::Info.to_string(), "info");
+        assert_eq!(MetricType::StateSet.to_string(), "stateset");
+        assert_eq!(format!("{}", MetricType::Unknown), "unknown");
+        assert_eq!(MetricType::Unknown.to_string(), "unknown");
+    }
 }
