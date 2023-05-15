8318f40db2d5727bfc49095ad35c0df8bb5ebacb
hengfeiyang <hengfei.yang@gmail.com>
2023-05-15 23:38:47 +0800

    fix: format

--- src/service/promql/binaries/add.rs
+++ src/service/promql/binaries/add.rs
@@ -6,9 +6,9 @@ pub fn add(lhs: &Value, rhs: &Value) -> Result<Value> {
     if let (Value::Float(lhs), Value::Float(rhs)) = (lhs.clone(), rhs.clone()) {
         Ok(Value::Float(lhs + rhs))
     } else {
-        return Err(DataFusionError::NotImplemented(format!(
+        Err(DataFusionError::NotImplemented(format!(
             "Unsupported Binary: {:?} + {:?}",
             lhs, rhs
-        )));
+        )))
     }
 }
