5d6ba6b8dca9566919480aa37b5c8ae9b1e1ef2e
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2023-05-18 06:31:35 +0300

    feat: adopt Prometheus' `extrapolated_rate` (#764)

--- src/service/promql/functions/delta.rs
+++ src/service/promql/functions/delta.rs
@@ -14,14 +14,22 @@
 
 use datafusion::error::Result;
 
-use crate::service::promql::value::{RangeValue, Value};
+use crate::service::promql::value::{extrapolated_rate, ExtrapolationKind, RangeValue, Value};
 
 pub(crate) fn delta(data: &Value) -> Result<Value> {
     super::eval_idelta(data, "delta", exec)
 }
 
-fn exec(range: &RangeValue) -> Option<f64> {
-    range
-        .extrapolate()
-        .map(|(first, last)| last.value - first.value)
+fn exec(series: &RangeValue) -> Option<f64> {
+    let tw = series
+        .time_window
+        .as_ref()
+        .expect("BUG: `delta` function requires time window");
+    extrapolated_rate(
+        &series.samples,
+        tw.eval_ts,
+        tw.range,
+        tw.offset,
+        ExtrapolationKind::Delta,
+    )
 }
