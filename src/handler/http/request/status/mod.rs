d5f69f455af424c6dec00e375c9ac2ad5862d6f1
hengfeiyang <hengfei.yang@gmail.com>
2023-05-24 21:43:52 +0800

    feat: add back cache status api

--- src/handler/http/request/status/mod.rs
+++ src/handler/http/request/status/mod.rs
@@ -13,11 +13,14 @@
 // limitations under the License.
 
 use actix_web::{get, HttpResponse};
+use ahash::AHashMap;
 use serde::Serialize;
 use std::io::Error;
 use utoipa::ToSchema;
 
+use crate::common::json;
 use crate::infra::config::{self, CONFIG, INSTANCE_ID, SYSLOG_ENABLED};
+use crate::infra::{cache, cluster};
 use crate::meta::functions::ZoFunction;
 use crate::service::search::datafusion::DEFAULT_FUNCTIONS;
 
@@ -76,4 +79,38 @@ pub async fn zo_config() -> Result<HttpResponse, Error> {
         timestamp_column: CONFIG.common.column_timestamp.clone(),
         syslog_enabled: *SYSLOG_ENABLED.read(),
     }))
+}
+
+#[get("/cache/status")]
+pub async fn cache_status() -> Result<HttpResponse, Error> {
+    let mut stats: AHashMap<&str, json::Value> = AHashMap::new();
+    stats.insert(
+        "LOCAL_NODE_UUID",
+        json::json!(cluster::LOCAL_NODE_UUID.clone()),
+    );
+    stats.insert(
+        "LOCAL_NODE_NAME",
+        json::json!(&config::CONFIG.common.instance_name),
+    );
+    stats.insert(
+        "LOCAL_NODE_ROLE",
+        json::json!(&config::CONFIG.common.node_role),
+    );
+
+    stats.insert("STREAM_STATS", json::json!({"stream_num": cache::stats::get_stream_stats_len(), "mem_size": cache::stats::get_stream_stats_in_memory_size()}));
+    let (max_size, cur_size) = cache::file_data::stats();
+    stats.insert(
+        "FILE_DATA",
+        json::json!({"memory_limit":max_size,"mem_size": cur_size}),
+    );
+    let (file_list_num, files_num, mem_size) = cache::file_list::get_file_num().unwrap();
+    stats.insert(
+        "FILE_LIST",
+        json::json!({"file_list_num":file_list_num, "files_num":files_num, "mem_size":mem_size}),
+    );
+
+    let tmpfs_mem_size = cache::tmpfs::stats().unwrap();
+    stats.insert("TMPFS", json::json!({ "mem_size": tmpfs_mem_size }));
+
+    Ok(HttpResponse::Ok().json(stats))
 }
