5e4a755c6711013b3736293e796d7c32372a4379
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-06-05 02:57:01 +0530

    feat: Span Events in trace span details (#890)

--- web/src/plugins/traces/TraceDetailsSidebar.vue
+++ web/src/plugins/traces/TraceDetailsSidebar.vue
@@ -50,22 +50,98 @@
     class="text-bold q-mx-sm span_details_tabs"
   >
     <q-tab name="tags" label="Attributes" style="text-transform: capitalize" />
+    <q-tab name="events" label="Events" style="text-transform: capitalize" />
   </q-tabs>
   <q-separator style="width: 100%" />
   <q-tab-panels v-model="activeTab" class="span_details_tab-panels">
     <q-tab-panel name="tags">
-      <div v-for="key in Object.keys(span)" :key="key">
-        <div class="row q-py-sm q-px-sm border-bottom">
-          <div class="col-12 text-subtitle2 text-grey-8">{{ key }}</div>
-          <div class="col-12 text-subtitle2">{{ span[key] }}</div>
+      <div v-for="key in Object.keys(spanDetails.attrs)" :key="key">
+        <div class="row q-py-xs q-px-sm border-bottom">
+          <span class="attr-text text-grey-9 q-pr-sm text-bold"
+            >{{ key }}:</span
+          >
+          <span class="attr-text">{{ spanDetails.attrs[key] }}</span>
         </div>
+      </div>
+    </q-tab-panel>
+    <q-tab-panel name="events">
+      <q-virtual-scroll
+        type="table"
+        ref="searchTableRef"
+        style="max-height: 100%"
+        :items="spanDetails.events"
+      >
+        <template v-slot:before>
+          <thead class="thead-sticky text-left">
+            <tr>
+              <th
+                v-for="(col, index) in eventColumns"
+                :key="'result_' + index"
+                class="table-header"
+                :data-test="`trace-events-table-th-${col.label}`"
+              >
+                {{ col.label }}
+              </th>
+            </tr>
+          </thead>
+        </template>
+
+        <template v-slot="{ item: row, index }">
+          <q-tr
+            :data-test="`trace-event-detail-${
+              row[store.state.zoConfig.timestamp_column]
+            }`"
+            :key="'expand_' + index"
+            @click="expandEvent(index)"
+            style="cursor: pointer"
+            class="pointer"
+          >
+            <q-td
+              v-for="column in eventColumns"
+              :key="index + '-' + column.name"
+              class="field_list"
+              style="cursor: pointer"
+            >
+              <div class="flex row items-center no-wrap">
+                <q-btn
+                  v-if="column.name === '@timestamp'"
+                  :icon="
+                    expandedEvents[index.toString()]
+                      ? 'expand_more'
+                      : 'chevron_right'
+                  "
+                  dense
+                  size="xs"
+                  flat
+                  class="q-mr-xs"
+                  @click.stop="expandEvent(index)"
+                ></q-btn>
+                {{ column.prop(row) }}
+              </div>
+            </q-td>
+          </q-tr>
+          <q-tr v-if="expandedEvents[index.toString()]">
+            <td colspan="2">
+              <pre class="log_json_content">{{ row }}</pre>
+            </td>
+          </q-tr>
+        </template>
+      </q-virtual-scroll>
+      <div
+        class="full-width text-center q-pt-lg text-bold"
+        v-if="!spanDetails.events.length"
+      >
+        No events present for this span
       </div>
     </q-tab-panel>
   </q-tab-panels>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { cloneDeep } from "lodash-es";
+import { date, type QTableProps } from "quasar";
+import { defineComponent, onBeforeMount, ref, watch } from "vue";
+import { useStore } from "vuex";
 
 export default defineComponent({
   name: "TraceDetailsSidebar",
@@ -81,15 +157,251 @@ export default defineComponent({
     const closeSidebar = () => {
       emit("close");
     };
+    const spanDetails: any = ref({
+      attrs: {},
+      events: [],
+    });
+    const pagination: any = ref({
+      rowsPerPage: 0,
+    });
+
+    watch(
+      () => props.span,
+      () => {
+        spanDetails.value = getFormattedSpanDetails();
+      }
+    );
+
+    onBeforeMount(() => {
+      spanDetails.value = getFormattedSpanDetails();
+    });
+
+    const store = useStore();
+    const expandedEvents: any = ref({});
+    const eventColumns = ref([
+      {
+        name: "@timestamp",
+        field: (row: any) =>
+          date.formatDate(
+            Math.floor(row[store.state.zoConfig.timestamp_column] / 1000000),
+            "MMM DD, YYYY HH:mm:ss.SSS Z"
+          ),
+        prop: (row: any) =>
+          date.formatDate(
+            Math.floor(row[store.state.zoConfig.timestamp_column] / 1000000),
+            "MMM DD, YYYY HH:mm:ss.SSS Z"
+          ),
+        label: "Timestamp",
+        align: "left",
+        sortable: true,
+      },
+      {
+        name: "source",
+        field: (row: any) => JSON.stringify(row),
+        prop: (row: any) => JSON.stringify(row),
+        label: "source",
+        align: "left",
+        sortable: true,
+      },
+    ]);
+
+    const expandEvent = (index: number) => {
+      if (expandedEvents.value[index.toString()])
+        delete expandedEvents.value[index.toString()];
+      else expandedEvents.value[index.toString()] = true;
+    };
+
+    const getFormattedSpanDetails = () => {
+      const spanDetails: { attrs: any; events: any[] } = {
+        attrs: {},
+        events: [],
+      };
+
+      spanDetails.attrs = cloneDeep(props.span);
+
+      if (spanDetails.attrs.events) delete spanDetails.attrs.events;
+
+      spanDetails.attrs.duration = spanDetails.attrs.duration + "ms";
+      spanDetails.attrs[store.state.zoConfig.timestamp_column] =
+        date.formatDate(
+          Math.floor(
+            spanDetails.attrs[store.state.zoConfig.timestamp_column] / 1000
+          ),
+          "MMM DD, YYYY HH:mm:ss.SSS Z"
+        );
+
+      spanDetails.events = JSON.parse(props.span.events).map(
+        (event: any) => event
+      );
+
+      return spanDetails;
+    };
+
     return {
       activeTab,
       closeSidebar,
+      eventColumns,
+      expandedEvents,
+      expandEvent,
+      pagination,
+      spanDetails,
+      store,
     };
   },
 });
 </script>
 
 <style scoped lang="scss">
+.attr-text {
+  font-size: 12px;
+  font-family: monospace;
+}
+.table-header {
+  // text-transform: capitalize;
+
+  .table-head-chip {
+    background-color: $accent;
+    padding: 0px;
+
+    .q-chip__content {
+      margin-right: 0.5rem;
+      font-size: 0.75rem;
+      color: $dark;
+    }
+
+    .q-chip__icon--remove {
+      height: 1rem;
+      width: 1rem;
+      opacity: 1;
+      margin: 0;
+
+      &:hover {
+        opacity: 0.7;
+      }
+    }
+
+    .q-table th.sortable {
+      cursor: pointer;
+      text-transform: capitalize;
+      font-weight: bold;
+    }
+  }
+
+  &.isClosable {
+    padding-right: 26px;
+    position: relative;
+
+    .q-table-col-close {
+      transform: translateX(26px);
+      position: absolute;
+      margin-top: 2px;
+      color: grey;
+      transition: transform 0.3s cubic-bezier(0.25, 0.8, 0.5, 1);
+    }
+  }
+
+  .q-table th.sortable {
+    cursor: pointer;
+    text-transform: capitalize;
+    font-weight: bold;
+  }
+
+  .log_json_content {
+    white-space: pre-wrap;
+  }
+}
+.q-table__top {
+  padding-left: 0;
+  padding-top: 0;
+}
+
+.q-table thead tr,
+.q-table tbody td,
+.q-table th,
+.q-table td {
+  height: 25px;
+  padding: 0px 5px;
+  font-size: 0.75rem;
+}
+
+.q-table__bottom {
+  width: 100%;
+}
+
+.q-table__bottom {
+  min-height: 40px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+
+.q-td {
+  overflow: hidden;
+  min-width: 100px;
+
+  .expanded {
+    margin: 0;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    word-break: break-all;
+  }
+}
+
+.thead-sticky tr > *,
+.tfoot-sticky tr > * {
+  position: sticky;
+  opacity: 1;
+  z-index: 1;
+  background: #f5f5f5;
+  color: #090909;
+}
+.thead-sticky tr:last-child > * {
+  top: 0;
+}
+
+.tfoot-sticky tr:first-child > * {
+  bottom: 0;
+}
+
+.field_list {
+  padding: 0px;
+  margin-bottom: 0.125rem;
+  position: relative;
+  overflow: visible;
+  cursor: default;
+  font-size: 12px;
+  font-family: monospace;
+
+  .field_overlay {
+    position: absolute;
+    height: 100%;
+    right: 0;
+    top: 0;
+    background-color: #ffffff;
+    border-radius: 6px;
+    padding: 0 6px;
+    visibility: hidden;
+    display: flex;
+    align-items: center;
+    transition: all 0.3s linear;
+
+    .q-icon {
+      cursor: pointer;
+      opacity: 0;
+      transition: all 0.3s linear;
+      margin: 0 1px;
+    }
+  }
+
+  &:hover {
+    .field_overlay {
+      visibility: visible;
+
+      .q-icon {
+        opacity: 1;
+      }
+    }
+  }
+}
 .span_details_tab-panels {
   height: calc(100% - 130px);
   overflow-y: scroll;
@@ -99,13 +411,17 @@ export default defineComponent({
 
 <style lang="scss">
 .span_details_tabs {
-  .span_details_tab-panels {
-    .q-tab-panel {
-      padding: 8px;
-    }
-  }
   .q-tab__indicator {
     display: none;
+  }
+  .q-tab--active {
+    border-bottom: 1px solid var(--q-primary);
+  }
+}
+
+.span_details_tab-panels {
+  .q-tab-panel {
+    padding: 8px 0 8px 8px;
   }
 }
 </style>
