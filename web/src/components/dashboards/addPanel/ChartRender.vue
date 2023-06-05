8af1d3b357b458b348ced807c469119feac3711c
Kirtan Thakkar <kirtan@kiara.tech>
2023-06-05 17:21:26 +0530

    add legend click events for showing hiding traces

--- web/src/components/dashboards/addPanel/ChartRender.vue
+++ web/src/components/dashboards/addPanel/ChartRender.vue
@@ -174,6 +174,96 @@ export default defineComponent({
                   }
               );
 
+              plotRef.value.on('plotly_legendclick', function(clickData: any) {
+                    var xlen = clickData.data.length;
+                    console.log(clickData);
+                    console.log("index", clickData.expandedIndex);
+
+                    var traces = clickData.data;
+                    var clickedIndex = clickData.curveNumber;
+                    
+                    // set a few things first
+                    for (var i = 0; i < xlen; i++) {
+                        if(!clickData.data[i].hasOwnProperty('visible')) {
+                            clickData.data[i].visible = true;
+                        }
+                    }
+
+                    console.log(JSON.stringify(clickData.data.map((it: any)=> ({name: it.name, visible: it.visible})), null, 2));
+
+                    // if(clickData.data[clickData.expandedIndex].visible == true) {
+                    //     console.log('legend update mode: toggle');
+                        
+                    //     for (var i = 0; i < xlen; i++) {
+                    //         console.log("checking for index", i);
+                            
+                    //         if (i != clickData.expandedIndex) {
+                    //             console.log('updating value to "True" for index type: ', i);
+                    //             clickData.data[i].visible = true;
+                    //         } else {
+                    //             console.log('skipped index update for ', i);
+                    //         }
+                    //     }    
+                    // } else {
+                    //     console.log('legend update mode: hide');
+
+                    //     for (var i = 0; i < xlen; i++) {
+                    //         console.log("checking for index", i);
+                    //         if (i != clickData.expandedIndex) {
+                    //             console.log('updating value to "True" for index type: ', i);
+                    //             clickData.data[i].visible = 'legendonly';
+                    //         } else {
+                    //             console.log('skipped index update for ', i);
+                    //         }
+                    //     }
+                    // }
+
+                    if (clickedIndex === -1) {
+                        // When all are visible and you click on any one, hide all except the clicked one
+                        for (var i = 0; i < traces.length; i++) {
+                        traces[i].visible = i === 0 ? true : 'legendonly';
+                        }
+                    } else if (traces[clickedIndex].visible === true) {
+                        // When you click on the one which is shown, and others are disabled, show all legends
+                        for (var i = 0; i < traces.length; i++) {
+                        traces[i].visible = true;
+                        }
+                    } else {
+                        // Otherwise, make the clicked legend visible and hide all others
+                        for (var i = 0; i < traces.length; i++) {
+                        traces[i].visible = i === clickedIndex ? true : 'legendonly';
+                        }
+                    }
+                    console.log(JSON.stringify(clickData.data.map((it: any)=> ({name: it.name, visible: it.visible})), null, 2));
+                    // return false
+                });
+
+                // plotRef.value.on('plotly_restyle', function(eventData:any) {
+                //     var clickedItem = eventData[0].entry;
+                //     var traces = plotRef.value.data;
+
+                //     // Check if the clicked legend item is already visible
+                //     var isVisible = clickedItem.visible === true || clickedItem.visible === 'legendonly';
+
+                //     // Toggle the visibility of the clicked legend item
+                //     Plotly.toggle(plotRef.value, clickedItem.curveNumber);
+
+                //     // If the clicked legend item was already visible, show all legends
+                //     if (isVisible) {
+                //         for (var i = 0; i < traces.length; i++) {
+                //         Plotly.restyle(plotRef.value, { visible: true }, i);
+                //         }
+                //     } else {
+                //         // Otherwise, hide all legends except the clicked one
+                //         for (var i = 0; i < traces.length; i++) {
+                //         if (i !== clickedItem.curveNumber) {
+                //             Plotly.restyle(plotRef.value, { visible: 'legendonly' }, i);
+                //         }
+                //         }
+                //     }
+                //     });
+
+
               // plotRef.value.on('plotly_afterplot', function () {
               //     !searchQueryData.data.length ? noData.value = "No Data" : noData.value = ""
               // })
@@ -625,7 +715,9 @@ export default defineComponent({
                     autosize: true,
                     legend: {
                         bgcolor: "#f7f7f7",
-                        orientation: "h"
+                        orientation: "h",
+                        // itemclick: false,
+                        // groupclick: "toggleothers"
                     },
                     margin: {
                         autoexpand: true,
