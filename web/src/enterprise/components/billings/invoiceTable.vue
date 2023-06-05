a788e584072d2b7a48ef25a10d2a8072c1fe84f1
Bhargav <BJP232004@GMAIL.COM>
2023-06-05 10:41:23 +0530

    feat: stripe base integration (#892)

--- web/src/enterprise/components/billings/invoiceTable.vue
+++ web/src/enterprise/components/billings/invoiceTable.vue
@@ -145,6 +145,8 @@ export default defineComponent({
     });
 
     const getInvoiceHistory = () => {
+      //temporary disabled as stripe integration is in progress
+      return;
       const dismiss = $q.notify({
         spinner: true,
         message: "Please wait while loading invoice history...",
