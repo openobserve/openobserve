52253c72f88e452e6024ffc63d24eccdf8a02e51
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-06-14 18:48:28 +0530

    added stream data explorer page

---
+++ web/src/components/logstream/explore/SearchBar.vue
@@ -0,0 +1,417 @@
+<!-- Copyright 2022 Zinc Labs Inc. and Contributors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http:www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. 
+-->
+
+<template>
+  <div class="logs-search-bar-component" id="searchBarComponent">
+    <div class="row flex justify-end align-center">
+      <div class="col-auto q-my-xs">
+        <q-btn
+          class="q-mr-sm download-logs-btn q-px-sm"
+          size="sm"
+          :disabled="
+            queryData.queryResults.hasOwnProperty('hits') &&
+            !queryData.queryResults.hits.length
+          "
+          icon="download"
+          title="Export logs"
+          @click="downloadLogs"
+        ></q-btn>
+        <div class="float-left">
+          <date-time
+            data-test="logs-search-bar-date-time-dropdown"
+            @date-change="updateDateTime"
+          />
+        </div>
+        <div class="search-time q-pl-sm float-left q-mr-sm">
+          <q-btn
+            data-test="logs-search-bar-refresh-btn"
+            data-cy="search-bar-refresh-button"
+            dense
+            flat
+            title="Run query"
+            class="q-pa-none search-button"
+            @click="searchData"
+            >Run query</q-btn
+          >
+        </div>
+      </div>
+    </div>
+    <div class="row query-editor-container">
+      <div class="col" style="border-top: 1px solid #dbdbdb; height: 100px">
+        <b>Query Editor:</b>
+        <query-editor
+          id="logsQueryEditor"
+          ref="queryEditorRef"
+          class="monaco-editor"
+          v-model:query="searchObj.data.query"
+          v-model:fields="searchObj.data.stream.selectedStreamFields"
+          v-model:functions="searchObj.data.stream.functions"
+          @update-query="updateQueryValue"
+          @run-query="searchData"
+        ></query-editor>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+// @ts-nocheck
+import { defineComponent, ref, onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { useQuasar } from "quasar";
+
+import DateTime from "@/components/DateTime.vue";
+import useLogs from "@/composables/useLogs";
+import QueryEditor from "@/components/QueryEditor.vue";
+import jsTransformService from "@/services/jstransform";
+
+import { Parser } from "node-sql-parser";
+import segment from "@/services/segment_analytics";
+import config from "@/aws-exports";
+
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import search from "../../services/search";
+import { emit } from "process";
+
+const defaultValue: any = () => {
+  return {
+    name: "",
+    function: "",
+    params: "row",
+    transType: "0",
+  };
+};
+
+export default defineComponent({
+  name: "ComponentSearchSearchBar",
+  props: {
+    queryData: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
+  components: {
+    DateTime,
+    QueryEditor,
+  },
+  emits: ["searchdata", "update-query"],
+  methods: {
+    searchData() {
+      if (this.searchObj.loading == false) {
+        // this.searchObj.runQuery = true;
+        this.$emit("searchdata");
+      }
+    },
+  },
+  setup(props, { emit }) {
+    const router = useRouter();
+    const { t } = useI18n();
+    const store = useStore();
+    const btnRefreshInterval = ref(null);
+
+    const { searchObj } = useLogs();
+    const queryEditorRef = ref(null);
+
+    const functionOptions = ref(searchObj.data.transforms);
+
+    const functionModel: string = ref(null);
+    const fnEditorRef: any = ref(null);
+    const confirmDialogVisible: boolean = ref(false);
+    let confirmCallback;
+    let fnEditorobj: any = null;
+
+    const updateQueryValue = (value: string) => {
+      emit("update-query", value);
+    };
+
+    const updateDateTime = (value: object) => {
+      searchObj.data.datetime = value;
+
+      if (config.isCloud == "true" && value.userChangedValue) {
+        let dateTimeVal;
+        if (value.tab === "relative") {
+          dateTimeVal = value.relative;
+        } else {
+          dateTimeVal = value.absolute;
+        }
+
+        segment.track("Button Click", {
+          button: "Date Change",
+          tab: value.tab,
+          value: dateTimeVal,
+          //user_org: this.store.state.selectedOrganization.identifier,
+          //user_id: this.store.state.userInfo.email,
+          stream_name: searchObj.data.stream.selectedStream.value,
+          page: "Search Logs",
+        });
+      }
+    };
+
+    const udpateQuery = () => {
+      // alert(searchObj.data.query);
+      if (queryEditorRef.value?.setValue)
+        queryEditorRef.value.setValue(props.queryData.query);
+    };
+
+    const jsonToCsv = (jsonData) => {
+      const replacer = (key, value) => (value === null ? "" : value);
+      const header = Object.keys(jsonData[0]);
+      let csv = header.join(",") + "\r\n";
+
+      for (let i = 0; i < jsonData.length; i++) {
+        const row = header
+          .map((fieldName) => JSON.stringify(jsonData[i][fieldName], replacer))
+          .join(",");
+        csv += row + "\r\n";
+      }
+
+      return csv;
+    };
+
+    const downloadLogs = () => {
+      const filename = "logs-data.csv";
+      const data = jsonToCsv(props.queryData.queryResults.hits);
+      const file = new File([data], filename, {
+        type: "text/csv",
+      });
+      const url = URL.createObjectURL(file);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+    };
+
+    const resetEditorLayout = () => {
+      setTimeout(() => {
+        queryEditorRef.value.resetEditorLayout();
+        fnEditorobj.layout();
+      }, 100);
+    };
+
+    return {
+      t,
+      store,
+      router,
+      fnEditorRef,
+      fnEditorobj,
+      searchObj,
+      queryEditorRef,
+      btnRefreshInterval,
+      confirmDialogVisible,
+      confirmCallback,
+      refreshTimes: searchObj.config.refreshTimes,
+      updateQueryValue,
+      updateDateTime,
+      udpateQuery,
+      downloadLogs,
+      resetEditorLayout,
+      functionModel,
+      functionOptions,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+#logsQueryEditor,
+#fnEditor {
+  height: calc(100% - 20px) !important;
+}
+#fnEditor {
+  width: 100%;
+  border-radius: 5px;
+  border: 0px solid #dbdbdb;
+  overflow: hidden;
+}
+
+.q-field--standard .q-field__control:before,
+.q-field--standard .q-field__control:focus:before,
+.q-field--standard .q-field__control:hover:before {
+  border: 0px !important;
+  border-color: none;
+  transition: none;
+}
+
+.logs-search-bar-component > .row:nth-child(2) {
+  height: calc(100% - 38px); /* or any other height you want to set */
+}
+
+.logs-search-bar-component {
+  padding-bottom: 1px;
+  height: 100%;
+  overflow: visible;
+
+  .function-dropdown {
+    width: 205px;
+    padding-bottom: 0px;
+    border: 1px solid #dbdbdb;
+    border-radius: 5px;
+    cursor: pointer;
+
+    .q-field__input {
+      cursor: pointer;
+      color: #36383a;
+      font-weight: 600;
+      font-size: 12px;
+    }
+    .q-field__native,
+    .q-field__control {
+      min-height: 29px;
+      height: 29px;
+      padding: 0px 0px 0px 4px;
+    }
+
+    .q-field__marginal {
+      height: 30px;
+    }
+  }
+
+  .q-toggle__inner {
+    font-size: 30px;
+  }
+
+  .q-toggle__label {
+    font-size: 12px;
+  }
+
+  .casesensitive-btn {
+    padding: 8px;
+    margin-left: -6px;
+    background-color: #d5d5d5;
+    border-radius: 0px 3px 3px 0px;
+  }
+
+  .search-field .q-field {
+    &__control {
+      border-radius: 3px 0px 0px 3px !important;
+    }
+
+    &__native {
+      font-weight: 600;
+    }
+  }
+
+  .search-time {
+    // width: 120px;
+    .q-btn-group {
+      border-radius: 3px;
+
+      .q-btn {
+        min-height: auto;
+      }
+    }
+  }
+
+  .search-dropdown {
+    padding: 0px;
+
+    .block {
+      color: $dark-page;
+      font-weight: 600;
+      font-size: 12px;
+    }
+
+    .q-btn-dropdown__arrow-container {
+      color: $light-text2;
+    }
+  }
+
+  .refresh-rate-dropdown-container {
+    width: 220px;
+
+    * .q-btn {
+      font-size: 12px !important;
+      padding-left: 8px;
+      padding-right: 8px;
+    }
+  }
+
+  .flex-start {
+    justify-content: flex-start;
+    align-items: flex-start;
+    display: flex;
+  }
+
+  .resultsOverChart {
+    margin-bottom: 0.75rem;
+    font-size: 0.875rem;
+    color: $dark-page;
+    font-weight: 700;
+  }
+
+  .ddlWrapper {
+    position: relative;
+    z-index: 10;
+
+    .listWrapper {
+      box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.1);
+      transition: height 0.25s ease;
+      height: calc(100vh - 146px);
+      background-color: white;
+      position: absolute;
+      top: 2.75rem;
+      width: 100%;
+      left: 0;
+
+      &:empty {
+        height: 0;
+      }
+
+      &,
+      .q-list {
+        border-radius: 3px;
+      }
+    }
+  }
+
+  .fields_autocomplete {
+    max-height: 250px;
+  }
+
+  .search-button {
+    width: 96px;
+    line-height: 29px;
+    font-weight: bold;
+    text-transform: initial;
+    font-size: 11px;
+    color: white;
+
+    .q-btn__content {
+      background: $primary;
+      border-radius: 0px 3px 3px 0px;
+
+      .q-icon {
+        font-size: 15px;
+        color: #ffffff;
+      }
+    }
+  }
+
+  .download-logs-btn {
+    height: 30px;
+  }
+}
+
+.query-editor-container {
+  height: calc(100% - 40px) !important;
+}
+</style>
---
+++ web/src/views/StreamExplorer.vue
@@ -0,0 +1,305 @@
+<template>
+  <div>
+    <SearchBar
+      ref="searchBarRef"
+      :query-data="queryData"
+      @searchdata="getQueryData"
+      @update-query="updateQuery"
+    ></SearchBar>
+    <StreamDataTable
+      class="stream-data-table"
+      :rows="tableData.rows"
+      :columns="tableData.columns"
+      :is-loading="!!isLoading.length"
+      @update:scroll="getDataOnScroll"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import SearchBar from "@/components/logstream/explore/SearchBar.vue";
+import stream from "@/services/stream";
+import {
+  defineComponent,
+  onBeforeMount,
+  onBeforeUpdate,
+  onMounted,
+  ref,
+  type Ref,
+} from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import StreamDataTable from "@/components/logstream/explore/StreamDataTable.vue";
+import { useQuasar } from "quasar";
+import search from "@/services/search";
+import { logsErrorMessage } from "@/utils/common";
+import { useI18n } from "vue-i18n";
+import { getConsumableDateTime } from "@/utils/commons";
+import { convertTimeFromMicroToMilli } from "@/utils/zincutils";
+import { b64EncodeUnicode } from "@/utils/zincutils";
+import { cloneDeep } from "lodash-es";
+
+export default defineComponent({
+  name: "StreamExplorer",
+  components: { SearchBar, StreamDataTable },
+  setup(props) {
+    const store = useStore();
+    const router = useRouter();
+    const streamData: any = ref(null);
+    const isLoading: Ref<boolean[]> = ref([]);
+    const { t } = useI18n();
+    const searchBarRef = ref(null);
+    const queryData = ref({
+      query: "",
+      errorMsg: "",
+      errorCode: 0,
+      dateTime: {
+        tab: "relative",
+        relative: {
+          period: {
+            label: "Minutes",
+            value: "Minutes",
+          },
+          value: 15,
+        },
+        absolute: {
+          date: {
+            from: "",
+            to: "",
+          },
+          startTime: "00:00",
+          endTime: "23:59",
+        },
+      },
+      pagination: {
+        currentPage: 0,
+        rowsPerPage: 150,
+      },
+      queryResults: {} as any,
+    });
+    const q = useQuasar();
+
+    const tableData = ref({
+      rows: [],
+      columns: [],
+    });
+    onBeforeMount(() => {
+      // From router decode the stream name and stream type
+      // and call the schema api
+      const params = router.currentRoute.value.query;
+
+      queryData.value.query = 'SELECT * FROM "' + params.stream_name + '"';
+      stream
+        .schema(
+          store.state.selectedOrganization.identifier,
+          params.stream_name as string,
+          params.stream_type as string
+        )
+        .then((res) => {
+          streamData.value = res.data;
+          tableData.value["columns"] = res.data.schema.map((field: any) => ({
+            name: field.name,
+            label: field.name,
+            align: "left",
+            field: (row: any) => JSON.stringify(row),
+            prop: (row: any) => JSON.stringify(row),
+            type: field.type,
+          }));
+          getQueryData();
+        })
+        .catch((err) => console.log(err));
+    });
+
+    onMounted(() => {
+      if (searchBarRef.value != null) {
+        searchBarRef.value.udpateQuery(queryData.value.query);
+      }
+    });
+
+    function Notify() {
+      return q.notify({
+        type: "positive",
+        message: "Waiting for response...",
+        timeout: 10000,
+        actions: [
+          {
+            icon: "close",
+            color: "white",
+            handler: () => {
+              /* ... */
+            },
+          },
+        ],
+      });
+    }
+
+    function ErrorException(message: string) {
+      isLoading.value.pop();
+      // searchObj.data.errorMsg = message;
+      q.notify({
+        type: "negative",
+        message: message,
+        timeout: 10000,
+        actions: [
+          {
+            icon: "close",
+            color: "white",
+            handler: () => {
+              /* ... */
+            },
+          },
+        ],
+      });
+    }
+
+    function getQueryData() {
+      try {
+        isLoading.value.push(true);
+        queryData.value.errorMsg = "";
+
+        const dismiss = Notify();
+
+        const queryReq = buildSearch();
+
+        if (queryReq == null) {
+          dismiss();
+          return false;
+        }
+
+        queryData.value.errorCode = 0;
+        search
+          .search({
+            org_identifier: store.state.selectedOrganization.identifier,
+            query: queryReq,
+            page_type: streamData.value?.stream_type || "logs",
+          })
+          .then((res) => {
+            isLoading.value.pop();
+            if (res.data.from > 0) {
+              queryData.value.queryResults["from"] = res.data.from;
+              queryData.value.queryResults.scan_size += res.data.scan_size;
+              queryData.value.queryResults.took += res.data.took;
+              queryData.value.queryResults.hits.push(...res.data.hits);
+            } else {
+              queryData.value.queryResults = res.data;
+            }
+
+            tableData.value.rows = queryData.value.queryResults.hits;
+
+            dismiss();
+          })
+          .catch((err) => {
+            isLoading.value.pop();
+            dismiss();
+            if (err.response != undefined) {
+              queryData.value.errorMsg = err.response.data.error;
+            } else {
+              queryData.value.errorMsg = err.message;
+            }
+            const customMessage = logsErrorMessage(err.response.data.code);
+            queryData.value.errorCode = err.response.data.code;
+            if (customMessage != "") {
+              queryData.value.errorMsg = t(customMessage);
+            }
+
+            // $q.notify({
+            //   message: searchObj.data.errorMsg,
+            //   color: "negative",
+            // });
+          });
+      } catch (e) {
+        console.error(e);
+        // throw errorE("Request failed.");
+      }
+    }
+
+    function buildSearch() {
+      try {
+        var req: any = {
+          query: {
+            sql: queryData.value.query,
+            start_time: (new Date().getTime() - 900000) * 1000,
+            end_time: new Date().getTime() * 1000,
+            from:
+              queryData.value.pagination.currentPage *
+              queryData.value.pagination.rowsPerPage,
+            size: parseInt(
+              queryData.value.pagination.rowsPerPage.toString(),
+              10
+            ),
+            sql_mode: "full",
+          },
+        };
+
+        var timestamps: any = getConsumableDateTime(queryData.value.dateTime);
+        if (streamData.value.stream_type === "enrichment_tables") {
+          if (streamData.value.stats) {
+            timestamps = {
+              start_time: new Date(
+                convertTimeFromMicroToMilli(
+                  streamData.value.stats.doc_time_min - 300000000
+                )
+              ),
+              end_time: new Date(
+                convertTimeFromMicroToMilli(
+                  streamData.value.stats.doc_time_max + 300000000
+                )
+              ),
+            };
+          }
+        }
+
+        if (
+          timestamps.start_time != "Invalid Date" &&
+          timestamps.end_time != "Invalid Date"
+        ) {
+          const startISOTimestamp: any =
+            new Date(timestamps.start_time.toISOString()).getTime() * 1000;
+          const endISOTimestamp: any =
+            new Date(timestamps.end_time.toISOString()).getTime() * 1000;
+
+          req.query.start_time = startISOTimestamp;
+          req.query.end_time = endISOTimestamp;
+        } else {
+          return false;
+        }
+
+        if (store.state.zoConfig.sql_base64_enabled) {
+          req["encoding"] = "base64";
+          req.query.sql = b64EncodeUnicode(req.query.sql);
+        }
+
+        return req;
+      } catch (e) {
+        console.error(e);
+        // throw new ErrorException(e.message);
+      }
+    }
+
+    const updateQuery = (value: string) => {
+      queryData.value.query = value;
+    };
+
+    const getDataOnScroll = () => {
+      queryData.value.pagination.currentPage++;
+      getQueryData();
+    };
+
+    return {
+      tableData,
+      queryData,
+      getQueryData,
+      searchBarRef,
+      updateQuery,
+      getDataOnScroll,
+      isLoading,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.stream-data-table {
+  height: calc(100vh - (140px + 57px));
+}
+</style>
