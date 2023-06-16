b903275dc4c80a535e6511a17b7cd82871a75508
Ankur Srivastava <best.ankur@gmail.com>
2023-06-16 17:59:34 +0200

    feat: initial structure of promql vector bin ops
    For binary operation between two vectors we support
    `and`, `or` and `unless` operations in this PR.

--- src/service/promql/binaries/vector_operations.rs
+++ src/service/promql/binaries/vector_operations.rs
@@ -1,10 +1,12 @@
-use datafusion::error::Result;
+use ahash::HashSet;
 
 use crate::service::promql::{
     binaries::scalar_binary_operations,
-    value::{InstantValue, Sample, Value},
+    value::{signature, InstantValue, Sample, Signature, Value},
 };
-use promql_parser::parser::BinaryExpr;
+use datafusion::error::{DataFusionError, Result};
+use promql_parser::parser::{token, BinaryExpr, VectorMatchCardinality};
+use rayon::prelude::*;
 
 /// Implement the operation between a vector and a float.
 ///
@@ -17,7 +19,7 @@ pub async fn vector_scalar_bin_op(
     let is_comparison_operator = expr.op.is_comparison_operator();
 
     let output: Vec<InstantValue> = left
-        .iter()
+        .par_iter()
         .map(|instant| {
             let value =
                 scalar_binary_operations(expr.op.id(), instant.sample.value, right).unwrap();
@@ -37,4 +39,158 @@ pub async fn vector_scalar_bin_op(
         })
         .collect();
     Ok(Value::Vector(output))
+}
+
+/// vector1 or vector2 results in a vector that contains all original elements (label sets + values)
+/// of vector1 and additionally all elements of vector2 which do not have matching label sets in vector1.
+///
+/// https://prometheus.io/docs/prometheus/latest/querying/operators/#logical-set-binary-operators
+fn vector_or(expr: &BinaryExpr, left: &[InstantValue], right: &[InstantValue]) -> Result<Value> {
+    if expr.modifier.as_ref().unwrap().card != VectorMatchCardinality::ManyToMany {
+        return Err(DataFusionError::NotImplemented(
+            "set operations must only use many-to-many matching".to_string(),
+        ));
+    }
+
+    if left.is_empty() {
+        return Ok(Value::Vector(right.to_vec()));
+    }
+
+    if right.is_empty() {
+        return Ok(Value::Vector(left.to_vec()));
+    }
+
+    let lhs_sig: HashSet<Signature> = left
+        .par_iter()
+        .map(|item| signature(&item.labels))
+        .collect();
+
+    // Add all right-hand side elements which have not been added from the left-hand side.
+    let right_instants: Vec<InstantValue> = right
+        .par_iter()
+        .filter(|item| {
+            let right_sig = signature(&item.labels);
+            !lhs_sig.contains(&right_sig)
+        })
+        .map(|item| item.clone())
+        .collect();
+
+    let output = [left, &right_instants].concat();
+    Ok(Value::Vector(output))
+}
+
+/// vector1 unless vector2 results in a vector consisting of the elements of vector1
+/// for which there are no elements in vector2 with exactly matching label sets.
+/// All matching elements in both vectors are dropped.
+///
+/// https://prometheus.io/docs/prometheus/latest/querying/operators/#logical-set-binary-operators
+fn vector_unless(
+    expr: &BinaryExpr,
+    left: &[InstantValue],
+    right: &[InstantValue],
+) -> Result<Value> {
+    if expr.modifier.as_ref().unwrap().card != VectorMatchCardinality::ManyToMany {
+        return Err(DataFusionError::NotImplemented(
+            "set operations must only use many-to-many matching".to_string(),
+        ));
+    }
+
+    // If right is empty, we simply return the left
+    // if left is empty we will return it anyway.
+    if left.is_empty() || right.is_empty() {
+        return Ok(Value::Vector(left.to_vec()));
+    }
+    // Generate all the signatures from the right hand.
+    let rhs_sig: HashSet<Signature> = right
+        .par_iter()
+        .map(|item| signature(&item.labels))
+        .collect();
+
+    // Now filter out all the matching labels from left.
+    let output: Vec<InstantValue> = left
+        .par_iter()
+        .filter(|item| {
+            let left_sig = signature(&item.labels);
+            !rhs_sig.contains(&left_sig)
+        })
+        .map(|val| val.clone())
+        .collect();
+    Ok(Value::Vector(output))
+}
+
+/// vector1 and vector2 results in a vector consisting of the elements of vector1 for which there
+/// are elements in vector2 with exactly matching label sets.
+/// Other elements are dropped. The metric name and values are carried over from the left-hand side vector.
+///
+/// https://prometheus.io/docs/prometheus/latest/querying/operators/#logical-set-binary-operators
+fn vector_and(expr: &BinaryExpr, left: &[InstantValue], right: &[InstantValue]) -> Result<Value> {
+    if expr.modifier.as_ref().unwrap().card != VectorMatchCardinality::ManyToMany {
+        return Err(DataFusionError::NotImplemented(
+            "set operations must only use many-to-many matching".to_string(),
+        ));
+    }
+
+    if left.is_empty() || right.is_empty() {
+        return Err(DataFusionError::NotImplemented(
+            "Either left or right operand is empty.".to_string(),
+        ));
+    }
+
+    let rhs_sig: HashSet<Signature> = right
+        .par_iter()
+        .map(|item| signature(&item.labels))
+        .collect();
+
+    // Now include all the matching ones from the right
+    let output: Vec<InstantValue> = left
+        .par_iter()
+        .filter(|item| {
+            let left_sig = signature(&item.labels);
+            rhs_sig.contains(&left_sig)
+        })
+        .map(|val| val.clone())
+        .collect();
+
+    Ok(Value::Vector(output))
+}
+
+/// Implement binary operations between two vectors
+///
+/// https://prometheus.io/docs/prometheus/latest/querying/operators/#comparison-binary-operators
+///
+/// Between two instant vectors, a binary arithmetic operator is applied to each entry in the
+/// left-hand side vector and its matching element in the right-hand vector. The result is
+/// propagated into the result vector with the grouping labels becoming the output label set.
+/// The metric name is dropped. Entries for which no matching entry in the right-hand vector
+/// can be found are not part of the result.
+///
+///
+/// Between two instant vectors, comparison binary operators behave as a filter by default,
+/// applied to matching entries. Vector elements for which the expression is not true or which
+/// do not find a match on the other side of the expression get dropped from the result, while
+/// the others are propagated into a result vector with the grouping labels becoming the output
+/// label set. If the bool modifier is provided, vector elements that would have been dropped
+/// instead have the value 0 and vector elements that would be kept have the value 1, with the
+/// grouping labels again becoming the output label set. The metric name is dropped if the bool
+/// modifier is provided.
+pub fn vector_bin_op(
+    expr: &BinaryExpr,
+    left: &[InstantValue],
+    right: &[InstantValue],
+) -> Result<Value> {
+    match expr.op.id() {
+        token::T_LAND => {
+            return vector_and(expr, left, right);
+        }
+
+        token::T_LOR => {
+            return vector_or(expr, left, right);
+        }
+        token::T_LUNLESS => {
+            return vector_unless(expr, left, right);
+        }
+        _ => {}
+    }
+
+    Ok(Value::None)
 }
