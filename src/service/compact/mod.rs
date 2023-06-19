7e5caeea9ec0c06f3d3f3e264221a83ba044b667
hengfeiyang <hengfei.yang@gmail.com>
2023-06-20 00:30:22 +0800

    fix: add debug logs for compactor

--- src/service/compact/mod.rs
+++ src/service/compact/mod.rs
@@ -157,6 +157,12 @@ pub async fn run_merge() -> Result<(), anyhow::Error> {
                 let org_id = org_id.clone();
                 let permit = semaphore.clone().acquire_owned().await.unwrap();
                 let task = tokio::task::spawn(async move {
+                    log::info!(
+                        "[COMPACTOR] start merge stream [{}/{}/{}]",
+                        org_id,
+                        stream_type,
+                        stream_name
+                    );
                     if let Err(e) = merge::merge_by_stream(
                         last_file_list_offset,
                         &org_id,
