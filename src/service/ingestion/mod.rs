c11e5f4aa58efebac92d83122d3fcad0777d1fdf
ziv <xiaozheng@alumni.tongji.edu.cn>
2023-07-08 09:38:07 +0800

    refactor: avoid clone

--- src/service/ingestion/mod.rs
+++ src/service/ingestion/mod.rs
@@ -422,7 +422,7 @@ mod tests {
         assert_eq!(
             get_hour_key(
                 1620000000,
-                vec!["country".to_string(), "sport".to_string()],
+                &vec!["country".to_string(), "sport".to_string()],
                 &local_val,
                 None
             ),
@@ -436,14 +436,14 @@ mod tests {
         local_val.insert("country".to_string(), Value::String("USA".to_string()));
         local_val.insert("sport".to_string(), Value::String("basketball".to_string()));
         assert_eq!(
-            get_hour_key(1620000000, vec![], &local_val, None),
+            get_hour_key(1620000000, &vec![], &local_val, None),
             "1970_01_01_00_keeping"
         );
     }
     #[test]
     fn test_get_hour_key_no_partition_keys_no_local_val() {
         assert_eq!(
-            get_hour_key(1620000000, vec![], &Map::new(), None),
+            get_hour_key(1620000000, &vec![], &Map::new(), None),
             "1970_01_01_00_keeping"
         );
     }
