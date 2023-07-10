e1ef8cfbe68cb60ec8c694ae7f4c181ad21326d5
Kirtan Thakkar <kirtan@kiara.tech>
2023-07-10 16:55:51 +0530

    show labels or name of the variable in the values picker

--- web/src/components/dashboards/VariablesValueSelector.vue
+++ web/src/components/dashboards/VariablesValueSelector.vue
@@ -2,16 +2,16 @@
   <div v-if="variablesData.values?.length > 0 && !variablesData.isVariablesLoading" class="flex q-mt-sm q-ml-sm">
     <div v-for="item in variablesData.values" class="q-mr-lg">
       <div v-if="item.type == 'query'">
-        <q-select outlined dense v-model="item.value" :options="item.options" :label="item.label"></q-select>
+        <q-select outlined dense v-model="item.value" :options="item.options" :label="item.label || item.name"></q-select>
       </div>
       <div v-else-if="item.type == 'constant'">
-        <q-input v-model="item.name" :label="item.label" dense outlined readonly></q-input>
+        <q-input v-model="item.name" :label="item.label || item.name" dense outlined readonly></q-input>
       </div>
       <div v-else-if="item.type == 'textbox'">
-        <q-input v-model="item.name" :label="item.label" dense outlined></q-input>
+        <q-input v-model="item.name" :label="item.label || item.name" dense outlined></q-input>
       </div>
       <div v-else-if="item.type == 'custom_fields'">
-        <q-select outlined dense v-model="item.value" :options="item.options" :label="item.label"></q-select>
+        <q-select outlined dense v-model="item.value" :options="item.options" :label="item.label || item.name"></q-select>
       </div>
     </div>
   </div>
