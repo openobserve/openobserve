c4746d857f684ad22de877e5ef849f226daa104a
Ankur Srivastava <ansrivas@users.noreply.github.com>
2023-07-12 07:08:35 +0200

    feat: promql stdvar and stddev aggregation functions (#1153)

--- src/service/promql/functions/stddev_over_time.rs
+++ src/service/promql/functions/stddev_over_time.rs
@@ -14,40 +14,12 @@
 
 use datafusion::error::Result;
 
+use crate::service::promql::common::std_deviation;
 use crate::service::promql::value::{RangeValue, Value};
 
 /// https://prometheus.io/docs/prometheus/latest/querying/functions/#stddev_over_time
 pub(crate) fn stddev_over_time(data: &Value) -> Result<Value> {
     super::eval_idelta(data, "stddev_over_time", exec, false)
-}
-
-fn mean(data: &[f64]) -> Option<f64> {
-    let sum = data.iter().sum::<f64>();
-    let count = data.len();
-
-    match count {
-        positive if positive > 0 => Some(sum / count as f64),
-        _ => None,
-    }
-}
-
-fn std_deviation(data: &[f64]) -> Option<f64> {
-    match (mean(data), data.len()) {
-        (Some(data_mean), count) if count > 0 => {
-            let variance = data
-                .iter()
-                .map(|value| {
-                    let diff = data_mean - *value;
-
-                    diff * diff
-                })
-                .sum::<f64>()
-                / count as f64;
-
-            Some(variance.sqrt())
-        }
-        _ => None,
-    }
 }
 
 fn exec(data: &RangeValue) -> Option<f64> {
