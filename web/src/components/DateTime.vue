8a4f2e3392c6d0e2117d21f65216f523c380e068
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-07-24 17:21:06 +0530

    fix: Date Time Selector do not get updated when a time range is selected from the chart
    fix #1235

--- web/src/components/DateTime.vue
+++ web/src/components/DateTime.vue
@@ -293,6 +293,8 @@ export default defineComponent({
 
     const displayValue = ref("");
 
+    const datePayload = ref({});
+
     const dateLocale = {
       daysShort: ["S", "M", "T", "W", "T", "F", "S"],
     };
@@ -333,6 +335,21 @@ export default defineComponent({
         if (props.autoApply) saveDate("absolute");
       },
       { deep: true }
+    );
+
+    watch(
+      () => props.defaultAbsoluteTime,
+      (value) => {
+        if (
+          value.startTime !== datePayload.value.startTime ||
+          value.endTime !== datePayload.value.endTime
+        ) {
+          setAbsoluteTime(value.startTime, value.endTime);
+        }
+      },
+      {
+        deep: true,
+      }
     );
 
     const setRelativeDate = (period, value) => {
@@ -408,6 +425,7 @@ export default defineComponent({
     const saveDate = (dateType) => {
       displayValue.value = getDisplayValue();
       const date = getConsumableDateTime();
+      datePayload.value = date;
       date["valueType"] = dateType || selectedType.value;
       emit("on:date-change", date);
     };
