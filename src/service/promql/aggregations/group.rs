b8db846f3e155f9480762fcddb3da785791c37ee
Ankur Srivastava <best.ankur@gmail.com>
2023-07-27 18:25:25 +0200

    feat: promql `group` keyword implementation + refactor
    Now supporting queries like:
    ```
    'group by(instance) (demo_memory_usage_bytes)'
    ```
    
    fix: check selector as the __name__ in case
    metric name is not provided and use it as a
    selector.
    
    ```
    ================================================================================
    General query tweaks:
    *  Openobserve is sometimes off by 1ms when parsing floating point start/end timestamps.
    *  Openobserve fractional tolerance amount for <agg>_over_time queries
    ================================================================================
    Total: 583 / 596 (97.82%) passed, 0 unsupported
    ```

---
+++ src/service/promql/aggregations/group.rs
@@ -0,0 +1,35 @@
+// Copyright 2022 Zinc Labs Inc. and Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use datafusion::error::Result;
+use promql_parser::parser::LabelModifier;
+
+use crate::service::promql::value::{InstantValue, Sample, Value};
+
+/// https://prometheus.io/docs/prometheus/latest/querying/operators/#aggregation-operators
+pub fn group(timestamp: i64, param: &Option<LabelModifier>, data: &Value) -> Result<Value> {
+    let score_values = super::eval_arithmetic(param, data, "group", |_total, _val| 1.0)?;
+    if score_values.is_none() {
+        return Ok(Value::None);
+    }
+    let values = score_values
+        .unwrap()
+        .values()
+        .map(|v| InstantValue {
+            labels: v.labels.clone(),
+            sample: Sample::new(timestamp, v.value),
+        })
+        .collect();
+    Ok(Value::Vector(values))
+}
