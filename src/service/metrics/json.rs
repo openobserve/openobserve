f9677480f8c9f8809246c0ee521b5852a928bacf
oasisk <ashish.j.kolhe@gmail.com>
2023-07-31 11:14:51 +0530

    v1

--- src/service/metrics/json.rs
+++ src/service/metrics/json.rs
@@ -236,7 +236,7 @@ pub async fn ingest(org_id: &str, body: web::Bytes, thread_id: usize) -> Result<
         let time_level = if let Some(details) = stream_partitioning_map.get(&stream_name) {
             details.partition_time_level
         } else {
-            PartitionTimeLevel::Daily
+            CONFIG.limit.metric_file_max_retention.as_str().into()
         };
 
         let mut stream_file_name = "".to_string();
