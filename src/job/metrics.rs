5cd8815eb851e702221726e50f52b8ca3a989131
hengfeiyang <hengfei.yang@gmail.com>
2023-08-01 12:37:49 +0800

    fix: change metrics update interval to 60s

--- src/job/metrics.rs
+++ src/job/metrics.rs
@@ -15,8 +15,8 @@ pub async fn run() -> Result<(), anyhow::Error> {
     load_query_cache_limit_bytes().await?;
     load_ingest_wal_used_bytes().await?;
 
-    // update metrics every 30 minutes
-    let mut interval = time::interval(time::Duration::from_secs(1800));
+    // update metrics every 60 seconds
+    let mut interval = time::interval(time::Duration::from_secs(60));
     interval.tick().await; // trigger the first run
     loop {
         interval.tick().await;
