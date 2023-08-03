059b2b71da49595b1185a4e57ec1e05753b6088e
ktx-keshavi <keshavi.bhalani@kiara.tech>
2023-08-03 18:04:43 +0530

    add extra options for import dashboard using url and json

--- web/src/views/Dashboards/ImportDashboard.vue
+++ web/src/views/Dashboards/ImportDashboard.vue
@@ -10,24 +10,24 @@
  limitations under the License. 
 -->
 <template>
-    <div class="q-mx-md q-my-md">
+  <div class="q-mx-md q-my-md">
     <div class="row items-center no-wrap">
       <div class="col">
         <div class="flex">
-          <q-btn no-caps @click="goBack()"  padding="xs" outline icon="arrow_back_ios_new" />
+          <q-btn no-caps @click="goBack()" padding="xs" outline icon="arrow_back_ios_new" />
           <div class="text-h6 q-ml-md">
             Import Dashboard
           </div>
         </div>
       </div>
     </div>
-    <q-separator class="q-my-sm"/>
-    <div>
+    <q-separator class="q-my-sm" />
+    <div style="width: 400px;">
       <q-form @submit="onSubmit">
         <div class="q-my-md">
           Import Dashboard from exported JSON file
         </div>
-        <div style="width: 400px; height: 100px;">
+        <div style="width: 400px;">
           <q-file filled bottom-slots v-model="jsonFile" label="Drop your file here" accept=".json">
             <template v-slot:prepend>
               <q-icon name="cloud_upload" @click.stop.prevent />
@@ -35,35 +35,48 @@
             <template v-slot:append>
               <q-icon name="close" @click.stop.prevent="jsonFile = null" class="cursor-pointer" />
             </template>
-
             <template v-slot:hint>
               .json files only
             </template>
           </q-file>
-        </div>
-
-        <div class="flex q-mt-lg">
-          <q-btn
-            v-close-popup
-            class="q-mb-md text-bold"
-            :label="t('function.cancel')"
-            text-color="light-text"
-            padding="sm md"
-            no-caps
-            @click="goBack()"
-          />
-          <q-btn
-            :disable="!jsonFile"
-            :label="t('dashboard.import')"
-            class="q-mb-md text-bold no-border q-ml-md"
-            color="secondary"
-            padding="sm xl"
-            type="submit"
-            no-caps
-            @click="importFile()"
-          />
+          <div>
+            <q-btn :disable="!jsonFile" :label="t('dashboard.import')" class="q-my-md text-bold no-border"
+              color="secondary" padding="sm xl" type="submit" no-caps @click="importFile()" />
+          </div>
         </div>
       </q-form>
+      <q-separator class="q-my-sm" />
+      <q-form @submit="onSubmit">
+        <div class="q-my-md">
+          Import Dashboard from URL
+        </div>
+        <div class="flex" style="width: 400px;">
+          <q-input v-model="url" style="width:275px;" label="Add your url" color="input-border" bg-color="input-bg" stack-label filled dense
+            label-slot />
+          <div>
+            <q-btn :disable="!url" class="text-bold no-border q-ml-md" :label="t('dashboard.import')"
+              color="secondary" type="submit" no-caps @click="importFromUrl()" padding="sm xl" />
+          </div>
+        </div>
+      </q-form>
+      <q-separator class="q-my-sm" />
+      <q-form @submit="onSubmit">
+        <div class="q-my-md">
+          Import Dashboard from JSON
+        </div>
+        <div style="width: 400px;" class="flex">
+          <q-input v-model="jsonStr" style="width: 400px;" label="JSON Object" color="input-border" dense filled type="textarea" />
+        </div>
+        <div class="q-my-md">
+          <q-btn :disable="!jsonStr" class="text-bold no-border q-mr-md" :label="t('dashboard.import')"
+            color="secondary" type="submit" padding="sm xl" no-caps @click="importFromJsonStr()" />
+          <q-btn v-close-popup class="text-bold" :label="t('function.cancel')" text-color="light-text" padding="sm xl"
+            no-caps @click="goBack()" />
+        </div>
+      </q-form>
+    </div>
+    <div>
+
     </div>
   </div>
 </template>
@@ -76,6 +89,7 @@ import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import { useQuasar } from "quasar";
 import dashboardService from "../../services/dashboards";
+import axios from 'axios';
 
 export default defineComponent({
   name: "Import Dashboard",
@@ -85,9 +99,24 @@ export default defineComponent({
     const store = useStore()
     const router = useRouter()
     const jsonFile = ref<any>()
+    const url = ref('')
+    const jsonStr = ref('')
     const $q = useQuasar();
 
     const importFile = async () => {
+      var reader = new FileReader();
+      reader.onload = function (reader) {
+        importDashboardFromJSON(reader.result)
+          .then(() => {
+            jsonFile.value = null
+          })
+      };
+      reader.readAsText(jsonFile.value)
+    }
+
+    const importDashboardFromJSON = (jsonObj: any) => {
+      console.log("---", jsonObj);
+
       // get the dashboard
       const dismiss = $q.notify({
         spinner: true,
@@ -95,51 +124,65 @@ export default defineComponent({
         timeout: 2000,
       });
 
-      var reader = new FileReader();
-      reader.onload = function() {
-        try {
-          const data = JSON.parse(reader.result)
+      try {
+        const data = typeof jsonObj == 'string' ? JSON.parse(jsonObj) : typeof jsonObj == 'object' ? jsonObj : jsonObj
 
-          //set owner name and creator name to import dashboard
-          data.owner= store.state.userInfo.name
-          data.created= new Date().toISOString()
+        console.log("try: data", data);
 
-          dashboardService.create(
-            store.state.selectedOrganization.identifier,
-            data
-          ).then((res: { data: any }) => {
-              jsonFile.value = null
-              dismiss();
-              $q.notify({
-                type: "positive",
-                message: `Dashboard Imported Successfully`,
-              });
-            })
-            .then(() => {
-              return getAllDashboards(store)
-            }).then(()=> {
-              router.push('/dashboards')
-            })
-            .catch((err: any) => {
-              console.log(err)
-              $q.notify({
-                type: "negative",
-                message: err?.response?.data["error"] ? JSON.stringify(err?.response?.data["error"]) : 'Dashboard import failed',
-              });
-              dismiss();
-            });
+        //set owner name and creator name to import dashboard
+        data.owner = store.state.userInfo.name
+        data.created = new Date().toISOString()
 
+        return dashboardService.create(
+          store.state.selectedOrganization.identifier,
+          data
+        ).then((res: { data: any }) => {
           dismiss();
-        } catch (error) {
-          
           $q.notify({
+            type: "positive",
+            message: `Dashboard Imported Successfully`,
+          });
+        })
+          .then(() => {
+            return getAllDashboards(store)
+          }).then(() => {
+            router.push('/dashboards')
+          })
+          .catch((err: any) => {
+            console.log(err)
+            $q.notify({
               type: "negative",
-              message: 'Invalid JSON format',
+              message: err?.response?.data["error"] ? JSON.stringify(err?.response?.data["error"]) : 'Dashboard import failed',
             });
             dismiss();
-        }
-      };
-      reader.readAsText(jsonFile.value);
+          }).finally(() => {
+            dismiss();
+          });
+      } catch (error) {
+        console.log(error);
+
+        $q.notify({
+          type: "negative",
+          message: 'Invalid JSON format',
+        });
+        dismiss();
+      }
+    }
+    const importFromUrl = async () => {
+      // get the dashboard
+      const urlData = url.value ? url.value : ''
+
+      const res = await axios.get(urlData);
+      console.log("res=", res);
+      importDashboardFromJSON(res.data).then(() => {
+        url.value = ''
+      })
+    }
+    const importFromJsonStr = async () => {
+      // get the dashboard
+      importDashboardFromJSON(jsonStr.value).then(() => {
+        jsonStr.value = ''
+      })
     }
 
     // back button to render dashboard List page
@@ -156,7 +199,11 @@ export default defineComponent({
       goBack,
       onSubmit,
       importFile,
-      jsonFile
+      jsonFile,
+      importFromUrl,
+      url,
+      jsonStr,
+      importFromJsonStr
     }
   }
 })
