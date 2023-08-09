ab0feda7dad1b421b303164eefa2468413139e64
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-08-09 17:22:13 +0530

    fix: added reDraw method check

--- web/src/plugins/logs/SearchResult.vue
+++ web/src/plugins/logs/SearchResult.vue
@@ -342,7 +342,8 @@ export default defineComponent({
       if (
         // eslint-disable-next-line no-prototype-builtins
         searchObj.data.histogram.hasOwnProperty("xData") &&
-        searchObj.data.histogram.xData.length > 0
+        searchObj.data.histogram.xData.length > 0 &&
+        plotChart.value?.reDraw
       ) {
         plotChart.value.reDraw(
           searchObj.data.histogram.xData,
