ab3db00b7f471e01a6f2e4c2da9e0c0137d66c80
Hengfei Yang <hengfei.yang@gmail.com>
2023-09-04 21:56:19 +0800

    fix: add shrink_to_fit after remove a wal file (#1458)

--- src/common/infra/file_list/sqlite.rs
+++ src/common/infra/file_list/sqlite.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use ahash::AHashMap as HashMap;
+use ahash::HashMap;
 use async_trait::async_trait;
 use chrono::Utc;
 use sqlx::{QueryBuilder, Row, Sqlite};
