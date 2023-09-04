e3b9a3130391ea36e561a010b9d85e43f44ccb11
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2023-09-04 15:31:09 +0530

    on query select stop expand collapse

--- web/src/components/dashboards/addPanel/DashboardQueryEditor.vue
+++ web/src/components/dashboards/addPanel/DashboardQueryEditor.vue
@@ -25,7 +25,7 @@
                 <div style="max-width: 600px">
                 <q-tabs v-if="promqlMode" v-model="dashboardPanelData.layout.currentQueryIndex" narrow-indicator dense inline-label outside-arrows mobile-arrows>
                     <q-tab no-caps :ripple="false" v-for="(tab, index) in dashboardPanelData.data.queries" :key="index" :name="index"
-                        :label="'Query ' + (index + 1)">
+                        :label="'Query ' + (index + 1)" @click.stop>
                         <q-icon
                             v-if="index > 0 || (index === 0 && dashboardPanelData.data.queries.length > 1)"
                             name="close"
