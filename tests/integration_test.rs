370aa3cb3cb054c73d6277159958e0fadacc44ce
hengfeiyang <hengfei.yang@gmail.com>
2023-09-04 10:04:20 +0800

    fix: unit tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -142,11 +142,13 @@ mod tests {
         e2e_list_alert_destinations().await;
         e2e_post_alert().await;
         e2e_get_alert().await;
-        e2e_delete_alert().await;
         e2e_list_alerts().await;
         e2e_list_real_time_alerts().await;
-        e2e_delete_alert_template().await;
+        e2e_delete_alert().await;
         e2e_delete_alert_destination().await;
+        e2e_delete_alert_template().await;
+
+        // syslog
         e2e_post_syslog_route().await;
         e2e_list_syslog_routes().await;
 
