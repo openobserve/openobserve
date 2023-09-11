be9481231017f943fa09f2d31b7f98e179933d2b
hengfeiyang <hengfei.yang@gmail.com>
2023-09-11 14:21:20 +0800

    fix: cancel datafusion execution when it timeout

--- src/service/promql/search/mod.rs
+++ src/service/promql/search/mod.rs
@@ -17,7 +17,6 @@ use futures::future::try_join_all;
 use std::{
     cmp::{max, min},
     sync::Arc,
-    time::Duration,
 };
 use tonic::{codec::CompressionEncoding, metadata::MetadataValue, transport::Channel, Request};
 use tracing::{info_span, Instrument};
@@ -146,7 +145,7 @@ async fn search_in_cluster(req: cluster_rpc::MetricsQueryRequest) -> Result<Valu
                     .parse()
                     .map_err(|_| Error::Message(format!("invalid org_id: {}", req.org_id)))?;
                 let mut request = tonic::Request::new(req);
-                request.set_timeout(Duration::from_secs(CONFIG.grpc.timeout));
+                // request.set_timeout(Duration::from_secs(CONFIG.grpc.timeout));
 
                 opentelemetry::global::get_text_map_propagator(|propagator| {
                     propagator.inject_context(
