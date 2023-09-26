114947380baa7dd13a7de7a5334c399eef1a58d8
oasisk <ashish.j.kolhe@gmail.com>
2023-09-26 09:28:39 +0530

    automatic migration

--- src/common/migration/dashboards.rs
+++ src/common/migration/dashboards.rs
@@ -2,9 +2,19 @@ use crate::common::infra::db as infra_db;
 
 pub async fn run() -> Result<(), anyhow::Error> {
     // load dashboards list
+
     let db_key = format!("/dashboard/");
     let data = infra_db::DEFAULT.list(&db_key).await?;
     for (key, val) in data {
+        let key = key.strip_prefix('/').unwrap_or(&key);
+        let len = key.split("/").collect::<Vec<&str>>().len();
+        if len > 3 {
+            println!(
+                "Skip dashboard migration as it is already part of folder: {}",
+                key
+            );
+            continue;
+        }
         let new_key = key.replace("/dashboard/", "/dashboard/default/");
         match infra_db::DEFAULT
             .put(&new_key, val, infra_db::NO_NEED_WATCH)
