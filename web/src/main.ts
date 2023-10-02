d2268699f661754d85e54fadd5517661b0eff746
omkark06 <omkar@zinclabs.io>
2023-10-02 15:56:01 +0530

    fix: updated sentry packages and added session replay

--- web/src/main.ts
+++ web/src/main.ts
@@ -62,11 +62,18 @@ const getConfig = async () => {
               /^\//,
             ],
           }),
+          new Sentry.Replay(),
         ],
         // Set tracesSampleRate to 1.0 to capture 100%
         // of transactions for performance monitoring.
         // We recommend adjusting this value in production
         tracesSampleRate: 1.0,
+        // This sets the sample rate to be 10%. You may want this to be 100% while
+        // in development and sample at a lower rate in production
+        replaysSessionSampleRate: 0.1,
+        // If the entire session is not sampled, use the below sample rate to sample
+        // sessions when an error occurs.
+        replaysOnErrorSampleRate: 1.0,
       });
     }
   });
