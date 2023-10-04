bceddcab0844a853c554447bf5214a5fdc94e122
ktx-keshavi <keshavi.bhalani@kiara.tech>
2023-10-04 18:15:56 +0530

    made simple component for scrape interval

--- web/src/components/settings/General.vue
+++ web/src/components/settings/General.vue
@@ -15,7 +15,47 @@
 
 <!-- eslint-disable vue/x-invalid-end-tag -->
 <template>
-  <q-page class="page q-pa-md"> General Settings </q-page>
+  <!-- <q-page class="page q-pa-md"> General Settings </q-page> -->
+  <q-card class="column full-height">
+    <div class="q-px-md q-py-md">
+      <div class="text-body1 text-bold">
+          Organization Settings
+    </div>
+  </div>
+    <q-separator />
+    <q-card-section class="q-w-md q-mx-lg">
+      <q-form ref="addFolderForm" @submit.stop="onSubmit.execute">
+        <q-input
+          v-model="scrapeIntereval"
+          label="Scrape Interval *"
+          color="input-border"
+          bg-color="input-bg"
+          class="q-py-md showLabelOnTop"
+          stack-label
+          outlined
+          filled
+          dense
+          :rules="[(val) => !!(val.trim()) || 'Scrape Interval is required']"
+          :lazy-rules="true"
+        />
+        <span>&nbsp;</span>
+
+        <div class="flex justify-center q-mt-lg">
+          <q-btn
+            data-test="dashboard-add-submit"
+            :disable="scrapeIntereval.trim() === ''"
+            :loading="onSubmit.isLoading.value"
+            :label="t('dashboard.save')"
+            class="q-mb-md text-bold no-border q-ml-md"
+            color="secondary"
+            padding="sm xl"
+            type="submit"
+            no-caps
+          />
+        </div>
+      </q-form>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script lang="ts">
@@ -25,6 +65,7 @@ import { useI18n } from "vue-i18n";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import { useQuasar } from "quasar";
+import { useLoading } from "@/composables/useLoading";
 
 export default defineComponent({
   name: "PageGeneralSettings",
@@ -33,12 +74,22 @@ export default defineComponent({
     const q = useQuasar();
     const store = useStore();
     const router: any = useRouter();
+    const scrapeIntereval = ref(store.state.organizationData.scrapeInterval);
+
+    const onSubmit = useLoading(async () => {
+      await store.dispatch("updateOrganization", {
+        ...store.state.organizationData,
+        scrapeInterval: scrapeIntereval.value,
+      });
+    });
 
     return {
       t,
       q,
       store,
       router,
+      scrapeIntereval,
+      onSubmit
     };
   },
 });
