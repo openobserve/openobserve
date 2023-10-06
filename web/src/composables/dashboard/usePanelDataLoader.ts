79fbb01b653ce6f2facae2659837d91f37911835
ktx-keshavi <keshavi.bhalani@kiara.tech>
2023-10-06 15:44:42 +0530

    rounding interval and followed by rate interval calculation

--- web/src/composables/dashboard/usePanelDataLoader.ts
+++ web/src/composables/dashboard/usePanelDataLoader.ts
@@ -16,6 +16,133 @@ import { ref, watch, reactive, toRefs, onMounted, onUnmounted } from "vue";
 import queryService from "../../services/search";
 import { useStore } from "vuex";
 
+const formatInterval = (interval: any) => {
+  switch(true) {
+    // 0.01s
+    case interval <= 10:
+      return {value : 1, unit : "ms"} // 0.001s
+    // 0.015s
+    case interval <= 15:
+      return {value : 10, unit : "ms"}  // 0.01s
+    // 0.035s
+    case interval <= 35:
+      return {value : 20, unit : "ms"}  // 0.02s
+    // 0.075s
+    case interval <= 75:
+      return {value : 50, unit : "ms"}  // 0.05s
+    // 0.15s
+    case interval <= 150:
+      return {value : 100, unit : "ms"}  // 0.1s
+    // 0.35s
+    case interval <= 350:
+      return {value : 200, unit : "ms"}  // 0.2s
+    // 0.75s
+    case interval <= 750:
+      return {value : 500, unit : "ms"}  // 0.5s
+    // 1.5s
+    case interval <= 1500:
+      return {value : 1, unit : "s"}  // 1s
+    // 3.5s
+    case interval <= 3500:
+      return {value : 2, unit : "s"}  // 2s
+    // 7.5s
+    case interval <= 7500:
+      return {value : 5, unit : "s"}  // 5s
+    // 12.5s
+    case interval <= 12500:
+      return {value : 10, unit : "s"}  // 10s
+    // 17.5s
+    case interval <= 17500:
+      return {value : 15, unit : "s"}  // 15s
+    // 25s
+    case interval <= 25000:
+      return {value : 20, unit : "s"}  // 20s
+    // 45s
+    case interval <= 45000:
+      return {value : 30, unit : "s"}  // 30s
+    // 1.5m
+    case interval <= 90000:
+      return {value : 1, unit : "m"}  // 1m
+    // 3.5m
+    case interval <= 210000:
+      return {value : 2, unit : "m"}  // 2m
+    // 7.5m
+    case interval <= 450000:
+      return {value : 5, unit : "m"}  // 5m
+    // 12.5m
+    case interval <= 750000:
+      return {value : 10, unit : "m"}  // 10m
+    // 17.5m
+    case interval <= 1050000:
+      return {value : 15, unit : "m"}  // 15m
+    // 25m
+    case interval <= 1500000:
+      return {value : 20, unit : "m"}  // 20m
+    // 45m
+    case interval <= 2700000:
+      return {value : 30, unit : "m"}  // 30m
+    // 1.5h
+    case interval <= 5400000:
+      return {value : 1, unit : "h"}  // 1h
+    // 2.5h
+    case interval <= 9000000:
+      return {value : 2, unit : "h"}  // 2h
+    // 4.5h
+    case interval <= 16200000:
+      return {value : 3, unit : "h"}  // 3h
+    // 9h
+    case interval <= 32400000:
+      return {value : 6, unit : "h"}  // 6h
+    // 24h
+    case interval <= 86400000:
+      return {value : 12, unit : "h"}  // 12h
+    // 48h
+    case interval <= 172800000:
+      return {value : 24, unit : "h"}  // 24h
+    // 1w
+    case interval <= 604800000:
+      return {value : 24, unit : "h"}  // 24h
+    // 3w
+    case interval <= 1814400000:
+      return {value : 1, unit : "w"}  // 1w
+    // 2y
+    case interval < 3628800000:
+      return {value : 30, unit : "d"}  // 30d
+    default:
+      return {value : 1, unit : "y"}  // 1y
+    }
+}
+
+const getTimeInSecondsBasedOnUnit = (seconds: any, unit: any) => {
+  switch(true){
+    case unit === "ms":
+      return seconds / 1000;
+    case unit === "s":
+      return seconds;
+    case unit === "m":
+      return seconds * 60;
+    case unit === "h":
+      return seconds * 60 * 60;
+    case unit === "d":
+      return seconds * 60 * 60 * 24;
+    case unit === "w":
+      return seconds * 60 * 60 * 24 * 7;
+    case unit === "y":
+      return seconds * 60 * 60 * 24 * 7 * 12;
+    default:
+      return seconds;
+  }  
+}
+
+const formateRateInterval = (interval: any) => {
+  const days = Math.floor(interval / (3600 * 24));
+  const hours = Math.floor((interval % (3600 * 24)) / 3600);
+  const minutes = Math.floor((interval % 3600) / 60);
+  const remainingSeconds = interval % 60;
+
+  return `${days.toString()}d${hours.toString()}h${minutes.toString()}m${remainingSeconds.toString()}s`;
+}
+
 export const usePanelDataLoader = (
   panelSchema: any,
   selectedTimeObj: any,
@@ -75,7 +202,7 @@ export const usePanelDataLoader = (
     }
     const query = {
       query: {
-        sql: replaceQueryValue(queryData),
+        sql: replaceQueryValue(queryData, endISOTimestamp, startISOTimestamp),
         sql_mode: "full",
         start_time: startISOTimestamp,
         end_time: endISOTimestamp,
@@ -88,45 +215,14 @@ export const usePanelDataLoader = (
     // Check if the query type is "promql"
     if (panelSchema.value.queryType == "promql") {
 
-      //fixed variables value calculations
-      //scrape interval by default 15 seconds
-      const scrapeInterval = store.state.organizationData.organizationSettings.scrapeInterval ?? 15;
-
-      // timestamp in seconds / chart panel width
-      const __interval = Math.floor((endISOTimestamp - startISOTimestamp)/(1000000*(chartPanelRef.value?.offsetWidth ?? 1000)));
-
-      //max(interval + scrapeInterval, 4 * scrapeInterval)
-      const __rate_interval = Math.max( __interval + scrapeInterval , 4 * scrapeInterval);      
-
-      const fixedVariables = [
-        {
-          name: "__interval",
-          value: `${__interval}s`
-        },
-        {
-          name: "__rate_interval",
-          value: `${__rate_interval <= 0 ? 1 : __rate_interval}s`
-        }
-      ]
-
       // Iterate through each query in the panel schema
       const queryPromises = panelSchema.value.queries?.map(async (it: any) => {
         
-        // replace fixed variables with its values
-        fixedVariables?.forEach((variable: any) => {
-          const variableName = `$${variable.name}`;
-          const variableValue = variable.value;
-          it.query = it.query.replaceAll(variableName, variableValue);
-        });      
-
-        console.log(chartPanelRef.value?.offsetWidth,it.query , "query");
-        
-
         // Call the metrics_query_range API
         return queryService
           .metrics_query_range({
             org_identifier: store.state.selectedOrganization.identifier,
-            query: replaceQueryValue(it.query),
+            query: replaceQueryValue(it.query, endISOTimestamp, startISOTimestamp),
             start_time: startISOTimestamp,
             end_time: endISOTimestamp,
           })
@@ -257,7 +353,44 @@ export const usePanelDataLoader = (
    * @param {any} query - The query to be modified.
    * @return {any} The modified query with replaced values.
    */
-  const replaceQueryValue = (query: any) => {
+  const replaceQueryValue = (query: any, endISOTimestamp: any, startISOTimestamp: any) => {
+
+    //fixed variables value calculations
+    //scrape interval by default 15 seconds
+    let scrapeInterval = store.state.organizationData.organizationSettings.scrapeInterval ?? 15;
+
+    // timestamp in seconds / chart panel width
+    let __interval = ((endISOTimestamp - startISOTimestamp)/(chartPanelRef.value?.offsetWidth ?? 1000))/1000;
+
+    // if less than 1, set it to 1
+    // minimum will be 1 millisecond
+    __interval = Math.max(15000, __interval);
+    
+    // round interval
+    const formattedInterval = formatInterval(__interval);
+
+    // calculate rate interval in seconds
+    // we need formatted interval value in seconds
+    let __rate_interval: any = Math.max( getTimeInSecondsBasedOnUnit(formattedInterval.value, formattedInterval.unit) + scrapeInterval , 4 * scrapeInterval);
+
+    const fixedVariables = [
+      {
+        name: "__interval",
+        value: `${formattedInterval.value}${formattedInterval.unit}`
+      },
+      {
+        name: "__rate_interval",
+        value: `${formateRateInterval(__rate_interval)}`
+      }
+    ];    
+
+    // replace fixed variables with its values
+    fixedVariables?.forEach((variable: any) => {
+      const variableName = `$${variable.name}`;
+      const variableValue = variable.value;
+      query = query.replaceAll(variableName, variableValue);
+    }); 
+
     if (currentDependentVariablesData?.length) {
 
       currentDependentVariablesData?.forEach((variable: any) => {
