a7ee1dde2d342bcf9ddc7ec3541cc846240d49ae
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2023-10-10 10:22:16 +0530

    lat, lng, zoom in geomap

--- web/src/components/dashboards/addPanel/ConfigPanel.vue
+++ web/src/components/dashboards/addPanel/ConfigPanel.vue
@@ -48,14 +48,14 @@
       <div v-if="dashboardPanelData.data.type == 'geomap'">
         <span>Initial View:</span>
         <div class="row">
-          <q-input  v-model.number="dashboardPanelData.data.config.map_view.lat" label="Latitude" color="input-border"
+          <q-input  v-model="dashboardPanelData.data.config.map_view.lat" label="Latitude" color="input-border"
             bg-color="input-bg" class="col-6 q-py-md showLabelOnTop" stack-label outlined filled dense label-slot :type="'number'">
           </q-input>
-          <q-input v-model.number="dashboardPanelData.data.config.map_view.lng" label="Longitude" color="input-border"
+          <q-input v-model="dashboardPanelData.data.config.map_view.lng" label="Longitude" color="input-border"
             bg-color="input-bg" class="col-6 q-py-md showLabelOnTop" stack-label outlined filled dense label-slot :type="'number'">
           </q-input>
         </div>
-        <q-input v-model.number="dashboardPanelData.data.config.zoom" label="Zoom" color="input-border"
+        <q-input v-model="dashboardPanelData.data.config.map_view.zoom" label="Zoom" color="input-border"
             bg-color="input-bg" class="q-py-md showLabelOnTop" stack-label outlined filled dense label-slot :type="'number'">
           </q-input>
       </div>
