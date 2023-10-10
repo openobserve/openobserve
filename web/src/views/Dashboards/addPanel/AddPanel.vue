f3db8bea564215c4f78a8a2a6e380a49c695c9b7
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2023-10-10 18:02:24 +0530

    folder id

--- web/src/views/Dashboards/addPanel/AddPanel.vue
+++ web/src/views/Dashboards/addPanel/AddPanel.vue
@@ -264,7 +264,7 @@ export default defineComponent({
     // };
     const loadDashboard = async () => {
 
-      let data = JSON.parse(JSON.stringify(await getDashboard(
+      let data = await getDashboard(
         store,
         route.query.dashboard,
         route.query.folder ?? "default"
