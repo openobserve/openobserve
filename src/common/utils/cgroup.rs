416b32b66488331efdf1088ae5343924b88b0fa8
Ashish Kolhe <35160958+oasisk@users.noreply.github.com>
2023-10-10 11:18:35 +0530

    fix: read cpu & memory limits from cgroup (#1632)
    based on cgroup version limits are part of different files:
    
    - v1 
      memory - /sys/fs/cgroup/memory/memory.limit_in_bytes
      cpu - sys/fs/cgroup/cpu/cpu.cfs_quota_us 
    
    - v2
      memory - /sys/fs/cgroup/memory.max
     cpu - /sys/fs/cgroup/cpu.max

--- src/common/utils/cgroup.rs
+++ src/common/utils/cgroup.rs
@@ -17,20 +17,25 @@ use sysinfo::SystemExt;
 /// Get cpu limit by cgroup or return the node cpu cores
 pub fn get_cpu_limit() -> usize {
     let cpu_num = if let Ok(val) = std::fs::read_to_string("/sys/fs/cgroup/cpu.max") {
-        if !val.to_lowercase().starts_with("max") {
+        if !val.is_empty() && !val.to_lowercase().starts_with("max") {
             let columns = val.split(' ').collect::<Vec<&str>>();
             let val = columns[0].parse::<usize>().unwrap_or_default();
-            if val < 100000 {
-                1 // maybe the limit less than 1 core
+            if val > 0 {
+                if val < 100000 {
+                    1 // maybe the limit less than 1 core
+                } else {
+                    val / 100000
+                }
             } else {
-                val / 100000
+                read_cpu_cgroup_v1()
             }
         } else {
-            0
+            read_cpu_cgroup_v1()
         }
     } else {
-        0
+        read_cpu_cgroup_v1()
     };
+
     if cpu_num > 0 {
         cpu_num
     } else {
@@ -43,19 +48,41 @@ pub fn get_cpu_limit() -> usize {
 /// Get memory limit by cgroup or return the node memory size
 pub fn get_memory_limit() -> usize {
     let mem_size = if let Ok(val) = std::fs::read_to_string("/sys/fs/cgroup/memory.max") {
-        if !val.to_lowercase().starts_with("max") {
+        if !val.is_empty() && !val.to_lowercase().starts_with("max") {
             val.trim_end().parse::<usize>().unwrap_or_default()
         } else {
-            0
+            read_memory_cgroup_v1()
         }
     } else {
-        0
+        read_memory_cgroup_v1()
     };
+
     if mem_size > 0 {
         mem_size
     } else {
         let mut system = sysinfo::System::new();
         system.refresh_memory();
         system.total_memory() as usize
+    }
+}
+
+fn read_cpu_cgroup_v1() -> usize {
+    if let Ok(val) = std::fs::read_to_string("/sys/fs/cgroup/cpu/cpu.cfs_quota_us") {
+        let ret_val = val.trim().to_string().parse::<usize>().unwrap_or_default();
+        if ret_val > 0 && ret_val < 100000 {
+            1 // maybe the limit less than 1 core
+        } else {
+            ret_val / 100000
+        }
+    } else {
+        0
+    }
+}
+
+fn read_memory_cgroup_v1() -> usize {
+    if let Ok(val) = std::fs::read_to_string("/sys/fs/cgroup/memory/memory.limit_in_bytes") {
+        val.trim_end().parse::<usize>().unwrap_or_default()
+    } else {
+        0
     }
 }
