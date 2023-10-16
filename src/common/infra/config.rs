3c510a3d4076ba4dd8d95f20b2af449592e34de1
oasisk <ashish.j.kolhe@gmail.com>
2023-10-16 15:59:09 +0530

    added actix keep alive & client request timeout config

--- src/common/infra/config.rs
+++ src/common/infra/config.rs
@@ -360,6 +360,10 @@ pub struct Limit {
     pub calculate_stats_interval: u64,
     #[env_config(name = "ZO_ENRICHMENT_TABLE_LIMIT", default = 10)] //size in mb
     pub enrichment_table_limit: usize,
+    #[env_config(name = "ZO_ACTIX_REQ_TIMEOUT", default = 30)] //size in mb
+    pub request_timeout: u64,
+    #[env_config(name = "ZO_ACTIX_KEEP_ALIVE", default = 30)] //size in mb
+    pub keep_alive: u64,
 }
 
 #[derive(EnvConfig)]
