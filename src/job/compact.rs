614dd45235cafadd99cf74a488bc673afd74ba9a
hengfeiyang <hengfei.yang@gmail.com>
2023-10-22 17:42:26 +0800

    fix: change the job interval to configure

--- src/job/compact.rs
+++ src/job/compact.rs
@@ -69,7 +69,9 @@ async fn run_delete() -> Result<(), anyhow::Error> {
 
 /// Delete files based on the file_file_deleted in the database
 async fn run_delete_files() -> Result<(), anyhow::Error> {
-    let mut interval = time::interval(time::Duration::from_secs(CONFIG.compact.interval));
+    let mut interval = time::interval(time::Duration::from_secs(
+        CONFIG.compact.delete_files_delay as u64,
+    ));
     interval.tick().await; // trigger the first run
     loop {
         interval.tick().await;
