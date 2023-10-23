03f5561aad4e49f42c87aad558a24efc73fc2047
ktx-kirtan <124270523+ktx-kirtan@users.noreply.github.com>
2023-10-23 15:43:15 +0530

    feat: rate interval variables for promql in dashboard (#1665)
    - UI for https://github.com/openobserve/openobserve/issues/1408
    
    ---------
    
    Co-authored-by: ktx-keshavi <keshavi.bhalani@kiara.tech>

--- web/src/components/settings/General.vue
+++ web/src/components/settings/General.vue
@@ -15,16 +15,58 @@
 
 <!-- eslint-disable vue/x-invalid-end-tag -->
 <template>
-  <q-page class="page q-pa-md"> General Settings </q-page>
+  <div>
+    <div class="q-px-md q-py-md">
+      <div class="text-body1 text-bold">
+        General Settings
+      </div>
+    </div>
+    <q-separator />
+    <div class="q-w-md q-mx-lg">
+      <q-form @submit.stop="onSubmit.execute">
+        <q-input
+          v-model.number="scrapeIntereval"
+          type="number"
+          min="0"
+          label="Scrape Interval (In Seconds) *"
+          color="input-border"
+          bg-color="input-bg"
+          class="q-py-md showLabelOnTop"
+          stack-label
+          outlined
+          filled
+          dense
+          :rules="[(val) => !!val || 'Scrape interval is required']"
+          :lazy-rules="true"
+        />
+        <span>&nbsp;</span>
+
+        <div class="flex justify-center q-mt-lg">
+          <q-btn
+            data-test="dashboard-add-submit"
+            :loading="onSubmit.isLoading.value"
+            :label="t('dashboard.save')"
+            class="q-mb-md text-bold no-border q-ml-md"
+            color="secondary"
+            padding="sm xl"
+            type="submit"
+            no-caps
+          />
+        </div>
+      </q-form>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 // @ts-ignore
-import { defineComponent, onBeforeMount, ref } from "vue";
+import { defineComponent, onActivated, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import { useQuasar } from "quasar";
+import { useLoading } from "@/composables/useLoading";
+import organizations from "@/services/organizations";
 
 export default defineComponent({
   name: "PageGeneralSettings",
@@ -33,12 +75,48 @@ export default defineComponent({
     const q = useQuasar();
     const store = useStore();
     const router: any = useRouter();
+    const scrapeIntereval = ref(store.state?.organizationData?.organizationSettings?.scrape_interval ?? 15);
+    
+    onActivated(() => {
+      scrapeIntereval.value = store.state?.organizationData?.organizationSettings?.scrape_interval ?? 15
+    });
+    
+    const onSubmit = useLoading(async () => {
+      try {
+        //set organizations settings in store
+        //scrape interval will be in number
+        store.dispatch("setOrganizationSettings", {
+          ...store.state?.organizationData?.organizationSettings,
+          scrape_interval: scrapeIntereval.value
+        });
+
+        //update settings in backend
+        await organizations.post_organization_settings(
+          store.state?.selectedOrganization?.identifier,
+          store.state?.organizationData?.organizationSettings
+        );
+
+        q.notify({
+          type: "positive",
+          message: "Organization settings updated",
+          timeout: 2000,
+        })
+      } catch (err: any) {
+        q.notify({
+          type: "negative",
+          message: err?.message || "something went wrong",
+          timeout: 2000,
+        })
+      }
+    });
 
     return {
       t,
       q,
       store,
       router,
+      scrapeIntereval,
+      onSubmit
     };
   },
 });
--- web/src/composables/dashboard/usePanelDataLoader.ts
+++ web/src/composables/dashboard/usePanelDataLoader.ts
@@ -16,6 +16,141 @@ import { ref, watch, reactive, toRefs, onMounted, onUnmounted } from "vue";
 import queryService from "../../services/search";
 import { useStore } from "vuex";
 
+const formatInterval = (interval: any) => {
+  switch(true) {
+    // 0.01s
+    case interval <= 10:
+      return {value : 1, unit : "ms"} // 0.001s
+    // 0.015s
+    case interval <= 15:
+      return {value : 10, unit : "ms"}  // 0.01s
+    // 0.035s
+    case interval <= 35:
+      return {value : 20, unit : "ms"}  // 0.02s
+    // 0.075s
+    case interval <= 75:
+      return {value : 50, unit : "ms"}  // 0.05s
+    // 0.15s
+    case interval <= 150:
+      return {value : 100, unit : "ms"}  // 0.1s
+    // 0.35s
+    case interval <= 350:
+      return {value : 200, unit : "ms"}  // 0.2s
+    // 0.75s
+    case interval <= 750:
+      return {value : 500, unit : "ms"}  // 0.5s
+    // 1.5s
+    case interval <= 1500:
+      return {value : 1, unit : "s"}  // 1s
+    // 3.5s
+    case interval <= 3500:
+      return {value : 2, unit : "s"}  // 2s
+    // 7.5s
+    case interval <= 7500:
+      return {value : 5, unit : "s"}  // 5s
+    // 12.5s
+    case interval <= 12500:
+      return {value : 10, unit : "s"}  // 10s
+    // 17.5s
+    case interval <= 17500:
+      return {value : 15, unit : "s"}  // 15s
+    // 25s
+    case interval <= 25000:
+      return {value : 20, unit : "s"}  // 20s
+    // 45s
+    case interval <= 45000:
+      return {value : 30, unit : "s"}  // 30s
+    // 1.5m
+    case interval <= 90000:
+      return {value : 1, unit : "m"}  // 1m
+    // 3.5m
+    case interval <= 210000:
+      return {value : 2, unit : "m"}  // 2m
+    // 7.5m
+    case interval <= 450000:
+      return {value : 5, unit : "m"}  // 5m
+    // 12.5m
+    case interval <= 750000:
+      return {value : 10, unit : "m"}  // 10m
+    // 17.5m
+    case interval <= 1050000:
+      return {value : 15, unit : "m"}  // 15m
+    // 25m
+    case interval <= 1500000:
+      return {value : 20, unit : "m"}  // 20m
+    // 45m
+    case interval <= 2700000:
+      return {value : 30, unit : "m"}  // 30m
+    // 1.5h
+    case interval <= 5400000:
+      return {value : 1, unit : "h"}  // 1h
+    // 2.5h
+    case interval <= 9000000:
+      return {value : 2, unit : "h"}  // 2h
+    // 4.5h
+    case interval <= 16200000:
+      return {value : 3, unit : "h"}  // 3h
+    // 9h
+    case interval <= 32400000:
+      return {value : 6, unit : "h"}  // 6h
+    // 24h
+    case interval <= 86400000:
+      return {value : 12, unit : "h"}  // 12h
+    // 48h
+    case interval <= 172800000:
+      return {value : 24, unit : "h"}  // 24h
+    // 1w
+    case interval <= 604800000:
+      return {value : 24, unit : "h"}  // 24h
+    // 3w
+    case interval <= 1814400000:
+      return {value : 1, unit : "w"}  // 1w
+    // 2y
+    case interval < 3628800000:
+      return {value : 30, unit : "d"}  // 30d
+    default:
+      return {value : 1, unit : "y"}  // 1y
+    }
+}
+
+const getTimeInSecondsBasedOnUnit = (seconds: any, unit: any) => {
+  switch(true){
+    case unit === "ms":
+      return seconds / 1000;
+    case unit === "s":
+      return seconds;
+    case unit === "m":
+      return seconds * 60;
+    case unit === "h":
+      return seconds * 60 * 60;
+    case unit === "d":
+      return seconds * 60 * 60 * 24;
+    case unit === "w":
+      return seconds * 60 * 60 * 24 * 7;
+    case unit === "y":
+      return seconds * 60 * 60 * 24 * 7 * 12;
+    default:
+      return seconds;
+  }  
+}
+
+const formateRateInterval = (interval: any) => {
+  let formattedStr = ""
+  const days = Math.floor(interval / (3600 * 24));
+  if( days > 0 ) formattedStr += days.toString() + "d";
+
+  const hours = Math.floor((interval % (3600 * 24)) / 3600);
+  if( hours > 0 ) formattedStr += hours.toString() + "h";
+
+  const minutes = Math.floor((interval % 3600) / 60);
+  if( minutes > 0 ) formattedStr += minutes.toString() + "m";
+
+  const remainingSeconds = interval % 60;
+  if( remainingSeconds > 0 ) formattedStr += remainingSeconds.toString() + "s";
+
+  return formattedStr;
+}
+
 export const usePanelDataLoader = (
   panelSchema: any,
   selectedTimeObj: any,
@@ -73,15 +208,6 @@ export const usePanelDataLoader = (
     } else {
       return;
     }
-    const query = {
-      query: {
-        sql: replaceQueryValue(queryData),
-        sql_mode: "full",
-        start_time: startISOTimestamp,
-        end_time: endISOTimestamp,
-        size: 0,
-      },
-    };
 
     state.loading = true;
 
@@ -90,12 +216,12 @@ export const usePanelDataLoader = (
 
       // Iterate through each query in the panel schema
       const queryPromises = panelSchema.value.queries?.map(async (it: any) => {
-
+        
         // Call the metrics_query_range API
         return queryService
           .metrics_query_range({
             org_identifier: store.state.selectedOrganization.identifier,
-            query: replaceQueryValue(it.query),
+            query: replaceQueryValue(it.query, startISOTimestamp, endISOTimestamp),
             start_time: startISOTimestamp,
             end_time: endISOTimestamp,
           })
@@ -129,7 +255,7 @@ export const usePanelDataLoader = (
               org_identifier: store.state.selectedOrganization.identifier,
               query: {
                 query: {
-                  sql: replaceQueryValue(it.query),
+                  sql: replaceQueryValue(it.query, startISOTimestamp, endISOTimestamp),
                   sql_mode: "full",
                   start_time: startISOTimestamp,
                   end_time: endISOTimestamp,
@@ -226,7 +352,51 @@ export const usePanelDataLoader = (
    * @param {any} query - The query to be modified.
    * @return {any} The modified query with replaced values.
    */
-  const replaceQueryValue = (query: any) => {
+  const replaceQueryValue = (query: any, startISOTimestamp: any, endISOTimestamp: any) => {
+
+    //fixed variables value calculations
+    //scrape interval by default 15 seconds
+    let scrapeInterval = store.state.organizationData.organizationSettings.scrape_interval ?? 15;
+
+    // timestamp in seconds / chart panel width
+    let __interval = ((endISOTimestamp - startISOTimestamp)/(chartPanelRef.value?.offsetWidth ?? 1000))/1000;
+
+    // if less than 1, set it to 1
+    // minimum will be 15000 millisecond
+    // __interval = Math.max(15000, __interval);
+    
+    // round interval
+    const formattedInterval = formatInterval(__interval);
+
+    // calculate rate interval in seconds
+    // we need formatted interval value in seconds
+    let __rate_interval: any = Math.max( getTimeInSecondsBasedOnUnit(formattedInterval.value, formattedInterval.unit) + scrapeInterval , 4 * scrapeInterval);
+
+    //get interval in ms
+    const __interval_ms = getTimeInSecondsBasedOnUnit(formattedInterval.value, formattedInterval.unit) * 1000;    
+
+    const fixedVariables = [
+      {
+        name: "__interval_ms",
+        value: `${__interval_ms}ms`
+      },
+      {
+        name: "__interval",
+        value: `${formattedInterval.value}${formattedInterval.unit}`
+      },
+      {
+        name: "__rate_interval",
+        value: `${formateRateInterval(__rate_interval)}`
+      }
+    ];    
+
+    // replace fixed variables with its values
+    fixedVariables?.forEach((variable: any) => {
+      const variableName = `$${variable.name}`;
+      const variableValue = variable.value;
+      query = query.replaceAll(variableName, variableValue);
+    }); 
+
     if (currentDependentVariablesData?.length) {
 
       currentDependentVariablesData?.forEach((variable: any) => {
