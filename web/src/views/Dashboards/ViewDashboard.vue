c85d88ec78f730d2528f59dffe0cc3376a7062c9
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2023-10-23 15:48:31 +0530

    name change

--- web/src/views/Dashboards/ViewDashboard.vue
+++ web/src/views/Dashboards/ViewDashboard.vue
@@ -290,6 +290,7 @@ export default defineComponent({
 
     // whenever the refreshInterval is changed, update the query params
     watch([refreshInterval, selectedDate], () => {
+      console.log("refreshInterval", refreshInterval.value);
       router.replace({
         query: {
           org_identifier: store.state.selectedOrganization.identifier,
@@ -323,6 +324,7 @@ export default defineComponent({
       selectedDate,
       currentTimeObj,
       refreshInterval,
+      openSettingsDialog,
       // ----------------
       refreshData,
       onDeletePanel,
