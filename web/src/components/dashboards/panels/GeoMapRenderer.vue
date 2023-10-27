f69b71f03e3cda2e9882e51f35e0840fa17a5401
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2023-10-27 15:53:11 +0530

    fix map switching errors

--- web/src/components/dashboards/panels/GeoMapRenderer.vue
+++ web/src/components/dashboards/panels/GeoMapRenderer.vue
@@ -41,13 +41,13 @@ export default defineComponent({
         let chart: any;
         let lmap: any;
         let lmapComponent: any;
-        const lmapOptions = {...props.data.options?.lmap} || {};
+        const lmapOptions = { ...props.data.options?.lmap } || {};
 
         const store = useStore();
         const windowResizeEventCallback = async () => {
             await nextTick();
             await nextTick();
-            chart.resize();
+            chart?.resize();
         };
 
         onMounted(async () => {
@@ -58,12 +58,14 @@ export default defineComponent({
             await nextTick();
             await nextTick();
             await nextTick();
-            chart = echarts.init(chartRef.value);
+            if (chartRef.value) {
+                chart = echarts.init(chartRef.value);
+            }
             const options = {
                 ...props.data.options,
                 lmap: lmapOptions,
             };
-            chart.setOption(options || {}, true);
+            chart?.setOption(options || {}, true);
             window.addEventListener("resize", windowResizeEventCallback);
 
             // Get Leaflet extension component
@@ -71,20 +73,22 @@ export default defineComponent({
             // add the following two comments to circumvent this
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
-            lmapComponent = chart.getModel().getComponent('lmap');
-            
+            lmapComponent = chart?.getModel()?.getComponent('lmap');
+
             // Get the instance of Leaflet
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
-            lmap = lmapComponent.getLeaflet();
+            lmap = lmapComponent?.getLeaflet();
 
-            L.tileLayer(
-            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            {
-                attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            if (lmap) {
+                L.tileLayer(
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    {
+                        attribution:
+                            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    }
+                ).addTo(lmap);
             }
-            ).addTo(lmap);
             if (props.data.options?.lmap?.center) {
                 lmap.setView([props.data.options?.lmap?.center[1], props.data.options?.lmap?.center[0]], props.data.options?.lmap?.zoom);
             }
@@ -94,41 +98,42 @@ export default defineComponent({
             window.removeEventListener("resize", windowResizeEventCallback);
             if (lmap) {
                 lmap.off();
-                lmap.remove();
             }
             if (chart) {
-                chart.dispose();
+                chart?.dispose();
             }
         });
 
         watch(() => props.data.options, async (newOptions) => {
             await nextTick();
-            chart.resize();
+            chart?.resize();
             const options = {
                 ...props.data.options,
                 lmap: lmapOptions,
             };
-            chart.setOption(options || {}, true);
-             // Get Leaflet extension component
+            chart?.setOption(options || {}, true);
+            // Get Leaflet extension component
             // getModel and getComponent do not seem to be exported in echarts typescript
             // add the following two comments to circumvent this
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
-            lmapComponent = chart.getModel().getComponent('lmap');
+            lmapComponent = chart?.getModel().getComponent('lmap');
 
             // Get the instance of Leaflet
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             lmap = lmapComponent.getLeaflet();
 
-            L.tileLayer(
-                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                {
-                    attribution:
-                        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                }
-            ).addTo(lmap);
-            if(props.data.options?.lmap?.center){
+            if (lmap) {
+                L.tileLayer(
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    {
+                        attribution:
+                            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    }
+                ).addTo(lmap);
+            }
+            if (props.data.options?.lmap?.center) {
                 lmap.setView([props.data.options?.lmap?.center[1], props.data.options?.lmap?.center[0]], props.data.options?.lmap?.zoom);
             }
         }, { deep: true });
