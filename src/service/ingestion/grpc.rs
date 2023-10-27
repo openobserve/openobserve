8ff2dcddcba99ed1ff08076b9cf00a4346b03749
Ashish Kolhe <35160958+oasisk@users.noreply.github.com>
2023-10-27 19:40:10 +0530

    fix: resource/span attributes retain data types (#1789)
    For GRPC ingestion resource attributes , span attributes values are
    currently being stored as string , the PR fixes these to retain data
    types.

--- src/service/ingestion/grpc.rs
+++ src/service/ingestion/grpc.rs
@@ -14,7 +14,7 @@
 
 use crate::common::utils::json;
 use opentelemetry_proto::tonic::{
-    common::v1::AnyValue,
+    common::v1::{any_value::Value, AnyValue},
     metrics::v1::{exemplar, number_data_point},
 };
 
@@ -24,35 +24,29 @@ pub fn get_val(attr_val: &Option<AnyValue>) -> json::Value {
     match attr_val {
         Some(local_val) => match &local_val.value {
             Some(val) => match val {
-                opentelemetry_proto::tonic::common::v1::any_value::Value::StringValue(
-                    inner_val,
-                ) => json::json!(inner_val.as_str()),
-                opentelemetry_proto::tonic::common::v1::any_value::Value::BoolValue(inner_val) => {
+                Value::StringValue(inner_val) => json::json!(inner_val.as_str()),
+                Value::BoolValue(inner_val) => {
                     json::json!(inner_val.to_string())
                 }
-                opentelemetry_proto::tonic::common::v1::any_value::Value::IntValue(inner_val) => {
+                Value::IntValue(inner_val) => {
                     json::json!(inner_val.to_string())
                 }
-                opentelemetry_proto::tonic::common::v1::any_value::Value::DoubleValue(
-                    inner_val,
-                ) => json::json!(inner_val.to_string()),
-                opentelemetry_proto::tonic::common::v1::any_value::Value::ArrayValue(inner_val) => {
+                Value::DoubleValue(inner_val) => json::json!(inner_val.to_string()),
+                Value::ArrayValue(inner_val) => {
                     let mut vals = vec![];
                     for item in inner_val.values.iter().cloned() {
                         vals.push(get_val(&Some(item)))
                     }
                     json::json!(vals)
                 }
-                opentelemetry_proto::tonic::common::v1::any_value::Value::KvlistValue(
-                    inner_val,
-                ) => {
+                Value::KvlistValue(inner_val) => {
                     let mut vals = json::Map::new();
                     for item in inner_val.values.iter().cloned() {
                         vals.insert(item.key, get_val(&item.value));
                     }
                     json::json!(vals)
                 }
-                opentelemetry_proto::tonic::common::v1::any_value::Value::BytesValue(inner_val) => {
+                Value::BytesValue(inner_val) => {
                     json::json!(inner_val)
                 }
             },
@@ -122,6 +116,46 @@ pub fn get_val_for_attr(attr_val: json::Value) -> json::Value {
     ().into()
 }
 
+pub fn get_val_with_type_retained(attr_val: &Option<AnyValue>) -> json::Value {
+    match attr_val {
+        Some(local_val) => match &local_val.value {
+            Some(val) => match val {
+                Value::StringValue(val) => {
+                    json::json!(val)
+                }
+                Value::BoolValue(val) => {
+                    json::json!(val)
+                }
+                Value::IntValue(val) => {
+                    json::json!(val)
+                }
+                Value::DoubleValue(val) => {
+                    json::json!(val)
+                }
+                Value::ArrayValue(val) => {
+                    let mut vals = vec![];
+                    for item in val.values.iter().cloned() {
+                        vals.push(get_val(&Some(item)))
+                    }
+                    json::json!(vals)
+                }
+                Value::KvlistValue(val) => {
+                    let mut vals = json::Map::new();
+                    for item in val.values.iter().cloned() {
+                        vals.insert(item.key, get_val(&item.value));
+                    }
+                    json::json!(vals)
+                }
+                Value::BytesValue(val) => {
+                    json::json!(val)
+                }
+            },
+            None => json::Value::Null,
+        },
+        None => json::Value::Null,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use opentelemetry_proto::tonic::common::v1::AnyValue;
@@ -132,68 +166,55 @@ mod tests {
     fn test_get_val() {
         let in_str = "Test".to_string();
         let str_val = AnyValue {
-            value: Some(
-                opentelemetry_proto::tonic::common::v1::any_value::Value::StringValue(
-                    in_str.clone(),
-                ),
-            ),
+            value: Some(Value::StringValue(in_str.clone())),
         };
         let resp = get_val(&Some(str_val));
         assert_eq!(resp.as_str().unwrap(), in_str);
 
         let in_bool = false;
         let bool_val = AnyValue {
-            value: Some(
-                opentelemetry_proto::tonic::common::v1::any_value::Value::BoolValue(in_bool),
-            ),
+            value: Some(Value::BoolValue(in_bool)),
         };
         let resp = get_val(&Some(bool_val));
         assert_eq!(resp.as_str().unwrap(), in_bool.to_string());
 
         let in_int = 20;
         let int_val = AnyValue {
-            value: Some(opentelemetry_proto::tonic::common::v1::any_value::Value::IntValue(in_int)),
+            value: Some(Value::IntValue(in_int)),
         };
         let resp = get_val(&Some(int_val.clone()));
         assert_eq!(resp.as_str().unwrap(), in_int.to_string());
 
         let in_double = 20.00;
         let double_val = AnyValue {
-            value: Some(
-                opentelemetry_proto::tonic::common::v1::any_value::Value::DoubleValue(in_double),
-            ),
+            value: Some(Value::DoubleValue(in_double)),
         };
         let resp = get_val(&Some(double_val));
         assert_eq!(resp.as_str().unwrap(), in_double.to_string());
 
         let in_arr = vec![int_val.clone()];
         let arr_val = AnyValue {
-            value: Some(
-                opentelemetry_proto::tonic::common::v1::any_value::Value::ArrayValue(
-                    opentelemetry_proto::tonic::common::v1::ArrayValue { values: in_arr },
-                ),
-            ),
+            value: Some(Value::ArrayValue(
+                opentelemetry_proto::tonic::common::v1::ArrayValue { values: in_arr },
+            )),
         };
         let resp = get_val(&Some(arr_val));
         assert!(!resp.as_array().unwrap().is_empty());
 
         let kv_val = AnyValue {
-            value: Some(
-                opentelemetry_proto::tonic::common::v1::any_value::Value::KvlistValue(
-                    opentelemetry_proto::tonic::common::v1::KeyValueList {
-                        values: vec![opentelemetry_proto::tonic::common::v1::KeyValue {
-                            key: in_str.clone(),
-                            value: Some(int_val.clone()),
-                        }],
-                    },
-                ),
-            ),
+            value: Some(Value::KvlistValue(
+                opentelemetry_proto::tonic::common::v1::KeyValueList {
+                    values: vec![opentelemetry_proto::tonic::common::v1::KeyValue {
+                        key: in_str.clone(),
+                        value: Some(int_val.clone()),
+                    }],
+                },
+            )),
         };
         let resp = get_val(&Some(kv_val));
         assert!(resp.as_object().unwrap().contains_key(&in_str));
 
-        let in_byte =
-            opentelemetry_proto::tonic::common::v1::any_value::Value::BytesValue(vec![8u8]);
+        let in_byte = Value::BytesValue(vec![8u8]);
 
         let byte_val = AnyValue {
             value: Some(in_byte),
