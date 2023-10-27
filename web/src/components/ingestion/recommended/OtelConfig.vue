a5ccf1f7a6041bcadef059578580f6375d2e807a
omkark06 <omkar@zinclabs.io>
2023-10-27 15:19:40 +0530

    fix: added recommended tabs content

--- web/src/components/ingestion/recommended/OtelConfig.vue
+++ web/src/components/ingestion/recommended/OtelConfig.vue
@@ -1,7 +1,87 @@
 <template>
-  <div class="text-h5">OTEL Config</div>
+  <div class="q-pa-md">
+    <div class="text-subtitle1 text-bold q-pl-xs">OTLP gRPC</div>
+    <ContentCopy class="q-mt-sm" :content="getOtelGrpcConfig" />
+
+    <div class="text-subtitle1 text-bold q-mt-md q-pl-xs">OTLP gRPC</div>
+    <ContentCopy class="q-mt-sm" :content="getOtelHttpConfig" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import ContentCopy from "@/components/CopyContent.vue";
+import { useStore } from "vuex";
+import { b64EncodeUnicode } from "../../../utils/zincutils";
+
+const store = useStore();
+
+const props = defineProps({
+  currOrgIdentifier: {
+    type: String,
+  },
+  currUserEmail: {
+    type: String,
+  },
+});
+
+const endpoint: Ref<Endpoint> = ref({
+  url: "",
+  host: "",
+  port: "",
+  protocol: "",
+  tls: "",
+});
+
+const url = new URL(store.state.API_ENDPOINT);
+
+endpoint.value = {
+  url: store.state.API_ENDPOINT,
+  host: url.hostname,
+  port: url.port || (url.protocol === "https:" ? "443" : "80"),
+  protocol: url.protocol.replace(":", ""),
+  tls: url.protocol === "https:" ? "On" : "Off",
+};
+
+const accessKey = computed(() => {
+  return b64EncodeUnicode(
+    `${props.currUserEmail}:${store.state.organizationData.organizationPasscode}`
+  );
+});
+
+const getOtelGrpcConfig = computed(() => {
+  return `exporters:
+  otlp/openobserve:
+      endpoint: localhost:5081
+      headers:
+        Authorization: "Basic ${accessKey.value}"
+        organization: default
+        stream-name: default
+      tls:
+        insecure: true
+  otlp/openobserve_k8s_events:
+      endpoint: localhost:5081
+      headers:
+        Authorization: "Basic ${accessKey.value}"
+        organization: default
+        stream-name: default
+      tls:
+        insecure: true`;
+});
+
+const getOtelHttpConfig = computed(() => {
+  return `exporters:
+  otlphttp/openobserve:
+    endpoint: ${endpoint.value.url}/api/${props.currOrgIdentifier}/
+    headers:
+      Authorization: Basic ${accessKey.value}
+      stream-name: default
+  otlphttp/openobserve_k8s_events:
+    endpoint: ${endpoint.value.url}/api/${props.currOrgIdentifier}/
+    headers:
+      Authorization: Basic ${accessKey.value}
+      stream-name: k8s_events`;
+});
+</script>
 
 <style scoped></style>
