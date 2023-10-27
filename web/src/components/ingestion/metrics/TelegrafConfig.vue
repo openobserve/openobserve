ebd8ec7d8de3ccd8a86f66f3485232a5b35ffc95
Bhargav <BJP232004@GMAIL.COM>
2023-10-27 17:18:38 +0530

    type fix

--- web/src/components/ingestion/metrics/TelegrafConfig.vue
+++ web/src/components/ingestion/metrics/TelegrafConfig.vue
@@ -65,7 +65,7 @@ export default defineComponent({
   },
   setup(props) {
     const store = useStore();
-    const endpoint: Ref<Endpoint> = ref({
+    const endpoint: any = ref({
       url: "",
       host: "",
       port: "",
