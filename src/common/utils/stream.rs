447e9c46af96efc9ace2fe8f75777e94b083b7ac
Hengfei Yang <hengfei.yang@gmail.com>
2023-11-01 00:01:21 +0800

    fix: distinct_values use search api (#1819)

--- src/common/utils/stream.rs
+++ src/common/utils/stream.rs
@@ -24,11 +24,6 @@ use crate::common::utils::json;
 
 #[inline(always)]
 pub fn stream_type_query_param_error() -> Result<HttpResponse, Error> {
-    /*  return Ok(HttpResponse::BadRequest().json(MetaHttpResponse::error(
-        http::StatusCode::BAD_REQUEST.into(),
-        Some("only 'type' query param with value 'logs' or 'metrics' allowed".to_string()),
-    ))); */
-
     Err(Error::new(
         ErrorKind::Other,
         "only 'type' query param with value 'logs' or 'metrics' allowed",
