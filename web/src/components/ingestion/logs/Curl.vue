04d4564213ff71736e87135ce5e59ef04654f377
Bhargav <BJP232004@GMAIL.COM>
2023-11-01 18:45:23 +0530

    fix: ingestion page ui changes (#1825)
    This PR contains below list of items:
    - Ingestion page improvements
    - Logs page Data table by expanding one of the record in logs table,
    select "table" mode field name is in red color to match with json tab.
    - Menu selection for child routes.

--- web/src/components/ingestion/logs/Curl.vue
+++ web/src/components/ingestion/logs/Curl.vue
@@ -16,7 +16,6 @@
 <template>
   <div class="tabContent q-ma-md">
     <div class="tabContent__head">
-      <div class="title" data-test="curl-title-text">CURL</div>
       <div class="copy_action">
         <q-btn
           data-test="curl-copy-btn"
@@ -35,8 +34,7 @@ curl -u {{ currUserEmail }}:{{
         store.state.organizationData.organizationPasscode
       }} -k {{ endpoint.url }}/api/{{
         currOrgIdentifier
-      }}/default/_json -d '[{"level":"info","job":"test","log":"test message for openobserve"}]'
-    </pre>
+      }}/default/_json -d '[{"level":"info","job":"test","log":"test message for openobserve"}]'</pre>
   </div>
 </template>
 
@@ -88,32 +86,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style scoped lang="scss">
-.tabContent {
-  background-color: rgba(136, 136, 136, 0.103);
-  // tab content bg color
-  padding: 1rem 1.25rem 0.5rem;
-  border-radius: 0.5rem;
-  &__head {
-    justify-content: space-between;
-    text-transform: uppercase;
-    align-items: center;
-    display: flex;
-    .title {
-      line-height: 1rem;
-      font-weight: 600;
-    }
-    .copy_action {
-      .q-btn {
-        // background-color: white;
-      }
-    }
-  }
-  pre {
-    white-space: pre-wrap;
-    word-wrap: break-word;
-    margin-bottom: 0;
-  }
-}
-</style>
