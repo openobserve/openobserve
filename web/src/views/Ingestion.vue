a50c54d11926ab5ff3362e72d3a1d73223c32cc5
Bhargav <BJP232004@GMAIL.COM>
2023-11-02 22:29:24 +0530

    fixed token issue

--- web/src/views/Ingestion.vue
+++ web/src/views/Ingestion.vue
@@ -215,13 +215,14 @@ export default defineComponent({
 
     onBeforeMount(() => {
       if (
-        !store.state.organizationData.organizationPasscode &&
-        router.currentRoute.value.name != "ingestion"
+        !store.state.organizationData.organizationPasscode
       ) {
         getOrganizationPasscode();
         getRUMToken();
       }
+    });
 
+    onMounted(() => {
       if (router.currentRoute.value.name === "ingestion") {
         router.push({
           name: "recommended",
