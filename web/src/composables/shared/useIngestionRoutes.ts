17d2fa5880f8f0854440165237e3be03e93962e4
Bhargav <BJP232004@GMAIL.COM>
2023-11-02 11:47:28 +0530

    fix: removed kinesis firehose and gcp ingestion tabs from opensource version (#1836)

--- web/src/composables/shared/useIngestionRoutes.ts
+++ web/src/composables/shared/useIngestionRoutes.ts
@@ -84,19 +84,9 @@ const useIngestionRoutes = () => {
                   component: Vector,
                 },
                 {
-                  path: "kinesisfirehose",
-                  name: "kinesisfirehose",
-                  component: KinesisFirehose,
-                },
-                {
                   path: "filebeat",
                   name: "filebeat",
                   component: FileBeat,
-                },
-                {
-                  path: "gcp",
-                  name: "gcpLogs",
-                  component: GcpPubSub,
                 },
               ],
             },
@@ -192,11 +182,28 @@ const useIngestionRoutes = () => {
     component: SyslogNg,
   };
 
+  const kinesisFirehose = {
+    path: "kinesisfirehose",
+    name: "kinesisfirehose",
+    component: KinesisFirehose,
+  };
+
+  const gcpPubSub = {
+    path: "gcp",
+    name: "gcpLogs",
+    component: GcpPubSub,
+  };
+
   if (config.isCloud === "false" || !config.isCloud) {
     ingestionRoutes[0].children
       .find((child: any) => child.name === "custom")
       .children.find((child: any) => child.name === "ingestLogs")
       ?.children.push(...[sysLog, sysLogNg]);
+  } else {
+    ingestionRoutes[0].children
+      .find((child: any) => child.name === "custom")
+      .children.find((child: any) => child.name === "ingestLogs")
+      ?.children.push(...[kinesisFirehose, gcpPubSub]);
   }
   return ingestionRoutes;
 };
