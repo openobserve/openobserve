ab139b20ce0d094891ba2d5143fc412f294b6c52
oasisk <ashish.j.kolhe@gmail.com>
2023-11-09 13:03:51 +0530

    rebased

--- src/common/meta/stream.rs
+++ src/common/meta/stream.rs
@@ -339,12 +339,6 @@ pub struct SchemaRecords {
     pub records: Vec<json::Value>,
 }
 
-
-pub struct SchemaRecords {
-    pub schema: Schema,
-    pub records: Vec<json::Value>,
-}
-
 #[derive(Clone, Copy, Default)]
 pub struct ScanStats {
     pub files: i64,
