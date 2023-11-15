5d330a9032af82c0004926148c54468ef851f854
baerwang <52104949+baerwang@users.noreply.github.com>
2023-11-15 18:11:30 +0800

    fix: date format (#1945)

--- web/src/utils/dashboard/convertDataIntoUnitValue.ts
+++ web/src/utils/dashboard/convertDataIntoUnitValue.ts
@@ -252,6 +252,6 @@ export const formatDate = (date: any) => {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
   
-    return `${year}-${day}-${month} ${hours}:${minutes}:${seconds}`;
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   };
   
