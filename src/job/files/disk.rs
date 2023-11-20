fa7a397bdec133f6124b29bd2368ea60f89dc26c
oasisk <ashish.j.kolhe@gmail.com>
2023-11-20 15:29:01 +0530

    move log statement

--- src/job/files/disk.rs
+++ src/job/files/disk.rs
@@ -518,8 +518,8 @@ async fn handle_metrics(
                 return Err(anyhow::anyhow!(err));
             }
         }
-        log::info!("[JOB] disk file conversion succeeded for: {}", &path_str);
     }
+    log::info!("[JOB] disk file conversion succeeded for: {}", &path_str);
     Ok(())
 }
 
