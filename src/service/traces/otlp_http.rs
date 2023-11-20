d9833a4b54efc8d34a1d4743a027b46e7ed504e1
Hengfei Yang <hengfei.yang@gmail.com>
2023-11-21 00:26:45 +0800

    fix: partiton_key for traces should have consistent behavior (#1979)

--- src/service/traces/otlp_http.rs
+++ src/service/traces/otlp_http.rs
@@ -378,8 +378,7 @@ pub async fn traces_json(
                     }
 
                     if partition_keys.is_empty() {
-                        let partition_key =
-                            format!("service_name={}", format_stream_name(&service_name));
+                        let partition_key = format!("service_name={}", service_name);
                         hour_key.push_str(&format!("/{}", format_partition_key(&partition_key)));
                     }
 
