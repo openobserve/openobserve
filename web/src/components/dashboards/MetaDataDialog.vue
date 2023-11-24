4326c81cd51402196e211e3bd7fb50f2059999ca
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2023-11-24 17:49:25 +0530

    some css and variables way to set in new line

--- web/src/components/dashboards/MetaDataDialog.vue
+++ web/src/components/dashboards/MetaDataDialog.vue
@@ -3,13 +3,13 @@
     <q-card style="min-width: 500px">
       <q-card-section class="q-pt-md">
         <div class="row items-center q-pb-none">
-          <h4 class="text-bold">Metadata Details</h4>
+          <h6 class="text-bold">Metadata Details</h6>
           <q-space />
           <q-btn icon="close" flat round dense v-close-popup />
         </div>
-        <div class="text-bold">Total Queries: {{ totalQueries }}</div>
+        <div class="text-bold">Total Query(s) Executed: {{ totalQueries }}</div>
         <div v-for="(query, index) in metaData.queries" :key="query.originalQuery">
-          <div class="text-bold q-py-sm">MetaData: {{ index + 1 }}</div>
+          <div class="text-bold q-py-sm">Query: {{ index + 1 }}</div>
           <q-card>
             <q-card-section>
               <q-table class="my-sticky-virtscroll-table" virtual-scroll  v-model:pagination="pagination"
@@ -38,14 +38,25 @@ export default defineComponent({
   setup(props) {
     const queryData = props.metaData.queries;
 
-    const getRows = (query: any) => [
-      ["Original Query", query.originalQuery],
-      ["Query", query.query],
-      ["Start Time", new Date(query.startTime / 1000).toLocaleString()],
-      ["End Time", new Date(query.endTime / 1000).toLocaleString()],
-      ["Query Type", query.queryType],
-      ["Variables", query.variables.map((variable: any) => `${variable.type}` === "variable" ? `${variable.name}: ${variable.value}` : `${variable.name} ${variable.operator} ${variable.value}`).join(", ")],
-    ];
+    const getRows = (query: any) => {
+      const rows: any[] = [
+        ["Original Query", query.originalQuery],
+        ["Query", query.query],
+        ["Start Time", new Date(query.startTime / 1000).toLocaleString()],
+        ["End Time", new Date(query.endTime / 1000).toLocaleString()],
+        ["Query Type", query.queryType],
+        ["Variables", ],
+      ];
+
+      const variableRows = query.variables.map((variable: any, variableIndex: number) => [
+        variable.type === 'variable' ? `${variable.type} -> ${variable.name}: ${variable.value}` : `${variable.type} -> ${variable.name} ${variable.operator} ${variable.value}`,
+        '',
+      ]);
+
+        rows.push(...variableRows);
+
+      return rows;
+    };
     const totalQueries = computed(() => queryData.length);
     const tableColumns = [
       { name: "key", label: "Name", align: "left", field: 0, sortable: true },
