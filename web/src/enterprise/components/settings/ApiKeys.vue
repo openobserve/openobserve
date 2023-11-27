58c2e84f413cdcf6cb2dd4fc25df57dbedf071af
Bhargav <BJP232004@GMAIL.COM>
2023-11-27 13:19:10 +0530

    fix: added user guide

--- web/src/enterprise/components/settings/ApiKeys.vue
+++ web/src/enterprise/components/settings/ApiKeys.vue
@@ -1,16 +1,17 @@
 <!-- Copyright 2023 Zinc Labs Inc.
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-     http:www.apache.org/licenses/LICENSE-2.0
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License. 
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 
 <!-- eslint-disable vue/x-invalid-end-tag -->
@@ -34,7 +35,8 @@
       </div>
     </div>
 
-    <div v-if="userAPIKey.length > 0">
+    <div v-if="loading" class="text-h6">Loading...</div>
+    <div v-else-if="userAPIKey.length > 0">
       <div v-for="key in userAPIKey" :key="key?.id">
         <div class="tabContent q-mb-md q-pa-sm">
           <label class="text-bold">{{ key.api_name }}</label>
@@ -75,9 +77,43 @@
           />
         </div>
       </div>
+
+      <!-- Instruction about how to use API key -->
+      <div class="text-subtitle1 q-mb-sm">
+        <span class="text-h6 text-bold"
+          >Incorporating API Keys: A How-To Guide</span
+        >
+        <q-separator />
+        The API key serves as the primary means of user authentication, enabling
+        access to a range of APIs. It is essential to include the API key as a
+        header in your requests. The designated attribute for transmitting the
+        API key is:
+        <div class="tabContent q-pb-md q-mt-md q-mb-md">
+          openobserve-apikey: [YOUR_API_KEY]
+        </div>
+        Ensure that you replace [YOUR_API_KEY] with the unique API key assigned
+        to you. This integration ensures secure and authorized access to the
+        diverse functionalities provided by the APIs.
+
+        <div class="q-my-md">
+          <span class="text-h6 text-bold">Example</span>
+          <q-separator />
+          <span class="text-subtitle text-bold">cURL</span>
+          <ContentCopy class="q-mt-sm" :content="curlConfig" />
+
+          <div class="q-mt-md">
+            <span class="text-subtitle text-bold">Node JS</span>
+            <ContentCopy class="q-mt-sm" :content="nodejsConfig" />
+          </div>
+          <div class="q-mt-md">
+            <span class="text-subtitle text-bold">Java</span>
+            <ContentCopy class="q-mt-sm" :content="javaConfig" />
+          </div>
+        </div>
+      </div>
     </div>
     <div v-else>
-      <label class="text-bold">{{t('user.apiKeyNotFound')}}</label>
+      <label class="text-bold">{{ t("user.apiKeyNotFound") }}</label>
     </div>
 
     <q-dialog
@@ -98,7 +134,7 @@
     <q-dialog v-model="toggleDeleteUserKeyDialog">
       <q-card style="width: 700px; max-width: 80vw">
         <q-card-section>
-          <div class="text-h6">Medium</div>
+          <div class="text-h6">Delete API Key</div>
         </q-card-section>
 
         <q-card-section class="q-pt-none">
@@ -107,13 +143,16 @@
 
         <q-card-actions align="right" class="bg-white text-teal">
           <q-btn
-            flat
+            unelevated
+            no-caps
             :label="t('common.ok')"
+            color="primary"
             @click="deleteUserKey(deleteID)"
             v-close-popup="true"
           />
-          <q-btn flat
-:label="t('common.cancel')" v-close-popup="true" />
+          <q-btn unelevated
+          no-caps :label="t('common.cancel')"
+v-close-popup="true" />
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -131,10 +170,11 @@ import { getImageURL } from "@/utils/zincutils";
 import { copyToClipboard, useQuasar } from "quasar";
 
 import AddUserKey from "./AddUserKey.vue";
+import ContentCopy from "@/components/CopyContent.vue";
 
 export default defineComponent({
   name: "PageIngestion",
-  components: { AddUserKey },
+  components: { AddUserKey, ContentCopy },
   methods: {
     deleteUserKey(id: string) {
       apiKeysService
@@ -175,14 +215,18 @@ export default defineComponent({
     const toggleDeleteUserKeyDialog: any = ref(false);
     const editRecordSet: any = ref();
     const deleteID: any = ref("");
+    const loading: any = ref(false);
 
     const listAPIKeys = () => {
+      loading.value = true;
       apiKeysService
         .list()
         .then((response) => {
+          loading.value = false;
           userAPIKey.value = response.data.data;
         })
         .catch(() => {
+          loading.value = false;
           q.notify({
             type: "negative",
             message: "Error while fetching API Keys.",
@@ -213,6 +257,73 @@ export default defineComponent({
         });
     };
 
+    const endpoint: any = ref({
+      url: "",
+      host: "",
+      port: "",
+      protocol: "",
+      tls: "",
+    });
+    const url = new URL(store.state.API_ENDPOINT);
+
+    endpoint.value = {
+      url: store.state.API_ENDPOINT,
+      host: url.hostname,
+      port: url.port || (url.protocol === "https:" ? "443" : "80"),
+      protocol: url.protocol.replace(":", ""),
+      tls: url.protocol === "https:" ? "On" : "Off",
+    };
+    const apiendpoint = endpoint.value.url + "/api/";
+
+    const javaConfig = `import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ApiExample {
+    public static void main(String[] args) throws Exception {
+        String apiKey = 'YOUR_API_KEY';
+        String apiUrl = '${apiendpoint}';
+
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod('GET');
+        connection.setRequestProperty('openobserve-apikey', apiKey);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+
+        in.close();
+        connection.disconnect();
+
+        System.out.println(content.toString());
+    }
+}`;
+
+    const nodejsConfig = `const fetch = require('node-fetch');
+
+const apiKey = 'YOUR_API_KEY';
+const apiUrl = '${apiendpoint}';
+
+fetch(apiUrl, {
+  method: 'GET',
+  headers: {
+    'openobserve-apikey': apiKey,
+  },
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+`;
+
+    const curlConfig = `curl -H "openobserve-apikey: YOUR_API_KEY" ${apiendpoint}`;
+
     return {
       t,
       q,
@@ -227,6 +338,11 @@ export default defineComponent({
       deleteID,
       toggleCreateUserKeyDialog,
       toggleDeleteUserKeyDialog,
+      apiendpoint,
+      javaConfig,
+      nodejsConfig,
+      curlConfig,
+      loading,
     };
   },
 });
