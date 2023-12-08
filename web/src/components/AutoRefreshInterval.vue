b4c6bc3d7a97db85416f047f67c4a4c15ebd7a44
Bhargav <BJP232004@GMAIL.COM>
2023-12-08 12:06:20 +0530

    fix: added locators for UI testing (#2150)

--- web/src/components/AutoRefreshInterval.vue
+++ web/src/components/AutoRefreshInterval.vue
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <template>
   <div class="q-pl-sm float-left">
     <q-btn-dropdown
+      data-test="logs-search-bar-refresh-interval-btn-dropdown"
       v-model="btnRefreshInterval"
       border
       no-caps
