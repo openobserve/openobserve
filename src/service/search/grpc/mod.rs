041e6f7567fde7da6a0f9bdf9523b1187272a401
hengfeiyang <hengfei.yang@gmail.com>
2023-12-18 09:08:09 +0800

    chore: update datafusion to 34

--- src/service/search/grpc/mod.rs
+++ src/service/search/grpc/mod.rs
@@ -291,10 +291,10 @@ fn check_memory_circuit_breaker(session_id: &str, scan_stats: &ScanStats) -> Res
         // left memory < datafusion * breaker_ratio and scan_size >=  left memory
         let left_mem = CONFIG.limit.mem_total - cur_memory.physical_mem;
         if (left_mem
-            < (CONFIG.memory_cache.datafusion_max_size * CONFIG.common.memory_circuit_breaker_ratio
-            / 100))
-            &&
-            (scan_size >= left_mem as i64 )
+            < (CONFIG.memory_cache.datafusion_max_size
+                * CONFIG.common.memory_circuit_breaker_ratio
+                / 100))
+            && (scan_size >= left_mem as i64)
         {
             let err = format!("fire memory_circuit_breaker, try to alloc {} bytes, now current memory usage is {} bytes, left memory {} bytes, left memory more than limit of [{} bytes] or scan_size more than left memory , please submit a new query with a short time range",
                               scan_size,
