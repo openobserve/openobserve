c17ddab3db72b79c98b1fa9bc8e72cd0cd8207b6
Hengfei Yang <hengfei.yang@gmail.com>
2023-12-24 23:10:26 +0800

    feat: add API for alert preview (#2303)

--- src/handler/http/request/alerts/mod.rs
+++ src/handler/http/request/alerts/mod.rs
@@ -303,3 +303,44 @@ async fn trigger_alert(
         },
     }
 }
+
+/// PreviewAlert
+#[utoipa::path(
+    context_path = "/api",
+    tag = "Alerts",
+    operation_id = "PreviewAlert",
+    security(
+        ("Authorization"= [])
+    ),
+    params(
+        ("org_id" = String, Path, description = "Organization name"),
+        ("stream_name" = String, Path, description = "Stream name"),
+        ("alert_name" = String, Path, description = "Alert name"),
+    ),
+    responses(
+        (status = 200, description = "Success",  content_type = "application/json", body = HttpResponse),
+        (status = 404, description = "NotFound", content_type = "application/json", body = HttpResponse),
+        (status = 500, description = "Failure",  content_type = "application/json", body = HttpResponse),
+    )
+)]
+#[get("/{org_id}/{stream_name}/alerts/{alert_name}/preview")]
+async fn preview_alert(
+    path: web::Path<(String, String, String)>,
+    req: HttpRequest,
+) -> Result<HttpResponse, Error> {
+    let (org_id, stream_name, name) = path.into_inner();
+    let query = web::Query::<HashMap<String, String>>::from_query(req.query_string()).unwrap();
+    let stream_type = match get_stream_type_from_request(&query) {
+        Ok(v) => v.unwrap_or_default(),
+        Err(e) => {
+            return Ok(MetaHttpResponse::bad_request(e));
+        }
+    };
+    match alerts::preview(&org_id, stream_type, &stream_name, &name).await {
+        Ok(data) => Ok(MetaHttpResponse::json(data)),
+        Err(e) => match e {
+            (http::StatusCode::NOT_FOUND, e) => Ok(MetaHttpResponse::not_found(e)),
+            (_, e) => Ok(MetaHttpResponse::internal_error(e)),
+        },
+    }
+}
