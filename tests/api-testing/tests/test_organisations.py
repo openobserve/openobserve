2c319d9c6f857970544252c99a1becede046e1bd
Ankur Srivastava <ansrivas@users.noreply.github.com>
2023-12-27 16:59:41 +0100

    feat: integrate API integration testing using pytest (#2250)
    Co-authored-by: neha00290 <neha@openobserve.ai>

---
+++ tests/api-testing/tests/test_organisations.py
@@ -0,0 +1,58 @@
+def test_e2e_organisations(create_session, base_url):
+    """Running an E2E test for get all the alerts list."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+
+    resp_get_allorgs = session.get(f"{url}api/{org_id}/organizations")
+
+    print(resp_get_allorgs.content)
+    assert (
+        resp_get_allorgs.status_code == 200
+    ), f"Get all alerts list 200, but got {resp_get_allorgs.status_code} {resp_get_allorgs.content}"
+
+
+def test_e2e_org_settings(create_session, base_url):
+    """Running an E2E test for get all the alerts list."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+
+    resp_get_orgsettings = session.get(f"{url}api/{org_id}/settings")
+
+    print(resp_get_orgsettings.content)
+    assert (
+        resp_get_orgsettings.status_code == 200
+    ), f"Get all alerts list 200, but got {resp_get_orgsettings.status_code} {resp_get_orgsettings.content}"
+
+
+def test_e2e_org_summary(create_session, base_url):
+    """Running an E2E test for get all the alerts list."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+
+    resp_get_orgsummary = session.get(f"{url}api/{org_id}/summary")
+
+    print(resp_get_orgsummary.content)
+    assert (
+        resp_get_orgsummary.status_code == 200
+    ), f"Get all alerts list 200, but got {resp_get_orgsummary.status_code} {resp_get_orgsummary.content}"
+
+
+def test_e2e_passcode(create_session, base_url):
+    """Running an E2E test for get all the alerts list."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+
+    resp_get_allorgs = session.get(f"{url}api/{org_id}/organizations/passcode")
+
+    print(resp_get_allorgs.content)
+    assert (
+        resp_get_allorgs.status_code == 200
+    ), f"Get all alerts list 200, but got {resp_get_allorgs.status_code} {resp_get_allorgs.content}"
