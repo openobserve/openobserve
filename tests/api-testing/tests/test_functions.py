fb65b9d29ac14590fc819052c6e08b1789340cd5
Ankur Srivastava <ansrivas@users.noreply.github.com>
2023-12-27 16:59:41 +0100

    feat: integrate API integration testing using pytest (#2250)
    Co-authored-by: neha00290 <neha@openobserve.ai>

---
+++ tests/api-testing/tests/test_functions.py
@@ -0,0 +1,245 @@
+def test_e2e_folder(create_session, base_url):
+    """Running an E2E test for all xfunctions."""
+
+    session = create_session
+    org_id = "default"
+
+    resp_get_allfunctions = session.get(f"{base_url}api/{org_id}/functions")
+
+    print(resp_get_allfunctions.content)
+    assert (
+        resp_get_allfunctions.status_code == 200
+    ), f"Get all functions list 200, but got {resp_get_allfunctions.status_code} {resp_get_allfunctions.content}"
+
+
+def test_e2e_createdeletefunction(create_session, base_url):
+    """Running an E2E test for create and delete functions."""
+
+    session = create_session
+    # Create a function
+    org_id = "default"
+    payload = {
+        "function": ".a=190025552",
+        "name": "pytestfunction",
+        "params": "row",
+        "transType": 0,
+    }
+
+    resp_create_function = session.post(
+        f"{base_url}api/{org_id}/functions", json=payload
+    )
+
+    print(resp_create_function.content)
+    assert (
+        resp_create_function.status_code == 200
+    ), f"Expected 200, but got {resp_create_function.status_code} {resp_create_function.content}"
+    resp_delete_function = session.delete(
+        f"{base_url}api/{org_id}/functions/pytestfunction"
+    )
+    assert (
+        resp_delete_function.status_code == 200
+    ), f"Deleting this function, but got {resp_delete_function.status_code} {resp_delete_function.content}"
+
+
+def test_e2e_invalidfunction(create_session, base_url):
+    """Running an E2E test for invalid function creation"""
+
+    session = create_session
+    # Create a function
+    org_id = "default"
+    payload = {"name": "...", "function": "...", "params": "row", "transType": 0}
+
+    resp_create_function = session.post(
+        f"{base_url}api/{org_id}/functions", json=payload
+    )
+
+    print(resp_create_function.content)
+    assert (
+        resp_create_function.status_code == 400
+    ), f"Invalid function creation, but got {resp_create_function.status_code} {resp_create_function.content}"
+
+
+def test_e2e_updatingfunction(create_session, base_url):
+    """Running an E2E test for update and delete an existing function."""
+
+    session = create_session
+    # Create a function
+    org_id = "default"
+    payload = {
+        "name": "pytestfunctions",
+        "function": ".a=190022",
+        "params": "row",
+        "transType": 0,
+    }
+
+    resp_create_function = session.post(
+        f"{base_url}api/{org_id}/functions", json=payload
+    )
+
+    print(resp_create_function.content)
+    assert (
+        resp_create_function.status_code == 200
+    ), f"Expected 200, but got {resp_create_function.status_code} {resp_create_function.content}"
+    session = create_session
+    # Create a function
+    org_id = "default"
+    payload = {
+        "name": "pytestfunctions",
+        "function": ".a=1900",
+        "params": "row",
+        "transType": 0,
+    }
+    resp_update_function = session.put(
+        f"{base_url}api/{org_id}/functions/pytestfunctions",
+        json=payload,
+    )
+    print(resp_create_function.content)
+    assert (
+        resp_update_function.status_code == 200
+    ), f"Updating this function, but got {resp_update_function.status_code} {resp_update_function.content}"
+    resp_delete_function = session.delete(
+        f"{base_url}api/{org_id}/functions/pytestfunctions"
+    )
+    assert (
+        resp_delete_function.status_code == 200
+    ), f"Deleting this function, but got {resp_delete_function.status_code} {resp_delete_function.content}"
+
+
+def test_e2e_duplicatefunction(create_session, base_url):
+    """Running an E2E test for function already exists."""
+
+    session = create_session
+    # Create a function
+    org_id = "default"
+    payload = {
+        "name": "pytestfunctions",
+        "function": ".a=190022",
+        "params": "row",
+        "transType": 0,
+    }
+
+    resp_create_function = session.post(
+        f"{base_url}api/{org_id}/functions", json=payload
+    )
+
+    print(resp_create_function.content)
+    assert (
+        resp_create_function.status_code == 200
+    ), f"Expected 200, but got {resp_create_function.status_code} {resp_create_function.content}"
+    resp_delete_function = session.delete(
+        f"{base_url}api/{org_id}/functions/pytestfunction"
+    )
+    session = create_session
+    # Create a function
+    org_id = "default"
+    payload = {
+        "name": "pytestfunctions",
+        "function": ".a=190022",
+        "params": "row",
+        "transType": 0,
+    }
+
+    resp_create_function = session.post(
+        f"{base_url}api/{org_id}/functions", json=payload
+    )
+
+    print(resp_create_function.content)
+    assert (
+        resp_create_function.status_code == 400
+    ), f"Expected 400 function already exists, but got {resp_create_function.status_code} {resp_create_function.content}"
+    resp_delete_function = session.delete(
+        f"{base_url}api/{org_id}/functions/pytestfunctions"
+    )
+    assert (
+        resp_delete_function.status_code == 200
+    ), f"Deleting this function, but got {resp_delete_function.status_code} {resp_delete_function.content}"
+
+
+def test_e2e_functionnotfound(create_session, base_url):
+    """Running an E2E test for function not found."""
+
+    session = create_session
+    # Create a function
+    org_id = "default"
+
+    resp_delete_function = session.delete(
+        f"{base_url}api/{org_id}/functions/pytestfunctionsss"
+    )
+    assert (
+        resp_delete_function.status_code == 404
+    ), f"Function not found, but got {resp_delete_function.status_code} {resp_delete_function.content}"
+
+
+def test_e2e_allfunctionstreams(create_session, base_url):
+    """Running an E2E test for all streams under functions."""
+
+    session = create_session
+    org_id = "default"
+    stream_name = "test"
+
+    resp_get_streamfunction = session.get(
+        f"{base_url}api/{org_id}/{stream_name}/functions"
+    )
+
+    print(resp_get_streamfunction.content)
+    assert (
+        resp_get_streamfunction.status_code == 200
+    ), f"Get all functions streams list 200, but got {resp_get_streamfunction.status_code} {resp_get_streamfunction.content}"
+
+
+def test_e2e_addDeleteStreamFunction(create_session, base_url):
+    """Running an E2E test for add stream to a function and delete."""
+
+    session = create_session
+    org_id = "default"
+    stream_name = "test"
+
+    # Create a function
+    org_id = "default"
+    payload = {
+        "function": ".a=190025552",
+        "name": "pytestfunction",
+        "params": "row",
+        "transType": 0,
+    }
+
+    resp_create_function = session.post(
+        f"{base_url}api/{org_id}/functions", json=payload
+    )
+
+    print(resp_create_function.content)
+    assert (
+        resp_create_function.status_code == 200
+    ), f"Expected 200, but got {resp_create_function.status_code} {resp_create_function.content}"
+
+    resp_get_streamfunction = session.get(
+        f"{base_url}api/{org_id}/{stream_name}/functions"
+    )
+
+    print(resp_get_streamfunction.content)
+    assert (
+        resp_get_streamfunction.status_code == 200
+    ), f"Get all functions streams list 200, but got {resp_get_streamfunction.status_code} {resp_get_streamfunction.content}"
+
+    payload = {"order": 2}
+    resp_add_streamfunction = session.post(
+        f"{base_url}api/{org_id}/{stream_name}/functions/pytestfunction",
+        json=payload,
+    )
+
+    print(resp_add_streamfunction.content)
+    assert (
+        resp_add_streamfunction.status_code == 200
+    ), f"Add stream to function 200, but got {resp_add_streamfunction.status_code} {resp_add_streamfunction.content}"
+    resp_delete_streamfunction = session.delete(
+        f"{base_url}api/{org_id}/{stream_name}/functions/pytestfunction"
+    )
+    assert (
+        resp_delete_streamfunction.status_code == 200
+    ), f"Deleted the function from stream 200, but got {resp_delete_streamfunction.status_code} {resp_delete_streamfunction.content}"
+    resp_delete_function = session.delete(
+        f"{base_url}api/{org_id}/functions/pytestfunction"
+    )
+    assert (
+        resp_delete_function.status_code == 200
+    ), f"Deleting this function, but got {resp_delete_function.status_code} {resp_delete_function.content}"
---
+++ tests/api-testing/tests/test_logs.py
@@ -0,0 +1,218 @@
+def test_e2e_ingestlogs(create_session, base_url):
+    """Running an E2E test logs ingestion."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+    stream_name = "newpy-tests"
+    payload = [
+        {
+            "Athlete": "newtemp",
+            "City": "Athens",
+            "Country": "HUN",
+            "Discipline": "Swimming",
+            "Sport": "Aquatics",
+            "Year": 1896,
+        },
+        {
+            "Athlete": "HERSCHMANN",
+            "City": "Athens",
+            "Country": "CHN",
+            "Discipline": "Swimming",
+            "Sport": "Aquatics",
+            "Year": 1896,
+        },
+    ]
+
+    resp_create_logstream = session.post(
+        f"{url}api/{org_id}/{stream_name}/_json", json=payload
+    )
+
+    print(resp_create_logstream.content)
+    assert (
+        resp_create_logstream.status_code == 200
+    ), f"Get all alerts list 200, but got {resp_create_logstream.status_code} {resp_create_logstream.content}"
+
+
+def test_e2e_nostreamname(create_session, base_url):
+    """Running an E2E test for adding logs without stream name."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+    stream_name = ""
+    payload = [
+        {
+            "Athlete": "newtemp",
+            "City": "Athens",
+            "Country": "HUN",
+            "Discipline": "Swimming",
+            "Sport": "Aquatics",
+            "Year": 1896,
+        },
+        {
+            "Athlete": "HERSCHMANN",
+            "City": "Athens",
+            "Country": "CHN",
+            "Discipline": "Swimming",
+            "Sport": "Aquatics",
+            "Year": 1896,
+        },
+    ]
+
+    resp_create_logstream = session.post(
+        f"{url}api/{org_id}/{stream_name}/_json", json=payload
+    )
+    print(resp_create_logstream.content)
+    assert (
+        resp_create_logstream.status_code == 404
+    ), f"Get all alerts list 200, but got {resp_create_logstream.status_code} {resp_create_logstream.content}"
+
+
+def test_e2e_invalidstreamname(create_session, base_url):
+    """Running an E2E test for adding invalid stream name."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+    stream_name = "newpy#$#$#$#$2@2"
+    payload = [
+        {
+            "Athlete": "newtemp",
+            "City": "Athens",
+            "Country": "HUN",
+            "Discipline": "Swimming",
+            "Sport": "Aquatics",
+            "Year": 1896,
+        },
+        {
+            "Athlete": "HERSCHMANN",
+            "City": "Athens",
+            "Country": "CHN",
+            "Discipline": "Swimming",
+            "Sport": "Aquatics",
+            "Year": 1896,
+        },
+    ]
+    resp_create_logstream = session.post(
+        f"{url}api/{org_id}/{stream_name}/_json", json=payload
+    )
+
+    print(resp_create_logstream.content)
+    assert (
+        resp_create_logstream.status_code == 404
+    ), f"Get all alerts list 200, but got {resp_create_logstream.status_code} {resp_create_logstream.content}"
+
+
+def test_e2e_vrl(create_session, base_url):
+    """Running an E2E test for search log query."""
+
+    session = create_session
+    org_id = "default"
+    stream_name = "gke_fluentbit"
+    access_key = "f="
+    headers = {
+        "Authorization": f"Basic {access_key}",
+        "Content-Type": "application/json",
+    }
+
+    payload = [
+        {
+            "query": {
+                "sql": f'select * from "{stream_name}" ',
+                "start_time": 1700629279639000,
+                "end_time": 1700630179639000,
+                "from": 0,
+                "size": 150,
+                "query_fn": "LmEgPTEgCiAu",
+            },
+            "aggs": {
+                "histogram": "select histogram(_timestamp, '10 second') AS zo_sql_key, count(*) AS zo_sql_num from query GROUP BY zo_sql_key ORDER BY zo_sql_key"
+            },
+        }
+    ]
+
+    resp_get_allalerts = session.post(
+        f"{base_url}web/logs?stream={stream_name}&period=15m&refresh=0&org_identifier={org_id}",
+        json=payload,
+        headers=headers,
+    )
+
+    print(resp_get_allalerts.content)
+    assert (
+        resp_get_allalerts.status_code == 200
+    ), f"Get all alerts list 200, but got {resp_get_allalerts.status_code} {resp_get_allalerts.content}"
+
+
+def test_e2e_getallstreams(create_session, base_url):
+    """Running an E2E test for get all the streams list."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+
+    resp_get_allstreams = session.get(f"{url}api/{org_id}/streams")
+
+    print(resp_get_allstreams.content)
+    assert (
+        resp_get_allstreams.status_code == 200
+    ), f"Get all alerts list 200, but got {resp_get_allstreams.status_code} {resp_get_allstreams.content}"
+
+
+def test_e2e_getschema(create_session, base_url):
+    """Running an E2E test for get stream schema."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+
+    resp_get_streamschema = session.get(f"{url}api/{org_id}/newpy_tests/schema")
+
+    print(resp_get_streamschema.content)
+    assert (
+        resp_get_streamschema.status_code == 200
+    ), f"Get all alerts list 200, but got {resp_get_streamschema.status_code} {resp_get_streamschema.content}"
+
+
+def test_e2e_deleteinvalidstreams(create_session, base_url):
+    """Running an E2E test for delete invalid stream."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+
+    resp_get_deletestreams = session.delete(f"{url}api/{org_id}/invalidstream")
+
+    print(resp_get_deletestreams.content)
+    assert (
+        resp_get_deletestreams.status_code == 404
+    ), f"Get all alerts list 200, but got {resp_get_deletestreams.status_code} {resp_get_deletestreams.content}"
+
+
+def test_e2e_incorrectstreamesettings(create_session, base_url):
+    """Running an E2E test for invalid stream settings."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+    resp_get_streamssettings = session.post(f"{url}api/{org_id}/newpy_tests/settings")
+
+    print(resp_get_streamssettings.content)
+    assert (
+        resp_get_streamssettings.status_code == 400
+    ), f"Get all alerts list 200, but got {resp_get_streamssettings.status_code} {resp_get_streamssettings.content}"
+
+
+def test_e2e_deletevalidstreams(create_session, base_url):
+    """Running an E2E test for delete streams."""
+
+    session = create_session
+    url = base_url
+    org_id = "default"
+
+    resp_get_allstreams = session.delete(f"{url}api/{org_id}/newpy_tests")
+
+    print(resp_get_allstreams.content)
+    assert (
+        resp_get_allstreams.status_code == 200
+    ), f"Get all alerts list 200, but got {resp_get_allstreams.status_code} {resp_get_allstreams.content}"
