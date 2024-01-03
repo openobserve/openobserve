e15c7aeb782abe1bccedb76f23e401c65e869f06
ktx-abhay <abhay.padamani@kiara.tech>
2024-01-03 12:25:36 +0530

    hashing to select color for promql chart

--- web/src/utils/dashboard/convertPromQLData.ts
+++ web/src/utils/dashboard/convertPromQLData.ts
@@ -22,6 +22,117 @@ import {
 import { utcToZonedTime } from "date-fns-tz";
 import { calculateGridPositions } from "./calculateGridForSubPlot";
 
+// const colorArray = [
+//   '#0056b3', '#003e80', '#00294d', '#001326',
+//   '#28a745', '#1f8e3e', '#156331', '#0e3f24', '#072717',
+//   '#dc3545', '#a52a35', '#751e26', '#4e141a', '#27090d',
+//   '#fd7e14', '#d46a11', '#a9560e', '#803f0a', '#551f06',
+//   '#ffc107', '#d9a106', '#b28605', '#8b6a04', '#634d03',
+//   '#6f42c1', '#57348f', '#412663', '#2a1940', '#160c20',
+//   '#e83e8c', '#b82d66', '#8c1f49', '#631433', '#3f0a1d'
+// ];
+
+const colorArray = 
+// [
+//   "#99cadd",
+//   "#a9bdd5",
+//   "#d6a9bc",
+//   "#c691a4",
+//   "#b87c94",
+//   "#ffcb20",
+//   "#ff3b3b",
+//   "#ff8d0c",
+//   "#42a6ff",
+//   "#ffb3ba",
+//   "#ffdfba",
+//   "#ffffba",
+//   "#baffc9",
+//   "#bae1ff",
+//   "#a8e6cf",
+//   "#dcedc1",
+//   "#ffd3b6",
+//   "#ffaaa5",
+//   "#ff8b94",
+//   "#ffbf00",
+//   "#ffcf40",
+//   "#ffdc73",
+//   "#b88c8c",
+//   "#ddadad",
+//   "#d6c7c7",
+//   "#9fb9bf",
+//   "#aec8ce",
+// ];
+
+// [
+//   "#E24D42", // Red
+//   "#1F78C1", // Blue
+//   "#BA43A9", // Purple
+//   "#705DA0", // Violet
+//   "#466803", // Green
+//   "#508642", // Dark Green
+//   "#447EBC", // Dark Blue
+//   "#C15C17", // Brown
+//   "#890F02", // Dark Red
+//   "#757575", // Grey
+//   "#70DBED", // Light Blue
+//   "#6ED0E0", // Turquoise
+//   "#B9D7D9", // Light Grey
+//   "#D683CE", // Light Purple
+//   "#E5AC0E", // Orange
+//   "#AEA2E0", // Lavender
+//   "#5195CE", // Bright Blue
+//   "#D9BF77", // Beige
+//   "#FADE2A", // Yellow
+//   "#2F575E", // Dark Turquoise
+//   "#99440A", // Dark Brown
+//   "#58140C", // Maroon
+//   "#052B51", // Navy
+//   "#511749", // Dark Violet
+//   "#3F2B5B", // Dark Indigo
+//   "#E0F9D7", // Light Green
+//   "#FCEACA", // Light Yellow
+//   "#CFFAFF", // Sky Blue
+//   "#F9E2D2", // Light Pink
+//   "#FCE2DE", // Pink
+//   "#BADFF4", // Light Bright Blue
+//   "#F9D9F9", // Light Lavender
+//   "#DEDAF7"  // Pale Lavender
+// ]
+
+[
+  "#5470c6",
+  "#91cc75",
+  "#fac858",
+  "#ee6666",
+  "#73c0de",
+  "#3ba272",
+  "#fc8452",
+  "#9a60b4",
+  "#dc1e1e",
+  "#9086e9",
+  "#27e162",
+  "#e08de6",
+  "#de6086",
+  "rgba(161,225,200,0.48)",
+  "#ec8ceb",
+  "#99e9f1",
+  "#eec59c"
+];
+
+function getIndexForName(name: any) {
+  let index = 0;
+  for (let i = 0; i < 15; i++) {
+    const charCode = name.charCodeAt(i) || 0;
+    index += charCode * (i + 1);
+  }
+  return index % colorArray.length;
+}
+
+function nameToColor(name: any) {
+  const index = getIndexForName(name);
+  return colorArray[index];
+}
+
 /**
  * Converts PromQL data into a format suitable for rendering a chart.
  *
@@ -304,6 +415,14 @@ export const convertPromQLData = (
                   metric.metric,
                   panelSchema.queries[index].config.promql_legend
                 ),
+                itemStyle: {
+                  color: nameToColor(
+                    getPromqlLegendName(
+                      metric.metric,
+                      panelSchema.queries[index].config.promql_legend
+                    )
+                  ),
+                },
                 // if utc then simply return the values by removing z from string
                 // else convert time from utc to zoned
                 // used slice to remove Z from isostring to pass as a utc
