ba910367fc97a064f331e9c7c3162c742149f568
ktx-abhay <abhay.padamani@kiara.tech>
2024-01-05 12:38:30 +0530

    add: function to find shades

--- web/src/utils/dashboard/colorPalette.ts
+++ web/src/utils/dashboard/colorPalette.ts
@@ -41,7 +41,6 @@ export const colorArrayByValue = [
   "#069a55",
 ];
 
-
 export const getIndexForName = (name: any, colorArray: any) => {
   let index = 0;
   for (let i = 0; i < 15; i++) {
@@ -49,31 +48,83 @@ export const getIndexForName = (name: any, colorArray: any) => {
     index += charCode * (i + 1);
   }
   return index % colorArray.length;
-}
+};
 
-export const nameToColor = (name: any, colorArray: any) => {
+const nameToColor = (name: any, colorArray: any) => {
   const index = getIndexForName(name, colorArray);
   return colorArray[index];
-}
+};
 
-export const getColor = (panelSchema: any) => {
+const scaleValue = (value: any, min: any, max: any) => {
+  return ((value - min) * 100) / (max - min);
+};
+
+const shadeColor = (color: any, value: any, min: any, max: any) => {
+  let percent: any = scaleValue(value, min, max);
+  let R: any = parseInt(color.substring(1, 3), 16);
+  let G: any = parseInt(color.substring(3, 5), 16);
+  let B: any = parseInt(color.substring(5, 7), 16);
+
+  R = parseInt(((R * (100 + percent)) / 100).toString());
+  G = parseInt(((G * (100 + percent)) / 100).toString());
+  B = parseInt(((B * (100 + percent)) / 100).toString());
+
+  R = R < 255 ? R : 255;
+  G = G < 255 ? G : 255;
+  B = B < 255 ? B : 255;
+
+  let RR = R.toString(16).length == 1 ? "0" + R.toString(16) : R.toString(16);
+  let GG = G.toString(16).length == 1 ? "0" + G.toString(16) : G.toString(16);
+  let BB = B.toString(16).length == 1 ? "0" + B.toString(16) : B.toString(16);
+
+  return "#" + RR + GG + BB;
+};
+
+export const getColor = (
+  panelSchema: any,
+  value?: any,
+  min?: any,
+  max?: any
+) => {
   // switch case based on panelSchema.color type
   switch (panelSchema?.color?.mode) {
-    case "fixed":
+    case "fixed": {
+      return panelSchema?.color?.fixedColor ?? "#5b8ff9";
+    }
+    case "shades": {
+      // based on selected color pass different shades of same color
+      return shadeColor(
+        panelSchema?.color?.fixedColor ?? "#5b8ff9",
+        value ?? 50,
+        min ?? 0,
+        max ?? 100
+      );
+    }
 
-    case "shades":
+    case "palette-classic": {
+      // return color using colorArrayBySeries
+      // NOTE: value will be series name
+      return nameToColor(value, colorArrayBySeries);
+    }
 
-    case "palette-classic":
+    case "continuous": {
+      // based on selected color and value pass different shades of same color
+      return shadeColor(
+        panelSchema?.color?.fixedColor ?? "#5b8ff9",
+        value ?? 50,
+        min ?? 0,
+        max ?? 100
+      );
+    }
 
-    case "palette-classic-by-name":
+    // case "scheme":
 
-    case "continuous-greens":
+    // case "opacity":
 
-    case "scheme":
-
-    case "opacity":
-        
-    default:
-      break;
+    default: {
+      // return color using colorArrayBySeries
+      // NOTE: value will be series name
+      return nameToColor(value, colorArrayBySeries);
+    }
   }
-}
+};
