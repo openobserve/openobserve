c5fc074cd2b425f81cc838b28d94872925501b46
ktx-abhay <abhay.padamani@kiara.tech>
2024-01-09 12:21:09 +0530

    add: table download option

--- web/src/components/dashboards/panels/TableRenderer.vue
+++ web/src/components/dashboards/panels/TableRenderer.vue
@@ -26,10 +26,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     :columns="data.columns"
     row-key="id"
   >
+    <template v-slot:top-right>
+      <q-btn
+        color="primary"
+        icon-right="archive"
+        label="Export to csv"
+        no-caps
+        style="height: 5px;"
+        @click="exportTable"
+      />
+    </template>
   </q-table>
 </template>
 
 <script lang="ts">
+import { exportFile, useQuasar } from "quasar";
 import { defineComponent, ref, onMounted, watch } from "vue";
 
 export default defineComponent({
@@ -42,10 +53,68 @@ export default defineComponent({
     },
   },
   setup(props: any) {
+    const $q = useQuasar();
+
+    function wrapCsvValue(val: any, formatFn?: any, row?: any) {
+      let formatted = formatFn !== void 0 ? formatFn(val, row) : val;
+
+      formatted =
+        formatted === void 0 || formatted === null ? "" : String(formatted);
+
+      formatted = formatted.split('"').join('""');
+      /**
+       * Excel accepts \n and \r in strings, but some other CSV parsers do not
+       * Uncomment the next two lines to escape new lines
+       */
+      // .split('\n').join('\\n')
+      // .split('\r').join('\\r')
+
+      return `"${formatted}"`;
+    }
+
+    const exportTable = () => {
+      // naive encoding to csv format
+      const content = [
+        props?.data?.columns?.map((col: any) => wrapCsvValue(col.label)),
+      ]
+        .concat(
+          props?.data?.rows?.map((row: any) =>
+            props?.data?.columns
+              ?.map((col: any) =>
+                wrapCsvValue(
+                  typeof col.field === "function"
+                    ? col.field(row)
+                    : row[col.field === void 0 ? col.name : col.field],
+                  col.format,
+                  row
+                )
+              )
+              .join(",")
+          )
+        )
+        .join("\r\n");
+
+      const status = exportFile("table-export.csv", content, "text/csv");
+
+      if (status === true) {
+        $q.notify({
+          message: "table downloaded as a csv",
+          color: "positive",
+          timeout: 2000,
+        });
+      } else {
+        $q.notify({
+          message: "Browser denied file download...",
+          color: "negative",
+          icon: "warning",
+        });
+      }
+    };
     return {
       pagination: ref({
         rowsPerPage: 0,
       }),
+      exportTable,
     };
   },
 });
