d3ae036dc762c392e67f769b76b9b711562c0bce
hengfeiyang <hengfei.yang@gmail.com>
2024-01-15 23:31:20 +0800

    feat: add node status api

--- src/ingester/src/lib.rs
+++ src/ingester/src/lib.rs
@@ -25,7 +25,7 @@ mod writer;
 
 pub use entry::Entry;
 pub use immutable::read_from_immutable;
-pub use writer::{check_memtable_size, get_writer, read_from_memtable, Writer};
+pub use writer::{check_memtable_size, flush_all, get_writer, read_from_memtable, Writer};
 
 pub async fn init() -> errors::Result<()> {
     // check uncompleted parquet files, need delete those files
