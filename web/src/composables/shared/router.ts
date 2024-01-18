ce406f963684da730ac7ffefa3359fac7895794c
omkark06 <omkar@zinclabs.io>
2024-01-18 14:22:18 +0530

    add: IAM changes
    1. Replaced Users Menu with IAM
    2. Added Users, Organizations, User Groups and Roles tabs

--- web/src/composables/shared/router.ts
+++ web/src/composables/shared/router.ts
@@ -56,6 +56,16 @@ const ErrorsDashboard = () =>
 const ApiDashboard = () =>
   import("@/components/rum/performance/ApiDashboard.vue");
 
+const IdentityAccessManagement = () =>
+  import("@/views/IdentityAccessManagement.vue");
+
+const AppGroups = () => import("@/components/iam/groups/AppGroups.vue");
+
+const AppRoles = () => import("@/components/iam/roles/AppRoles.vue");
+
+const AppOrganizations = () =>
+  import("@/components/iam/organizations/AppOrganizations.vue");
+
 import { routeGuard } from "@/utils/zincutils";
 import useIngestionRoutes from "./useIngestionRoutes";
 const useRoutes = () => {
@@ -196,6 +206,48 @@ const useRoutes = () => {
       beforeEnter(to: any, from: any, next: any) {
         routeGuard(to, from, next);
       },
+    },
+    {
+      path: "iam",
+      name: "iam",
+      component: IdentityAccessManagement,
+      beforeEnter(to: any, from: any, next: any) {
+        routeGuard(to, from, next);
+      },
+      children: [
+        {
+          path: "users",
+          name: "users",
+          component: Users,
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+        {
+          path: "organizations",
+          name: "organizations",
+          component: AppOrganizations,
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+        {
+          path: "groups",
+          name: "groups",
+          component: AppGroups,
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+        {
+          path: "roles",
+          name: "roles",
+          component: AppRoles,
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+      ],
     },
     {
       path: "member_subscription",
