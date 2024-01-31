2787759fdc93df7615081bf48d04ade6b51ef6f3
Neha P <144308844+neha00290@users.noreply.github.com>
2024-01-31 17:53:01 +0530

    test: fix saved view deletion under logs (#2618)
    - fixed saved views delete testcase

--- tests/ui-testing/cypress/e2e/tests/logs_tests.cy.js
+++ tests/ui-testing/cypress/e2e/tests/logs_tests.cy.js
@@ -254,25 +254,6 @@ describe("Logs testcases", () => {
     applyQueryButton();
   });
 
-  it("should add invalid query and display error", () => {
-    // Type the value of a variable into an input field
-    cy.intercept("GET", logData.ValueQuery).as("value");
-
-    applyQueryButton();
-    cy.get('[data-cy="date-time-button"]').click({ force: true });
-    cy.get('[data-test="date-time-relative-6-w-btn"] > .q-btn__content').click({
-      force: true,
-    });
-    cy.get('[data-test="logs-search-bar-query-editor"]').type(
-      "kubernetes_labels_deploy="
-    );
-    cy.wait(3000)
-    cy.get('[data-cy="search-bar-refresh-button"]', { timeout: 2000 }).click({
-      force: true,
-    });
-    cy.get('[data-test="logs-search-error-message"]',{timeout:2000}).should("exist");
-  });
-
   it("should click run query after SQL toggle on but without any query", () => {
     // Wait for 2 seconds
     cy.wait(3000);
@@ -1064,7 +1045,7 @@ describe("Logs testcases", () => {
     cy.get(".q-notification__message").should('not.exist')
   });
 
-  it.skip("should redirect to logs after clicking on stream explorer via stream page", () => {
+  it("should redirect to logs after clicking on stream explorer via stream page", () => {
     // cy.intercept("GET", logData.ValueQuery).as("value");
     cy.get('[data-cy="index-field-search-input"]').type('code')
     cy.get('[data-test="log-search-expand-code-field-btn"]').click()
@@ -1090,12 +1071,33 @@ describe("Logs testcases", () => {
       );
     });
     cy.get('[data-test="logs-search-saved-views-btn"] > .q-btn-dropdown__arrow-container > .q-btn__content > .q-icon').click()
-    cy.get('[data-test="logs-search-bar-delete-streamlogsnavigate-saved-view-btn"]').each(($button) => {
-      // Click on each delete button
-      cy.wrap($button).click();
-      cy.get('[data-test="confirm-button"]').click({force:true})
+    cy.get('[data-test="logs-search-bar-delete-streamlogsnavigate-saved-view-btn"]').each(($button, index) => {
+      if (index === 2) {
+        // Click on the delete button for the first item
+        cy.wrap($button).click();
+        cy.get('[data-test="confirm-button"]').click({ force: true });
+      }
     });
- 
   });
+
+
+  it("should add invalid query and display error", () => {
+    // Type the value of a variable into an input field
+    cy.intercept("GET", logData.ValueQuery).as("value");
+
+    applyQueryButton();
+    cy.get('[data-cy="date-time-button"]').click({ force: true });
+    cy.get('[data-test="date-time-relative-15-m-btn"] > .q-btn__content > .block').click({
+      force: true,
+    });
+    cy.get('[data-test="logs-search-bar-query-editor"]', {timeout:2000}).type(
+      "kubernetes"
+    );
+    cy.wait(2000)
+    cy.get('[data-cy="search-bar-refresh-button"] > .q-btn__content').click({force:true})
+    cy.wait("@allsearch")
+    cy.get('[data-test="logs-search-error-message"]',{timeout:2000}).should("exist");
+  });
+
 
 });
