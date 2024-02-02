c5197f987821047dd847cc404df5e80bcd857752
ktx-abhay <abhay.padamani@kiara.tech>
2024-02-02 13:09:54 +0530

    fix: increase panel col num

--- web/src/utils/dashboard/convertDashboardSchemaVersion.ts
+++ web/src/utils/dashboard/convertDashboardSchemaVersion.ts
@@ -87,6 +87,11 @@ export function convertDashboardSchemaVersion(data: any) {
       break;
     }
     case 2: {
+      // layout width migration from 12 col number to 48 col number
+      data.panels.forEach((panelItem: any) => {
+        panelItem.layout.w = panelItem.layout.w * 4;
+      });
+
       // convert array of panels to array of tabs
       data.tabs = [
         {
