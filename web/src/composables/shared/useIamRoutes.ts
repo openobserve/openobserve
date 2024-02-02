9717963334c44c35694a46d1df00203ca331cf40
Omkar Kesarkhane <127078473+omkarK06@users.noreply.github.com>
2024-02-02 13:02:14 +0530

    fix: fixed IAM routes (#2637)

---
+++ web/src/composables/shared/useIamRoutes.ts
@@ -0,0 +1,110 @@
+// Copyright 2023 Zinc Labs Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import config from "@/aws-exports";
+import { routeGuard } from "@/utils/zincutils";
+
+const IdentityAccessManagement = () =>
+  import("@/views/IdentityAccessManagement.vue");
+
+const AppGroups = () => import("@/components/iam/groups/AppGroups.vue");
+
+const AppRoles = () => import("@/components/iam/roles/AppRoles.vue");
+
+const EditRole = () => import("@/components/iam/roles/EditRole.vue");
+
+const EditGroup = () => import("@/components/iam/groups/EditGroup.vue");
+
+import Organizations from "@/enterprise/components/organizations/Organization.vue";
+
+import Users from "@/views/User.vue";
+
+const useEnterpriseRoutes = () => {
+  const routes: any = [
+    {
+      path: "iam",
+      name: "iam",
+      component: IdentityAccessManagement,
+      beforeEnter(to: any, from: any, next: any) {
+        routeGuard(to, from, next);
+      },
+      children: [
+        {
+          path: "users",
+          name: "users",
+          component: Users,
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+      ],
+    },
+  ];
+
+  console.log(config.isCloud, typeof config.isEnterprise);
+  if (config.isCloud == "true" || config.isEnterprise == "true") {
+    routes[0].children.push(
+      ...[
+        {
+          path: "organizations",
+          name: "organizations",
+          component: Organizations,
+          meta: {
+            keepAlive: true,
+          },
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+        {
+          path: "groups",
+          name: "groups",
+          component: AppGroups,
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+        {
+          path: "groups/edit/:group_name",
+          name: "editGroup",
+          component: EditGroup,
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+        {
+          path: "roles",
+          name: "roles",
+          component: AppRoles,
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+        {
+          path: "roles/edit/:role_name",
+          name: "editRole",
+          component: EditRole,
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+      ]
+    );
+  }
+
+  return routes;
+};
+
+export default useEnterpriseRoutes;
