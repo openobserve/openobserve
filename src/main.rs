10e813d4a6658e56c330c5c6b395844590a50752
oasisk <ashish.j.kolhe@gmail.com>
2024-02-13 11:25:01 +0530

    changes

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ use openobserve::{
                 file_list::Filelister,
                 logs::LogsServer,
                 metrics::{ingester::Ingester, querier::Querier},
-                search::Searcher,
+                search::intra_cluster::Searcher,
                 traces::TraceServer,
                 usage::UsageServerImpl,
             },
