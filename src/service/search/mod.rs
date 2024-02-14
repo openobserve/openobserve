763724bc495806b07af59f08c13f5fdab52991f6
Bhargav <BJP232004@GMAIL.COM>
2024-02-14 16:00:07 +0530

    fix merge issue

--- src/service/search/mod.rs
+++ src/service/search/mod.rs
@@ -83,42 +83,6 @@ pub async fn search(
     search_in_cluster(req).await
 }
 
-#[tracing::instrument(name = "service:search_partition_multi:enter", skip(req))]
-pub async fn search_partition_multi(
-    session_id: &str,
-    org_id: &str,
-    stream_type: StreamType,
-    req: &search::MultiSearchPartitionRequest,
-) -> Result<search::SearchPartitionResponse, Error> {
-    let mut res = search::SearchPartitionResponse::default();
-
-    for query in &req.sql {
-        match search_partition(
-            session_id,
-            org_id,
-            stream_type,
-            &search::SearchPartitionRequest {
-                start_time: req.start_time,
-                end_time: req.end_time,
-                sql: query.to_string(),
-            },
-        )
-        .await
-        {
-            Ok(resp) => {
-                
-                if resp.partitions.len() > res.partitions.len() {
-                    res = resp;
-                }
-            }
-            Err(err) => {
-                log::error!("search_partition_multi error: {:?}", err);
-            }
-        };
-    }
-    Ok(res)
-}
-
 #[tracing::instrument(name = "service:search_partition:enter", skip(req))]
 pub async fn search_partition(
     session_id: &str,
