ce2c034f3437a263cb6bad9c493a526c880ec03e
Ankur Srivastava <best.ankur@gmail.com>
2024-02-26 13:32:06 +0100

    fix: ensure aggregations dont go via index logic

--- src/job/files/idx.rs
+++ src/job/files/idx.rs
@@ -13,11 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::{
-    fs,
-    io::{self, BufRead, BufReader},
-    path::Path,
-};
+use std::{fs, io::BufReader, path::Path};
 
 use arrow::ipc::reader::StreamReader;
 use config::{
