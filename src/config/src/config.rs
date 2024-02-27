df1c57e160235879471003dc175bf57bbeaaeeb7
Prabhat Sharma <hi.prabhat@gmail.com>
2024-02-27 13:20:47 -0800

    fix: change ZO_RESTRICTED_ROUTES_ON_EMPTY_DATA default to false (#2803)
    change ZO_RESTRICTED_ROUTES_ON_EMPTY_DATA to false. This will not
    restrict self hosted users from visiting all the pages unless data is
    ingested.

--- src/config/src/config.rs
+++ src/config/src/config.rs
@@ -360,7 +360,7 @@ pub struct Common {
     pub memory_circuit_breaker_ratio: usize,
     #[env_config(
         name = "ZO_RESTRICTED_ROUTES_ON_EMPTY_DATA",
-        default = true,
+        default = false,
         help = "Control the redirection of a user to ingestion page in case there is no stream found."
     )]
     pub restricted_routes_on_empty_data: bool,
