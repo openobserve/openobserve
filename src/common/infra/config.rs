3d8dd3d8e44071e8d9f8e9c433c694af92fbc45b
Ashish Kolhe <35160958+oasisk@users.noreply.github.com>
2024-03-08 19:30:50 +0530

    fix: cache latest schema (#2915)

--- src/common/infra/config.rs
+++ src/common/infra/config.rs
@@ -45,6 +45,7 @@ pub static BUILD_DATE: &str = env!("GIT_BUILD_DATE");
 // global cache variables
 pub static KVS: Lazy<RwHashMap<String, bytes::Bytes>> = Lazy::new(Default::default);
 pub static STREAM_SCHEMAS: Lazy<RwAHashMap<String, Vec<Schema>>> = Lazy::new(Default::default);
+pub static STREAM_SCHEMAS_LATEST: Lazy<RwAHashMap<String, Schema>> = Lazy::new(Default::default);
 pub static STREAM_SETTINGS: Lazy<RwAHashMap<String, StreamSettings>> = Lazy::new(Default::default);
 pub static STREAM_FUNCTIONS: Lazy<RwHashMap<String, StreamFunctionsList>> =
     Lazy::new(DashMap::default);
