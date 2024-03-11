0bc883a0b5f708e2ed6672896e3fc1de92eb7441
Bhargav <BJP232004@GMAIL.COM>
2024-03-11 12:39:49 +0530

    histogram toggle load only histogram data

--- web/src/composables/useLogs.ts
+++ web/src/composables/useLogs.ts
@@ -1295,10 +1295,13 @@ const useLogs = () => {
             query: queryReq,
             page_type: searchObj.data.stream.streamType,
           })
-          .then((res) => {
+          .then(async (res) => {
             searchObj.loading = false;
+            alert(res.data.aggs)
             searchObj.data.queryResults.aggs = res.data.aggs;
             searchObj.data.queryResults.total = res.data.total;
+            await nextTick();
+            alert(searchObj.data.queryResults.aggs)
             generateHistogramData();
             // searchObj.data.histogram.chartParams.title = getHistogramTitle();
             searchObj.loadingHistogram = false;
@@ -1568,6 +1571,7 @@ const useLogs = () => {
       const xData: number[] = [];
       const yData: number[] = [];
 
+      alert(searchObj.data.queryResults.aggs)
       if (
         searchObj.data.queryResults.hasOwnProperty("aggs") &&
         searchObj.data.queryResults.aggs
@@ -1601,6 +1605,7 @@ const useLogs = () => {
         errorMsg: "",
         errorDetail: "",
       };
+      alert(searchObj.data.histogram.xData.length)
     } catch (e: any) {
       console.log("Error while generating histogram data");
     }
