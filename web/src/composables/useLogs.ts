1a169d376991ae13622d53c6a80e9c06626b9e93
Bhargav <BJP232004@GMAIL.COM>
2024-03-11 17:24:35 +0530

    fix: loag page improvement

--- web/src/composables/useLogs.ts
+++ web/src/composables/useLogs.ts
@@ -1295,13 +1295,10 @@ const useLogs = () => {
             query: queryReq,
             page_type: searchObj.data.stream.streamType,
           })
-          .then(async (res) => {
+          .then((res) => {
             searchObj.loading = false;
-            alert(res.data.aggs)
             searchObj.data.queryResults.aggs = res.data.aggs;
             searchObj.data.queryResults.total = res.data.total;
-            await nextTick();
-            alert(searchObj.data.queryResults.aggs)
             generateHistogramData();
             // searchObj.data.histogram.chartParams.title = getHistogramTitle();
             searchObj.loadingHistogram = false;
@@ -1571,7 +1568,6 @@ const useLogs = () => {
       const xData: number[] = [];
       const yData: number[] = [];
 
-      alert(searchObj.data.queryResults.aggs)
       if (
         searchObj.data.queryResults.hasOwnProperty("aggs") &&
         searchObj.data.queryResults.aggs
@@ -1605,7 +1601,6 @@ const useLogs = () => {
         errorMsg: "",
         errorDetail: "",
       };
-      alert(searchObj.data.histogram.xData.length)
     } catch (e: any) {
       console.log("Error while generating histogram data");
     }
