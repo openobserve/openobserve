2fbac2e48f299929261254085af2bcd6501115fb
Bhargav <BJP232004@GMAIL.COM>
2024-03-12 16:15:40 +0530

    fix: duplicate call on refresh logs page with sql mode

--- web/src/plugins/logs/Index.vue
+++ web/src/plugins/logs/Index.vue
@@ -733,8 +733,10 @@ export default defineComponent({
         this.searchObj.meta.sqlMode = false;
         this.searchObj.data.query = "";
         this.searchObj.data.editorValue = "";
-        this.searchObj.loading = true;
-        this.getQueryData();
+        if (this.searchObj.loading == false) {
+          this.searchObj.loading = true;
+          this.getQueryData();
+        }
       }
       if (this.searchObj.loading == false) {
         this.searchObj.loading = true;
