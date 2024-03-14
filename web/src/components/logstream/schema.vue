88b621ffeee07079562bbe7062d38b9854a382d7
Bhargav <BJP232004@GMAIL.COM>
2024-03-14 14:11:34 +0530

    fix: added loading state on stream page (#2957)

--- web/src/components/logstream/schema.vue
+++ web/src/components/logstream/schema.vue
@@ -24,7 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           </div>
         </div>
         <div class="col-auto">
-          <q-btn v-close-popup="true" round flat icon="close" />
+          <q-btn v-close-popup="true" round
+flat icon="close" />
         </div>
       </div>
     </q-card-section>
