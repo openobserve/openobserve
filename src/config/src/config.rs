ed872470ad15392aa813571510e40e13db97a9d3
Hengfei Yang <hengfei.yang@gmail.com>
2024-03-14 13:08:20 +0800

    fix: field not found when merging (#2967)

--- src/config/src/config.rs
+++ src/config/src/config.rs
@@ -533,7 +533,7 @@ pub struct Limit {
     pub enrichment_table_limit: usize,
     #[env_config(name = "ZO_ACTIX_REQ_TIMEOUT", default = 30)] // seconds
     pub request_timeout: u64,
-    #[env_config(name = "ZO_ACTIX_KEEP_ALIVE", default = 10)] // seconds
+    #[env_config(name = "ZO_ACTIX_KEEP_ALIVE", default = 5)] // seconds
     pub keep_alive: u64,
     #[env_config(name = "ZO_ACTIX_SHUTDOWN_TIMEOUT", default = 10)] // seconds
     pub shutdown_timeout: u64,
