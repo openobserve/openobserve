2ed6be2c787c0821921fb5aee1d825ba3cb4a7da
Omkar Kesarkhane <127078473+omkarK06@users.noreply.github.com>
2024-03-15 16:20:11 +0530

    fix: RUM error explorer UI issues (#2990) (#2998)
    fix #2982
    1. Fix RUM performance dashboards overlapping issue
    2. Fixed search field not found issues in Error Explorer when data is
    not present
    3. Fixed filtering of session events in session viewer
    4. Partition key not getting restored in UI after saving  #2976
    
    ---------
    
    ---------
    
    Co-authored-by: Kirtan Thakkar <kirtan@kiara.tech>
    Co-authored-by: Hengfei Yang <hengfei.yang@gmail.com>

--- web/src/components/rum/AppTable.vue
+++ web/src/components/rum/AppTable.vue
@@ -13,85 +13,83 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
-
 <template>
-  <!-- 
-    Create a table like SearchResults.vue component.
-    1. Props for columns, rows and loading
-    2. use virtual scroll
-    3. Column should have boolean property to close column. closable: true
-    4. Column should have boolean property to hide column. hidden: true
-    5. Component should have highlight property to highlight search text
-    6. Rows should have boolean property to expand row. expandable: true
-   -->
-  <div class="q-pa-md">
-    <q-table
-      style="height: calc(100vh - 240px)"
-      flat
-      bordered
-      ref="tableRef"
-      :title="title"
-      :rows="rows"
-      :columns="columns as []"
-      :table-colspan="9"
-      row-key="index"
-      virtual-scroll
-      :virtual-scroll-item-size="48"
-      :rows-per-page-options="[0]"
-      @virtual-scroll="onScroll"
-      hide-bottom
-    >
-      <template v-slot:header="props">
-        <q-tr :props="props" class="thead-sticky">
-          <q-th
-            v-for="col in props.cols"
-            :key="col.name"
+  <div :style="{ height: height }" class="app-table-container">
+    <template v-if="!rows.length">
+      <div class="q-pt-md text-center text-subtitle">
+        <NoData />
+      </div>
+    </template>
+    <template v-else>
+      <q-table
+        flat
+        bordered
+        ref="tableRef"
+        :title="title"
+        :rows="rows"
+        :columns="(columns as [])"
+        :table-colspan="9"
+        row-key="index"
+        :virtual-scroll="virtualScroll"
+        :virtual-scroll-item-size="48"
+        :rows-per-page-options="[0]"
+        @virtual-scroll="onScroll"
+        class="full-height"
+        hide-bottom
+      >
+        <template v-slot:header="props">
+          <q-tr :props="props" class="thead-sticky">
+            <q-th
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+              :style="col.style"
+            >
+              {{ col.label }}
+            </q-th>
+          </q-tr>
+        </template>
+        <template v-slot:body="props">
+          <q-tr :props="props" :key="`m_${props.row.index}`">
+            <q-td
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+              @click="handleDataClick(col.name, props.row)"
+            >
+              <template v-if="col.slot">
+                <slot :name="col.slotName" :column="props" />
+              </template>
+              <template v-else-if="col.type === 'action'">
+                <q-icon :name="col.icon" size="24px" class="cursor-pointer" />
+              </template>
+              <template v-else>
+                {{ col.value }}
+              </template>
+            </q-td>
+          </q-tr>
+          <q-tr
+            v-show="props.expand"
             :props="props"
-            :style="col.style"
+            :key="`e_${props.row.index}`"
+            class="q-virtual-scroll--with-prev"
           >
-            {{ col.label }}
-          </q-th>
-        </q-tr>
-      </template>
-
-      <template v-slot:body="props">
-        <q-tr :props="props" :key="`m_${props.row.index}`">
-          <q-td
-            v-for="col in props.cols"
-            :key="col.name"
-            :props="props"
-            @click="handleDataClick(col.name, props.row)"
-          >
-            <template v-if="col.slot">
-              <slot :name="col.slotName" />
-            </template>
-            <template v-else-if="col.type === 'action'">
-              <q-icon :name="col.icon" size="24px" class="cursor-pointer" />
-            </template>
-            <template v-else>
-              {{ col.value }}
-            </template>
-          </q-td>
-        </q-tr>
-        <q-tr
-          v-show="props.expand"
-          :props="props"
-          :key="`e_${props.row.index}`"
-          class="q-virtual-scroll--with-prev"
-        >
-          <q-td colspan="100%">
-            <div class="text-left">
-              This is expand slot for row above: {{ props.row.name }} (Index:
-              {{ props.row.index }}).
-            </div>
-          </q-td>
-        </q-tr>
-      </template>
-    </q-table>
+            <q-td colspan="100%">
+              <div class="text-left">
+                This is expand slot for row above: {{ props.row.name }} (Index:
+                {{ props.row.index }}).
+              </div>
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
+import NoData from "../shared/grid/NoData.vue";
+
 defineProps({
   columns: {
     type: Array,
@@ -119,6 +117,14 @@ defineProps({
     type: String,
     default: "",
   },
+  virtualScroll: {
+    type: Boolean,
+    default: true,
+  },
+  height: {
+    type: String,
+    default: "100%",
+  },
 });
 
 const emit = defineEmits(["event-emitted"]);
@@ -127,16 +133,29 @@ const handleDataClick = (columnName: string, row: any) => {
   emit("event-emitted", "cell-click", { columnName, row });
 };
 
-const onScroll = () => {};
+const onScroll = (e: any) => {
+  try {
+    emit("event-emitted", "scroll", e);
+  } catch (e) {
+    console.log("error", e);
+  }
+};
 </script>
 
-<style>
-.thead-sticky,
-.tfoot-sticky {
-  position: sticky;
-  top: 0;
-  opacity: 1;
-  z-index: 1;
-  background-color: #f5f5f5;
+<style lang="scss">
+.app-table-container {
+  .thead-sticky,
+  .tfoot-sticky {
+    position: sticky;
+    top: 0;
+    opacity: 1;
+    z-index: 1;
+    background: #f5f5f5;
+  }
+
+  .q-table--dark .thead-sticky,
+  .q-table--dark .tfoot-sticky {
+    background: #565656 !important;
+  }
 }
 </style>
