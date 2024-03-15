e92e17b1d7751294f7f953535149c51a3ad4f233
Bhargav <BJP232004@GMAIL.COM>
2024-03-15 17:38:37 +0530

    fix: in case of aggregation query in sql mode pagination will be disabled.

--- web/src/plugins/logs/SearchResult.vue
+++ web/src/plugins/logs/SearchResult.vue
@@ -29,6 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         </div>
         <div class="col-6 text-right q-pr-md q-gutter-xs pagination-block">
           <q-pagination
+            v-if="searchObj.meta.resultGrid.showPagination"
             :disable="searchObj.loading == true"
             v-model="pageNumberInput"
             :key="
