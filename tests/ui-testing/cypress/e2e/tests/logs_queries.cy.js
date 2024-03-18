a1a2386b1bccf2176c7f903df1267b3e5dd2df95
Bhargav <BJP232004@GMAIL.COM>
2024-03-18 23:12:32 +0530

    fix: pagination disable on limit query (#3037)

--- tests/ui-testing/cypress/e2e/tests/logs_queries.cy.js
+++ tests/ui-testing/cypress/e2e/tests/logs_queries.cy.js
@@ -98,6 +98,9 @@ describe("Logs testcases", () => {
     cy.get('[data-test="logs-search-bar-query-editor"] > .monaco-editor')
       .click() // Click on the editor to focus
       .type(" WHERE match_all_indexed_ignore_case('provide_credentials')");
+    cy.get("[data-test='logs-search-bar-refresh-btn']", {
+      timeout: 2000,
+    }).click({ force: true });
     cy.get('[data-test="logs-search-result-records-per-page"]').click();
     cy.get(".q-virtual-scroll__content:last").click();
 
