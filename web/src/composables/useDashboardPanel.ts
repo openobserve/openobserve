f6a95cdeef3e0be346e392b11d68a05f4838965e
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-03-19 17:06:51 +0530

    added map type and some config changes

--- web/src/composables/useDashboardPanel.ts
+++ web/src/composables/useDashboardPanel.ts
@@ -66,6 +66,9 @@ const getDefaultDashboardPanelData: any = () => ({
       base_map: {
         type: "osm",
       },
+      map_type: {
+        type: "World",
+      },
       map_view: {
         zoom: 1,
         lat: 0,
