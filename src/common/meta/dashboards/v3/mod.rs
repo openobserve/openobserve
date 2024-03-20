a6bf93ea78a32752bd71ec3018b2298e8b07e0e4
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-03-20 11:04:39 +0530

    map type in backend

--- src/common/meta/dashboards/v3/mod.rs
+++ src/common/meta/dashboards/v3/mod.rs
@@ -181,6 +181,8 @@ pub struct PanelConfig {
     #[serde(skip_serializing_if = "Option::is_none")]
     legend_width: Option<LegendWidth>,
     base_map: Option<BaseMap>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    map_type: Option<MapType>,
     map_view: Option<MapView>,
     #[serde(skip_serializing_if = "Option::is_none")]
     map_symbol_style: Option<MapSymbolStyle>,
@@ -311,6 +313,12 @@ pub struct CustomFieldsOption {
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
 pub struct BaseMap {
+    #[serde(rename = "type")]
+    pub type_field: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
+pub struct MapType {
     #[serde(rename = "type")]
     pub type_field: Option<String>,
 }
