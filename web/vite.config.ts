c6c2be2c37cf54f2c7d44a67da19d97b4e42b656
omkark06 <omkar@zinclabs.io>
2024-03-28 16:00:00 +0530

    core: fixed vue-i18n unsafe-eval csp issue
    Updated vue-i18n version
    Added __INTLIFY_JIT_COMPILATION__ flag in vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -91,6 +91,7 @@ export default defineConfig({
     __VUE_I18N_FULL_INSTALL__: true,
     __VUE_I18N_LEGACY_API__: false,
     __INTLIFY_PROD_DEVTOOLS__: false,
+    __INTLIFY_JIT_COMPILATION__: true,
   },
   server: {
     port: 8081,
