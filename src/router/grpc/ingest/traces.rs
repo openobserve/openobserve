a29e0b15a9ba730aee0c87f14ff70733ba7c39b4
Hengfei Yang <hengfei.yang@gmail.com>
2024-04-01 21:58:00 +0800

    perf: add grpc client reuse

--- src/router/grpc/ingest/traces.rs
+++ src/router/grpc/ingest/traces.rs
@@ -19,10 +19,7 @@ use opentelemetry_proto::tonic::collector::trace::v1::{
     trace_service_client::TraceServiceClient, trace_service_server::TraceService,
     ExportTraceServiceRequest, ExportTraceServiceResponse,
 };
-use tonic::{
-    codec::CompressionEncoding, metadata::MetadataValue, transport::Channel, Request, Response,
-    Status,
-};
+use tonic::{codec::CompressionEncoding, metadata::MetadataValue, Request, Response, Status};
 use tracing_opentelemetry::OpenTelemetrySpanExt;
 
 use crate::{common::infra::cluster, service::search::MetadataMap};
@@ -47,7 +44,6 @@ impl TraceService for TraceServer {
         }
 
         // call ingester
-        let grpc_addr = super::get_rand_ingester_addr().await?;
         let mut request = Request::from_parts(metadata, extensions, message);
         opentelemetry::global::get_text_map_propagator(|propagator| {
             propagator.inject_context(
@@ -59,18 +55,7 @@ impl TraceService for TraceServer {
         let token: MetadataValue<_> = cluster::get_internal_grpc_token()
             .parse()
             .map_err(|_| Status::internal("invalid token".to_string()))?;
-        let channel = Channel::from_shared(grpc_addr.clone())
-            .unwrap()
-            .connect()
-            .await
-            .map_err(|err| {
-                log::error!(
-                    "[ROUTER] grpc->ingest->traces: node: {}, connect err: {:?}",
-                    &grpc_addr,
-                    err
-                );
-                Status::internal("connect querier error".to_string())
-            })?;
+        let channel = super::get_ingeter_channel().await?;
         let client = TraceServiceClient::with_interceptor(channel, move |mut req: Request<()>| {
             req.metadata_mut().insert("authorization", token.clone());
             Ok(req)
