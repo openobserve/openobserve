6534fccdd373ffa8c5cbb30dce565fb991a73b5e
Hengfei Yang <hengfei.yang@gmail.com>
2024-04-05 21:46:40 +0800

    fix: add tokio console (#3175)
    Debug mode:
    ```
    RUSTFLAGS="--cfg tokio_unstable" cargo build --features=tokio-console
    ```
    
    Relase mode:
    ```
    RUSTFLAGS="--cfg tokio_unstable" cargo build --features=tokio-console --profile=release-profiling
    ```

--- src/main.rs
+++ src/main.rs
@@ -92,6 +92,9 @@ use tracing_subscriber::{
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    #[cfg(feature = "tokio-console")]
+    console_subscriber::init();
+
     // let tokio steal the thread
     let rt_handle = tokio::runtime::Handle::current();
     std::thread::spawn(move || {
@@ -120,7 +123,13 @@ async fn main() -> Result<(), anyhow::Error> {
     }
 
     // setup logs
-    let _guard: Option<WorkerGuard> = if CONFIG.log.events_enabled {
+    #[cfg(feature = "tokio-console")]
+    let enable_tokio_console = true;
+    #[cfg(not(feature = "tokio-console"))]
+    let enable_tokio_console = false;
+    let _guard: Option<WorkerGuard> = if enable_tokio_console {
+        None
+    } else if CONFIG.log.events_enabled {
         let logger = zo_logger::ZoLogger {
             sender: zo_logger::EVENT_SENDER.clone(),
         };
