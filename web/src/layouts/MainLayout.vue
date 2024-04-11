09a330a2527233bb9000bbe3baa2567731ea358d
Bhargav <BJP232004@GMAIL.COM>
2024-04-11 14:38:06 +0530

    fix: logout issue

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -406,12 +406,11 @@ export default defineComponent({
       window.open(zoBackendUrl + "/swagger/index.html", "_blank");
     },
     signout() {
-      // if (config.isEnterprise == "true") {
-      invlidateLoginData();
-      // }
       this.store.dispatch("logout");
       useLocalCurrentUser("", true);
       useLocalUserInfo("", true);
+      invlidateLoginData();
+      window.location.href = getLogoutURL();
       this.$router.push("/logout");
     },
     goToHome() {
