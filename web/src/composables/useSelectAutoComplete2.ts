711e6d20dbb25ecf0f5b6b2e5782a0a0d59e10c5
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-04-15 10:14:53 +0530

    based on regex gets the values

--- web/src/composables/useSelectAutoComplete2.ts
+++ web/src/composables/useSelectAutoComplete2.ts
@@ -26,7 +26,7 @@ export const useSelectAutoComplete2 = (
       return;
     }
 
-    const regex = /\{(\w+)/g;
+    const regex = /{([^{}]*)$/;
     console.log("[useSelectAutoComplete2] Regex:", regex);
 
     const match = regex.exec(val);
@@ -35,26 +35,18 @@ export const useSelectAutoComplete2 = (
     if (match) {
       console.log("[useSelectAutoComplete2] Match found:", match);
 
-      const placeholder = match[1];
-      const needle = val.replace(regex, "").toLowerCase();
+      const needle = match[1].toLowerCase();
       console.log("[useSelectAutoComplete2] Extracted needle:", needle);
 
       filteredOptions.value = options.value?.filter((option: any) => {
         const value =
           typeof option === "object" ? option[searchKey] : option.toString();
         const lowerCaseValue = value.toLowerCase();
-        const lowerCaseNeedle = needle.toLowerCase();
-        // console.log("[useSelectAutoComplete2] Check value:", lowerCaseValue);
-        // console.log("[useSelectAutoComplete2] Check needle:", lowerCaseNeedle);
         console.log(
           "[useSelectAutoComplete2] Condition:",
-          lowerCaseValue.indexOf(lowerCaseNeedle) > -1 &&
-            (lowerCaseValue.startsWith(placeholder) || !placeholder)
+          lowerCaseValue.includes(needle)
         );
-        return (
-          lowerCaseValue.indexOf(lowerCaseNeedle) > -1 &&
-          (lowerCaseValue.startsWith(placeholder) || !placeholder)
-        );
+        return lowerCaseValue.includes(needle);
       });
     } else {
       console.log("[useSelectAutoComplete2] No match found:", val);
@@ -64,9 +56,9 @@ export const useSelectAutoComplete2 = (
         const value =
           typeof option === "object" ? option[searchKey] : option.toString();
         const lowerCaseValue = value.toLowerCase();
-        // console.log("[useSelectAutoComplete2] Check value:", lowerCaseValue);
-        // console.log("[useSelectAutoComplete2] Check needle:", needle);
-        return lowerCaseValue.indexOf(needle) > -1;
+        console.log("[useSelectAutoComplete2] Check value:", lowerCaseValue);
+        console.log("[useSelectAutoComplete2] Check needle:", needle);
+        return lowerCaseValue.includes(needle);
       });
     }
   };
