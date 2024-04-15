e6eeda36fa7d9ecf04def124a76995b89edc6b61
oasisk <ashish.j.kolhe@gmail.com>
2024-04-15 18:01:48 +0530

    working

--- src/service/logs/mod.rs
+++ src/service/logs/mod.rs
@@ -13,10 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::{
-    collections::{HashMap, HashSet},
-    sync::Arc,
-};
+use std::{collections::HashMap, sync::Arc};
 
 use anyhow::Result;
 use arrow_schema::{DataType, Field, Schema};
