d46bdaa70b52da9b656ffcb00528458aaf7af0f8
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-04-15 16:33:47 +0530

    update: changes of  route based on its enterprise version

---
+++ web/src/composables/shared/useManagementRoutes.ts
@@ -0,0 +1,50 @@
+import config from "@/aws-exports";
+import { routeGuard } from "@/utils/zincutils";
+const Settings = () => import("@/components/settings/index.vue");
+
+const useManagementRoutes = () => {
+  const routes: any = [
+    {
+      path: "settings",
+      name: "settings",
+      component: Settings,
+      meta: {
+        keepAlive: true,
+      },
+      beforeEnter(to: any, from: any, next: any) {
+        routeGuard(to, from, next);
+      },
+      children: [
+        {
+          path: "general",
+          name: "general",
+          component: () => import("@/components/settings/General.vue"),
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+      ],
+    },
+  ];
+  if (config.isCloud == "true" || config.isEnterprise == "true") {
+    routes[0].children.push(
+      ...[
+        {
+          path: "query_management",
+          name: "query_management",
+          component: () =>
+            import("@/components/queries/RunningQueriesList.vue"),
+          meta: {
+            keepAlive: true,
+          },
+          beforeEnter(to: any, from: any, next: any) {
+            routeGuard(to, from, next);
+          },
+        },
+      ]
+    );
+  }
+  return routes;
+};
+
+export default useManagementRoutes;
