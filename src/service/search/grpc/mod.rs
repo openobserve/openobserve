127fef9d168dd1401764090ab827a11ed433e4ed
Huaijin <haohuaijin@gmail.com>
2024-04-15 17:08:28 +0800

    add todo

--- src/service/search/grpc/mod.rs
+++ src/service/search/grpc/mod.rs
@@ -341,7 +341,7 @@ fn check_memory_circuit_breaker(trace_id: &str, scan_stats: &ScanStats) -> Resul
 }
 
 // generate parquet file search schema
-// TODO: add order by and group by field to schema
+// TODO: add order by and group by field to schema, support select * ;
 fn generate_search_schema(
     sql: Arc<Sql>,
     schema_latest_map: &HashMap<&String, &Arc<Field>>,
