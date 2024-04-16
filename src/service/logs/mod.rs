762ff81458814cba0044ad8019b5221133954fe7
oasisk <ashish.j.kolhe@gmail.com>
2024-04-16 08:16:35 +0530

    add as json

--- src/service/logs/mod.rs
+++ src/service/logs/mod.rs
@@ -448,27 +448,19 @@ struct StreamMeta<'a> {
 }
 
 pub fn refactor_map(original_map: &mut Map<String, Value>, defined_schema_keys: &Vec<String>) {
-    let mut non_interesting_concat = String::new();
+    let mut non_schema_map = HashMap::new();
 
     for (key, value) in original_map.iter() {
         if !defined_schema_keys.contains(key) {
-            // Assuming value is a simple string for concatenation purposes
-            non_interesting_concat.push_str(&format!("{}:{}, ", key, get_string_value(value)));
+            non_schema_map.insert(key.clone(), get_string_value(value));
         }
-    }
-
-    if !non_interesting_concat.is_empty() {
-        non_interesting_concat.pop();
-        non_interesting_concat.pop();
     }
 
     original_map.retain(|key, _| defined_schema_keys.contains(key));
 
-    if !non_interesting_concat.is_empty() {
-        original_map.insert(
-            "_non_schema".to_string(),
-            Value::String(non_interesting_concat),
-        );
+    if !non_schema_map.is_empty() {
+        let non_schema_json = serde_json::to_string(&non_schema_map).unwrap_or_default();
+        original_map.insert("_non_schema".to_string(), Value::String(non_schema_json));
     }
 }
 
