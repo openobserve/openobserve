e24d2ba00711faf242408f3e667c6363d38d6520
omkark06 <omkar@zinclabs.io>
2024-04-18 11:58:42 +0530

    added stream routing component

---
+++ web/src/components/functions/StreamRouting.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <template
+      v-for="streamRoute in (streamRoutes as streamRoute[])"
+      :key="streamRoute.name"
+    >
+      <div
+        data-test="add-alert-name-input"
+        class="alert-name-input o2-input"
+        style="padding-top: 12px"
+      >
+        <q-input
+          v-model="streamRoute.streamName"
+          :label="t('alerts.name') + ' *'"
+          color="input-border"
+          bg-color="input-bg"
+          class="showLabelOnTop"
+          stack-label
+          outlined
+          filled
+          dense
+          v-bind:readonly="streamRoute.isUpdating"
+          v-bind:disable="streamRoute.isUpdating"
+          :rules="[(val: any) => !!val.trim() || 'Field is required!']"
+          tabindex="0"
+          style="min-width: 480px"
+        />
+      </div>
+    </template>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref, type Ref } from "vue";
+import { useI18n } from "vue-i18n";
+
+interface RouteCondition {
+  column: string;
+  value: any;
+}
+
+interface streamRoute {
+  streamName: string;
+  name: string;
+  column: string;
+  value: any;
+  isUpdating: boolean;
+}
+
+const { t } = useI18n();
+
+const streamRoutes: Ref<streamRoute | []> = ref([]);
+</script>
+
+<style scoped></style>
