db8c12d019500665caba440bbe9c7c800cc173b1
domyway <weizhao029@gmail.com>
2024-04-18 19:29:46 +0800

    fix: code update

--- src/main.rs
+++ src/main.rs
@@ -92,14 +92,20 @@ use tracing_subscriber::{
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     #[cfg(feature = "tokio-console")]
+    let socket_addr: SocketAddr = format!(
+        "{}:{}",
+        CONFIG.tokio_console.tokio_console_server_addr,
+        CONFIG.tokio_console.tokio_console_server_port
+    )
+    .as_str()
+    .parse()?;
+
+    #[cfg(feature = "tokio-console")]
     console_subscriber::ConsoleLayer::builder()
         .retention(Duration::from_secs(
             CONFIG.tokio_console.tokio_console_retention,
         ))
-        .server_addr((
-            CONFIG.tokio_console.tokio_console_server_addr.as_str(),
-            CONFIG.tokio_console.tokio_console_server_port,
-        ))
+        .server_addr(socket_addr)
         .init();
 
     // let tokio steal the thread
