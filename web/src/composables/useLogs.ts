c96b2fc92113fc863fc04cdde19fe9c733f98156
Bhargav <BJP232004@GMAIL.COM>
2024-04-18 16:53:12 +0530

    Update useLogs.ts

--- web/src/composables/useLogs.ts
+++ web/src/composables/useLogs.ts
@@ -2342,8 +2342,10 @@ const useLogs = () => {
       }
 
       let streamName: string = "";
-      if(searchObj.data.stream.selectedStream.length>1) {
-        streamName = b64EncodeUnicode(searchObj.data.stream.selectedStream.join(","));
+      if (searchObj.data.stream.selectedStream.length > 1) {
+        streamName =
+          b64EncodeUnicode(searchObj.data.stream.selectedStream.join(",")) ||
+          "";
       } else {
         streamName = searchObj.data.stream.selectedStream[0];
       }
@@ -2356,7 +2358,8 @@ const useLogs = () => {
           query_context: sqlContext,
           query_fn: query_fn,
           stream_type: searchObj.data.stream.streamType,
-          is_multistream: searchObj.data.stream.selectedStream.length>1 ? true: false,
+          is_multistream:
+            searchObj.data.stream.selectedStream.length > 1 ? true : false,
         })
         .then((res) => {
           searchObj.loading = false;
