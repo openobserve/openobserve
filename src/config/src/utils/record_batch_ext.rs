e843842e6a0670d04ad96519b01db8795b6385da
Huaijin <haohuaijin@gmail.com>
2024-04-19 13:55:33 +0800

    fix: json record have diff field

--- src/config/src/utils/record_batch_ext.rs
+++ src/config/src/utils/record_batch_ext.rs
@@ -17,12 +17,13 @@ use std::sync::Arc;
 
 use arrow::{
     array::{
-        make_builder, ArrayBuilder, BooleanBuilder, Float64Builder, Int64Builder, NullBuilder,
-        StringBuilder, UInt64Builder,
+        make_builder, new_null_array, ArrayBuilder, ArrayRef, BooleanBuilder, Float64Builder,
+        Int64Builder, NullBuilder, StringBuilder, UInt64Builder,
     },
     record_batch::RecordBatch,
 };
 use arrow_schema::{ArrowError, DataType, Schema};
+use hashbrown::HashSet;
 
 use super::schema_ext::SchemaExt;
 use crate::FxIndexMap;
@@ -45,9 +46,18 @@ pub fn convert_json_to_record_batch(
     schema: &Arc<Schema>,
     data: &[Arc<serde_json::Value>],
 ) -> Result<RecordBatch, ArrowError> {
+    // collect all keys that use in this record batch
+    let mut keys = HashSet::new();
+    for record in data.iter() {
+        for (k, _) in record.as_object().unwrap() {
+            keys.insert(k);
+        }
+    }
+
     let mut builders: FxIndexMap<&String, (&DataType, Box<dyn ArrayBuilder>)> = schema
         .fields()
         .iter()
+        .filter(|f| keys.contains(f.name()))
         .map(|f| {
             (
                 f.name(),
@@ -57,7 +67,9 @@ pub fn convert_json_to_record_batch(
         .collect();
 
     for record in data.iter() {
+        let mut record_keys = HashSet::new();
         for (k, v) in record.as_object().unwrap() {
+            record_keys.insert(k);
             let (data_type, builder) = builders.get_mut(k).unwrap();
             match data_type {
                 DataType::Utf8 => {
@@ -103,10 +115,79 @@ pub fn convert_json_to_record_batch(
                 }
             }
         }
+
+        // fill null for missing keys
+        builders.iter_mut().for_each(|(k, (data_type, b))| {
+            if !record_keys.contains(k) {
+                match data_type {
+                    DataType::Utf8 => {
+                        b.as_any_mut()
+                            .downcast_mut::<StringBuilder>()
+                            .unwrap()
+                            .append_null();
+                    }
+                    DataType::Int64 => {
+                        b.as_any_mut()
+                            .downcast_mut::<Int64Builder>()
+                            .unwrap()
+                            .append_null();
+                    }
+                    DataType::UInt64 => {
+                        b.as_any_mut()
+                            .downcast_mut::<UInt64Builder>()
+                            .unwrap()
+                            .append_null();
+                    }
+                    DataType::Float64 => {
+                        b.as_any_mut()
+                            .downcast_mut::<Float64Builder>()
+                            .unwrap()
+                            .append_null();
+                    }
+                    DataType::Boolean => {
+                        b.as_any_mut()
+                            .downcast_mut::<BooleanBuilder>()
+                            .unwrap()
+                            .append_null();
+                    }
+                    DataType::Null => {
+                        b.as_any_mut()
+                            .downcast_mut::<NullBuilder>()
+                            .unwrap()
+                            .append_null();
+                    }
+                    _ => {}
+                }
+            }
+        });
     }
 
-    RecordBatch::try_new(
-        schema.clone(),
-        builders.iter_mut().map(|(_, (_, v))| v.finish()).collect(),
-    )
+    let mut cols: Vec<ArrayRef> = Vec::with_capacity(schema.fields().len());
+    for field in schema.fields() {
+        if let Some((_, builder)) = builders.get_mut(field.name()) {
+            cols.push(builder.finish());
+        } else {
+            cols.push(new_null_array(field.data_type(), data.len()))
+        }
+    }
+
+    RecordBatch::try_new(schema.clone(), cols)
+}
+
+// adapt batch to schema
+pub fn adapt_batch(schema: &Schema, batch: &RecordBatch) -> Result<RecordBatch, ArrowError> {
+    let batch_schema = &*batch.schema();
+    let batch_cols = batch.columns().to_vec();
+
+    let mut cols: Vec<ArrayRef> = Vec::with_capacity(schema.fields().len());
+    for field in schema.fields() {
+        if let Some((batch_idx, _)) = batch_schema.column_with_name(field.name().as_str()) {
+            cols.push(Arc::clone(&batch_cols[batch_idx]));
+        } else {
+            cols.push(new_null_array(field.data_type(), batch.num_rows()))
+        }
+    }
+
+    let merged_schema = Arc::new(schema.clone());
+    RecordBatch::try_new(merged_schema, cols)
 }
