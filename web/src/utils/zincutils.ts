d659e847b26077da805847fc0827e0706b7aa869
Bhargav <BJP232004@GMAIL.COM>
2024-04-19 12:03:08 +0530

    Update zincutils.ts

--- web/src/utils/zincutils.ts
+++ web/src/utils/zincutils.ts
@@ -668,3 +668,28 @@ export const convertToCamelCase = (str: string) => {
 
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 };
+
+export function SetCookie(name: string, value: string, days: number) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+export function GetCookie(name: string) {
+  const nameEQ = name + "=";
+  const cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) == " ") {
+      cookie = cookie.substring(1, cookie.length);
+    }
+    if (cookie.indexOf(nameEQ) == 0) {
+      return cookie.substring(nameEQ.length, cookie.length);
+    }
+  }
+  return null;
+}
