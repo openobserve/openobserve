910acba6887431eba8eb68d825fc19de242a3891
oasisk <ashish.j.kolhe@gmail.com>
2024-04-19 17:34:47 +0530

    changes

--- src/common/meta/pipelines.rs
+++ src/common/meta/pipelines.rs
@@ -13,22 +13,56 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use std::collections::HashMap;
+
 use config::meta::stream::{RoutingCondition, StreamType};
-use hashbrown::HashMap;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-#[derive(Clone, Debug, Serialize, Deserialize, ToSchema)]
+use crate::common::meta::functions::StreamFunctionsList;
+
+#[derive(Clone, Debug, Serialize, Deserialize, ToSchema, PartialEq)]
 pub struct PipeLine {
     pub name: String,
+    #[serde(default)]
     pub description: String,
+    #[serde(default)]
     pub stream_name: String,
+    #[serde(default)]
     pub stream_type: StreamType,
-    #[serde(skip_serializing_if = "Option::None")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub routing: Option<HashMap<String, Vec<RoutingCondition>>>,
+}
+
+impl PipeLine {
+    pub fn into_response(self, functions: Option<StreamFunctionsList>) -> PipeLineResponse {
+        PipeLineResponse {
+            name: self.name,
+            description: self.description,
+            stream_name: self.stream_name,
+            stream_type: self.stream_type,
+            routing: self.routing,
+            functions,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, ToSchema, PartialEq)]
+pub struct PipeLineResponse {
+    pub name: String,
+    #[serde(default)]
+    pub description: String,
+    #[serde(default)]
+    pub stream_name: String,
+    #[serde(default)]
+    pub stream_type: StreamType,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub routing: Option<HashMap<String, Vec<RoutingCondition>>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub functions: Option<StreamFunctionsList>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, ToSchema)]
 pub struct PipeLineList {
-    pub list: Vec<PipeLine>,
+    pub list: Vec<PipeLineResponse>,
 }
