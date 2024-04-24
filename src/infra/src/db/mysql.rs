df0959c88261b6c1f59354c7c6cddeb457363e2d
Hengfei Yang <hengfei.yang@gmail.com>
2024-04-24 23:51:08 +0800

    fix: schema cache error with prefix

--- src/infra/src/db/mysql.rs
+++ src/infra/src/db/mysql.rs
@@ -380,8 +380,8 @@ impl super::Db for MysqlDb {
                 )
             } else {
                 format!(
-                    r#"DELETE FROM meta WHERE module = '{}' AND key1 = '{}' AND key2 LIKE '{}%';"#,
-                    module, key1, key2
+                    r#"DELETE FROM meta WHERE module = '{}' AND key1 = '{}' AND (key2 = '{}' OR key2 LIKE '{}/%');"#,
+                    module, key1, key2, key2
                 )
             }
         } else {
@@ -413,7 +413,7 @@ impl super::Db for MysqlDb {
             sql = format!("{} AND key1 = '{}'", sql, key1);
         }
         if !key2.is_empty() {
-            sql = format!("{} AND key2 LIKE '{}%'", sql, key2);
+            sql = format!("{} AND (key2 = '{}' OR key2 LIKE '{}/%')", sql, key2, key2);
         }
         sql = format!("{} ORDER BY start_dt ASC", sql);
 
@@ -442,7 +442,7 @@ impl super::Db for MysqlDb {
             sql = format!("{} AND key1 = '{}'", sql, key1);
         }
         if !key2.is_empty() {
-            sql = format!("{} AND key2 LIKE '{}%'", sql, key2);
+            sql = format!("{} AND (key2 = '{}' OR key2 LIKE '{}/%')", sql, key2, key2);
         }
 
         sql = format!("{} ORDER BY start_dt ASC", sql);
@@ -476,7 +476,7 @@ impl super::Db for MysqlDb {
             sql = format!("{} AND key1 = '{}'", sql, key1);
         }
         if !key2.is_empty() {
-            sql = format!("{} AND key2 LIKE '{}%'", sql, key2);
+            sql = format!("{} AND (key2 = '{}' OR key2 LIKE '{}/%')", sql, key2, key2);
         }
         let pool = CLIENT.clone();
         let count: i64 = sqlx::query_scalar(&sql).fetch_one(&pool).await?;
