2ca05709ce331af0f1c98cbf86d1e0bf40f449c5
Hengfei Yang <hengfei.yang@gmail.com>
2024-04-25 00:44:37 +0800

    fix: ent build

--- src/service/db/mod.rs
+++ src/service/db/mod.rs
@@ -53,7 +53,11 @@ pub(crate) async fn put(
     start_dt: Option<i64>,
 ) -> Result<()> {
     #[cfg(feature = "enterprise")]
-    let value_clone = value.clone();
+    let value_clone = if O2_CONFIG.super_cluster.enabled {
+        Some(value.clone())
+    } else {
+        None
+    };
 
     let db = infra_db::get_db().await;
     db.put(key, value, need_watch, start_dt).await?;
@@ -63,7 +67,7 @@ pub(crate) async fn put(
     if O2_CONFIG.super_cluster.enabled {
         o2_enterprise::enterprise::super_cluster::queue::put(
             key,
-            value_clone,
+            value_clone.unwrap(),
             need_watch,
             start_dt,
         )
