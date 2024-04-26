138ac5d8fe199bbf6e3e662ce2014805218ab440
Subhra264 <chakrabortysubhradeep556@gmail.com>
2024-04-26 06:54:32 +0530

    fix: report cache and alert manager should install chrome (#3348)
    Co-authored-by: Ankur Srivastava <best.ankur@gmail.com>
    Co-authored-by: Hengfei Yang <hengfei.yang@gmail.com>

--- src/service/db/dashboards/reports.rs
+++ src/service/db/dashboards/reports.rs
@@ -131,7 +131,23 @@ pub async fn watch() -> Result<(), anyhow::Error> {
         match ev {
             db::Event::Put(ev) => {
                 let item_key = ev.key.strip_prefix(key).unwrap();
-                let item_value: Report = json::from_slice(&ev.value.unwrap()).unwrap();
+                let item_value: Report = if config::CONFIG.common.meta_store_external {
+                    match db::get(&ev.key).await {
+                        Ok(val) => match json::from_slice(&val) {
+                            Ok(val) => val,
+                            Err(e) => {
+                                log::error!("Error getting value: {}", e);
+                                continue;
+                            }
+                        },
+                        Err(e) => {
+                            log::error!("Error getting value: {}", e);
+                            continue;
+                        }
+                    }
+                } else {
+                    json::from_slice(&ev.value.unwrap()).unwrap()
+                };
                 DASHBOARD_REPORTS.insert(item_key.to_owned(), item_value);
             }
             db::Event::Delete(ev) => {
