da42243f0884d55b9a984343e26b1f75eb75eba0
Bhargav <BJP232004@GMAIL.COM>
2024-04-26 11:11:14 +0530

    fix: apply saved view error fix (#3350)
    Fix for saved apply directly without navigating stream page.

--- web/src/plugins/logs/SearchBar.vue
+++ web/src/plugins/logs/SearchBar.vue
@@ -166,7 +166,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                             side
                             @click.stop="handleDeleteSavedView(props.row)"
                           >
-                            <q-icon name="delete" color="grey" size="xs" />
+                            <q-icon name="delete"
+color="grey" size="xs" />
                           </q-item-section>
                         </q-item> </q-td
                     ></template>
@@ -346,7 +347,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                 </q-item-section>
               </q-item>
               <q-separator />
-              <q-item class="q-pa-sm saved-view-item" clickable v-close-popup>
+              <q-item class="q-pa-sm saved-view-item"
+clickable v-close-popup>
                 <q-item-section
                   @click.stop="toggleCustomDownloadDialog"
                   v-close-popup
