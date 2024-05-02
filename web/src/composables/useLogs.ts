4d67c4e0b737c7f51b8fba1a3e3b9b912cfbb619
Bhargav <BJP232004@GMAIL.COM>
2024-05-02 17:41:49 +0530

    fix: issues while code merge

--- web/src/composables/useLogs.ts
+++ web/src/composables/useLogs.ts
@@ -2248,7 +2248,7 @@ const useLogs = () => {
                 ? fieldToStreamsMap[row.name]
                 : [],
               isInterestingField:
-                searchObj.data.stream.interestingFieldList.includes(group.name)
+                searchObj.data.stream.interestingFieldList.includes(row.name)
                   ? true
                   : false,
             });
@@ -3067,7 +3067,6 @@ const useLogs = () => {
     fieldValues,
     extractFields,
     getQueryData,
-    extractFields,
     searchAroundData,
     updateGridColumns,
     refreshData,
