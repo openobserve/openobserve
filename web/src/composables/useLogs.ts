2389c5ce82902203de1544bf12bd2b4a6c73b963
Bhargav <BJP232004@GMAIL.COM>
2024-05-03 15:27:23 +0530

    fixed selection issue

--- web/src/composables/useLogs.ts
+++ web/src/composables/useLogs.ts
@@ -552,7 +552,7 @@ const useLogs = () => {
     return searchObj.data.filterErrMsg === "" ? true : false;
   };
 
-  async function buildSearch() {
+  function buildSearch() {
     try {
       let query = searchObj.data.editorValue;
       searchObj.data.filterErrMsg = "";
@@ -833,7 +833,8 @@ const useLogs = () => {
                   .selectedStreamFields) {
                   if (
                     streamField?.name == field &&
-                    streamField?.streams.indexOf(item) > -1
+                    streamField?.streams.indexOf(item) > -1 &&
+                    listOfFields.indexOf(field) == -1
                   ) {
                     listOfFields.push(field);
                   }
@@ -1414,7 +1415,7 @@ const useLogs = () => {
       }
     } catch (e: any) {
       searchObj.loading = false;
-      showErrorNotification(notificationMsg.value || "Something went wrong.");
+      showErrorNotification(notificationMsg.value || "Something went wrong." + e);
       notificationMsg.value = "";
     }
   };
