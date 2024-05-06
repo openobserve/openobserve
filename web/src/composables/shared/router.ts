69f2112c1cd8d6df97b91f2789de297078fad21e
Omkar Kesarkhane <127078473+omkarK06@users.noreply.github.com>
2024-05-06 23:36:24 +0530

    fix: Ingestion pipeline UI issues (#3418)
    Removed duplicated menus in pipeline tab and duplicated pipeline routes

--- web/src/composables/shared/router.ts
+++ web/src/composables/shared/router.ts
@@ -257,38 +257,6 @@ const useRoutes = () => {
             },
           ],
         },
-        {
-          path: "pipelines",
-          name: "pipelines",
-          component: PipelinesList,
-          beforeEnter(to: any, from: any, next: any) {
-            routeGuard(to, from, next);
-          },
-        },
-        {
-          path: "edit/pipeline",
-          name: "pipelineEditor",
-          component: PipelineEditor,
-          beforeEnter(to: any, from: any, next: any) {
-            routeGuard(to, from, next);
-          },
-        },
-        {
-          path: "pipelines",
-          name: "pipelines",
-          component: PipelinesList,
-          beforeEnter(to: any, from: any, next: any) {
-            routeGuard(to, from, next);
-          },
-        },
-        {
-          path: "edit/pipeline",
-          name: "pipelineEditor",
-          component: PipelineEditor,
-          beforeEnter(to: any, from: any, next: any) {
-            routeGuard(to, from, next);
-          },
-        },
       ],
     },
     {
