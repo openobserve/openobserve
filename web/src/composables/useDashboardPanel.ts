f71ed93b8d84138ab1fb8836ae3b646e568a93b2
ktx-abhay <abhay.padamani@kiara.tech>
2024-05-09 16:09:03 +0530

    fix: reactivity issue for dashboardpaneldata state

--- web/src/composables/useDashboardPanel.ts
+++ web/src/composables/useDashboardPanel.ts
@@ -141,7 +141,7 @@ const getDefaultDashboardPanelData: any = () => ({
 });
 
 let dashboardPanelDataObj: any = reactive({
-  dashboard: { ...getDefaultDashboardPanelData() },
+  dashboard: reactive({ ...getDefaultDashboardPanelData() }),
 });
 
 const useDashboardPanelData = (pageKey: string = "dashboard") => {
@@ -150,10 +150,12 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
 
   // Initialize the state for this page key if it doesn't already exist
   if (!dashboardPanelDataObj[pageKey]) {
-    dashboardPanelDataObj[pageKey] = { ...getDefaultDashboardPanelData() };
+    dashboardPanelDataObj[pageKey] = reactive({
+      ...getDefaultDashboardPanelData(),
+    });
   }
 
-  const dashboardPanelData = dashboardPanelDataObj[pageKey];
+  const dashboardPanelData = reactive(dashboardPanelDataObj[pageKey]);
   const cleanupDraggingFields = () => {
     dashboardPanelData.meta.dragAndDrop.currentDragArea = null;
     dashboardPanelData.meta.dragAndDrop.targetDragIndex = -1;
@@ -1839,7 +1841,6 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
       } else {
         query = sqlchart();
       }
-      console.log("query", query);
       dashboardPanelData.data.queries[
         dashboardPanelData.layout.currentQueryIndex
       ].query = query;
@@ -1897,11 +1898,7 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
         ].customQuery &&
         dashboardPanelData.data.queryType === "sql"
       ) {
-        console.log("if condition");
-
         makeAutoSQLQuery();
-      } else {
-        console.log("else condition");
       }
     },
     { deep: true }
