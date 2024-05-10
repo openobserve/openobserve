c8de6860ee47ea70c8dba95d022d0c72f36eac56
Bhargav <BJP232004@GMAIL.COM>
2024-05-10 16:14:48 +0530

    Update SearchBar.vue

--- web/src/plugins/logs/SearchBar.vue
+++ web/src/plugins/logs/SearchBar.vue
@@ -1689,8 +1689,8 @@ export default defineComponent({
               delete extractedObj.data.stream.streamLists;
               delete searchObj.data.stream.selectedStream;
               delete searchObj.meta.regions;
-              if(extractedObj.meta.hasOwnProperty("regions")) {
-                searchObj.meta.regions = extractedObj.meta.regions;
+              if (extractedObj.meta.hasOwnProperty("regions")) {
+                searchObj.meta["regions"] = extractedObj.meta.regions;
               }
               extractedObj.data.transforms = searchObj.data.transforms;
               extractedObj.data.stream.functions =
