58b7de14f8a74269abea0d3f1f4c921eaa001cbb
Bhargav <BJP232004@GMAIL.COM>
2024-05-10 22:53:32 +0530

    Update Index.vue

--- web/src/plugins/logs/Index.vue
+++ web/src/plugins/logs/Index.vue
@@ -444,7 +444,6 @@ export default defineComponent({
     });
 
     onBeforeMount(async () => {
-      searchObj.meta.quickMode = store.state.zoConfig.quick_mode_enabled;
       searchObj.loading = true;
       searchObj.meta.pageType = "logs";
       if (
