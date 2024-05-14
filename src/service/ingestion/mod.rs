cf4c47362aac97281289863aa8a03a1db3216489
oasisk <ashish.j.kolhe@gmail.com>
2024-05-14 13:16:39 +0530

    fix

--- src/service/ingestion/mod.rs
+++ src/service/ingestion/mod.rs
@@ -529,9 +529,6 @@ pub async fn get_user_defined_schema(
                 if !fields.contains(&CONFIG.common.column_timestamp) {
                     fields.insert(CONFIG.common.column_timestamp.to_string());
                 }
-                if !fields.contains(&CONFIG.common.all_fields_name) {
-                    fields.insert(CONFIG.common.all_fields_name.to_string());
-                }
                 user_defined_schema_map.insert(stream.stream_name.to_string(), fields);
             }
         }
