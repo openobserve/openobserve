f6463e3d6eb81dead90aed3e48c234f76473a7f3
Hengfei Yang <hengfei.yang@gmail.com>
2024-05-22 11:48:21 +0800

    fix: search count wrong (#3560)

--- src/service/search/cluster/mod.rs
+++ src/service/search/cluster/mod.rs
@@ -648,8 +648,6 @@ async fn merge_grpc_result(
         }
         // handle aggs
         for agg in resp.aggs {
-            // insert count
-            let value = batches.entry(format!("agg_{}", agg.name)).or_default();
             if !agg.hits.is_empty() {
                 let buf = Cursor::new(agg.hits);
                 let reader = ipc::reader::FileReader::try_new(buf, None).unwrap();
@@ -657,13 +655,13 @@ async fn merge_grpc_result(
                     .into_iter()
                     .map(std::result::Result::unwrap)
                     .collect::<Vec<_>>();
+                if agg.name == "_count" && node.role.contains(&Role::Ingester) {
+                    let value = batches.entry("agg_ingester_count".to_string()).or_default();
+                    value.extend(batch.clone());
+                }
+                let value = batches.entry(format!("agg_{}", agg.name)).or_default();
                 value.extend(batch);
             }
-        }
-        if node.role.contains(&Role::Ingester) && batches.contains_key("agg__count") {
-            let batch = batches.get("agg__count").unwrap().clone();
-            let value = batches.entry("agg_ingester_count".to_string()).or_default();
-            value.extend(batch);
         }
     }
 
