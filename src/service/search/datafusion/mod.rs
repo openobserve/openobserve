241d8c5d248211bd260faf482e01149539a8280d
Taiming Liu <90490946+taimingl@users.noreply.github.com>
2024-05-28 14:04:36 -0700

    refactor: rename match_all & match_all_indexed (#3613)
    match_all                  renamed to match_all_raw
    match_all_indexed   renamed to match_all
    
    same pattern applied to *_ignore_case functions
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Bug Fixes**
    - Improved clarity in function names for better understanding and
    usability.
    
    - **Tests**
    - Updated SQL queries in test cases to reflect changes in function
    names, ensuring accurate test results.
    
    - **Chores**
    - Adjusted logging statements to provide better insights during
    debugging.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/service/search/datafusion/mod.rs
+++ src/service/search/datafusion/mod.rs
@@ -58,6 +58,14 @@ pub const REGEX_NOT_MATCH_UDF_NAME: &str = "re_not_match";
 
 pub const DEFAULT_FUNCTIONS: [ZoFunction; 8] = [
     ZoFunction {
+        name: "match_all_raw",
+        text: "match_all_raw('v')",
+    },
+    ZoFunction {
+        name: "match_all_raw_ignore_case",
+        text: "match_all_raw_ignore_case('v')",
+    },
+    ZoFunction {
         name: "match_all",
         text: "match_all('v')",
     },
@@ -66,20 +74,12 @@ pub const DEFAULT_FUNCTIONS: [ZoFunction; 8] = [
         text: "match_all_ignore_case('v')",
     },
     ZoFunction {
-        name: "match_all_indexed",
-        text: "match_all_indexed('v')",
-    },
-    ZoFunction {
-        name: "match_all_indexed_ignore_case",
-        text: "match_all_indexed_ignore_case('v')",
-    },
-    ZoFunction {
         name: MATCH_UDF_NAME,
-        text: "match_all('v')",
+        text: "match_all_raw('v')",
     },
     ZoFunction {
         name: MATCH_UDF_IGNORE_CASE_NAME,
-        text: "match_all_ignore_case('v')",
+        text: "match_all_raw_ignore_case('v')",
     },
     ZoFunction {
         name: REGEX_MATCH_UDF_NAME,
