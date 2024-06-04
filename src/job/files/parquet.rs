419d30d215f380e438658e4f354c7f53cb597e38
Hengfei Yang <hengfei.yang@gmail.com>
2024-06-04 20:56:42 +0800

    fix: rollback changes for ingester

--- src/job/files/parquet.rs
+++ src/job/files/parquet.rs
@@ -487,13 +487,14 @@ async fn merge_files(
 
     for file in files_with_size.iter() {
         if new_file_size > 0
-            && (new_file_size + file.meta.original_size > cfg.compact.max_file_size as i64
+            && ((new_file_size + file.meta.original_size > cfg.compact.max_file_size as i64)
                 || (cfg.limit.file_move_fields_limit > 0
                     && group_schema_field_num > cfg.limit.file_move_fields_limit))
         {
             break;
         }
         new_file_size += file.meta.original_size;
+
         new_file_list.push(file.clone());
     }
     let mut retain_file_list = new_file_list.clone();
