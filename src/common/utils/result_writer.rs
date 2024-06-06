4bc3230babb97ef56288320d941fcecb4084e9ef
oasisk <ashish.j.kolhe@gmail.com>
2024-06-06 17:20:58 +0530

    changes

--- src/common/utils/result_writer.rs
+++ src/common/utils/result_writer.rs
@@ -3,15 +3,20 @@ use std::path;
 use config::get_config;
 use tokio::{fs::OpenOptions, io::AsyncWriteExt};
 
-pub async fn cache_results_to_disk(file_name: &str, data: &str) -> std::io::Result<()> {
+pub async fn cache_results_to_disk(
+    file_path: &str,
+    file_name: &str,
+    data: &str,
+) -> std::io::Result<()> {
     let cfg = get_config();
-    let path = path::Path::new(&cfg.common.result_cache_dir).join(file_name);
-    std::fs::create_dir_all(&cfg.common.result_cache_dir).expect("create cache dir success");
+    let path = path::Path::new(&cfg.common.result_cache_dir).join(file_path);
+    std::fs::create_dir_all(&path).expect("create cache dir success");
+    let file_path = path.join(file_name);
     let mut file = OpenOptions::new()
         .write(true)
         .create(true)
         .truncate(true)
-        .open(&path)
+        .open(&file_path)
         .await?;
 
     file.write(data.as_bytes()).await?;
