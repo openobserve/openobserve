29ec9f929f34a40e3c0e2def766dc1a126d4daf1
oasisk <ashish.j.kolhe@gmail.com>
2024-06-06 17:20:58 +0530

    changes

--- src/service/search/mod.rs
+++ src/service/search/mod.rs
@@ -54,6 +54,7 @@ use crate::{
 pub(crate) mod cluster;
 pub(crate) mod datafusion;
 pub(crate) mod grpc;
+pub mod result_utils;
 pub(crate) mod sql;
 
 pub static SEARCH_SERVER: Lazy<Searcher> = Lazy::new(Searcher::new);
