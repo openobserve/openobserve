f7b28d8de4fa53b22636d0a09288ee4c61250756
Bhargav <BJP232004@GMAIL.COM>
2024-06-14 09:50:39 +0530

    Update useLogs.ts

--- web/src/composables/useLogs.ts
+++ web/src/composables/useLogs.ts
@@ -2395,7 +2395,6 @@ const useLogs = () => {
                           schemaMaps[schemaFieldsIndex].streams[0]
                         ] - 1;
                     }
-
                     commonSchemaMaps.push(fieldObj);
                     commonSchemaFields.push(field.name);
                     searchObj.data.stream.expandGroupRowsFieldCount["common"] =
