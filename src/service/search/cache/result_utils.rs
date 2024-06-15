399be431bd560e8c51ac5beb7255e66e63f9cbe2
oasisk <ashish.j.kolhe@gmail.com>
2024-06-15 16:29:41 +0530

    changes

--- src/service/search/cache/result_utils.rs
+++ src/service/search/cache/result_utils.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Read, Write};
 
-use config::utils::{base64, json};
+use base64::Engine;
 use flate2::{read::ZlibDecoder, write::ZlibEncoder, Compression};
 use sqlparser::{
     ast::{Expr, Function, Query, SelectItem, SetExpr, Statement},
@@ -57,18 +57,22 @@ const AGGREGATE_UDF_LIST: [&str; 7] = [
 ];
 
 pub fn encode_sql_to_foldername(sql_query: &str) -> io::Result<String> {
-    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::best());
     encoder.write_all(sql_query.as_bytes())?;
     let compressed_bytes = encoder.finish()?;
 
-    Ok(base64::encode(json::from_slice(&compressed_bytes).unwrap()))
+    Ok(base64::engine::general_purpose::STANDARD.encode(compressed_bytes))
 }
 
 pub fn decode_foldername_to_sql(folder_name: &str) -> io::Result<String> {
-    let compressed_bytes = base64::decode(folder_name)?;
-    let mut decoder = ZlibDecoder::new(compressed_bytes.as_bytes());
+    let compressed_bytes = base64::engine::general_purpose::STANDARD
+        .decode(folder_name.as_bytes())
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
+    let mut decoder = ZlibDecoder::new(&compressed_bytes[..]);
     let mut decompressed_bytes = Vec::new();
+
     decoder.read_to_end(&mut decompressed_bytes)?;
 
-    Ok(String::from_utf8(decompressed_bytes).unwrap())
+    String::from_utf8(decompressed_bytes).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
 }
