b28e3a1fa9a8bc38812f4d963fd20d169d26f468
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-06-17 10:04:07 +0530

    remove unnecessary code

--- web/src/utils/dashboard/datetimeStartPoint.ts
+++ web/src/utils/dashboard/datetimeStartPoint.ts
@@ -5,25 +5,7 @@ export const dateBin = (interval: number, date: Date, origin: Date): Date => {
 
   const remainder = durationSinceOrigin % interval;
 
-  //   if (remainder < interval / 2) {
   const adjustedDate = subSeconds(date, remainder);
-  //     return adjustedDate;
-  //   } else {
-  //     const adjustedDate = addSeconds(date, interval - remainder);
+
   return adjustedDate;
-  //   }
 };
-
-// function main(): void {
-
-// const interval = 10 * 60; // 10 minutes in seconds
-
-// const date = new Date(Date.UTC(2024, 5, 5, 21, 33, 0)); // Note: Months are 0-indexed in JavaScript
-
-// const origin = new Date(Date.UTC(2001, 0, 1, 0, 0, 0)); // January is month 0
-
-// const binnedDate = dateBin(interval, date, origin);
-
-// }
-
-// main();
