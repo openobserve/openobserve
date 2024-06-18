d605181c6a0400979181c542837dd3cc3c5e9bf9
oasisk <ashish.j.kolhe@gmail.com>
2024-06-18 18:24:22 +0530

    changes

--- src/common/meta/search.rs
+++ src/common/meta/search.rs
@@ -2,13 +2,13 @@ use config::meta::search::Response;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-#[derive(Clone, Debug, Serialize, Deserialize, ToSchema)]
+#[derive(Clone, Debug, Serialize, Deserialize, ToSchema, Default)]
 pub struct CachedQueryResponse {
     pub cached_response: Response,
     pub deltas: Vec<QueryDelta>,
     pub has_pre_cache_delta: bool,
 }
-#[derive(Clone, Debug, Serialize, Deserialize, ToSchema)]
+#[derive(Clone, Debug, Serialize, Deserialize, ToSchema, Default)]
 pub struct QueryDelta {
     pub delta_start_time: i64,
     pub delta_end_time: i64,
