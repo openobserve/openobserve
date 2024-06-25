22e226b969b6db54081508e43b051256882293c4
oasisk <ashish.j.kolhe@gmail.com>
2024-06-25 20:55:28 +0530

    fix

--- src/service/search/cache/cacher.rs
+++ src/service/search/cache/cacher.rs
@@ -65,6 +65,9 @@ pub async fn check_cache(
     if !is_aggregate && origin_sql.contains('*') {
         result_ts_col = Some(cfg.common.column_timestamp.clone());
     }
+    if !is_aggregate && origin_sql.contains('*') {
+        result_ts_col = Some(cfg.common.column_timestamp.clone());
+    }
 
     let result_ts_col = result_ts_col.unwrap();
     if let Some(interval) = meta.histogram_interval {
