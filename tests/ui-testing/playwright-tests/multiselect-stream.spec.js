9532605bc9ca464a121f5c2f4e9cc70c48edc4c9
Neha P <neha@openobserve.ai>
2024-06-27 12:29:28 +0530

    remove searcharound test

--- tests/ui-testing/playwright-tests/multiselect-stream.spec.js
+++ tests/ui-testing/playwright-tests/multiselect-stream.spec.js
@@ -234,13 +234,13 @@ await page.waitForTimeout(1000);
     
   });
 
-  test('should display search around in histogram mode', async ({ page }) => {
-    await multistreamselect(page);
-    await page.waitForTimeout(1000);
-    await page.locator('[data-test="log-table-column-0-source"]').click();
-    const isDisabled = await page.locator('[data-test="logs-detail-table-search-around-btn"]').isDisabled();
-    expect(isDisabled).toBe(true);
-  });
+  // test('should display search around in histogram mode', async ({ page }) => {
+  //   await multistreamselect(page);
+  //   await page.waitForTimeout(1000);
+  //   await page.locator('[data-test="log-table-column-0-source"]').click();
+  //   const isDisabled = await page.locator('[data-test="logs-detail-table-search-around-btn"]').isDisabled();
+  //   expect(isDisabled).toBe(true);
+  // });
   
 
 })
