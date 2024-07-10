46aeb6bd29f378670511d3d41f5b342fa0c4e60d
Ashish Kolhe <35160958+oasisk@users.noreply.github.com>
2024-07-10 22:27:03 +0530

    chore: debug query result cache issue (#3943)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
      - Enhanced logging for search cache operations.
      
    - **Bug Fixes**
      - Improved error tracing during cache checks.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/service/search/cluster/cacher.rs
+++ src/service/search/cluster/cacher.rs
@@ -24,6 +24,7 @@ use crate::{
     service::search::infra_cluster,
 };
 
+#[tracing::instrument(name = "service:search:cluster:cacher:get_cached_results", skip_all)]
 pub async fn get_cached_results(
     query_key: String,
     file_path: String,
@@ -224,19 +225,25 @@ pub async fn get_cached_results(
         .iter()
         .filter(|(_, cache_meta)| {
             // to make sure there is overlap between cache time range and query time range &
+            log::info!(
+                "[CACHE CANDIDATES {trace_id}] get_cached_results->grpc: cache_meta.response_start_time: {}, cache_meta.response_end_time: {}",
+                cache_meta.response_start_time,
+                cache_meta.response_end_time);
+                
 
             cache_meta.response_start_time <= cache_req.q_end_time
                 && cache_meta.response_end_time >= cache_req.q_start_time
         })
         .max_by_key(|(_, result)| {
-            result.response_end_time.min(cache_req.q_end_time)
-                - result.response_start_time.max(cache_req.q_start_time)
+            result.response_end_time -result.response_start_time
         }) {
         Some((node, result)) => {
             log::info!(
-                "[trace_id {trace_id}] get_cached_results->grpc: node: {}, get cached result took {} ms",
+                "[CACHE SELECTED {trace_id}] get_cached_results->cluster: node: {}, get cached result took {} ms selected cache cache_meta.response_start_time: {}, cache_meta.response_end_time: {}",
                 &node.grpc_addr,
-                start.elapsed().as_millis()
+                start.elapsed().as_millis(),
+                result.response_start_time,
+                result.response_end_time
             );
             Some(result.clone())
         }
