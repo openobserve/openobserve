622d5de2004a9469f38899aca4534929925cc922
oasisk <ashish.j.kolhe@gmail.com>
2024-07-10 15:34:16 +0530

    only duration

--- src/service/search/cluster/cacher.rs
+++ src/service/search/cluster/cacher.rs
@@ -235,10 +235,7 @@ pub async fn get_cached_results(
                 && cache_meta.response_end_time >= cache_req.q_start_time
         })
         .max_by_key(|(_, result)| {
-              let overlap = result.response_end_time.min(cache_req.q_end_time)
-            - result.response_start_time.max(cache_req.q_start_time);
-        let total_duration = result.response_end_time - result.response_start_time;
-         (total_duration ,overlap )
+            result.end_time -result.start_time
         }) {
         Some((node, result)) => {
             log::info!(
