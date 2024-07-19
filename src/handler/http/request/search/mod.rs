00d3aa005bfb576660b1e75f355fc4f2678a1b9a
oasisk <ashish.j.kolhe@gmail.com>
2024-07-19 14:40:18 +0530

    cache duplicates issue

--- src/handler/http/request/search/mod.rs
+++ src/handler/http/request/search/mod.rs
@@ -1622,7 +1622,7 @@ async fn write_results(
     let largest_ts = cmp::max(first_rec_ts, last_rec_ts);
 
     let cache_end_time = if largest_ts > 0 && largest_ts < req_query_end_time {
-        last_rec_ts
+        largest_ts
     } else {
         req_query_end_time
     };
