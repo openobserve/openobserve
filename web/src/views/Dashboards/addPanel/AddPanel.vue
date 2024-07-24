74076ae3f918a7460594b2efae2729e1aebb8711
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-07-24 16:57:54 +0530

    fix: solved undefined error on edit panel for promql chart

--- web/src/views/Dashboards/addPanel/AddPanel.vue
+++ web/src/views/Dashboards/addPanel/AddPanel.vue
@@ -540,7 +540,7 @@ export default defineComponent({
         !dashboardPanelData.data.config.unit &&
         !dashboardPanelData.data.config.unit_custom &&
         dashboardPanelData.data.queries[0].fields.x.length == 0 &&
-        dashboardPanelData.data.queries[0].fields.breakdown.length == 0 &&
+        dashboardPanelData.data.queries[0].fields?.breakdown?.length == 0 &&
         dashboardPanelData.data.queries[0].fields.y.length == 0 &&
         dashboardPanelData.data.queries[0].fields.z.length == 0 &&
         dashboardPanelData.data.queries[0].fields.filter.length == 0 &&
