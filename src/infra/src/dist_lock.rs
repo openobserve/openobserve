86cb647212e6f5b83c5ba6ded8b2bce67520f8ce
Hengfei Yang <hengfei.yang@gmail.com>
2024-07-24 14:26:25 +0800

    chore: format code

--- src/infra/src/dist_lock.rs
+++ src/infra/src/dist_lock.rs
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 use hashbrown::HashSet;
 
 use crate::{
