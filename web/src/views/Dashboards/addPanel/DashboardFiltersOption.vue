373128c5082afaedda68d19d9256257b3e127df6
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-08-02 16:33:38 +0530

    feat: as string is added in query dont want to add it from here

--- web/src/views/Dashboards/addPanel/DashboardFiltersOption.vue
+++ web/src/views/Dashboards/addPanel/DashboardFiltersOption.vue
@@ -190,7 +190,7 @@ export default defineComponent({
         } else {
           value = it.multiSelect
             ? "(" + "$" + "{" + it.name + "}" + ")"
-            : "'" + "$" + it.name + "'";
+            :  "$" + it.name ;
         }
 
         return {
