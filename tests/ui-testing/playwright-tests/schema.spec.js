4e63eb3f5d791594e5a3132095464b1add92e39f
hengfei yang <hengfei.yang@gmail.com>
2024-08-04 22:11:32 +0800

    fix: ui testing

--- tests/ui-testing/playwright-tests/schema.spec.js
+++ tests/ui-testing/playwright-tests/schema.spec.js
@@ -7,56 +7,55 @@ test.describe.configure({ mode: 'parallel' });
 const streamName = `stream${Date.now()}`;
 
 async function login(page) {
-      await page.goto(process.env["ZO_BASE_URL"]);
-//  await page.getByText('Login as internal user').click();
-      await page.waitForTimeout(1000);
-      await page
-        .locator('[data-cy="login-user-id"]')
-        .fill(process.env["ZO_ROOT_USER_EMAIL"]);
-      //Enter Password
-      await page.locator('label').filter({ hasText: 'Password *' }).click();
-      await page
-        .locator('[data-cy="login-password"]')
-        .fill(process.env["ZO_ROOT_USER_PASSWORD"]);
-      await page.locator('[data-cy="login-sign-in"]').click();
-  //     await page.waitForTimeout(4000);
-  // await page.goto(process.env["ZO_BASE_URL"]);
+  await page.goto(process.env["ZO_BASE_URL"]);
+  //  await page.getByText('Login as internal user').click();
+  await page.waitForTimeout(1000);
+  await page
+    .locator('[data-cy="login-user-id"]')
+    .fill(process.env["ZO_ROOT_USER_EMAIL"]);
+  //Enter Password
+  await page.locator('label').filter({ hasText: 'Password *' }).click();
+  await page
+    .locator('[data-cy="login-password"]')
+    .fill(process.env["ZO_ROOT_USER_PASSWORD"]);
+  await page.locator('[data-cy="login-sign-in"]').click();
 }
 
-
 async function ingestion(page) {
-    const orgId = process.env["ORGNAME"];
-    const streamName = "e2e_automate";
-    const basicAuthCredentials = Buffer.from(
-      `${process.env["ZO_ROOT_USER_EMAIL"]}:${process.env["ZO_ROOT_USER_PASSWORD"]}`
-    ).toString('base64');
-  
-    const headers = {
-      "Authorization": `Basic ${basicAuthCredentials}`,
-      "Content-Type": "application/json",
-    };
-    const response = await page.evaluate(async ({ url, headers, orgId, streamName, logsdata }) => {
-        const fetchResponse = await fetch(`${url}/api/${orgId}/${streamName}/_json`, {
-          method: 'POST',
-          headers: headers,
-          body: JSON.stringify(logsdata)
-        });
-        return await fetchResponse.json();
-      }, {
-        url: process.env.INGESTION_URL,
-        headers: headers,
-        orgId: orgId,
-        streamName: streamName,
-        logsdata: logsdata
-      });
-      console.log(response);
-    }
+  const orgId = process.env["ORGNAME"];
+  const streamName = "e2e_automate";
+  const basicAuthCredentials = Buffer.from(
+    `${process.env["ZO_ROOT_USER_EMAIL"]}:${process.env["ZO_ROOT_USER_PASSWORD"]}`
+  ).toString('base64');
 
-const selectStreamAndStreamTypeForLogs = async (page,stream) => {await page.waitForTimeout(
-  4000);await page.locator(
-  '[data-test="log-search-index-list-select-stream"]').click({ force: true });await page.locator(
-  "div.q-item").getByText(`${stream}`).first().click({ force: true });
+  const headers = {
+    "Authorization": `Basic ${basicAuthCredentials}`,
+    "Content-Type": "application/json",
+  };
+  const response = await page.evaluate(async ({ url, headers, orgId, streamName, logsdata }) => {
+    const fetchResponse = await fetch(`${url}/api/${orgId}/${streamName}/_json`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(logsdata)
+    });
+    return await fetchResponse.json();
+  }, {
+    url: process.env.INGESTION_URL,
+    headers: headers,
+    orgId: orgId,
+    streamName: streamName,
+    logsdata: logsdata
+  });
+  console.log(response);
+}
+
+const selectStreamAndStreamTypeForLogs = async (page, stream) => {
+  await page.waitForTimeout(
+    4000); await page.locator(
+      '[data-test="log-search-index-list-select-stream"]').click({ force: true }); await page.locator(
+        "div.q-item").getByText(`${stream}`).first().click({ force: true });
 };
+
 test.describe("Schema testcases", () => {
   // let logData;
   function removeUTFCharacters(text) {
@@ -73,22 +72,23 @@ test.describe("Schema testcases", () => {
       force: true,
     });
     // get the data from the search variable
-    await expect.poll(async () => (await search).status()).toBe(200); 
+    await expect.poll(async () => (await search).status()).toBe(200);
   }
- 
+
   test.beforeEach(async ({ page }) => {
     await login(page);
-    await page.waitForTimeout(5000)
+    await page.waitForTimeout(1000)
     await ingestion(page);
+    await page.waitForTimeout(2000)
 
     await page.goto(
       `${logData.logsUrl}?org_identifier=${process.env["ORGNAME"]}`
     );
     const allsearch = page.waitForResponse("**/api/default/_search**");
-    await selectStreamAndStreamTypeForLogs(page,logData.Stream);
+    await selectStreamAndStreamTypeForLogs(page, logData.Stream);
     await applyQueryButton(page);
   });
- 
+
   test('stream schema settings updated to be displayed under logs', async ({ page }) => {
     await page.locator('[data-test="menu-link-\\/streams-item"]').click();
     await page.getByPlaceholder('Search Stream').click();
@@ -103,7 +103,7 @@ test.describe("Schema testcases", () => {
     await page.getByRole('cell', { name: 'kubernetes_annotations_kubernetes_io_psp' }).click();
     await page.waitForTimeout(1000);
     await ingestion(page);
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(2000);
     await page.locator('button').filter({ hasText: 'close' }).click();
     await page.getByRole('button', { name: 'Explore' }).first().click();
     await page.waitForTimeout(1000);
@@ -127,7 +127,7 @@ test.describe("Schema testcases", () => {
     await page.locator('button').filter({ hasText: 'close' }).click();
     await page.waitForTimeout(1000);
     await ingestion(page);
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(2000);
     await page.getByRole('button', { name: 'Explore' }).first().click();
     await page.waitForTimeout(3000);
     await page.locator('[data-test="date-time-btn"]').click();
@@ -137,9 +137,7 @@ test.describe("Schema testcases", () => {
     await page.waitForTimeout(2000);
     await page.locator('[data-test="log-table-column-1-\\@timestamp"] [data-test="table-row-expand-menu"]').click();
     await page.getByText('arrow_drop_down_timestamp:').click();
- 
-  })
-
+  });
 
   test('should display stream details on navigating from blank stream to stream with details', async ({ page }) => {
     await page.locator('[data-test="menu-link-\\/streams-item"]').click();
@@ -176,34 +174,33 @@ test.describe("Schema testcases", () => {
   });
 
   test('should details on navigating from blank stream to stream with details', async ({ page }) => {
-  await page.locator('[data-test="menu-link-\\/streams-item"]').click();
-  await page.getByPlaceholder('Search Stream').click();
-  await page.getByPlaceholder('Search Stream').fill('e2e_automate');
-  await page.getByRole('button', { name: 'Stream Detail' }).first().click();
-  await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubectl_kubernetes_io_default_container-field-fts-key-checkbox"]').click();
-  await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubernetes_io_psp-field-fts-key-checkbox"]').click();
-  await page.locator('[data-test="schema-add-field-button"]').click();
-  await page.locator('[data-test="schema-update-settings-button"]').click();
-  await page.locator('[data-test="add-stream-add-field-btn"]').click();
-  await page.getByPlaceholder('Name *').click();
-  await page.getByPlaceholder('Name *').fill('newtest');
-  await page.locator('[data-test="schema-update-settings-button"]').click();
-  await page.locator('[data-test="schema-stream-delete-newtest-field-fts-key-checkbox"]').click();
-  await page.locator('[data-test="schema-add-field-button"]').click();
-  await page.locator('[data-test="schema-update-settings-button"]').click();
-  await page.getByRole('cell', { name: 'newtest' }).first().click();
-  await page.locator('[data-test="schema-stream-delete-newtest-field-fts-key-checkbox"]').first().click();
-  await page.locator('[data-test="schema-delete-button"]').click();
-  await page.locator('[data-test="confirm-button"]').click();
-  await page.waitForTimeout(2000);
-  await page.locator('button').filter({ hasText: 'close' }).click()
-  await page.waitForTimeout(1000);
-  await page.getByRole('button', { name: 'Stream Detail' }).first().click();
-  await page.locator('[data-test="tab-schemaFields"]').click();
-  await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubectl_kubernetes_io_default_container-field-fts-key-checkbox"]').click();
-  await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubernetes_io_psp-field-fts-key-checkbox"]').click();
-  await page.locator('[data-test="schema-add-field-button"]').click();
-  await page.locator('[data-test="schema-update-settings-button"]').click();
-
-  })
+    await page.locator('[data-test="menu-link-\\/streams-item"]').click();
+    await page.getByPlaceholder('Search Stream').click();
+    await page.getByPlaceholder('Search Stream').fill('e2e_automate');
+    await page.getByRole('button', { name: 'Stream Detail' }).first().click();
+    await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubectl_kubernetes_io_default_container-field-fts-key-checkbox"]').click();
+    await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubernetes_io_psp-field-fts-key-checkbox"]').click();
+    await page.locator('[data-test="schema-add-field-button"]').click();
+    await page.locator('[data-test="schema-update-settings-button"]').click();
+    await page.locator('[data-test="add-stream-add-field-btn"]').click();
+    await page.getByPlaceholder('Name *').click();
+    await page.getByPlaceholder('Name *').fill('newtest');
+    await page.locator('[data-test="schema-update-settings-button"]').click();
+    await page.locator('[data-test="schema-stream-delete-newtest-field-fts-key-checkbox"]').click();
+    await page.locator('[data-test="schema-add-field-button"]').click();
+    await page.locator('[data-test="schema-update-settings-button"]').click();
+    await page.getByRole('cell', { name: 'newtest' }).first().click();
+    await page.locator('[data-test="schema-stream-delete-newtest-field-fts-key-checkbox"]').first().click();
+    await page.locator('[data-test="schema-delete-button"]').click();
+    await page.locator('[data-test="confirm-button"]').click();
+    await page.waitForTimeout(2000);
+    await page.locator('button').filter({ hasText: 'close' }).click()
+    await page.waitForTimeout(1000);
+    await page.getByRole('button', { name: 'Stream Detail' }).first().click();
+    await page.locator('[data-test="tab-schemaFields"]').click();
+    await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubectl_kubernetes_io_default_container-field-fts-key-checkbox"]').click();
+    await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubernetes_io_psp-field-fts-key-checkbox"]').click();
+    await page.locator('[data-test="schema-add-field-button"]').click();
+    await page.locator('[data-test="schema-update-settings-button"]').click();
+  });
 })
