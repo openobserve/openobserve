a2bc76eb76710987cfa02906d77b862e1aa3104e
Sai Nikhil <nikhil@openobserve.ai>
2024-08-07 18:24:32 +0530

    Reapply "fix: monaco-editor-border-issue solved"
    This reverts commit a06f118d06d9e4a89016058b6a64e0c3644ee39b.

--- web/src/components/QueryEditor.vue
+++ web/src/components/QueryEditor.vue
@@ -359,7 +359,6 @@ export default defineComponent({
 
 <style lang="scss">
 .logs-query-editor {
-  --vscode-focusBorder: gray !important;
   .monaco-editor,
   .monaco-editor .monaco-editor {
     padding: 0px 0px 0px 0px !important;
