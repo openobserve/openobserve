e70be88573692897d277d357b7e88a9a371326fc
Sai Nikhil <nikhil@openobserve.ai>
2024-08-16 17:17:49 +0530

    Revert "fix : added condition according to the data we are getting"
    This reverts commit 55dfcf3750f464e749a60a079511f9eee0d5dced.

--- web/src/components/alerts/AddAlert.vue
+++ web/src/components/alerts/AddAlert.vue
@@ -1251,7 +1251,8 @@ export default defineComponent({
 
     async onSubmit() {
       if (
-        this.formData.is_real_time == "false" && this.formData.query_condition.type == 'sql' && 
+        !this.formData.is_real_time &&
+        this.formData.query_condition.type == "sql" &&
         !this.getParser(this.formData.query_condition.sql)
       ) {
         this.q.notify({
