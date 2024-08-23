e2b1421a9949372ab0c7b7cd4a28e00001fa91e0
Hengfei Yang <hengfei.yang@gmail.com>
2024-08-23 17:40:25 +0800

    fix: traces link panic (#4296)

--- src/service/traces/mod.rs
+++ src/service/traces/mod.rs
@@ -204,10 +204,22 @@ pub async fn handle_trace_request(
                     for link_att in link.attributes {
                         link_att_map.insert(link_att.key, get_val(&link_att.value.as_ref()));
                     }
+                    let span_id: String = SpanId::from_bytes(
+                        link.span_id
+                            .try_into()
+                            .expect("slice with incorrect length"),
+                    )
+                    .to_string();
+                    let trace_id: String = TraceId::from_bytes(
+                        link.trace_id
+                            .try_into()
+                            .expect("slice with incorrect length"),
+                    )
+                    .to_string();
                     links.push(SpanLink {
                         context: SpanLinkContext {
-                            span_id: String::from_utf8(link.span_id).unwrap(),
-                            trace_id: String::from_utf8(link.trace_id).unwrap(),
+                            span_id,
+                            trace_id,
                             trace_flags: Some(link.flags),
                             trace_state: Some(link.trace_state),
                         },
