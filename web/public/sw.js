15f23492e723e628dbeefcd56228e3d0e83606bc
Bhargav <BJP232004@GMAIL.COM>
2024-08-23 14:05:41 +0530

    fix: removed debug statement & path settings

--- web/public/sw.js
+++ web/public/sw.js
@@ -93,19 +93,16 @@ self.addEventListener('activate', function(event) {
     event.respondWith(
       caches
         .open(cacheVersion).then(function (cache) {
-          console.log("event.request", event.request);
-          console.log("cache", cache);
           return cache.match(event.request);
         })
         .then(function (response) {
           if (response) {
             return response.clone();
           }
-          console.log(event.request, "event.request 2 in fetch");
+
           var fetchRequest = event.request;
           return fetch(fetchRequest)
             .then(function (response) {
-              console.log("response", JSON.stringify(response));
               if (
                 !response ||
                 response.status !== 200 ||
