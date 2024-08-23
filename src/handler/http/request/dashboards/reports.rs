133ee5354e67c6a9fb6202bb56e0b10a10adabcc
Sai Nikhil <nikhil@openobserve.ai>
2024-08-23 12:46:28 +0530

    Revert "fix: use correct http code for forbidden errors"
    This reverts commit 208412913f09add3591a995c883fd3ae463a1144.

--- src/handler/http/request/dashboards/reports.rs
+++ src/handler/http/request/dashboards/reports.rs
@@ -202,7 +202,7 @@ async fn delete_report(path: web::Path<(String, String)>) -> Result<HttpResponse
     match reports::delete(&org_id, &name).await {
         Ok(_) => Ok(MetaHttpResponse::ok("Report deleted")),
         Err(e) => match e {
-            (http::StatusCode::CONFLICT, e) => Ok(MetaHttpResponse::conflict(e)),
+            (http::StatusCode::FORBIDDEN, e) => Ok(MetaHttpResponse::forbidden(e)),
             (http::StatusCode::NOT_FOUND, e) => Ok(MetaHttpResponse::not_found(e)),
             (_, e) => Ok(MetaHttpResponse::internal_error(e)),
         },
