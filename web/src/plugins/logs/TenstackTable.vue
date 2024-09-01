c51d60e2e51bf271306bbfd0416ede0b52dd923e
Omkar Kesarkhane <127078473+omkarK06@users.noreply.github.com>
2024-09-01 16:01:37 +0530

    fix: table columns reordering issue (#4423)
    fix #4424 
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    
    - **New Features**
    - Introduced a new utility for reordering selected fields based on grid
    column order, enhancing user experience when managing data fields.
    - Improved drag-and-drop functionality for table columns, ensuring the
    timestamp column is prioritized in the order.
    
    - **Bug Fixes**
    	- Enhanced error handling by streamlining logging processes.
    
    - **Documentation**
    - Updated component methods to clarify the use of new field management
    logic.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- web/src/plugins/logs/TenstackTable.vue
+++ web/src/plugins/logs/TenstackTable.vue
@@ -56,7 +56,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           }"
           tag="tr"
           @start="(event) => handleDragStart(event)"
-          @end="(event) => handleDragEnd(event)"
+          @end="() => handleDragEnd()"
           class="tw-flex items-center"
         >
           <th
@@ -615,17 +615,16 @@ const handleDragStart = (event: any) => {
   }
 };
 
-const handleDragEnd = async (event: any) => {
+const handleDragEnd = async () => {
   if (
     columnOrder.value.includes(store.state.zoConfig.timestamp_column) &&
     columnOrder.value[0] !== store.state.zoConfig.timestamp_column
   ) {
-    await nextTick();
-    const newItem = columnOrder.value[event.newIndex];
-    columnOrder.value[event.newIndex] = columnOrder.value[event.oldIndex];
-    columnOrder.value[event.oldIndex] = newItem;
-
-    columnOrder.value = [...columnOrder.value];
+    const newColumnOrder = columnOrder.value.filter(
+      (column: any) => column !== store.state.zoConfig.timestamp_column,
+    );
+    newColumnOrder.unshift(store.state.zoConfig.timestamp_column);
+    columnOrder.value = [...newColumnOrder];
   }
 };
 
