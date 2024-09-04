387a5689a81d3eb6b4ae896ba247480c908e984a
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2024-09-04 12:26:00 +0530

    fix: use ofga mapping in tuple object type for search permission checking

--- src/handler/http/request/search/multi_streams.rs
+++ src/handler/http/request/search/multi_streams.rs
@@ -173,6 +173,8 @@ pub async fn search_multi(
         // Check permissions on stream
         #[cfg(feature = "enterprise")]
         {
+            use o2_enterprise::enterprise::openfga::meta::mapping::OFGA_MODELS;
+
             use crate::common::{
                 infra::config::USERS,
                 utils::auth::{is_root_user, AuthExtractor},
@@ -181,6 +183,7 @@ pub async fn search_multi(
             if !is_root_user(user_id) {
                 let user: meta::user::User =
                     USERS.get(&format!("{org_id}/{user_id}")).unwrap().clone();
+                let stream_type_str = stream_type.to_string();
 
                 if user.is_external
                     && !crate::handler::http::auth::validator::check_permissions(
@@ -188,7 +191,13 @@ pub async fn search_multi(
                         AuthExtractor {
                             auth: "".to_string(),
                             method: "GET".to_string(),
-                            o2_type: format!("{}:{}", stream_type, resp.stream_name),
+                            o2_type: format!(
+                                "{}:{}",
+                                OFGA_MODELS
+                                    .get(stream_type_str.as_str())
+                                    .map_or(stream_type_str.as_str(), |model| model.key),
+                                resp.stream_name
+                            ),
                             org_id: org_id.clone(),
                             bypass_check: false,
                             parent_id: "".to_string(),
