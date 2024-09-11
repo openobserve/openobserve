09cb363687138e65508c764d6ff00c16afb503f7
hengfei yang <hengfei.yang@gmail.com>
2024-09-11 11:28:53 +0800

    fix: hashing unit test

--- src/common/infra/cluster/mod.rs
+++ src/common/infra/cluster/mod.rs
@@ -619,13 +619,13 @@ mod tests {
 
         // gxhash hash
         let data = [
-            ["test", "node-q-4", "node-c-7"],
-            ["test1", "node-q-3", "node-c-3"],
-            ["test2", "node-q-7", "node-c-2"],
-            ["test3", "node-q-7", "node-c-6"],
-            ["test4", "node-q-9", "node-c-3"],
-            ["test5", "node-q-5", "node-c-2"],
-            ["test6", "node-q-8", "node-c-3"],
+            ["test", "node-q-4", "node-c-8"],
+            ["test1", "node-q-6", "node-c-2"],
+            ["test2", "node-q-2", "node-c-0"],
+            ["test3", "node-q-6", "node-c-3"],
+            ["test4", "node-q-1", "node-c-6"],
+            ["test5", "node-q-1", "node-c-0"],
+            ["test6", "node-q-2", "node-c-1"],
         ];
 
         remove_node_from_consistent_hash(&node, &Role::Querier, Some(RoleGroup::Interactive)).await;
