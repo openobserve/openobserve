dd287cd700a781403730e4482796e14c86a9f526
Omkar Kesarkhane <127078473+omkarK06@users.noreply.github.com>
2024-09-11 10:44:27 +0530

    fix: fixed tabs column issue in scheduled dashboard table (#4516)
    #4508 
    Showing the wrong last trigger date; it displays as 1970.
    An ID is being shown in the tab field.
     
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a loading spinner for improved user experience during data
    fetching in the Scheduled Dashboards.
    - Enhanced the View Dashboard component to dynamically receive tab data,
    improving robustness.
    
    - **Improvements**
    - Renamed and clarified the `tabs` property to `reportTypeTabs` for
    better readability.
    - Updated timestamp handling for consistent formatting across reports,
    shifting from milliseconds to microseconds.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- web/src/components/reports/CreateReport.vue
+++ web/src/components/reports/CreateReport.vue
@@ -862,7 +862,7 @@ const defaultReport = {
   password: "",
   timezone: "UTC",
   timezoneOffset: 0,
-  lastTriggeredAt: 0,
+  lastTriggeredAt: null,
   createdAt: "",
   updatedAt: "",
   owner: "",
@@ -1322,7 +1322,7 @@ const saveReport = async () => {
   } else {
     formData.value.createdAt = new Date().toISOString();
     formData.value.owner = store.state.userInfo.email;
-    formData.value.lastTriggeredAt = new Date().getTime();
+    formData.value.lastTriggeredAt = null;
     formData.value.lastEditedBy = store.state.userInfo.email;
     formData.value.updatedAt = new Date().toISOString();
   }
