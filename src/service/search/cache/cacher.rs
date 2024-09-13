3841a296316e08314f017853a1acd5522da1968b
Ashish Kolhe <35160958+oasisk@users.noreply.github.com>
2024-09-13 08:56:50 +0530

    fix: cache - duplicate records issue (#4537)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    	- Improved delta calculation logic for more accurate data processing.
    - Enhanced caching mechanism to prioritize the earliest timestamps for
    better performance.
    - Introduced flexible value retrieval from the database based on
    configuration settings.
    
    - **Bug Fixes**
    - Fixed issues with gap detection in results to ensure valid data is
    considered.
    - Enhanced error handling during value retrieval to prevent state
    updates on failures.
    
    - **Refactor**
    - Streamlined conditions for calculating `delta_end_time` and cache
    starting points.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: hengfei yang <hengfei.yang@gmail.com>

--- src/service/search/cache/cacher.rs
+++ src/service/search/cache/cacher.rs
@@ -634,9 +634,8 @@ fn calculate_deltas_multi(
             meta.cached_response.hits.len()
         );
         cache_duration += meta.response_end_time - meta.response_start_time;
-        let delta_end_time = if current_end_time != start_time && histogram_interval > 0 {
-            // If there is a histogram interval, we need to adjust the end time to the nearest
-            // interval
+        let delta_end_time = if histogram_interval > 0 && !meta.cached_response.hits.is_empty() {
+            // If histogram interval > 0, we need to adjust the end time to the nearest interval
             let mut end_time = meta.response_start_time;
             if end_time % histogram_interval != 0 {
                 end_time = end_time - (end_time % histogram_interval);
@@ -658,7 +657,11 @@ fn calculate_deltas_multi(
     }
 
     // Check if there is a gap at the end
-    if current_end_time < end_time {
+    if current_end_time < end_time
+        && results.last().map_or(false, |last_meta| {
+            !last_meta.cached_response.hits.is_empty()
+        })
+    {
         deltas.push(QueryDelta {
             delta_start_time: current_end_time,
             delta_end_time: end_time,
