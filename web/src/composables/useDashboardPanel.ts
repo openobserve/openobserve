7285576725182f9ca89fb1c53b12bf4731f6f7ff
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-09-13 12:00:09 +0530

    fix: remove breakdown field on switch

--- web/src/composables/useDashboardPanel.ts
+++ web/src/composables/useDashboardPanel.ts
@@ -940,6 +940,9 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
         dashboardPanelData.data.queries[
           dashboardPanelData.layout.currentQueryIndex
         ].fields.z = [];
+        dashboardPanelData.data.queries[
+          dashboardPanelData.layout.currentQueryIndex
+        ].fields.breakdown = [];
         dashboardPanelData.data.queries?.forEach((query: any) => {
           query.fields.latitude = null;
           query.fields.longitude = null;
