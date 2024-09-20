64829300ef83256094390aaeafcae00f4cf27245
ktx-abhay <abhay.padamani@kiara.tech>
2024-09-20 17:34:34 +0530

    add: initial ui changes for table cell color

---
+++ web/src/components/dashboards/addPanel/ValueMappingPopUp.vue
@@ -0,0 +1,165 @@
+<!-- Copyright 2023 Zinc Labs Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http:www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. 
+-->
+
+<!-- eslint-disable vue/no-unused-components -->
+<template>
+  <div
+    style="padding: 0px 10px; min-width: 30%"
+    class="scroll o2-input"
+    data-test="dashboard-value-mapping-popup"
+  >
+    <div
+      class="flex justify-between items-center q-pa-md"
+      style="border-bottom: 2px solid gray; margin-bottom: 5px"
+    >
+      <div class="flex items-center q-table__title q-mr-md">
+        <span>Value Mappings</span>
+      </div>
+    </div>
+    <div>
+      <div>
+        <draggable
+          v-model="dashboardPanelData.data.config.mappings"
+          :options="dragOptions"
+          @mousedown.stop="() => {}"
+          data-test="dashboard-addpanel-config-value-mapping-drag"
+        >
+          <div
+            v-for="(mapping, index) in dashboardPanelData.data.config.mappings"
+            :key="index"
+            class="draggable-row"
+          >
+            <div class="draggable-handle">
+              <q-icon
+                name="drag_indicator"
+                color="grey-13"
+                class="'q-mr-xs"
+                data-test="dashboard-tab-settings-drag-handle"
+              />
+            </div>
+            <div class="draggable-content">
+              <input
+                v-model="mapping.name"
+                class="edit-input"
+                data-test="dashboard-tab-settings-tab-name-edit"
+              />
+
+              <span class="q-ml-lg">
+                <q-btn
+                  :icon="outlinedDelete"
+                  :title="t('dashboard.delete')"
+                  class="q-ml-xs"
+                  padding="sm"
+                  unelevated
+                  size="sm"
+                  round
+                  flat
+                  data-test="dashboard-tab-settings-tab-delete-btn"
+                  ></q-btn>
+                  <!-- @click.stop="deleteItem(mapping.tabId)" -->
+              </span>
+            </div>
+          </div>
+        </draggable>
+      </div>
+      <q-btn
+        @click="addValueMapping"
+        style="cursor: pointer; padding: 0px 5px"
+        label="+ Add a new mapping"
+        no-caps
+        data-test="dashboard-addpanel-config-value-mapping-add-btn"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { inject, ref } from "vue";
+import { defineComponent } from "vue";
+import { useI18n } from "vue-i18n";
+import { useStore } from "vuex";
+import useDashboardPanelData from "../../../composables/useDashboardPanel";
+import { outlinedDelete } from "@quasar/extras/material-icons-outlined";
+import { onMounted } from "vue";
+
+export default defineComponent({
+  name: "ValueMappingPopUp",
+  components: {},
+  props: {},
+  emits: ["close"],
+  setup(props, { emit }) {
+    const { t } = useI18n();
+    const store = useStore();
+    const dashboardPanelDataPageKey = inject(
+      "dashboardPanelDataPageKey",
+      "dashboard",
+    );
+    const { dashboardPanelData } = useDashboardPanelData(
+      dashboardPanelDataPageKey,
+    );
+
+    const dragOptions = ref({
+      animation: 200,
+    });
+
+    const mappingTypes = [
+      {
+        label: "Value",
+        value: "value",
+      },
+      {
+        label: "Range",
+        value: "range",
+      },
+    ];
+
+    const addValueMapping = () => {
+      dashboardPanelData.data.config.mappings.push({
+        type: "value",
+        value: "",
+        color: "#000000",
+      });
+    };
+
+    const removeValueMappingByIndex = (index: number) => {
+      dashboardPanelData.data.config.mappings.splice(index, 1);
+    };
+
+    onMounted(() => {
+      if (!dashboardPanelData.data.config.mappings) {
+        dashboardPanelData.data.config.mappings = [];
+      }
+
+      // if mappings is empty, add default value mapping
+      if (dashboardPanelData.data.config.mappings.length == 0) {
+        addValueMapping();
+      }
+    });
+
+    return {
+      t,
+      dashboardPanelData,
+      store,
+      addValueMapping,
+      removeValueMappingByIndex,
+      mappingTypes,
+      dragOptions,
+      outlinedDelete,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
