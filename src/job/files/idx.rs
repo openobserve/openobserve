82f00838afd54602b84247f42d2b42510920ecb0
hengfei yang <hengfei.yang@gmail.com>
2024-09-27 19:36:36 +0800

    perf: remove unnecessary setting for paruqet writer

--- src/job/files/idx.rs
+++ src/job/files/idx.rs
@@ -75,7 +75,7 @@ pub(crate) async fn write_parquet_index_to_disk(
 
     // write parquet file
     let mut buf_parquet = Vec::new();
-    let mut writer = new_parquet_writer(&mut buf_parquet, &schema, &[], &[], &file_meta);
+    let mut writer = new_parquet_writer(&mut buf_parquet, &schema, &[], &file_meta);
     for batch in batches {
         writer.write(&batch).await?;
     }
