b86db04bd7c2862d60f0af97877e44b33c220382
Sai Nikhil <nikhil@openobserve.ai>
2024-09-29 13:11:04 +0530

    feat: added custom time range in alerts page

--- web/src/components/alerts/AlertList.vue
+++ web/src/components/alerts/AlertList.vue
@@ -726,6 +726,10 @@ export default defineComponent({
       formData.value = alerts.value.find(
         (alert: any) => alert.uuid === props.row?.uuid,
       ) as Alert;
+      if( formData.value){
+        console.log(formData,"formdata")
+        formData.value.query_condition.multi_time_range = [{offSet:"30m"}];
+      }
       let action;
       if (!props.row) {
         isUpdated.value = false;
