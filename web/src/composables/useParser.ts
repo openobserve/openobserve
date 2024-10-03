d4365124733accf4de52ec7b23dbc393109bb180
Bhargav <BJP232004@GMAIL.COM>
2024-10-03 13:03:27 +0530

    fix: reverted changes

--- web/src/composables/useParser.ts
+++ web/src/composables/useParser.ts
@@ -1,6 +1,8 @@
+import DatafusionSQL from "@openobserve/node-sql-parser/build/DatafusionSQL";
 const useParser = () => {
   const sqlParser = async () => {
-    const Parser: any = await import("node-sql-parser/build/DatafusionSQL");
+    console.log(DatafusionSQL, "DatafusionSQL");
+    const Parser: any = DatafusionSQL.Parser;
 
     if (Parser) {
       return new Parser.default.Parser();
