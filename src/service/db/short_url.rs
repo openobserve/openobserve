83c3bed0a470d3ead0cb51787619dd54d7e3f88b
hengfei yang <hengfei.yang@gmail.com>
2024-10-04 21:52:40 +0800

    fix: format code

--- src/service/db/short_url.rs
+++ src/service/db/short_url.rs
@@ -16,51 +16,48 @@
 use std::sync::Arc;
 
 use anyhow::anyhow;
+use bytes::Bytes;
 use config::utils::json;
-use infra::db::Event;
-
-use crate::{
-    common::infra::config::URL_MAP,
-    service::db::{self},
+use infra::{
+    db::{Event, NEED_WATCH},
+    short_url,
 };
+
+use crate::{common::infra::config::SHORT_URLS, service::db};
 
 // DBKey to set short URL's
 pub const SHORT_URL_KEY: &str = "/short_urls/";
 
 pub async fn get(short_id: &str) -> Result<String, anyhow::Error> {
-    match URL_MAP.get(short_id) {
+    match SHORT_URLS.get(short_id) {
         Some(val) => Ok(val.to_string()),
         None => {
-            let val = db::get(&format!("{SHORT_URL_KEY}{short_id}"))
+            let val = short_url::get(short_id)
                 .await
                 .map_err(|_| anyhow!("Short URL not found in db"))?;
+            SHORT_URLS.insert(short_id.to_string(), val.original_url.to_string());
 
-            let original_url: String = json::from_slice(&val)
-                .map_err(|_| anyhow!("Failed to deserialize original url from db"))?;
-
-            URL_MAP.insert(short_id.to_string(), original_url.clone());
-
-            Ok(original_url)
+            Ok(val.original_url)
         }
     }
 }
 
 pub async fn set(short_id: &str, original_url: &str) -> Result<(), anyhow::Error> {
-    URL_MAP.insert(short_id.to_string(), original_url.to_string());
-
+    SHORT_URLS.insert(short_id.to_string(), original_url.to_string());
+    short_url::add(short_id, original_url).await?;
+    // trigger watch event
     db::put(
         &format!("{SHORT_URL_KEY}{short_id}"),
-        json::to_vec(&original_url).unwrap().into(),
-        db::NEED_WATCH,
+        Bytes::new(),
+        NEED_WATCH,
         None,
     )
     .await?;
-
     Ok(())
 }
 
 pub async fn get_by_original_url(original_url: &str) -> Option<String> {
-    if let Some(short_id) = URL_MAP.iter().find_map(|entry| {
+    if let Some(short_id) = SHORT_URLS.iter().find_map(|entry| {
         let (k, v) = entry.pair();
         if v == original_url {
             return Some(k.clone());
@@ -75,7 +72,7 @@ pub async fn get_by_original_url(original_url: &str) -> Option<String> {
     match db::get(&format!("{SHORT_URL_KEY}{original_url}")).await {
         Ok(val) => match json::from_slice::<String>(&val) {
             Ok(short_id) => {
-                URL_MAP.insert(short_id.clone(), original_url.clone());
+                SHORT_URLS.insert(short_id.clone(), original_url.clone());
                 Some(short_id)
             }
             Err(e) => {
@@ -112,28 +109,18 @@ pub async fn watch() -> Result<(), anyhow::Error> {
         match ev {
             Event::Put(ev) => {
                 let item_key = ev.key.strip_prefix(key).unwrap();
-                let item_value = if config::get_config().common.meta_store_external {
-                    match db::get(&ev.key).await {
-                        Ok(val) => match json::from_slice(&val) {
-                            Ok(val) => val,
-                            Err(e) => {
-                                log::error!("Error getting value: {}", e);
-                                continue;
-                            }
-                        },
-                        Err(e) => {
-                            log::error!("Error getting value: {}", e);
-                            continue;
-                        }
+                let item_value = match short_url::get(&item_key).await {
+                    Ok(val) => val.original_url,
+                    Err(e) => {
+                        log::error!("Error getting value: {}", e);
+                        continue;
                     }
-                } else {
-                    json::from_slice(&ev.value.unwrap()).unwrap()
                 };
-                URL_MAP.insert(item_key.to_string(), item_value);
+                SHORT_URLS.insert(item_key.to_string(), item_value);
             }
             Event::Delete(ev) => {
                 let item_key = ev.key.strip_prefix(key).unwrap();
-                URL_MAP.remove(item_key);
+                SHORT_URLS.remove(item_key);
             }
             Event::Empty => {}
         }
@@ -147,7 +134,7 @@ pub async fn cache() -> Result<(), anyhow::Error> {
     for (item_key, item_value) in ret {
         let short_id = item_key.strip_prefix(key).unwrap();
         let original_url: String = json::from_slice(&item_value).unwrap();
-        URL_MAP.insert(short_id.to_owned(), original_url);
+        SHORT_URLS.insert(short_id.to_owned(), original_url);
     }
     log::info!("Short URLs Cached");
     Ok(())
