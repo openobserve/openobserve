89e545afc8f81685c0fb4c7b5634270e6b22f849
hengfei yang <hengfei.yang@gmail.com>
2024-10-09 16:57:20 +0800

    add optiton for control min datafusion partition

--- src/config/src/config.rs
+++ src/config/src/config.rs
@@ -962,6 +962,8 @@ pub struct Limit {
     pub consistent_hash_vnodes: usize,
     #[env_config(name = "ZO_DATAFUSION_FILE_STAT_CACHE_MAX_ENTRIES", default = 100000)]
     pub datafusion_file_stat_cache_max_entries: usize,
+    #[env_config(name = "ZO_DATAFUSION_MIN_PARTITION_NUM", default = 2)]
+    pub datafusion_min_partition_num: usize,
     #[env_config(
         name = "ZO_ENRICHMENT_TABLE_LIMIT",
         default = 256,
