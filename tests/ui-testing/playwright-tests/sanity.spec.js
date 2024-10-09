98750b4a4335a4fc9b2e9be9698b30450bd94969
Neha P <neha@openobserve.ai>
2024-10-09 16:22:32 +0530

    remove login text

--- tests/ui-testing/playwright-tests/sanity.spec.js
+++ tests/ui-testing/playwright-tests/sanity.spec.js
@@ -10,7 +10,7 @@ const dashboardName = `AutomatedDashboard${Date.now()}`;
 
 async function login(page) {
   await page.goto(process.env["ZO_BASE_URL"]);
-await page.getByText('Login as internal user').click();
+// await page.getByText('Login as internal user').click();
   console.log("ZO_BASE_URL", process.env["ZO_BASE_URL"]);
   await page.waitForTimeout(1000);
   await page
