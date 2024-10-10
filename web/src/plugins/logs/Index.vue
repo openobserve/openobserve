78fdbc7728c7f2c2faa56c61b2ff9881e099e3c3
Bhargav <BJP232004@GMAIL.COM>
2024-10-10 15:56:19 +0530

    feat: code refactoring for node-sql-parser with datafusion function

--- web/src/plugins/logs/Index.vue
+++ web/src/plugins/logs/Index.vue
@@ -878,6 +878,7 @@ export default defineComponent({
 
     function removeFieldByName(data, fieldName) {
       return data.filter((item: any) => {
+        console.log("item", item);
         if (item.expr) {
           if (
             item.expr.type === "column_ref" &&
