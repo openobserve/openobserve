e7d9820dba6d25e9ba9c955d9c212c5369d9a4f6
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-10-10 12:25:03 +0530

    feat: added icon based on its theme

---
+++ web/src/components/icons/dashboards/Smooth.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg
+    width="132"
+    height="84"
+    viewBox="0 0 132 84"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6.07188 78C2.5731 -18.122 127.973 -20.5371 125.976 78"
+      stroke="currentColor"
+      stroke-width="4"
+    />
+    <circle cx="66" cy="6" r="6" fill="currentColor" />
+    <circle cx="126" cy="78" r="6" fill="currentColor" />
+    <circle cx="6" cy="78" r="6" fill="currentColor" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Smooth",
+});
+</script>
