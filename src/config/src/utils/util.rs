bb047872cd83f249c5c430ce165599d881a112a1
Hengfei Yang <hengfei.yang@gmail.com>
2024-10-10 16:34:37 +0800

    perf: add more option for db connection pool (#4747)

--- src/config/src/utils/util.rs
+++ src/config/src/utils/util.rs
@@ -13,22 +13,25 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pub mod arrow;
-pub mod asynchronism;
-pub mod base64;
-pub mod cgroup;
-pub mod file;
-pub mod flatten;
-pub mod hash;
-pub mod inverted_index;
-pub mod json;
-pub mod md5;
-pub mod parquet;
-pub mod rand;
-pub mod record_batch_ext;
-pub mod schema;
-pub mod schema_ext;
-pub mod sql;
-pub mod str;
-pub mod time;
-pub mod util;
+pub fn zero_or<T>(v: T, def: T) -> T
+where
+    T: PartialEq + Default,
+{
+    if v == Default::default() { def } else { v }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_zero_or() {
+        assert_eq!(zero_or(0, 1), 1);
+        assert_eq!(zero_or(2, 1), 2);
+        assert_eq!(zero_or(0, 0), 0);
+        assert_eq!(zero_or(0.0, 1.1), 1.1);
+        assert_eq!(zero_or(2.1, 1.1), 2.1);
+        assert_eq!(zero_or("", "v"), "v");
+        assert_eq!(zero_or("vv", "v"), "vv");
+    }
+}
