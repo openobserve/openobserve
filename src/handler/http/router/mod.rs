2134b32e181c27ac91650288a6ee3e35132295d3
oasisk <ashish.j.kolhe@gmail.com>
2024-10-11 11:17:27 +0530

    removed the scope /short

--- src/handler/http/router/mod.rs
+++ src/handler/http/router/mod.rs
@@ -226,15 +226,6 @@ pub fn get_basic_routes(cfg: &mut web::ServiceConfig) {
             .service(status::stream_fields),
     );
 
-    cfg.service(
-        web::scope("/short")
-            .wrap(HttpAuthentication::with_fn(
-                super::auth::validator::validate_short_or_redirect,
-            ))
-            .wrap(cors.clone())
-            .service(short_url::retrieve),
-    );
-
     if get_config().common.swagger_enabled {
         cfg.service(
             SwaggerUi::new("/swagger/{_:.*}")
