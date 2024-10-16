70af4e11a9a6d92ea9fc88a810272f258b37df4d
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-10-16 15:26:31 +0530

    refactor: key name change

--- web/src/composables/useDashboardPanel.ts
+++ web/src/composables/useDashboardPanel.ts
@@ -74,7 +74,7 @@ const getDefaultDashboardPanelData: any = () => ({
       },
       drilldown: [],
       mark_line: [],
-      unit_mappings: [],
+      override_config: [],
       connect_nulls: false,
       no_value_replacement: "",
       wrap_table_cells: false,
