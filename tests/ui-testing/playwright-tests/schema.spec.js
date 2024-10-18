a51859f25663f98889d8a85b5706535c41489acc
Neha P <neha@openobserve.ai>
2024-10-18 16:35:28 +0530

    test: fix schema test

--- tests/ui-testing/playwright-tests/schema.spec.js
+++ tests/ui-testing/playwright-tests/schema.spec.js
@@ -8,7 +8,7 @@ const streamName = `stream${Date.now()}`;
 
 async function login(page) {
   await page.goto(process.env["ZO_BASE_URL"]);
-  //  await page.getByText('Login as internal user').click();
+  // await page.getByText('Login as internal user').click();
   await page.waitForTimeout(1000);
   await page
     .locator('[data-cy="login-user-id"]')
@@ -104,6 +104,7 @@ test.describe("Schema testcases", () => {
     await page.getByPlaceholder('Search Stream').click();
     await page.getByPlaceholder('Search Stream').fill('e2e_automate');
     await page.getByRole('button', { name: 'Stream Detail' }).first().click();
+    await page.locator('[data-test="log-stream-use_approx-toggle-btn"] div').first().click();
     await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubectl_kubernetes_io_default_container-field-fts-key-checkbox"]').click();
     await page.locator('[data-test="schema-stream-delete-kubernetes_annotations_kubernetes_io_psp-field-fts-key-checkbox"]').click();
     await page.locator('[data-test="schema-add-field-button"]').click();
