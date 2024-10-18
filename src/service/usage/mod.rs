53c86fc38f3fd353937ec757ba6d12dea2231de3
Huaijin <haohuaijin@gmail.com>
2024-10-18 15:03:48 +0800

    feat: add short and long query to usage reported (#4827)
    cherry-pick from https://github.com/openobserve/openobserve/pull/4784

--- src/service/usage/mod.rs
+++ src/service/usage/mod.rs
@@ -106,6 +106,7 @@ pub async fn report_request_usage_stats(
             took_wait_in_queue: stats.took_wait_in_queue,
             result_cache_ratio: None,
             is_partial: stats.is_partial,
+            work_group: None,
         });
     };
 
@@ -142,6 +143,7 @@ pub async fn report_request_usage_stats(
         took_wait_in_queue: stats.took_wait_in_queue,
         result_cache_ratio: stats.result_cache_ratio,
         is_partial: stats.is_partial,
+        work_group: stats.work_group,
     });
     if !usage.is_empty() {
         publish_usage(usage).await;
