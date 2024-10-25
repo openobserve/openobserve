e905583276e1f6d7fe1beb554d1e7cd2f8e855cf
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-10-25 19:18:34 +0530

    based on search query

--- tests/ui-testing/playwright-tests/visualize.spec.js
+++ tests/ui-testing/playwright-tests/visualize.spec.js
@@ -147,9 +147,12 @@ test.describe(" visualize UI testcases", () => {
         '[data-test="field-list-item-logs-e2e_automate-kubernetes_container_image"] [data-test="dashboard-add-y-data"]'
       )
       .click();
+    const search = page.waitForResponse(logData.applyQuery);
     await page.locator('[data-test="logs-search-bar-visualize-refresh-btn"]').click();
+    await expect.poll(async () => (await search).status()).toBe(200);
     await page.waitForLoadState('networkidle');
     await page.waitForTimeout(1000);
+    
     await page.locator('[data-test="chart-renderer"] canvas').click({
         position: {
           x: 470,
