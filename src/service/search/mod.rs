58bd41a8293af7517b2caa55248993f09be8add4
Ashish Kolhe <35160958+oasisk@users.noreply.github.com>
2024-10-25 17:15:53 +0530

    chore: add search type to query manager (#4912)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced an optional `search_type` field in the QueryStatus,
    allowing for more detailed tracking of search events.
    - Enhanced the search functionality to include the search event type in
    the response, improving the information provided about query statuses.
    
    - **Improvements**
    - Updated search operations to better handle incoming requests and
    enhance filtering logic for search queries.
    	- Refined error handling and logging throughout the search process.
    
    These changes aim to provide users with richer metadata and improved
    search capabilities.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/service/search/mod.rs
+++ src/service/search/mod.rs
@@ -381,6 +381,9 @@ pub async fn search(
         let sql = Some(in_req.query.sql.clone());
         let start_time = Some(in_req.query.start_time);
         let end_time = Some(in_req.query.end_time);
+        let s_event_type = in_req
+            .search_type
+            .map(|s_event_type| s_event_type.to_string());
         // set search task
         SEARCH_SERVER
             .insert(
@@ -394,6 +397,7 @@ pub async fn search(
                     sql,
                     start_time,
                     end_time,
+                    s_event_type,
                 ),
             )
             .await;
@@ -694,6 +698,8 @@ pub async fn search_partition(
 #[cfg(feature = "enterprise")]
 pub async fn query_status() -> Result<search::QueryStatusResponse, Error> {
     // get nodes from cluster
+
+    use std::str::FromStr;
     let mut nodes = match infra_cluster::get_cached_online_query_nodes(None).await {
         Some(nodes) => nodes,
         None => {
@@ -829,6 +835,9 @@ pub async fn query_status() -> Result<search::QueryStatusResponse, Error> {
         } else {
             "Long"
         };
+        let search_type: Option<search::SearchEventType> = result
+            .search_type
+            .map(|s_event_type| search::SearchEventType::from_str(&s_event_type).unwrap());
         status.push(search::QueryStatus {
             trace_id: result.trace_id,
             created_at: result.created_at,
@@ -840,6 +849,7 @@ pub async fn query_status() -> Result<search::QueryStatusResponse, Error> {
             query,
             scan_stats,
             work_group: work_group.to_string(),
+            search_type,
         });
     }
 
