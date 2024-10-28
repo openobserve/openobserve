736aedb0cf70e2f650deac674714c80b0db01508
ShyamOOAI <shyam@openobserve.ai>
2024-10-28 20:00:01 +0530

    test updated submit button locator

--- tests/ui-testing/pages/managementPage.js
+++ tests/ui-testing/pages/managementPage.js
@@ -3,52 +3,56 @@ import { expect } from '@playwright/test';
 
 export const path = require('path');
 export
-    class ManagementPage {
+    class ManagementPage {
 
-    constructor(page) {
-        this.page = page;
+    constructor(page) {
+        this.page = page;
 
-        this.homeIcon = page.locator("[name ='home']");
-        //this.managementMenuItem = page.getByText('Management');
-        this.managementMenuItem = page.locator('[data-test="menu-link-/settings/-item"]');
-        this.submitButton = page.locator("[type='submit']").nth(1);
-        this.customLogoText = page.locator("[aria-label ='Custom Logo Text']");
-        this.saveButton = page.locator('[data-test="settings_ent_logo_custom_text_save_btn"]');
-        this.deleteLogoButton = page.locator('[data-test="setting_ent_custom_logo_img_delete_btn"]');
-        this.confirmDeleteButton = page.locator('[data-test="logs-search-bar-confirm-dialog-ok-btn"]');
-        this.fileUploadInput = page.locator('input[data-test="setting_ent_custom_logo_img_file_upload"]');
-    }
+        this.homeIcon = page.locator("[name ='home']");
+        //this.managementMenuItem = page.getByText('Management');
+        this.managementMenuItem = page.locator('[data-test="menu-link-/settings/-item"]');
+        //this.submitButton = page.locator("[type='submit']").nth(1);
+        this.submitButton = page.locator("[data-test='settings_ent_logo_custom_text_edit_btn']");
+        this.customLogoText = page.locator("[aria-label ='Custom Logo Text']");
+        this.saveButton = page.locator('[data-test="settings_ent_logo_custom_text_save_btn"]');
+        this.deleteLogoButton = page.locator('[data-test="setting_ent_custom_logo_img_delete_btn"]');
+        this.confirmDeleteButton = page.locator('[data-test="logs-search-bar-confirm-dialog-ok-btn"]');
+        this.fileUploadInput = page.locator('input[data-test="setting_ent_custom_logo_img_file_upload"]');
+    }
 
 
-    async navigateToManagement() {
-        await this.page.waitForSelector("[name ='home']");
-        await this.homeIcon.hover();
-        await this.page.waitForSelector('[data-test="menu-link-/settings/-item"]');
-        await this.managementMenuItem.click({ force: true });
-    }
+    async navigateToManagement() {
+        await this.page.waitForSelector("[name ='home']");
+        await this.homeIcon.hover();
+        await this.page.waitForSelector('[data-test="menu-link-/settings/-item"]');
+        await this.managementMenuItem.click({ force: true });
+    }
 
-    async updateCustomLogoText(text) {
-      
-        await this.submitButton.click({ force: true });
-        await this.customLogoText.fill(text);
-        await this.page.waitForSelector('[data-test="settings_ent_logo_custom_text_save_btn"]');
-        await this.saveButton.click({ force: true });
-        await this.page.waitForTimeout(5000);
+    async updateCustomLogoText(text) {
 
-    }
+        await this.submitButton.click({ force: true });
+        await this.page.waitForSelector("[aria-label ='Custom Logo Text']");
+        await this.customLogoText.fill(text);
+        await this.page.waitForSelector('[data-test="settings_ent_logo_custom_text_save_btn"]');
+        await this.saveButton.click({ force: true });
+        await this.page.waitForTimeout(5000);
 
-    async uploadLogo(filePath) {
-        const isVisible = await this.deleteLogoButton.isVisible();
-        if (isVisible) {
-            await this.page.waitForSelector('[data-test="setting_ent_custom_logo_img_delete_btn"]');
-            await this.deleteLogoButton.click({ force: true });
-            await this.page.waitForSelector('[data-test="logs-search-bar-confirm-dialog-ok-btn"]');
-            await this.confirmDeleteButton.click({ force: true });
-        }
-      //  await this.fileUploadInput.setInputFiles(filePath);
-        await this.page.setInputFiles('input[ data-test="setting_ent_custom_logo_img_file_upload"]', filePath);
-        await this.page.waitForTimeout(5000);
-        
+    }
 
-    }
+    async uploadLogo(filePath) {
+        const isVisible = await this.deleteLogoButton.isVisible();
+        if (isVisible) {
+            await this.page.waitForSelector('[data-test="setting_ent_custom_logo_img_delete_btn"]');
+            await this.deleteLogoButton.click({ force: true });
+            await this.page.waitForSelector('[data-test="logs-search-bar-confirm-dialog-ok-btn"]');
+            await this.confirmDeleteButton.click({ force: true });
+            
+            
+        }
+        //  await this.fileUploadInput.setInputFiles(filePath);
+        await this.page.setInputFiles('input[ data-test="setting_ent_custom_logo_img_file_upload"]', filePath);
+        await this.page.waitForTimeout(5000);
+
+
+    }
 }
