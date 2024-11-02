5f50363c31a7431355b651121fa0c906138a4a1c
ShyamOOAI <shyam@openobserve.ai>
2024-11-02 12:02:15 +0530

    test one test case

--- tests/ui-testing/pages/dashboardPage.js
+++ tests/ui-testing/pages/dashboardPage.js
@@ -1,5 +1,5 @@
 import { expect } from '@playwright/test';
-import { CommomnLocator } from '../pages/CommonLocator';
+
 import { dateTimeButtonLocator, relative30SecondsButtonLocator, absoluteTabLocator, Past30SecondsValue } from '../pages/CommonLocator.js';
 
 
@@ -7,10 +7,13 @@ export class DashboardPage {
   constructor(page) {
     this.page = page;
 
+    this.dashboardName = `dashboard${Date.now()}`;
     this.dashboardsMenuItem = page.locator('[data-test="menu-link-\\/dashboards-item"]');
     this.addDashboardButton = page.locator('[data-test="dashboard-add"]');
-    this.dashboardNameInput = '[data-test="add-dashboard-name"]';
-    this.dashboardSubmitButton = '[data-test="dashboard-add-submit"]';
+    this.dashboardNameInput = page.locator('[data-test="add-dashboard-name"]');
+    this.dashboardSubmitButton = page.locator('[data-test="dashboard-add-submit"]');
+    this.savePanelButton = page.locator('[data-test="dashboard-panel-save"]');
+    this.dashboardPanelNameInput = page.locator('[data-test="dashboard-panel-name"]');
 
     this.dateTimeButton = dateTimeButtonLocator;
     this.relative30SecondsButton = page.locator(relative30SecondsButtonLocator);
@@ -23,21 +26,61 @@ export class DashboardPage {
   }
 
   async navigateToDashboards() {
+    await this.page.waitForSelector('[data-test="menu-link-\\/dashboards-item"]');
     await this.dashboardsMenuItem.click();
     await this.page.waitForTimeout(5000);
   }
 
-  async addDashboard(dashboardName) {
+  async createDashboard() {
+    await this.page.waitForSelector('[data-test="dashboard-add"]');
     await this.addDashboardButton.click();
     await this.page.waitForTimeout(5000);
-    await expect(this.page.locator(this.dashboardNameInput)).toBeVisible();
-    await this.page.locator(this.dashboardNameInput).fill(dashboardName);
-    await expect(this.page.locator(this.dashboardSubmitButton)).toBeVisible();
+    
+    await this.dashboardNameInput.fill(this.dashboardName);
+    
+    await this.page.waitForSelector('[data-test="dashboard-add-submit"]');
+    await this.dashboardSubmitButton.click();
+    await this.page.waitForTimeout(2000);
+    await this.page
+      .locator('[data-test="dashboard-if-no-panel-add-panel-btn"]')
+      .click();
+    await this.page.waitForTimeout(3000);
+    
+    await expect(this.page.getByText("Dashboard added successfully.")).toBeVisible({
+      timeout: 3000,
+    });
 
-    await this.page.locator(this.dashboardSubmitButton).click();
+    await this.page.locator('label').filter({ hasText: 'Streamarrow_drop_down' }).locator('i').click();
+    await this.page.getByRole('option', { name: 'default', exact: true }).locator('div').nth(2).click();
+    await this.page.locator('div').filter({ hasText: /^drag_indicator$/ }).nth(1).click();
+    await this.page.waitForSelector('[data-test="dashboard-panel-name"]');
+    await this.page.locator('[data-test="dashboard-panel-name"]').click();
+    await this.page.locator('[data-test="dashboard-panel-name"]').fill('AutoP');
+    await this.page.locator('[data-test="dashboard-panel-name"]').press('Enter');
+    await this.page.locator('[data-test="field-list-item-logs-default-k8s_app_instance"] [data-test="dashboard-add-y-data"]').click();
+    await this.page.waitForSelector('[data-test="dashboard-apply"]');
+    await this.page.locator('[data-test="dashboard-apply"]').click();
     await this.page.waitForTimeout(5000);
+
   }
 
+  async deleteDashboard() {
+
+    //await this.page.getByRole("cell", { name: this.dashboardName }).click();
+    await this.page.reload();
+    await this.page.waitForTimeout(2000);
+    //await this.page.locator('[data-test="dashboard-delete"]').nth(1).click()
+    await this.page.locator("//td[contains(text(),'" + this.dashboardName + "')]/following-sibling::td[@class='q-td text-center']/child::button[@data-test='dashboard-delete']").click({ force: true });
+   
+    //await this.page.locator('[data-test="confirm-button"]').click()
+    await this.page.waitForTimeout(2000);
+    await this.page.locator('[data-test="confirm-button"]:visible').click();
+    await expect(this.page.getByRole('alert')).toContainText('Dashboard deleted successfully.');
+    
+
+  }
+
+  
   async setTimeToPast30Seconds() {
     // Set the time filter to the last 30 seconds
     await this.page.locator(this.dateTimeButton).click();
--- tests/ui-testing/pages/loginPage.js
+++ tests/ui-testing/pages/loginPage.js
@@ -6,17 +6,22 @@ export class LoginPage {
     this.page = page;
     this.userIdInput = page.locator('[data-test="login-user-id"]');
     this.passwordInput = page.locator('[data-test="login-password"]');
-    this.loginButton = this.page.getByRole('button', { name: 'Login' });
+    //this.loginButton = this.page.getByRole('button', { name: 'Login' });
+    this.loginButton = page.locator('[data-cy="login-sign-in"]');
   }
 
   async gotoLoginPage() {
     await this.page.goto(process.env["ZO_BASE_URL"]);
   }
 
-  async login(username, password) {
+  async login() {
     // Fill in user credentials and login
-    await this.userIdInput.fill(username);
-    await this.passwordInput.fill(password);
+    await this.page.waitForSelector('[data-test="login-user-id"]');
+    await this.userIdInput.fill(process.env["ZO_ROOT_USER_EMAIL"]);
+    await this.page.waitForSelector('[data-test="login-password"]');
+    await this.passwordInput.fill(process.env["ZO_ROOT_USER_PASSWORD"]);
+    await this.page.waitForSelector('[data-cy="login-sign-in"]');
     await this.loginButton.click();
+    
   }
 }
