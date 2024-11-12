bd459628bbe07b2813bf1be56664c2538d7eb4bd
ktx-abhay <abhay.padamani@kiara.tech>
2024-11-12 15:03:53 +0530

    add: raw query support

---
+++ web/src/components/dashboards/addPanel/dynamicFunction/RawQueryBuilder.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="row">RAW QUERY</div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+//   import useDashboardPanelData from "@/composables/useDashboardPanel";
+
+export default {
+  name: "RawQueryBuilder",
+  setup() {
+    //   const dashboardPanelDataPageKey = inject(
+    //     "dashboardPanelDataPageKey",
+    //     "dashboard",
+    //   );
+    //   const { dashboardPanelData } = useDashboardPanelData(
+    //     dashboardPanelDataPageKey
+    //   );
+
+    const fields = ref({
+      functionName: "histogram",
+      args: [
+        {
+          type: "field",
+          fieldName: "_timestamp",
+        },
+        {
+          type: "histogramInterval",
+          value: "5 min",
+        },
+      ],
+    });
+
+    return {
+      fields,
+    };
+  },
+};
+</script>
+<style scoped></style>
