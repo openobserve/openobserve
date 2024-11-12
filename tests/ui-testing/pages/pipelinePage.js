1a37c0922677a3696f2295078b828e20bfd2eafa
Neha P <neha@openobserve.ai>
2024-11-12 16:38:50 +0530

    updated tests

--- tests/ui-testing/pages/pipelinePage.js
+++ tests/ui-testing/pages/pipelinePage.js
@@ -1,3 +1,4 @@
+const { expect } = require('@playwright/test')
 class PipelinePage {
   constructor(page) {
     this.page = page;
@@ -9,6 +10,7 @@ class PipelinePage {
       '[data-test="pipeline-list-add-pipeline-btn"]'
     );
     this.streamButton = page.getByRole("button", { name: "Stream" }).first();
+    this.queryButton = page.getByRole("button", { name: "Query" });
     this.vueFlowPane = page.locator(".vue-flow__pane");
     this.logsDropdown = page.locator("div").filter({ hasText: /^logs$/ });
     this.logsOption = page
@@ -36,7 +38,35 @@ class PipelinePage {
     this.deleteButton = page.locator("button").filter({ hasText: "delete" });
     this.confirmDeleteButton = page.locator('[data-test="confirm-button"]');
     this.secondStreamButton = page.getByRole('button', { name: 'Stream' }).nth(1);
+    this.functionButton =  page.getByRole('button', { name: 'Function' })
+    this.conditionButton =  page.getByRole('button', { name: 'Condition' })
    this.selectPreviousNodeDropdown = page.getByLabel('Select Previous Node');
+   this.previousNodeDropdown = page.locator('[data-test="previous-node-dropdown-input-stream-node-option"]');
+   this.previousNodeDropdownSecond = page.locator('[data-test="previous-node-dropdown-input-stream-node-option"]:last-child');
+    this.createStreamToggle = page.locator('[data-test="create-stream-toggle"] div').nth(2);
+    this.saveStreamButton = page.locator('[data-test="save-stream-btn"]');
+    this.inputNodeStreamSaveButton = page.locator('[data-test="input-node-stream-save-btn"]')
+    this.pipelineSearchInput = page.locator('[data-test="pipeline-list-search-input"]');
+    this.confirmDeleteButton = page.locator('[data-test="confirm-button"]');
+    this.deletionSuccessMessage = page.getByText('Pipeline deleted successfully')
+    this.sqlEditor = page.locator('[data-test="scheduled-pipeline-sql-editor"]');
+    this.sqlQueryInput = page.locator('.view-lines');
+    this.frequencyUnit = page.locator('[data-test="scheduled-pipeline-frequency-unit"]');
+    this.saveQueryButton = page.locator('[data-test="stream-routing-query-save-btn"]');
+    this.createFunctionToggle = page.locator('[data-test="create-function-toggle"] div').nth(2);
+    this.functionNameLabel = page.locator('[data-test="add-function-node-routing-section"]').getByLabel('Name');
+    this.associateFunctionSaveButton = page.locator('[data-test="associate-function-save-btn"]');
+    this.functionNameRequiredError = page.getByText('Function Name is required')
+    this.functionRequiredError = page.getByText('Function is required')
+    this.streamSelectionError = page.getByText('Please select Stream from the')
+    this.columnInput = page.getByPlaceholder('Column'); // Locator for the "Column" input field
+    this.columnOption = page.getByRole('option', { name: 'kubernetes_container_name' })
+    this.fieldRequiredError = page.getByText('Field is required!')
+
+this.functionNameInput = page.locator('[data-test="add-function-node-routing-section"] input[aria-label="Name"]');
+this.addConditionSaveButton = page.locator('[data-test="add-condition-save-btn"]');
+
+
 
   }
 
@@ -53,10 +83,11 @@ class PipelinePage {
     await this.streamButton.click();
   }
 
-  async dragStreamToTarget(streamElement) {
+  async dragStreamToTarget(streamElement, offset = { x: 0, y: 0 }) {
     const targetBox = await this.vueFlowPane.boundingBox();
     const streamBox = await streamElement.boundingBox();
-  
+    targetBox.x += offset.x;
+  targetBox.y += offset.y;
     if (streamBox && targetBox) {
       await this.page.mouse.move(streamBox.x + streamBox.width / 2, streamBox.y + streamBox.height / 2);
       await this.page.mouse.down();
@@ -120,11 +151,123 @@ class PipelinePage {
 
   async selectAndDragSecondStream() {
     await this.secondStreamButton.click();
-    await this.dragStreamToTarget(this.secondStreamButton);
+    await this.dragStreamToTarget(this.secondStreamButton,{ x: 80, y: 80 });
   }
+
+
+  async selectAndDragFunction() {
+    await this.secondStreamButton.click();
+    await this.dragStreamToTarget(this.functionButton, { x: 50, y: 50 });
+  }
+
+
   async selectPreviousNode() {
     await this.selectPreviousNodeDropdown.click();
   }
+
+  async selectPreviousNodeDrop() {
+    await this.previousNodeDropdownSecond.click();
+  }
+
+  // Method to toggle the create stream option
+  async toggleCreateStream() {
+    await this.createStreamToggle.click();
+  }
+
+  async clickSaveStream() {
+    await this.saveStreamButton.click();
+  }
+
+  // Method to click the input node stream save button
+  async clickInputNodeStreamSave() {
+    await this.inputNodeStreamSaveButton.click();
+  }
+  async searchPipeline(pipelineName) {
+    await this.pipelineSearchInput.click();
+    await this.pipelineSearchInput.fill(pipelineName);
+  }
+
+  async confirmDeletePipeline() {
+    await this.confirmDeleteButton.click();
+  }
+
+  // Method to verify deletion success message
+  async verifyPipelineDeleted() {
+    await this.deletionSuccessMessage.click();
+  }
+
+
+  async selectStream() {
+    await this.queryButton.click();
+  }
+  async clickSqlEditor() {
+    await this.sqlEditor.click();
+  }
+
+  // Method to type the SQL query
+  async typeSqlQuery(query) {
+    await this.sqlQueryInput.click();
+    await this.page.keyboard.type(query);
+  }
+
+  // Method to select the frequency unit dropdown
+  async selectFrequencyUnit() {
+    await this.frequencyUnit.click();
+  }
+
+  // Method to save the query
+  async saveQuery() {
+    await this.saveQueryButton.click();
+  }
+
+
+  async selectFunction() {
+    await this.functionButton.click();
+  }
+
+  async toggleCreateFunction() {
+    await this.createFunctionToggle.click();
+}
+
+async clickFunctionLable(name) {
+  // Click the label and then fill in the input that appears
+  await this.functionNameLabel.click();
+}
+async saveFunction() {
+  await this.associateFunctionSaveButton.click();
+}
+
+async enterFunctionName(name) {
+  await this.functionNameInput.fill(name);
+}
+
+async assertFunctionNameRequiredErrorVisible() {
+  await expect(this.functionNameRequiredError).toBeVisible();
+}
+
+async assertFunctionRequiredErrorVisible() {
+  await expect(this.functionRequiredError).toBeVisible();
+}
+async assertStreamSelectionErrorVisible() {
+  await expect(this.streamSelectionError).toBeVisible();
+}
+
+async selectAndDragCondition() {
+  await this.dragStreamToTarget(this.conditionButton, { x: 50, y: 50 });
+}
+async fillColumnAndSelectOption(columnName) {
+  await this.columnInput.click();
+  await this.columnInput.fill(columnName);
+  await this.columnOption.click();
+}
+async saveCondition() {
+  await this.addConditionSaveButton.click();
+}
+async verifyFieldRequiredError() {
+  await this.fieldRequiredError.waitFor({ state: 'visible' });
+  await this.fieldRequiredError.click();
+}
+
 }
 
 export default PipelinePage;
