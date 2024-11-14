1e7f268c16a32d375dc4d6707fcf77c1142a614a
oasisk <ashish.j.kolhe@gmail.com>
2024-11-14 17:05:02 +0530

    only search Interactive nodes

--- src/service/search/cluster/cache_multi.rs
+++ src/service/search/cluster/cache_multi.rs
@@ -37,7 +37,11 @@ pub async fn get_cached_results(
     cache_req: CacheQueryRequest,
 ) -> Vec<CachedQueryResponse> {
     // get nodes from cluster
-    let mut nodes = match infra_cluster::get_cached_online_query_nodes(None).await {
+    let mut nodes = match infra_cluster::get_cached_online_query_nodes(Some(
+        config::meta::cluster::RoleGroup::Interactive,
+    ))
+    .await
+    {
         Some(nodes) => nodes,
         None => {
             log::error!("[trace_id {trace_id}] get_cached_results: no querier node online");
