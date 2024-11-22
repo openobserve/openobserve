d990514de4e1b8f294cb5e315d2b20575be6eee5
Patrick Sullivan <patrick.sullivan@hey.com>
2024-11-22 15:22:32 -0500

    ...

--- src/config/src/meta/dashboards/mod.rs
+++ src/config/src/meta/dashboards/mod.rs
@@ -13,8 +13,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::hash::Hash;
-
 use chrono::{DateTime, FixedOffset, Utc};
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
