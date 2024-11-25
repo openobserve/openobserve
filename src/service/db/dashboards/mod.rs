46cce3de5916fd14e035501d7caf787140f6f697
Patrick Sullivan <patrick.sullivan@hey.com>
2024-11-25 10:23:38 -0600

    feat: JSON-indpendent hashing of dashboard models (#5133)
    The previous mechanism for generating hashes for dashboard models
    depended on hashing the JSON representation of the dashboard that was
    stored in the `meta` table.
    
    As part of the migration of dashboards out of the `meta` table and into
    a dedicated `dashboards` table, this ☝️ presented some problems:
    
    Specifically when we want to translate from the Dashboard ORM model to a
    `core::meta::dashboards::Dashboard` model, we need to generate the hash
    at the time of translation since `hash` is a field on
    `core::meta::dashboards::Dashboard`. We can't simply generate a hash
    from the JSON stored in the `dashboards.data` column because this JSON
    no longer contains fields for `title`, `owner`, `description`, and
    `role`. Those fields are now stored in DB columns, and changes to those
    columns in the DB columns would *not* result in changes to the hash on
    the JSON blob.
    
    Therefore, rather than hashing the JSON blob, this PR implements hash
    functions on the Rust types for the dashboards. Therefore changes to
    fields in those Rust structs will result in changes to the hash values,
    but changes to the way in which the data is stored in the DB will not
    affect the hash values.
    
    ⚠️ Note that this PR is changing the hash function on the dashboard
    models, so hashes generated for the same model before and after this
    deployment will not be the same. My understanding is that this shouldn't
    be an issue since hashes are just used as a way of checking on the UI if
    the dashboard model has been updated to avoid overwriting the changes of
    another user. I believe that this change to the dashboard function might
    just have the effect (around the time of deployment) where it would
    appear to the UI that the dashboard has been updated when, in fact, it
    has not.
    
    https://github.com/openobserve/o2-enterprise/actions/runs/11979723563
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new type alias `OrdF64` for enhanced precision in
    floating-point operations.
    - Added the `Hash` trait to multiple dashboard-related structs and
    enums, enabling their use in hash-based collections.
    
    - **Bug Fixes**
    - Improved error handling and assertion clarity in API testing for
    folder and dashboard operations.
    
    - **Chores**
    - Standardized hashing mechanism for dashboard objects in database
    operations.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/service/db/dashboards/mod.rs
+++ src/service/db/dashboards/mod.rs
@@ -13,10 +13,12 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use std::hash::{Hash, Hasher};
+
 use actix_web::web;
 use config::{
     meta::dashboards::{v1, v2, v3, v4, v5, Dashboard, DashboardVersion},
-    utils::{hash::Sum64, json},
+    utils::json,
 };
 
 use crate::service::db;
@@ -31,13 +33,14 @@ pub(crate) async fn get(
 ) -> Result<Dashboard, anyhow::Error> {
     let key = format!("/dashboard/{org_id}/{folder}/{dashboard_id}");
     let bytes = db::get(&key).await?;
-    let dash_str = std::str::from_utf8(&bytes)?;
-    let hash = config::utils::hash::gxhash::new()
-        .sum64(dash_str)
-        .to_string();
     let d_version: DashboardVersion = json::from_slice(&bytes)?;
+    let mut hasher = std::hash::DefaultHasher::new();
+    hasher.write_i32(d_version.version);
+
     if d_version.version == 1 {
         let dash: v1::Dashboard = json::from_slice(&bytes)?;
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
         Ok(Dashboard {
             v1: Some(dash),
             version: 1,
@@ -46,6 +49,8 @@ pub(crate) async fn get(
         })
     } else if d_version.version == 2 {
         let dash: v2::Dashboard = json::from_slice(&bytes)?;
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
         Ok(Dashboard {
             v2: Some(dash),
             version: 2,
@@ -54,6 +59,8 @@ pub(crate) async fn get(
         })
     } else if d_version.version == 3 {
         let dash: v3::Dashboard = json::from_slice(&bytes)?;
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
         Ok(Dashboard {
             v3: Some(dash),
             version: 3,
@@ -62,6 +69,8 @@ pub(crate) async fn get(
         })
     } else if d_version.version == 4 {
         let dash: v4::Dashboard = json::from_slice(&bytes)?;
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
         Ok(Dashboard {
             v4: Some(dash),
             version: 4,
@@ -70,6 +79,8 @@ pub(crate) async fn get(
         })
     } else {
         let dash: v5::Dashboard = json::from_slice(&bytes)?;
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
         Ok(Dashboard {
             v5: Some(dash),
             version: 5,
@@ -88,21 +99,25 @@ pub(crate) async fn put(
     hash: Option<&str>,
 ) -> Result<Dashboard, anyhow::Error> {
     let key = format!("/dashboard/{org_id}/{folder}/{}", dashboard_id);
-    if let Ok(existing_dash_bytes) = db::get(&key).await {
-        let existing_dash_str = std::str::from_utf8(&existing_dash_bytes)?;
-        let existing_dash_hash = config::utils::hash::gxhash::new().sum64(existing_dash_str);
+    if let Ok(existing_dash) = get(org_id, dashboard_id, folder).await {
+        let existing_dash_hash = existing_dash.hash;
+
         let Some(Ok(hash_val)) = hash.map(|hash_str| hash_str.parse::<u64>()) else {
             return Err(anyhow::anyhow!(
                 "Request to update existing dashboard with missing or invalid hash value. BUG"
             ));
         };
-        if hash_val != existing_dash_hash {
+        if hash_val.to_string() != existing_dash_hash {
             return Err(anyhow::anyhow!(
                 "Conflict: Failed to save due to concurrent changes. Please refresh the page after backing up your work to avoid losing changes."
             ));
         }
     };
+
     let d_version: DashboardVersion = json::from_slice(&body)?;
+    let mut hasher = std::hash::DefaultHasher::new();
+    hasher.write_i32(d_version.version);
+
     if d_version.version == 1 {
         let mut dash: v1::Dashboard = json::from_slice(&body)?;
         dash.title = dash.title.trim().to_string();
@@ -111,10 +126,8 @@ pub(crate) async fn put(
         };
         dash.dashboard_id = dashboard_id.to_string();
         let value: bytes::Bytes = json::to_vec(&dash)?.into();
-        let dash_str = std::str::from_utf8(&value)?;
-        let hash = config::utils::hash::gxhash::new()
-            .sum64(dash_str)
-            .to_string();
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
 
         match db::put(&key, value, db::NO_NEED_WATCH, None).await {
             Ok(_) => Ok(Dashboard {
@@ -133,10 +146,8 @@ pub(crate) async fn put(
         };
         dash.dashboard_id = dashboard_id.to_string();
         let value: bytes::Bytes = json::to_vec(&dash)?.into();
-        let dash_str = std::str::from_utf8(&value)?;
-        let hash = config::utils::hash::gxhash::new()
-            .sum64(dash_str)
-            .to_string();
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
         match db::put(&key, value, db::NO_NEED_WATCH, None).await {
             Ok(_) => Ok(Dashboard {
                 v2: Some(dash),
@@ -154,10 +165,8 @@ pub(crate) async fn put(
         };
         dash.dashboard_id = dashboard_id.to_string();
         let value: bytes::Bytes = json::to_vec(&dash)?.into();
-        let dash_str = std::str::from_utf8(&value)?;
-        let hash = config::utils::hash::gxhash::new()
-            .sum64(dash_str)
-            .to_string();
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
         match db::put(&key, value, db::NO_NEED_WATCH, None).await {
             Ok(_) => Ok(Dashboard {
                 v3: Some(dash),
@@ -175,10 +184,8 @@ pub(crate) async fn put(
         };
         dash.dashboard_id = dashboard_id.to_string();
         let value: bytes::Bytes = json::to_vec(&dash)?.into();
-        let dash_str = std::str::from_utf8(&value)?;
-        let hash = config::utils::hash::gxhash::new()
-            .sum64(dash_str)
-            .to_string();
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
         match db::put(&key, value, db::NO_NEED_WATCH, None).await {
             Ok(_) => Ok(Dashboard {
                 v4: Some(dash),
@@ -196,10 +203,8 @@ pub(crate) async fn put(
         };
         dash.dashboard_id = dashboard_id.to_string();
         let value: bytes::Bytes = json::to_vec(&dash)?.into();
-        let dash_str = std::str::from_utf8(&value)?;
-        let hash = config::utils::hash::gxhash::new()
-            .sum64(dash_str)
-            .to_string();
+        dash.hash(&mut hasher);
+        let hash = hasher.finish().to_string();
         match db::put(&key, value, db::NO_NEED_WATCH, None).await {
             Ok(_) => Ok(Dashboard {
                 v5: Some(dash),
@@ -219,13 +224,14 @@ pub(crate) async fn list(org_id: &str, folder: &str) -> Result<Vec<Dashboard>, a
         .await?
         .into_values()
         .map(|val| {
-            let dash_str = std::str::from_utf8(&val)?;
-            let hash = config::utils::hash::gxhash::new()
-                .sum64(dash_str)
-                .to_string();
             let d_version: DashboardVersion = json::from_slice(&val).unwrap();
+            let mut hasher = std::hash::DefaultHasher::new();
+            hasher.write_i32(d_version.version);
+
             if d_version.version == 1 {
                 let dash: v1::Dashboard = json::from_slice(&val).unwrap();
+                dash.hash(&mut hasher);
+                let hash = hasher.finish().to_string();
                 Ok(Dashboard {
                     v1: Some(dash),
                     version: 1,
@@ -234,6 +240,8 @@ pub(crate) async fn list(org_id: &str, folder: &str) -> Result<Vec<Dashboard>, a
                 })
             } else if d_version.version == 2 {
                 let dash: v2::Dashboard = json::from_slice(&val).unwrap();
+                dash.hash(&mut hasher);
+                let hash = hasher.finish().to_string();
                 Ok(Dashboard {
                     v2: Some(dash),
                     version: 2,
@@ -242,6 +250,8 @@ pub(crate) async fn list(org_id: &str, folder: &str) -> Result<Vec<Dashboard>, a
                 })
             } else if d_version.version == 3 {
                 let dash: v3::Dashboard = json::from_slice(&val).unwrap();
+                dash.hash(&mut hasher);
+                let hash = hasher.finish().to_string();
                 Ok(Dashboard {
                     v3: Some(dash),
                     version: 3,
@@ -250,6 +260,8 @@ pub(crate) async fn list(org_id: &str, folder: &str) -> Result<Vec<Dashboard>, a
                 })
             } else if d_version.version == 4 {
                 let dash: v4::Dashboard = json::from_slice(&val).unwrap();
+                dash.hash(&mut hasher);
+                let hash = hasher.finish().to_string();
                 Ok(Dashboard {
                     v4: Some(dash),
                     version: 4,
@@ -258,6 +270,8 @@ pub(crate) async fn list(org_id: &str, folder: &str) -> Result<Vec<Dashboard>, a
                 })
             } else {
                 let dash: v5::Dashboard = json::from_slice(&val).unwrap();
+                dash.hash(&mut hasher);
+                let hash = hasher.finish().to_string();
                 Ok(Dashboard {
                     v5: Some(dash),
                     version: 5,
