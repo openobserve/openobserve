8de66df89b05d3426af50922cac9023f878a6ccc
Patrick Sullivan <patrick.sullivan@hey.com>
2024-11-25 14:09:57 -0500

    deletion migration in transaction

--- src/infra/src/table/migration/m20241119_000003_delete_metas.rs
+++ src/infra/src/table/migration/m20241119_000003_delete_metas.rs
@@ -1,3 +1,4 @@
+use sea_orm::{EntityTrait, TransactionTrait};
 use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
@@ -6,7 +7,10 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager.exec_stmt(delete_meta_dashboards_stmnt()).await?;
+        let db = manager.get_connection();
+        let txn = db.begin().await?;
+        dashboards::Entity::delete_many().exec(&txn).await?;
+        txn.commit().await?;
         Ok(())
     }
 
@@ -16,48 +20,35 @@ impl MigrationTrait for Migration {
     }
 }
 
-/// Statement to delete dashboard records from the meta table.
-fn delete_meta_dashboards_stmnt() -> DeleteStatement {
-    Query::delete()
-        .from_table(Meta::Table)
-        .and_where(Expr::col(Meta::Module).eq("dashboard"))
-        .to_owned()
-}
+// The schemas of tables might change after subsequent migrations. Therefore
+// this migration only references ORM models in private submodules that should
+// remain unchanged rather than ORM models in the `entity` module that will be
+// updated to reflect the latest changes to table schemas.
 
-/// Identifiers used in queries on the meta table.
-#[derive(DeriveIden)]
-enum Meta {
-    Table,
-    Module,
-}
+/// Representation of the dashboards table at the time this migration executes.
+mod dashboards {
+    use sea_orm::entity::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use collapse::*;
-
-    use super::*;
-
-    #[test]
-    fn postgres() {
-        collapsed_eq!(
-            &delete_meta_dashboards_stmnt().to_string(PostgresQueryBuilder),
-            r#"DELETE FROM "meta" WHERE "module" = 'dashboard'"#
-        );
+    #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+    #[sea_orm(table_name = "dashboards")]
+    pub struct Model {
+        #[sea_orm(primary_key)]
+        pub id: i64,
+        pub dashboard_id: String,
+        pub folder_id: i64,
+        pub owner: String,
+        pub role: Option<String>,
+        pub title: String,
+        #[sea_orm(column_type = "Text", nullable)]
+        pub description: Option<String>,
+        pub data: Json,
+        pub version: i32,
+        pub created_at: i64,
     }
 
-    #[test]
-    fn mysql() {
-        collapsed_eq!(
-            &delete_meta_dashboards_stmnt().to_string(MysqlQueryBuilder),
-            r#"DELETE FROM `meta` WHERE `module` = 'dashboard'"#
-        );
-    }
+    // There are relations but they are not important to this migration.
+    #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+    pub enum Relation {}
 
-    #[test]
-    fn sqlite() {
-        assert_eq!(
-            &delete_meta_dashboards_stmnt().to_string(SqliteQueryBuilder),
-            r#"DELETE FROM "meta" WHERE "module" = 'dashboard'"#
-        );
-    }
+    impl ActiveModelBehavior for ActiveModel {}
 }
