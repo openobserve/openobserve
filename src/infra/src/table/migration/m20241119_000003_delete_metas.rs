946433ce34dd9aeff602e905f838418430a23f50
Patrick Sullivan <patrick.sullivan@hey.com>
2024-11-25 14:33:53 -0500

    fix

--- src/infra/src/table/migration/m20241119_000003_delete_metas.rs
+++ src/infra/src/table/migration/m20241119_000003_delete_metas.rs
@@ -1,4 +1,21 @@
-use sea_orm::{EntityTrait, TransactionTrait};
+// Copyright 2024 OpenObserve Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Deletes dashboard records from the meta table.
+
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, TransactionTrait};
 use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
@@ -8,11 +25,11 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let txn = manager.get_connection().begin().await?;
-        dashboards::Entity::delete_many().exec(&txn).await?;
+        meta::Entity::delete_many().exec(&txn).await?;
 
         meta::Entity::delete_many()
             .filter(meta::Column::Module.eq("dashboard"))
-            .exec(txn)
+            .exec(&txn)
             .await?;
 
         txn.commit().await?;
