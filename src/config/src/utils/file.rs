fc184a7416b641eb93a528078ba59fe87097a42d
hengfei yang <hengfei.yang@gmail.com>
2024-12-02 22:02:02 +0800

    fix: unit test

--- src/config/src/utils/file.rs
+++ src/config/src/utils/file.rs
@@ -176,12 +176,19 @@ mod tests {
     fn test_scan_files() {
         let content = b"Some Text";
         let file_name = "sample.parquet";
+        let dir_name = "./scan_dir/";
+        std::fs::create_dir(dir_name).unwrap();
 
-        put_file_contents(file_name, content).unwrap();
-        assert_eq!(get_file_contents(file_name, None).unwrap(), content);
-        assert!(get_file_meta(file_name).unwrap().is_file());
-        assert!(!scan_files("./", "parquet", None).unwrap().is_empty());
-        std::fs::remove_file(file_name).unwrap();
+        let file_name = dir_name.to_string() + file_name;
+        put_file_contents(&file_name, content).unwrap();
+        assert_eq!(get_file_contents(&file_name, None).unwrap(), content);
+        assert!(get_file_meta(&file_name).unwrap().is_file());
+        assert!(!scan_files("./scan_dir/", "parquet", None)
+            .unwrap()
+            .is_empty());
+
+        std::fs::remove_file(&file_name).unwrap();
+        std::fs::remove_dir(&dir_name).unwrap();
     }
 
     #[test]
