f94f493be5335a96adbc15f7ab5c9911cbb73c96
Patrick Sullivan <patrick.sullivan@hey.com>
2024-12-02 19:25:44 -0500

    fix typo

--- src/handler/http/models/mod.rs
+++ src/handler/http/models/mod.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-//! This module contains models that can be serialialized and deserialized as
-//! JSON for HTTP responses and requests.
+//! This module contains models that can be serialized and deserialized as JSON
+//! for HTTP responses and requests.
 
 pub mod folders;
