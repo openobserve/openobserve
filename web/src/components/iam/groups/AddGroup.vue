f7f2553bede8be82ec90aa5758e0b942f825bd42
Sai Nikhil <nikhil@openobserve.ai>
2024-12-12 15:30:54 +0530

    fix: minor fixes

--- web/src/components/iam/groups/AddGroup.vue
+++ web/src/components/iam/groups/AddGroup.vue
@@ -139,12 +139,14 @@ const saveGroup = () => {
       });
     })
     .catch((err) => {
-      q.notify({
+      if(err.response.status != 403){
+        q.notify({
         message: "Error while creating group",
         color: "negative",
         position: "bottom",
         timeout: 3000,
       });
+      }
       console.log(err);
     });
 };
