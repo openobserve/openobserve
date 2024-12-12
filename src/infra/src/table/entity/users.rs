4880a1feb6d99cf47c0390fe595cafeb91aba2b3
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2024-12-12 12:55:09 +0530

    fix: org_users and users table primary key auto increment = false

--- src/infra/src/table/entity/users.rs
+++ src/infra/src/table/entity/users.rs
@@ -5,7 +5,7 @@ use sea_orm::entity::prelude::*;
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
 #[sea_orm(table_name = "users")]
 pub struct Model {
-    #[sea_orm(primary_key)]
+    #[sea_orm(primary_key, auto_increment = false)]
     pub id: String,
     #[sea_orm(unique)]
     pub email: String,
