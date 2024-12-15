dab7fa3330ddbd4fb0ebbecd55938aa485432db2
Taiming Liu <liutaiming3@gmail.com>
2024-12-15 15:04:50 -0800

    migration: general sea-orm entity

--- src/infra/src/table/entity/templates.rs
+++ src/infra/src/table/entity/templates.rs
@@ -18,6 +18,15 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(has_many = "super::destinations::Entity")]
+    Destinations,
+}
+
+impl Related<super::destinations::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Destinations.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
