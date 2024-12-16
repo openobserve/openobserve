6ee5a54d0b2fa71c28fcba9f0b5265605ada6e9e
Bhargav <BJP232004@GMAIL.COM>
2024-12-16 16:15:48 +0530

    fix: values api for single stream selection filter context (#5372)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced log display functionality with improved query context
    handling for multiple streams.
    - Improved error handling for filter validity when selecting multiple
    streams.
    	- Adjusted pagination logic for accurate total fields display.
    - Added new buttons and toggles in the search bar for better control
    over query results.
    - Introduced confirmation dialog for switching between visualization and
    log modes.
    
    - **Bug Fixes**
    - Refined logic for updating field values to ensure correct aggregation
    and display.
    	- Enhanced error handling for download attempts in the search bar.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- web/src/plugins/logs/SearchBar.vue
+++ web/src/plugins/logs/SearchBar.vue
@@ -1260,6 +1260,7 @@ export default defineComponent({
     };
 
     const updateQueryValue = (value: string) => {
+      searchObj.data.editorValue = value;
       if (searchObj.meta.quickMode === true) {
         const parsedSQL = fnParsedSQL();
         if (
