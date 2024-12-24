e1f45a3f2f443198e29dfb033c03491644d1036b
Yashodhan Joshi <yjdoc2@gmail.com>
2024-12-24 15:43:04 +0530

    feat: add cipher keys table and interface for rest of the code
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- src/job/cipher.rs
+++ src/job/cipher.rs
@@ -13,17 +13,48 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use config::get_config;
+use infra::table::cipher::{self, ListFilter};
+use o2_enterprise::enterprise::cipher::CipherData;
 
 use crate::cipher::registry::REGISTRY;
 
 pub async fn run() -> Result<(), anyhow::Error> {
-    // TODO impl
-    let config = get_config();
-    let key = crate::cipher::cipher::Key::new("".to_string());
+    log::info!("retrieving and caching cipher keys in memory");
+    let filter = ListFilter {
+        org: None,
+        kind: Some(cipher::EntryKind::CipherKey),
+    };
+
+    let data_list = cipher::list_filtered(filter, None).await?;
+
+    let data_list: Result<Vec<_>, serde_json::Error> = data_list
+        .into_iter()
+        .map(|e| {
+            let cdata: Result<CipherData, _> = serde_json::from_str(&e.data);
+            cdata.map(|d| (format!("{}:{}", e.org, e.name), d))
+        })
+        .collect();
+    let data_list = data_list?;
+
+    let mut keys = Vec::with_capacity(data_list.len());
+
+    for (name, data) in data_list {
+        let key = match data.get_key().await {
+            Ok(k) => k,
+            Err(e) => {
+                // we continue here, so the successful keys can be used
+                log::warn!("error in retrieving key {name}  : {e}");
+                continue;
+            }
+        };
+        keys.push((name, key));
+    }
     {
         let mut reg = REGISTRY.write();
-        reg.add_key("test_key".to_string(), Box::new(key));
+        for (name, key) in keys {
+            reg.add_key(name, Box::new(key));
+        }
     }
+    log::info!("successfully cached cipher keys in memory");
     Ok(())
 }
