487320621c8419b80b0abc3c3011a225ae5b6ce0
Yashodhan Joshi <yjdoc2@gmail.com>
2024-12-24 11:35:57 +0530

    fix: add cfg enterprise
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- src/cipher/providers/local.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Hash, Clone, Debug, Serialize, Deserialize)]
-pub enum Algorithm {}
-
-pub struct Local {
-    pub algorithm: Algorithm,
-    pub key: String,
-}
