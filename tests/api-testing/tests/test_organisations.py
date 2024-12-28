faaea8f34e2d35cb62cb78a055190191882202f7
ShyamOOAI <shyam@openobserve.ai>
2024-12-28 15:39:44 +0530

    Updated as per organisations requirements
    Updated as per organisations requirements

--- tests/api-testing/tests/test_organisations.py
+++ tests/api-testing/tests/test_organisations.py
@@ -1,5 +1,5 @@
 def test_e2e_organisations(create_session, base_url):
-    """Running an E2E test for get all the alerts list."""
+    """Running an E2E test for get all the organisations list."""
 
     session = create_session
     url = base_url
@@ -10,11 +10,11 @@ def test_e2e_organisations(create_session, base_url):
     print(resp_get_allorgs.content)
     assert (
         resp_get_allorgs.status_code == 200
-    ), f"Get all alerts list 200, but got {resp_get_allorgs.status_code} {resp_get_allorgs.content}"
+    ), f"Get all organisations list 200, but got {resp_get_allorgs.status_code} {resp_get_allorgs.content}"
 
 
 def test_e2e_org_settings(create_session, base_url):
-    """Running an E2E test for get all the alerts list."""
+    """Running an E2E test for get all the organisations settings list."""
 
     session = create_session
     url = base_url
@@ -25,11 +25,11 @@ def test_e2e_org_settings(create_session, base_url):
     print(resp_get_orgsettings.content)
     assert (
         resp_get_orgsettings.status_code == 200
-    ), f"Get all alerts list 200, but got {resp_get_orgsettings.status_code} {resp_get_orgsettings.content}"
+    ), f"Get all organisations settings list 200, but got {resp_get_orgsettings.status_code} {resp_get_orgsettings.content}"
 
 
 def test_e2e_org_summary(create_session, base_url):
-    """Running an E2E test for get all the alerts list."""
+    """Running an E2E test for get all the organisations summary list."""
 
     session = create_session
     url = base_url
@@ -40,11 +40,11 @@ def test_e2e_org_summary(create_session, base_url):
     print(resp_get_orgsummary.content)
     assert (
         resp_get_orgsummary.status_code == 200
-    ), f"Get all alerts list 200, but got {resp_get_orgsummary.status_code} {resp_get_orgsummary.content}"
+    ), f"Get all organisations summary list 200, but got {resp_get_orgsummary.status_code} {resp_get_orgsummary.content}"
 
 
 def test_e2e_passcode(create_session, base_url):
-    """Running an E2E test for get all the alerts list."""
+    """Running an E2E test for get all the passcode list."""
 
     session = create_session
     url = base_url
