1cd757149e9e2d6bc298ed056df2d2c2d6ab0cdb
Bhargav <BJP232004@GMAIL.COM>
2024-12-30 11:36:19 +0530

    feat: UI changes commit

--- web/src/components/cipherkeys/AddCipherKey.vue
+++ web/src/components/cipherkeys/AddCipherKey.vue
@@ -66,7 +66,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                 !!val
                   ? isValidResourceName(val) ||
                     `Characters like :, ?, /, #, and spaces are not allowed.`
-                  : t('common.nameRequired'),
+                  : 'Name is required',
             ]"
             tabindex="0"
           />
@@ -74,7 +74,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         <div class="col-4 o2-input flex q-mx-md q-mt-md">
           <q-select
             data-test="add-cipher-key-type-input"
-            v-model="formData.type"
+            v-model="formData.key.store.type"
             :label="t('cipherKey.type') + ' *'"
             color="input-border"
             bg-color="input-bg"
@@ -84,10 +84,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             filled
             dense
             :options="cipherKeyTypes"
-            :option-value="'value'"
-            :option-label="'label'"
+            option-value="value"
+            option-label="label"
+            map-options
             emit-value
-            :rules="[(val: any) => !!val || t('common.typeRequired')]"
+            :rules="[(val: any) => !!val || 'Type is required']"
             tabindex="0"
           />
         </div>
@@ -107,19 +108,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           :title="
             t('cipherKey.step1') +
             ' (Type: ' +
-            getTypeLabel(formData.type) +
+            getTypeLabel(formData.key.store.type) +
             ')'
           "
           icon="addition"
           :done="step > 1"
         >
           <div>
-            <add-local-type
-              v-if="formData.type == 'local'"
+            <add-openobserve-type
+              v-if="formData.key.store.type == 'openobserve'"
               v-model:formData="formData"
             />
             <add-akeyless-type
-              v-else-if="formData.type == 'akeyless'"
+              v-else-if="formData.key.store.type == 'akeyless'"
               v-model:formData="formData"
             />
           </div>
@@ -141,41 +142,115 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           icon="addition"
           :done="step > 2"
         >
-          <div>Dropdown for encryption mechanism</div>
+          <add-encryption-mechanism v-model:formData="formData" />
           <q-stepper-navigation>
             <q-btn
-              data-test="add-report-step1-continue-btn"
-              @click="step = 2"
-              color="secondary"
-              label="Continue"
+              data-test="add-cipher-key-step2-back-btn"
+              flat
+              @click="step = 1"
+              color="primary"
+              :label="t('common.back')"
+              class="q-ml-sm"
               no-caps
             />
           </q-stepper-navigation>
         </q-step>
       </q-stepper>
+      <div class="flex justify-start q-px-md q-py-sm full-width">
+        <q-btn
+          data-test="add-cipher-key-cancel-btn"
+          class="text-bold"
+          :label="t('common.cancel')"
+          text-color="light-text"
+          padding="sm md"
+          no-caps
+          @click="openCancelDialog"
+        />
+        <q-btn
+          data-test="add-cipher-key-save-btn"
+          :label="t('common.save')"
+          class="text-bold no-border q-ml-md"
+          color="secondary"
+          padding="sm xl"
+          no-caps
+          @click="onSubmit"
+        />
+      </div>
     </q-form>
+    <ConfirmDialog
+      v-model="dialog.show"
+      :title="dialog.title"
+      :message="dialog.message"
+      @update:ok="dialog.okCallback"
+      @update:cancel="dialog.show = false"
+    />
   </q-page>
 </template>
 <script lang="ts" setup>
 import { ref, onBeforeMount, onActivated, defineEmits, computed } from "vue";
 import { useI18n } from "vue-i18n";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 import { isValidResourceName } from "@/utils/zincutils";
-import AddLocalType from "@/components/cipherkeys/AddLocalType.vue";
+import AddOpenobserveType from "@/components/cipherkeys/AddOpenobserveType.vue";
 import AddAkeylessType from "@/components/cipherkeys/AddAkeylessType.vue";
+import AddEncryptionMechanism from "@/components/cipherkeys/AddEncryptionMechanism.vue";
+import ConfirmDialog from "@/components/ConfirmDialog.vue";
 
-const emit = defineEmits(["get:templates", "cancel:hideform"]);
+const emit = defineEmits(["cancel:hideform"]);
 const { t } = useI18n();
+const router = useRouter();
+const store = useStore();
 const addCipherKeyFormRef: any = ref();
 const isUpdatingCipherKey: any = ref(false);
 const step = ref(1);
 const formData: any = ref({
   name: "",
-  type: "local",
+  key: {
+    store: {
+      type: "openobserve",
+      akeyless: {
+        base_url: "",
+        access_id: "",
+        auth: {
+          type: "access_key",
+          access_key: "",
+          ldap: {
+            username: "",
+            password: "",
+          },
+        },
+        store: {
+          type: "static_secret",
+          static_secret: "",
+          dfc: {
+            name: "",
+            iv: "",
+            encrypted_data: "",
+          },
+        },
+      },
+      local: "",
+    },
+    mechanism: {
+      type: "simple",
+      simple_algorithm: "aes-256-siv",
+    },
+  },
 });
 const cipherKeyTypes = computed(() => [
-  { label: "Local", value: "local" },
+  { label: "OpenObserve", value: "openobserve" },
   { label: "Akeyless", value: "akeyless" },
 ]);
+
+const originalData: Ref<string> = ref("");
+
+const dialog = ref({
+  show: false,
+  title: "",
+  message: "",
+  okCallback: () => {},
+});
 
 onActivated(() => setupTemplateData());
 onBeforeMount(() => {
@@ -207,5 +282,20 @@ const onSubmit = () => {
   // } else {
   //   addTemplate();
   // }
+};
+
+const openCancelDialog = () => {
+  if (originalData.value === JSON.stringify(formData.value)) {
+    goToCipherList();
+    return;
+  }
+  dialog.value.show = true;
+  dialog.value.title = "Discard Changes";
+  dialog.value.message = "Are you sure you want to cancel changes?";
+  dialog.value.okCallback = goToCipherList;
+};
+
+const goToCipherList = () => {
+  emit("cancel:hideform");
 };
 </script>
