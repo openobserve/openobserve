123985dbab5d57ea08c3624e93bdbc723e1f9d84
Bhargav <BJP232004@GMAIL.COM>
2024-12-30 23:13:54 +0530

    Update prelude.rs

--- src/infra/src/table/entity/prelude.rs
+++ src/infra/src/table/entity/prelude.rs
@@ -6,5 +6,5 @@ pub use super::{
     search_job_results::Entity as SearchJobResults, search_jobs::Entity as SearchJobs,
     search_queue::Entity as SearchQueue,
     alerts::Entity as Alerts, cipher_keys::Entity as CipherKeys, dashboards::Entity as Dashboards,
-    folders::Entity as Folders, search_queue::Entity as SearchQueue,
+    folders::Entity as Folders,
 };
