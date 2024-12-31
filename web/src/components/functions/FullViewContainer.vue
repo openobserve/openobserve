5ca9b718f769e6df79105f1f27685671a360e80a
omkark06 <omkar@zinclabs.io>
2024-12-31 11:20:40 +0530

    fix: fixed dark mode issues

--- web/src/components/functions/FullViewContainer.vue
+++ web/src/components/functions/FullViewContainer.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="tw-bg-gray-200 tw-py-[2px]">
+  <div
+    class="tw-py-[2px]"
+    :class="store.state.theme === 'dark' ? 'tw-bg-gray-500' : 'tw-bg-gray-200 '"
+  >
     <div class="tw-flex tw-justify-between">
       <div class="tw-flex tw-items-center">
         <q-icon
           name="keyboard_arrow_up"
           @click="expanded = !expanded"
-          class="tw-mr-1 tw-cursor-pointer tw-transition-all tw-text-gray-500"
-          :class="expanded ? 'tw-transform tw-rotate-180' : ''"
+          class="tw-mr-1 tw-cursor-pointer tw-transition-all"
+          :class="[
+            store.state.theme === 'dark'
+              ? 'tw-text-gray-100'
+              : 'tw-text-gray-500',
+            expanded ? 'tw-transform tw-rotate-180' : '',
+          ]"
           size="20px"
         />
         <div
-          class="tw-text-gray-500 tw-text-[14px] tw-font-bold"
-          :class="labelClass"
+          class="tw-text-[14px] tw-font-bold"
+          :class="[
+            store.state.theme === 'dark'
+              ? 'tw-text-gray-100'
+              : 'tw-text-gray-500',
+            labelClass,
+          ]"
         >
           {{ label }}
         </div>
@@ -31,6 +44,7 @@
 </template>
 <script setup lang="ts">
 import { defineProps, ref, onMounted, computed, watch, defineEmits } from "vue";
+import { useStore } from "vuex";
 
 const props = defineProps({
   name: {
@@ -56,6 +70,8 @@ const props = defineProps({
 });
 
 const emits = defineEmits(["update:isExpanded"]);
+
+const store = useStore();
 
 const expanded = computed({
   get: () => props.isExpanded,
