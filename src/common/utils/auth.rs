56a4f0617209dff650e4d110ab309d9987b9ad4b
Taiming Liu <liutaiming3@gmail.com>
2025-01-01 19:51:48 -0800

    migrate: zinc_cp users -> o2 users

--- src/common/utils/auth.rs
+++ src/common/utils/auth.rs
@@ -14,9 +14,11 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use actix_web::{dev::Payload, Error, FromRequest, HttpRequest};
-use argon2::{password_hash::SaltString, Algorithm, Argon2, Params, PasswordHasher, Version};
 use base64::Engine;
-use config::{meta::user::UserRole, utils::json};
+use config::{
+    meta::user::UserRole,
+    utils::{hash::get_passcode_hash, json},
+};
 use futures::future::{ready, Ready};
 #[cfg(feature = "enterprise")]
 use o2_enterprise::enterprise::common::infra::config::get_config as get_o2_config;
@@ -79,17 +81,7 @@ pub(crate) fn get_hash(pass: &str, salt: &str) -> String {
     match hash {
         Some(ret_hash) => ret_hash.value().to_string(),
         None => {
-            let t_cost = 4;
-            let m_cost = 2048;
-            let p_cost = 2;
-            let params = Params::new(m_cost, t_cost, p_cost, None).unwrap();
-            let ctx = Argon2::new(Algorithm::Argon2d, Version::V0x10, params);
-            let password = pass.as_bytes();
-            let salt_string = SaltString::encode_b64(salt.as_bytes()).unwrap();
-            let password_hash = ctx
-                .hash_password(password, &salt_string)
-                .unwrap()
-                .to_string();
+            let password_hash = get_passcode_hash(pass, salt);
             PASSWORD_HASH.insert(key, password_hash.clone());
             password_hash
         }
