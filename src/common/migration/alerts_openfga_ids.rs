3116e3035d66bf314e77a9e331aa4d2a73010bb4
Patrick Sullivan <patrick.sullivan@hey.com>
2025-01-03 15:00:13 -0600

    migration

--- src/common/migration/alerts_openfga_ids.rs
+++ src/common/migration/alerts_openfga_ids.rs
@@ -2,29 +2,31 @@ use infra::{
     db::{connect_to_orm, ORM_CLIENT},
     table::entity::alerts,
 };
-use o2_enterprise::enterprise::{
-    common::infra::config::get_config as get_o2_config,
-    openfga::{add_init_ofga_tuples, authorizer::authz::get_ownership_tuple},
-};
+use o2_enterprise::enterprise::common::infra::config::get_config as get_o2_config;
 use sea_orm::{EntityTrait, PaginatorTrait};
+
+use crate::common::{
+    meta::authz::Authz,
+    utils::auth::{remove_ownership, set_ownership},
+};
 
 /// Generates a neww ID in OpenFGA for each alert using the alert's KSUID.
 pub async fn run() -> Result<(), anyhow::Error> {
     if !get_o2_config().openfga.enabled {
         return Ok(());
     }
+
+    o2_enterprise::enterprise::openfga::authorizer::authz::init_open_fga().await;
+    let _ = crate::common::infra::ofga::init().await;
     let conn = ORM_CLIENT.get_or_init(connect_to_orm).await;
 
     let mut pages = alerts::Entity::find().paginate(conn, 100);
     while let Some(alerts) = pages.fetch_and_next().await? {
-        let mut tuples = vec![];
         for a in alerts {
-            get_ownership_tuple(&a.org, "alerts", &a.id, &mut tuples);
+            remove_ownership(&a.org, "alerts", Authz::new(&a.name)).await;
+            set_ownership(&a.org, "alerts", Authz::new(&a.id)).await;
         }
-        add_init_ofga_tuples(tuples).await;
     }
 
-    o2_enterprise::enterprise::openfga::authorizer::authz::init_open_fga().await;
-    let _ = crate::common::infra::ofga::init().await;
     Ok(())
 }
