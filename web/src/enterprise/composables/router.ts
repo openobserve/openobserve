e1b668f3f4826fa6b867ac7e8719116ad6d5d092
Bhargav <BJP232004@GMAIL.COM>
2025-01-04 19:21:36 +0530

    removed billing menus

--- web/src/enterprise/composables/router.ts
+++ web/src/enterprise/composables/router.ts
@@ -21,33 +21,34 @@ import Usage from "@/enterprise/components/billings/usage.vue";
 const useEnvRoutes = () => {
   const parentRoutes: any = [];
 
-  const homeChildRoutes = [
-    {
-      path: "billings",
-      name: "billings",
-      component: Billing,
-      meta: {
-        keepAlive: false,
-      },
-      children: [
-        {
-          path: "usage",
-          name: "usage",
-          component: Usage,
-        },
-        {
-          path: "plans",
-          name: "plans",
-          component: Plans,
-        },
-        {
-          path: "invoice_history",
-          name: "invoice_history",
-          component: InvoiceHistory,
-        },
-      ],
-    },
-  ];
+  const homeChildRoutes = [];
+
+
+  // {
+  //   path: "billings",
+  //   name: "billings",
+  //   component: Billing,
+  //   meta: {
+  //     keepAlive: false,
+  //   },
+  //   children: [
+  //     {
+  //       path: "usage",
+  //       name: "usage",
+  //       component: Usage,
+  //     },
+  //     {
+  //       path: "plans",
+  //       name: "plans",
+  //       component: Plans,
+  //     },
+  //     {
+  //       path: "invoice_history",
+  //       name: "invoice_history",
+  //       component: InvoiceHistory,
+  //     },
+  //   ],
+  // },
 
   return { parentRoutes, homeChildRoutes };
 };
