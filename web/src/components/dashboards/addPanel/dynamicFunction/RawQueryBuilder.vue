4d14942333c5adb21535b283f84000aa437d48ee
ktx-abhay <abhay.padamani@kiara.tech>
2025-01-07 10:52:24 +0530

    add: raw query component

--- web/src/components/dashboards/addPanel/dynamicFunction/RawQueryBuilder.vue
+++ web/src/components/dashboards/addPanel/dynamicFunction/RawQueryBuilder.vue
@@ -1,9 +1,31 @@
 <template>
-  <div class="row">RAW QUERY</div>
+  <div>
+    <div class="q-mb-sm rawQuery">Query</div>
+    <q-input
+      outlined
+      v-model="fields.rawQuery"
+      filled
+      autogrow
+      class="showLabelOnTop"
+    />
+    <q-input
+      v-model="fields.alias"
+      label="Alias"
+      color="input-border"
+      bg-color="input-bg"
+      class="q-py-md showLabelOnTop"
+      stack-label
+      filled
+      dense
+      label-slot
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
+import { useStore } from "vuex";
+
 //   import useDashboardPanelData from "@/composables/useDashboardPanel";
 
 export default {
@@ -17,7 +39,11 @@ export default {
     //     dashboardPanelDataPageKey
     //   );
 
+    const store = useStore();
+
     const fields = ref({
+      rawQuery: "",
+      alias: "",
       functionName: "histogram",
       args: [
         {
@@ -32,9 +58,18 @@ export default {
     });
 
     return {
+      store,
       fields,
     };
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.rawQuery {
+  margin-top: 5px;
+  margin-bottom: 5px;
+  font-size: 14px;
+  font-weight: 600;
+  color: #666666;
+}
+</style>
