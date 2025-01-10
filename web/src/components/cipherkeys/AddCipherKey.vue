332b2090236d4e79b0d43cf4a273f19db216c0c3
Bhargav <BJP232004@GMAIL.COM>
2025-01-10 15:52:32 +0530

    fix: update form

--- web/src/components/cipherkeys/AddCipherKey.vue
+++ web/src/components/cipherkeys/AddCipherKey.vue
@@ -286,7 +286,7 @@ const setupTemplateData = () => {
         name,
       )
         .then((response) => {
-          formData.value = {...response.data, ...formData.value};
+          formData.value = { ...formData.value, ...response.data };
           originalData.value = JSON.stringify(formData.value);
         })
         .catch((error) => {
