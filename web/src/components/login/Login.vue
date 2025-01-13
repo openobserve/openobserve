c4c124247135fafae231212477b339b25d696ead
Bhargav <BJP232004@GMAIL.COM>
2025-01-13 14:55:09 +0530

    fix: added condition for sso enabled

--- web/src/components/login/Login.vue
+++ web/src/components/login/Login.vue
@@ -220,6 +220,7 @@ export default defineComponent({
 
     onBeforeMount(() => {
       if (
+        showSSO.value &&
         router.currentRoute.value.redirectedFrom.query.action == "getstarted"
       ) {
         autoRedirectDexLogin.value = true;
