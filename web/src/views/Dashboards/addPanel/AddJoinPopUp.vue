8c1d3bea868c6b6b78e4592408841a736c795c94
ktx-abhay <abhay.padamani@kiara.tech>
2025-01-16 19:05:13 +0530

    add: initial setup for join

---
+++ web/src/views/Dashboards/addPanel/AddJoinPopUp.vue
@@ -0,0 +1,110 @@
+<!-- Copyright 2023 OpenObserve Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http:www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. 
+-->
+
+<!-- eslint-disable vue/no-unused-components -->
+<template>
+  <div
+    style="padding: 0px 10px; min-width: 60%"
+    class="scroll o2-input"
+    data-test="dashboard-drilldown-popup"
+  >
+    <div
+      class="flex justify-between items-center q-pa-md"
+      style="border-bottom: 2px solid gray; margin-bottom: 5px"
+    >
+      <div class="flex items-center q-table__title q-mr-md">
+        <span data-test="dashboard-drilldown-title" v-if="isEditMode"
+          >Edit Join
+        </span>
+        <span data-test="dashboard-drilldown-title" v-else>Add Join</span>
+      </div>
+    </div>
+
+    <q-card-actions class="confirmActions">
+      <q-btn
+        unelevated
+        no-caps
+        class="q-mr-sm"
+        @click="$emit('close')"
+        data-test="cancel-button"
+      >
+        {{ t("confirmDialog.cancel") }}
+      </q-btn>
+      <q-btn
+        unelevated
+        no-caps
+        class="no-border"
+        color="primary"
+        style="min-width: 60px"
+        data-test="confirm-button"
+        :label="isEditMode ? 'Update' : 'Add'"
+      />
+      <!-- :disable="isFormValid" -->
+      <!-- @click="saveDrilldown" -->
+    </q-card-actions>
+  </div>
+</template>
+
+<script lang="ts">
+import { inject } from "vue";
+import { defineComponent } from "vue";
+import { useI18n } from "vue-i18n";
+import { useStore } from "vuex";
+export default defineComponent({
+  name: "AddJoinPopUp",
+  components: {},
+  props: {
+    isEditMode: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["close"],
+  setup(props, { emit }) {
+    const { t } = useI18n();
+    const store = useStore();
+
+    return {
+      t,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.selected {
+  background-color: var(--q-primary) !important;
+  font-weight: bold;
+  color: white;
+}
+.dropdownDiv {
+  display: flex;
+  align-items: center;
+  margin: 10px 0px;
+  width: 100%;
+}
+
+.dropdownLabel {
+  width: 150px;
+}
+
+.dropdown {
+  min-width: 100%;
+}
+
+:deep(.no-case .q-field__native > :first-child) {
+  text-transform: none !important;
+}
+</style>
---
+++ web/src/views/Dashboards/addPanel/DashboardJoinsOption.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <div
+      v-if="
+        !(
+          dashboardPanelData.data.queries[
+            dashboardPanelData.layout.currentQueryIndex
+          ].customQuery && dashboardPanelData.data.queryType == 'sql'
+        )
+      "
+      style="display: flex; flex-direction: row"
+      class="q-pl-md"
+    >
+      <div class="layout-name">{{ t("panel.joins") }}</div>
+      <span class="layout-separator">:</span>
+      <div
+        class="axis-container droppable scroll row"
+        data-test="dashboard-filter-layout"
+      >
+        <div
+          class="row q-mr-sm q-my-xs"
+          v-for="(itemY, index) in dashboardPanelData.data.queries[
+            dashboardPanelData.layout.currentQueryIndex
+          ].fields?.y"
+          :key="index"
+        >
+          <div
+            v-if="
+              dashboardPanelData.meta.dragAndDrop.targetDragIndex == index &&
+              dashboardPanelData.meta.dragAndDrop.currentDragArea == 'y'
+            "
+            class="dragItem"
+          >
+            &nbsp;
+          </div>
+          <q-btn-group class="axis-field">
+            <div>
+              <!-- <q-icon
+                name="drag_indicator"
+                color="grey-13"
+                size="13px"
+                class="cursor-grab q-my-xs"
+              /> -->
+              <!-- icon-right="arrow_drop_down" -->
+              <q-btn
+                no-caps
+                dense
+                color="primary"
+                square
+                :no-wrap="true"
+                size="sm"
+                :label="index"
+                :data-test="`dashboard-y-item-${itemY?.column}`"
+                class="q-pl-sm"
+              >
+              </q-btn>
+              <q-btn
+                style="height: 100%"
+                size="xs"
+                dense
+                :data-test="`dashboard-y-item-${itemY?.column}-remove`"
+                icon="close"
+                />
+                <!-- @click="removeYAxisItem(itemY?.column)" -->
+            </div>
+          </q-btn-group>
+        </div>
+        <q-btn
+          icon="add"
+          color="primary"
+          size="xs"
+          round
+          class="add-btn"
+          data-test="dashboard-add-join-btn"
+          @click="showAddJoinPopUp = !showAddJoinPopUp"
+        />
+      </div>
+    </div>
+    <q-dialog v-model="showAddJoinPopUp">
+      <AddJoinPopUp
+        :class="store.state.theme == 'dark' ? 'dark-mode' : 'bg-white'"
+      />
+    </q-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, inject } from "vue";
+import useDashboardPanelData from "../../../composables/useDashboardPanel";
+import { useI18n } from "vue-i18n";
+import { useStore } from "vuex";
+import AddJoinPopUp from "./AddJoinPopUp.vue";
+
+export default defineComponent({
+  name: "DashboardJoinOption",
+  components: {
+    AddJoinPopUp,
+  },
+  props: [],
+
+  setup(props) {
+    const dashboardPanelDataPageKey = inject(
+      "dashboardPanelDataPageKey",
+      "dashboard",
+    );
+
+    const { dashboardPanelData, removeFilterItem, loadFilterItem } =
+      useDashboardPanelData(dashboardPanelDataPageKey);
+
+    const { t } = useI18n();
+    const store = useStore();
+
+    const showAddJoinPopUp = ref(false);
+
+    return {
+      t,
+      store,
+      dashboardPanelData,
+      removeFilterItem,
+      loadFilterItem,
+      showAddJoinPopUp,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.layout-name {
+  font-size: 14px;
+  white-space: nowrap;
+  min-width: 130px;
+  display: flex;
+  align-items: center;
+}
+
+.layout-separator {
+  display: flex;
+  align-items: center;
+  margin-left: 2px;
+  margin-right: 2px;
+}
+
+.axis-container {
+  margin: 5px;
+}
+</style>
