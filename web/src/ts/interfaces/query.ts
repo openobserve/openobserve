b313c079b76b05f2dd1e8b2b237295d3ae035de0
loaki07 <rustyloaki@gmail.com>
2025-01-20 18:17:17 +0530

    chore: fmt

--- web/src/ts/interfaces/query.ts
+++ web/src/ts/interfaces/query.ts
@@ -51,7 +51,7 @@ export interface WebSocketSearchResponse {
       new_end_time?: number;
       scan_size?: number;
     };
-    streaming_aggs? : boolean;
+    streaming_aggs?: boolean;
   };
 }
 
