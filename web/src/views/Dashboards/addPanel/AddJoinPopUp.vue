310524134a7c9700bf34d0163e513e2ed2a0f91e
ktx-abhay <abhay.padamani@kiara.tech>
2025-01-20 10:53:04 +0530

    add: condition for join query

--- web/src/views/Dashboards/addPanel/AddJoinPopUp.vue
+++ web/src/views/Dashboards/addPanel/AddJoinPopUp.vue
@@ -31,6 +31,135 @@
         <span data-test="dashboard-drilldown-title" v-else>Add Join</span>
       </div>
     </div>
+    <div>
+      <div
+        class="tw-flex tw-flex-row tw-w-full tw-gap-10 items-center q-table__title q-mr-md"
+      >
+        <q-select
+          outlined
+          filled
+          v-model="mainStream"
+          :options="[]"
+          :disable="true"
+          label="Joining Stream"
+          class="q-py-md showLabelOnTop tw-w-1/3"
+          stack-label
+          data-test="dashboard-config-panel-join-from"
+        />
+
+        <q-select
+          outlined
+          filled
+          v-model="modelValue.joinType"
+          :options="joinOptions"
+          label="With Join Type"
+          class="q-py-md showLabelOnTop tw-w-1/3"
+          stack-label
+          data-test="dashboard-config-panel-join-type"
+        />
+
+        <q-select
+          outlined
+          filled
+          v-model="modelValue.stream"
+          :options="streamOptions"
+          label="On Stream"
+          class="q-py-md showLabelOnTop tw-w-1/3"
+          stack-label
+          data-test="dashboard-config-panel-join-to"
+        />
+      </div>
+
+      <q-separator />
+
+      <div>
+        <span class="tw-w-full tw-text-center tw-mt-5 tw-text-lg">On</span>
+        <div
+          v-for="(arg, argIndex) in modelValue.conditions"
+          :key="argIndex"
+          class="tw-w-full tw-flex tw-flex-col"
+        >
+          <div>
+            <div>
+              <label :for="'arg-' + argIndex"
+                >condition {{ argIndex + 1 }}</label
+              >
+            </div>
+            <div class="tw-flex tw-gap-x-3">
+              <!-- field1 selector -->
+              <!-- @filter="filterFunctionsOptions" -->
+              <q-select
+                input-debounce="0"
+                behavior="menu"
+                use-input
+                borderless
+                hide-selected
+                fill-input
+                v-model="modelValue.conditions[argIndex].field1"
+                :options="getFieldList()"
+                dense
+                filled
+                label="Select Field"
+                :data-test="`dashboard-join-condition-field1-${argIndex}`"
+                class="tw-w-40 tw-min-w-40"
+              />
+
+              <!-- operator selector -->
+              <q-select
+                behavior="menu"
+                borderless
+                hide-selected
+                v-model="modelValue.conditions[argIndex].operation"
+                :options="operationOptions"
+                dense
+                filled
+                label="Select Operation"
+                :data-test="`dashboard-join-condition-operation-${argIndex}`"
+                class="tw-w-40 tw-min-w-40"
+              />
+
+              <q-select
+                input-debounce="0"
+                behavior="menu"
+                use-input
+                borderless
+                hide-selected
+                fill-input
+                v-model="modelValue.conditions[argIndex].field2"
+                :options="getFieldList()"
+                dense
+                filled
+                label="Select Field"
+                :data-test="`dashboard-join-condition-field2-${argIndex}`"
+                class="tw-w-40 tw-min-w-40"
+              />
+
+              <!-- Remove argument button -->
+              <!-- only allow if more than 1 -->
+              <q-btn
+                v-if="modelValue.conditions.length > 1"
+                :data-test="`dashboard-join-condition-remove-${argIndex}`"
+                icon="close"
+                dense
+                flat
+                round
+                @click="removeCondition(argIndex)"
+                class="tw-h-10 tw-w-10"
+              />
+            </div>
+          </div>
+        </div>
+        <q-btn
+          @click="addCondition()"
+          color="primary"
+          label="+ Add Condition"
+          padding="5px 14px"
+          class="tw-mt-3"
+          no-caps
+          dense
+        />
+      </div>
+    </div>
 
     <q-card-actions class="confirmActions">
       <q-btn
@@ -70,14 +199,63 @@ export default defineComponent({
       type: Boolean,
       default: false,
     },
+    mainStream: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: Object,
+      default: () => {
+        return {
+          stream: "",
+          joinType: "inner",
+          conditions: [
+            {
+              field1: "",
+              field2: "",
+              logicalOperator: "and",
+              operation: "=",
+            },
+          ],
+        };
+      },
+    },
   },
   emits: ["close"],
   setup(props, { emit }) {
     const { t } = useI18n();
     const store = useStore();
 
+    const streamOptions = ["default1", "default2", "default3"];
+    const joinOptions = ["inner", "left", "right"];
+
+    const getFieldList = () => {
+      return ["field1", "field2", "field3"];
+    };
+
+    const removeCondition = (argIndex: number) => {
+      props.modelValue.conditions.splice(argIndex, 1);
+    };
+
+    const addCondition = () => {
+      props.modelValue.conditions.push({
+        field1: "",
+        field2: "",
+        logicalOperator: "and",
+        operation: "",
+      });
+    };
+
+    const operationOptions = ["=", "!=", ">", "<", ">=", "<="];
+
     return {
       t,
+      streamOptions,
+      joinOptions,
+      getFieldList,
+      removeCondition,
+      addCondition,
+      operationOptions,
     };
   },
 });
