84d54cd31e7e431bcdca1ba32863c3f9c62a3c42
udev <uddhavdave1@gmail.com>
2025-01-21 22:29:44 +0530

    rmv dbg calls

--- src/infra/src/table/action_scripts.rs
+++ src/infra/src/table/action_scripts.rs
@@ -149,6 +149,38 @@ pub async fn add(action: &Action) -> Result<(), errors::Error> {
     Ok(())
 }
 
+pub async fn update(action: &Action) -> Result<(), errors::Error> {
+    let id = action.id.ok_or(Error::Message("id not set".to_string()))?;
+    let record = ActiveModel {
+        id: Set(id.to_string()),
+        name: Set(action.name.clone()),
+        org_id: Set(action.org_id.to_string()),
+        file_path: Set(action
+            .zip_file_path
+            .clone()
+            .ok_or(Error::Message("file path not set".to_string()))?),
+        env: Set(serde_json::json!(action.environment_variables.clone())),
+        execution_details: Set(action.execution_details.clone()),
+        cron_expr: Set(action.cron_expr.clone()),
+        created_at: Set(action.created_at),
+        last_modified_at: Set(action.last_modified_at),
+        last_executed_at: Set(action.last_executed_at),
+        last_successful_at: Set(action.last_successful_at),
+        description: Set(action.description.clone()),
+        file_name: Set(action.zip_file_name.clone()),
+        created_by: Set(action.created_by.clone()),
+        status: Set(action.status.clone()),
+        origin_cluster_url: Set(action.origin_cluster_url.clone()),
+    };
+
+    // make sure only one client is writing to the database(only for sqlite)
+    let _lock = get_lock().await;
+
+    let client = ORM_CLIENT.get_or_init(connect_to_orm).await;
+    Entity::update(record).exec(client).await?;
+
+    Ok(())
+}
 pub async fn remove(org_id: &str, id: &str) -> Result<(), errors::Error> {
     // make sure only one client is writing to the database(only for sqlite)
     let _lock = get_lock().await;
