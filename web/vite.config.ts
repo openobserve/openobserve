ddd9fede32fdee824b75b9ecebabcf46aba77bad
Bhargav <BJP232004@GMAIL.COM>
2025-01-24 20:35:42 +0530

    fix: fixed all build issues

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -29,6 +29,7 @@ import visualizer from "rollup-plugin-visualizer";
 import "dotenv/config";
 
 import istanbul from "vite-plugin-istanbul";
+import tailwindcss from "@tailwindcss/vite";
 
 // Load environment variables from the appropriate .env file
 if (process.env.NODE_ENV === "production") {
@@ -105,11 +106,14 @@ export default defineConfig({
   },
   base: "./",
   plugins: [
+    tailwindcss(),
     vue({
       template: { transformAssetUrls },
     }),
     quasar({
-      sassVariables: "src/styles/quasar-variables.sass",
+      sassVariables: fileURLToPath(
+        new URL('src/styles/quasar-variables.sass', import.meta.url)
+      )
     }),
     process.env.VITE_COVERAGE === "true" &&
       istanbul({
