462ee16fad75a1e1b3bfb8ea1b676135984f9aa2
Sai Nikhil <nikhil@openobserve.ai>
2025-02-03 14:30:51 +0530

    enabled only to test one time

--- tests/ui-testing/playwright.config.js
+++ tests/ui-testing/playwright.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: false,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
