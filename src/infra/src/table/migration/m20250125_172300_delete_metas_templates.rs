dcb3ebe78cedababe427b163306687f5ff33a251
Taiming Liu <liutaiming3@gmail.com>
2025-02-03 11:35:16 +0800

    fix merge issues

--- src/infra/src/table/migration/m20250125_172300_delete_metas_templates.rs
+++ src/infra/src/table/migration/m20250125_172300_delete_metas_templates.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Deletes dashboard records from the meta table.
+//! Deletes template records from the meta table.
 
 use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, TransactionTrait};
 use sea_orm_migration::prelude::*;
