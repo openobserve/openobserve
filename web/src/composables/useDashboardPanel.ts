af0b5bc0b08b7fa1d6cff489b245a96347d92fa4
sai nikhil kethe <nikhil@openobserve.ai>
2025-02-09 13:12:19 +0530

    predefined indentation changed

--- web/src/composables/useDashboardPanel.ts
+++ web/src/composables/useDashboardPanel.ts
@@ -104,7 +104,7 @@ const getDefaultDashboardPanelData: any = () => ({
     },
     htmlContent: "",
     markdownContent: "",
-    customChartContent: `\ // To know more about ECharts , \n// visit: https://echarts.apache.org/examples/en/index.html \n// Example: https://echarts.apache.org/examples/en/editor.html?c=line-simple \n// Define your ECharts 'option' here. The 'data' variable is available for use \n// Example Usage: data[0].map((val)=>val.x_axis). \n \t option = {  \n \n  \t \t \t };
+    customChartContent: `\ // To know more about ECharts , \n// visit: https://echarts.apache.org/examples/en/index.html \n// Example: https://echarts.apache.org/examples/en/editor.html?c=line-simple \n// Define your ECharts 'option' here. The 'data' variable is available for use \n// Example Usage: data[0].map((val)=>val.x_axis).\noption = {  \n \n};
   `,
     customChartResult: {},
     queryType: "sql",
@@ -193,7 +193,7 @@ const dashboardPanelDataObj: any = {
 };
 
 const getDefaultCustomChartText = () => {
-  return `\ // To know more about ECharts , \n// visit: https://echarts.apache.org/examples/en/index.html \n// Example: https://echarts.apache.org/examples/en/editor.html?c=line-simple \n// Define your ECharts 'option' here. The 'data' variable is available for use \n// Example Usage: data[0].map((val)=>val.x_axis). \n \t option = {  \n \n  \t \t \t };`
+  return `\ // To know more about ECharts , \n// visit: https://echarts.apache.org/examples/en/index.html \n// Example: https://echarts.apache.org/examples/en/editor.html?c=line-simple \n// Define your ECharts 'option' here. The 'data' variable is available for use \n// Example Usage: data[0].map((val)=>val.x_axis).\noption = {  \n \n};`
 }
 
 const useDashboardPanelData = (pageKey: string = "dashboard") => {
