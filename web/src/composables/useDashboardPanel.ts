6693379f99ebfc8dc1d65e1e2ad81d5ee794abd2
ktx-abhay <abhay.padamani@kiara.tech>
2025-02-10 10:30:54 +0530

    add: query building using parser

--- web/src/composables/useDashboardPanel.ts
+++ web/src/composables/useDashboardPanel.ts
@@ -20,7 +20,11 @@ import useNotifications from "./useNotifications";
 import { splitQuotedString, escapeSingleQuotes } from "@/utils/zincutils";
 import { extractFields } from "@/utils/query/sqlUtils";
 import { validateSQLPanelFields } from "@/utils/dashboard/convertDataIntoUnitValue";
-import { buildSQLQueryFromInput } from "@/utils/dashboard/convertDataIntoUnitValue";
+import {
+  buildSQLQueryFromInput,
+  buildSQLJoinsFromInput,
+} from "@/utils/dashboard/convertDataIntoUnitValue";
+import { buildSQLQueryWithParser } from "@/utils/query/sqlUtils";
 
 const colors = [
   "#5960b2",
@@ -2199,6 +2203,19 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
   };
 
   const sqlchart = () => {
+    console.log(
+      "sqlchart",
+      dashboardPanelData,
+      buildSQLQueryWithParser(
+        dashboardPanelData.data.queries[
+          dashboardPanelData.layout.currentQueryIndex
+        ].fields,
+        dashboardPanelData.data.queries[
+          dashboardPanelData.layout.currentQueryIndex
+        ].joins,
+      ),
+    );
+
     // STEP 1: first check if there is at least 1 field selected
     if (
       dashboardPanelData.data.queries[
@@ -2305,7 +2322,11 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
       dashboardPanelData.data.queries[
         dashboardPanelData.layout.currentQueryIndex
       ].fields?.stream
-    }" `;
+    }" ${buildSQLJoinsFromInput(
+      dashboardPanelData.data.queries[
+        dashboardPanelData.layout.currentQueryIndex
+      ].joins,
+    )}`;
 
     // Add the AND/OR condition logic
     const filterData =
@@ -2761,6 +2782,9 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
       dashboardPanelData.data.queries[
         dashboardPanelData.layout.currentQueryIndex
       ].config.limit,
+      dashboardPanelData.data.queries[
+        dashboardPanelData.layout.currentQueryIndex
+      ].joins,
     ],
     () => {
       // only continue if current mode is auto query generation
