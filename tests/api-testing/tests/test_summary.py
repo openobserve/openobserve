904ec154115aa9993659b4f4e409f3bcd769543f
ShyamOOAI <shyam@openobserve.ai>
2025-02-12 12:50:31 +0530

    updated to check Super Cluster

--- tests/api-testing/tests/test_summary.py
+++ tests/api-testing/tests/test_summary.py
@@ -44,10 +44,7 @@ def create_session():
     session.close()  # Clean up after tests
 
 
-# @pytest.fixture
-# def base_url():
-#     """Provide the base URL for the API."""
-#     return ZO_BASE_URL_SC
+
 
 def test_ingest_data(create_session, base_url):
     """Ingest data into the OpenObserve running instance."""
@@ -62,56 +59,37 @@ def test_ingest_data(create_session, base_url):
     resp_ing = session.post(url_ing, data=data, headers={"Content-Type": "application/json"})
 
     print("Data ingested successfully for Summary, status code:", resp_ing.status_code)
-    print("Response content:", resp_ing.text)  # Add this for debugging
+    print("Response content:", resp_ing.text)  
     assert resp_ing.status_code == 200, f"Data ingestion failed, status code: {resp_ing.status_code}"
-
-def test_disable_websocket(create_session, base_url):
-    """Disable WebSocket ingestion for the instance."""
-    session = create_session
-    session.auth = HTTPBasicAuth(ZO_ROOT_USER_EMAIL, ZO_ROOT_USER_PASSWORD)
-
-    url = base_url
-    payload_disable_webs = {
-        "scrape_interval": 15,
-        "span_id_field_name": "span_id",
-        "trace_id_field_name": "trace_id",
-        "toggle_ingestion_logs": True,
-        "enable_websocket_search": False
-    }
-
-    resp_disable_webs = session.post(f"{url}api/{org_id}/settings", json=payload_disable_webs)
-
-    print("Disable Websocket response:", resp_disable_webs.content) 
-     # Add this for debugging
-    assert resp_disable_webs.status_code == 200, f"WebSocket disable failed, status code: {resp_disable_webs.status_code}"
 
 
 @pytest.fixture
-def base_url():
-    """Provide the base URL for the API."""
+def base_url_sc():
+    """Provide the base URL for the API of Super Cluster."""
     return ZO_BASE_URL_SC
 
-def test_summary(create_session, base_url):
+def test_summary(create_session, base_url_sc):
     """Run an E2E test for summary mode."""
     session = create_session
     session.auth = HTTPBasicAuth(ZO_ROOT_USER_EMAIL, ZO_ROOT_USER_PASSWORD)  # Add this line
 
-    url = base_url
+    url_sc = base_url_sc
 
-    print("URL for super cluster:", url) 
+    print("URL for super cluster:", url_sc) 
+
     time.sleep(5)  # Increase this time if necessary
 
-    res_summary = session.get(f"{url}api/{org_id}/summary")
+    res_summary_sc = session.get(f"{url_sc}api/{org_id}/summary")
 
-    print("Summary response:", res_summary.content)  # Add this for debugging
-    assert res_summary.status_code == 200, f"Summary failed, status code: {res_summary.status_code}"
+    print("Summary SC response:", res_summary_sc.content)  # Add this for debugging
+    assert res_summary_sc.status_code == 200, f"Summary failed, status code: {res_summary_sc.status_code}"
 
-    res_data_summary = res_summary.json()
+    res_data_summary_sc = res_summary_sc.json()
 
     # Validate the number of streams
-    actual_num_streams= res_data_summary["streams"]["num_streams"]
-    expected_num_streams = 1  # Adjust based on our expectations
+    actual_num_streams_sc= res_data_summary_sc["streams"]["num_streams"]
+    expected_num_streams_sc = 1  # Adjust based on our expectations
 
-    assert actual_num_streams == expected_num_streams, f"Expected to be {expected_num_streams}, but got {actual_num_streams}"
+    assert actual_num_streams_sc == expected_num_streams_sc, f"Expected to be {expected_num_streams_sc}, but got {actual_num_streams_sc}"
 
     
