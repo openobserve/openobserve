ec5d3b220ac72aa9ee85a2f90b99fe96938c1f93
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2025-02-14 16:21:12 +0530

    fix: dashboard when switching to auto mode issue

--- web/src/composables/useDashboardPanel.ts
+++ web/src/composables/useDashboardPanel.ts
@@ -3039,6 +3039,7 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
         // auto query mode selected
         // remove the custom fields from the list
         dashboardPanelData.meta.stream.customQueryFields = [];
+        dashboardPanelData.meta.stream.vrlFunctionFieldList = [];
       }
       // if (dashboardPanelData.data.queryType == "promql") {
       //     updatePromQLQuery()
