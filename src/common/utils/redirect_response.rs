742739aac416d8d5df412ef2e694671f9264d034
hengfei yang <hengfei.yang@gmail.com>
2025-02-14 17:52:04 +0800

    fix: short url redirect issue

--- src/common/utils/redirect_response.rs
+++ src/common/utils/redirect_response.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 OpenObserve Inc.
+// Copyright 2025 OpenObserve Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -52,7 +52,8 @@ impl RedirectResponse {
     }
 
     fn build_redirect_response(&self) -> HttpResponse {
-        let redirect_uri = self.build_full_redirect_uri();
+        let mut redirect_uri = self.build_full_redirect_uri();
+        redirect_uri = redirect_uri.trim_matches('"').to_string();
         if redirect_uri.len() < 1024 {
             HttpResponse::Found()
                 .append_header((LOCATION, redirect_uri))
