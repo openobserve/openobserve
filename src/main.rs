9fa324e9328031578a86d0d1f0ef15c0c9aae82f
hengfei yang <hengfei.yang@gmail.com>
2025-02-16 22:52:06 +0800

    http keepalive allow set to zero

--- src/main.rs
+++ src/main.rs
@@ -687,10 +687,9 @@ async fn init_http_server() -> Result<(), anyhow::Error> {
             ))
             .wrap(RequestTracing::new())
     })
-    .keep_alive(KeepAlive::Timeout(Duration::from_secs(max(
-        1,
+    .keep_alive(KeepAlive::Timeout(Duration::from_secs(
         cfg.limit.http_keep_alive,
-    ))))
+    )))
     .client_request_timeout(Duration::from_secs(max(1, cfg.limit.http_request_timeout)))
     .shutdown_timeout(max(1, cfg.limit.http_shutdown_timeout));
     let server = if cfg.http.tls_enabled {
@@ -798,10 +797,9 @@ async fn init_http_server_without_tracing() -> Result<(), anyhow::Error> {
                 r#"%a "%r" %s %b "%{Content-Length}i" "%{Referer}i" "%{User-Agent}i" %T"#,
             ))
     })
-    .keep_alive(KeepAlive::Timeout(Duration::from_secs(max(
-        1,
+    .keep_alive(KeepAlive::Timeout(Duration::from_secs(
         cfg.limit.http_keep_alive,
-    ))))
+    )))
     .client_request_timeout(Duration::from_secs(max(1, cfg.limit.http_request_timeout)))
     .shutdown_timeout(max(1, cfg.limit.http_shutdown_timeout));
     let server = if cfg.http.tls_enabled {
@@ -1040,10 +1038,9 @@ async fn init_script_server() -> Result<(), anyhow::Error> {
             ))
             .wrap(RequestTracing::new())
     })
-    .keep_alive(KeepAlive::Timeout(Duration::from_secs(max(
-        1,
+    .keep_alive(KeepAlive::Timeout(Duration::from_secs(
         cfg.limit.keep_alive,
-    ))))
+    )))
     .client_request_timeout(Duration::from_secs(max(1, cfg.limit.request_timeout)))
     .shutdown_timeout(max(1, cfg.limit.http_shutdown_timeout));
     let server = if cfg.http.tls_enabled {
