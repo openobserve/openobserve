7eb60c7f1a02a3398c6dd55f48bc64c9a99d6f0b
ktx-abhay <abhay.padamani@kiara.tech>
2025-02-25 17:44:42 +0530

    fix: save dashboard to cache

--- web/src/composables/dashboard/usePanelDataLoader.ts
+++ web/src/composables/dashboard/usePanelDataLoader.ts
@@ -1236,7 +1236,7 @@ export const usePanelDataLoader = (
                 );
               }
 
-              // saveCurrentStateToCache();
+              saveCurrentStateToCache();
             }
           }
 
