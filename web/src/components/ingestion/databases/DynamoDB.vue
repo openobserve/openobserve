7fa5e9aa51affe55157cb489a844bd5111a9f061
Bhargav <BJP232004@GMAIL.COM>
2025-02-26 18:59:39 +0530

    feat: final database pages commit

--- web/src/components/ingestion/databases/DynamoDB.vue
+++ web/src/components/ingestion/databases/DynamoDB.vue
@@ -32,6 +32,4 @@ const docURL = databaseDocURLs[name];
       <div class="tw-font-bold tw-pt-6 tw-pb-2">
         Click <a :href="docURL" target="_blank" class="text-blue-500 hover:text-blue-600" style="text-decoration: underline">here</a> to check further documentation.
       </div>
-    </div>
   </div>
-</template>
