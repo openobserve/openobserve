d73606ed91f41a0592131f82a289cb2bdf7450af
Subhra264 <chakrabortysubhradeep556@gmail.com>
2025-03-03 15:01:36 +0530

    fix: scheduled pipeline don't pause after retries (#6147)
    Fixes three problems -
    - [x] Pipeline pauses automatically after reaching max retries.
    - [x] There could be some data loss if `(now - start_time)` is not a
    multiple of `period_num_microseconds + 1`.
    - [x] If derived stream fails, it should retry immediately in the next
    batch until it reaches max retries. After reaching max retries, it
    should retry at the next scheduled run.
    
    ---------
    
    Co-authored-by: sai nikhil kethe <nikhil@openobserve.ai>
    Co-authored-by: Taiming Liu <liutaiming3@gmail.com>

--- src/config/src/config.rs
+++ src/config/src/config.rs
@@ -1156,6 +1156,8 @@ pub struct Limit {
     pub derived_stream_schedule_interval: i64,
     #[env_config(name = "ZO_SCHEDULER_MAX_RETRIES", default = 3)]
     pub scheduler_max_retries: i32,
+    #[env_config(name = "ZO_SCHEDULER_PIPELINE_MAX_RETRIES", default = 20)]
+    pub scheduler_pipeline_max_retries: i32,
     #[env_config(name = "ZO_SCHEDULER_PAUSE_ALERT_AFTER_RETRIES", default = false)]
     pub pause_alerts_on_retries: bool,
     #[env_config(
