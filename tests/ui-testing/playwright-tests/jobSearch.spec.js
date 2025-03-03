d13b98b0856d4c39871e15e6bf1d74b1bbbd4cd8
ShyamOOAI <shyam@openobserve.ai>
2025-03-03 16:30:17 +0530

    Have added test cases for explore and view job details

--- tests/ui-testing/playwright-tests/jobSearch.spec.js
+++ tests/ui-testing/playwright-tests/jobSearch.spec.js
@@ -2,16 +2,18 @@ import { test, expect } from "./baseFixtures.js";
 import { LoginPage } from '../pages/loginPage.js';
 import { LogsPage } from '../pages/logsPage.js';
 import { IngestionPage } from '../pages/ingestionPage.js';
+import { JobSchedulerPage } from '../pages/jobScheduler.js';    
 
 test.describe.configure({ mode: 'parallel' });
 
 test.describe("Job Search for schedule query", () => {
-    let loginPage, logsPage, ingestionPage;
+    let loginPage, logsPage, ingestionPage, jobSchedulerPage;
     const stream = "e2e_automate";
     test.beforeEach(async ({ page }) => {
         loginPage = new LoginPage(page);
         ingestionPage = new IngestionPage(page);
         logsPage = new LogsPage(page);
+        jobSchedulerPage = new JobSchedulerPage(page, process.env["ZO_BASE_URL"]);
         await loginPage.gotoLoginPage();
         await loginPage.loginAsInternalUser();
         await loginPage.login(); // Login as root user
@@ -34,39 +36,7 @@ test.describe("Job Search for schedule query", () => {
 
     });
 
-    test("Create and Delete Job Search for schedule query", async ({ page }) => {
-
-        await logsPage.navigateToLogs();
-        await logsPage.selectStreamDropDown();
-        await logsPage.selectStreamAndStreamTypeForLogs(stream);
-        await logsPage.enableSQLMode();
-        await logsPage.queryJobSearch();
-        await logsPage.selectRunQuery();
-        await logsPage.searchSchedulerDropdown();
-        await logsPage.searchSchedulerCreate();
-        await logsPage.searchSchedulerSubmit();
-        await page.goto(process.env["ZO_BASE_URL_SC_UI"] + "/web/logs?action=search_scheduler&org_identifier=default&type=search_scheduler_list");
-        await page.waitForTimeout(10000);
-
-        // await logsPage.submitSearchJob();
-        // await page.waitForTimeout(10000);
-        // Submit the search job
-        const jobId = await logsPage.submitSearchJob();
-
-    // Print the Job ID
-        console.log(`Job ID: ${jobId}`);
-
-    // Optionally, add assertions to verify the Job ID
-        expect(jobId).not.toBeNull();
-
-        const traceId = await logsPage.getTraceIdByJobId(jobId);
-        console.log(`Trace ID: ${traceId}`);    
-        
-        await logsPage.clickJobID();
-        await page.waitForTimeout(10000);
-
-    });
-
+    
     test("Create Job Search for schedule query with invalid data", async ({ page }) => {
 
         await logsPage.navigateToLogs();
@@ -80,11 +50,91 @@ test.describe("Job Search for schedule query", () => {
         await logsPage.searchSchedulerInvalid();
         await logsPage.validateInvalidData();
        
+    });
+
+    test("Create and Delete Job Search for schedule query", async ({ page }) => {
+
+    
+        await page.goto(process.env["ZO_BASE_URL_SC_UI"] + "/web/logs?action=search_scheduler&org_identifier=default&type=search_scheduler_list");
+        await page.waitForTimeout(10000);
+        const jobId = await jobSchedulerPage.submitSearchJob();
+        console.log(`Job ID: ${jobId}`);
+        expect(jobId).not.toBeNull();
+        const traceId = await jobSchedulerPage.getTraceIdByJobId(jobId);
+        console.log(`Trace ID: ${traceId}`);    
+        await jobSchedulerPage.deleteJobSearch(traceId);
+      
 
     });
 
+    test("Create and restart Job Search for schedule query", async ({ page }) => {
 
+    
+        await page.goto(process.env["ZO_BASE_URL_SC_UI"] + "/web/logs?action=search_scheduler&org_identifier=default&type=search_scheduler_list");
+        await page.waitForTimeout(10000);
+        const jobId = await jobSchedulerPage.submitSearchJob();
+        console.log(`Job ID: ${jobId}`);
+        expect(jobId).not.toBeNull();
+        const traceId = await jobSchedulerPage.getTraceIdByJobId(jobId);
+        console.log(`Trace ID: ${traceId}`);  
+        await page.waitForTimeout(10000);  
+        await jobSchedulerPage.restartJobSearch(traceId);
+      
 
+    });
+
+    test("Create and cancel Job Search for schedule query", async ({ page }) => {
+
+    
+        await page.goto(process.env["ZO_BASE_URL_SC_UI"] + "/web/logs?action=search_scheduler&org_identifier=default&type=search_scheduler_list");
+        await page.waitForTimeout(10000);
+        const jobId = await jobSchedulerPage.submitSearchJob();
+        console.log(`Job ID: ${jobId}`);
+        expect(jobId).not.toBeNull();
+        const traceId = await jobSchedulerPage.getTraceIdByJobId(jobId);
+        console.log(`Trace ID: ${traceId}`);  
+        await jobSchedulerPage.cancelJobSearch(traceId);
+      
+
+    });
+
+    test("Create and explore Job Search for schedule query", async ({ page }) => {
+
+        await page.goto(process.env["ZO_BASE_URL_SC_UI"] + "/web/logs?action=search_scheduler&org_identifier=default&type=search_scheduler_list");
+        await page.waitForTimeout(10000);
+        const jobId = await jobSchedulerPage.submitSearchJob();
+        console.log(`Job ID: ${jobId}`);
+        expect(jobId).not.toBeNull();
+        const traceId = await jobSchedulerPage.getTraceIdByJobId(jobId);
+        console.log(`Trace ID: ${traceId}`);  
+        await page.waitForTimeout(10000);
+        await jobSchedulerPage.exploreJob(traceId);     
+
+    });
+
+    
+
+    test("Create and view Job Search for schedule query details", async ({ page }) => {
+
+        await page.goto(process.env["ZO_BASE_URL_SC_UI"] + "/web/logs?action=search_scheduler&org_identifier=default&type=search_scheduler_list");
+        await page.waitForTimeout(10000);
+        const jobId = await jobSchedulerPage.submitSearchJob();
+        console.log(`Job ID: ${jobId}`);    
+        expect(jobId).not.toBeNull();
+        const traceId = await jobSchedulerPage.getTraceIdByJobId(jobId);
+        console.log(`Trace ID: ${traceId}`);  
+        await jobSchedulerPage.viewJobDetails(traceId);
+
+    });
+
+    
+
+    
+    
+    
+    
+
+    
 
 
 
