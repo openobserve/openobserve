222878344a1705066179e9cc5c0bc0933bff07f0
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2025-03-03 12:31:38 +0530

    fix: build

--- src/service/alerts/scheduler/handlers.rs
+++ src/service/alerts/scheduler/handlers.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::{collections::HashMap str::FromStr, time::Instant};
+use std::{collections::HashMap, str::FromStr, time::Instant};
 
 use chrono::{Duration, FixedOffset, Utc};
 use config::{
