35626d3a23ddca2cfada0bf9ff70b2bb5d7803d5
hengfei yang <hengfei.yang@gmail.com>
2025-03-04 13:33:53 +0800

    fix: cargo clippy

--- src/infra/src/errors/mod.rs
+++ src/infra/src/errors/mod.rs
@@ -220,7 +220,7 @@ impl From<TemplateError> for Error {
 
 impl std::fmt::Display for ErrorCodes {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.to_string())
+        write!(f, "{}", self.to_json())
     }
 }
 
