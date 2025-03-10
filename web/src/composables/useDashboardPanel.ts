615e2b91be2f97f2e98ab24710fe803965e46c68
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2025-03-10 11:26:23 +0530

    feat: not in, starts with, ends with support added

--- web/src/composables/useDashboardPanel.ts
+++ web/src/composables/useDashboardPanel.ts
@@ -1996,7 +1996,11 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
           selectFilter += `${condition.column} IN (${formatINValue(
             condition.value,
           )})`;
-        } else if (condition.operator === "match_all") {
+        } else if (condition.operator === "NOT IN")
+          selectFilter += `${condition.column} NOT IN (${formatINValue(
+            condition.value,
+          )})`;
+        else if (condition.operator === "match_all") {
           selectFilter += `match_all(${formatValue(condition.value)})`;
         } else if (condition.operator === "match_all_raw") {
           selectFilter += `match_all_raw(${formatValue(condition.value)})`;
@@ -2038,6 +2042,12 @@ const useDashboardPanelData = (pageKey: string = "dashboard") => {
               break;
             case "Not Contains":
               selectFilter += `NOT LIKE '%${condition.value}%'`;
+              break;
+            case "Starts With":
+              selectFilter += `LIKE '${condition.value}%'`;
+              break;
+            case "Ends With":
+              selectFilter += `LIKE '%${condition.value}'`;
               break;
             default:
               selectFilter += `${condition.operator} ${formatValue(
