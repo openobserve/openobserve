24fcc08bc321ae74b6c835639b04fc674ae41842
ktx-akshay <akshay.patil@kiara.tech>
2025-03-17 13:28:09 +0530

    create utils folder

--- tests/ui-testing/playwright-tests/utils/dashCreation.js
+++ tests/ui-testing/playwright-tests/utils/dashCreation.js
@@ -1,4 +1,6 @@
 import { expect } from "playwright/test";
+// import logData from "../cypress/fixtures/log.json"
+
 // fuction of Dashboard page and Apply query button
 export const waitForDashboardPage = async function (page) {
   const dashboardListApi = page.waitForResponse(
