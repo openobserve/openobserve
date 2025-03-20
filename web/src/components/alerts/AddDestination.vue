05405f1ad60836ca9d296e01c82d6464ac36d0ed
omkark06 <omkar@zinclabs.io>
2025-03-20 11:50:37 +0530

    fix: fetching actions in destination creation

--- web/src/components/alerts/AddDestination.vue
+++ web/src/components/alerts/AddDestination.vue
@@ -333,6 +333,7 @@ import { isValidResourceName } from "@/utils/zincutils";
 import AppTabs from "@/components/common/AppTabs.vue";
 import actionService from "@/services/action_scripts";
 import config from "@/aws-exports";
+import useActions from "@/composables/useActions";
 
 const props = defineProps({
   templates: {
@@ -373,6 +374,8 @@ const router = useRouter();
 const actionOptions = ref<{ value: string; label: string; type: string }[]>([]);
 
 const filteredActions = ref<any[]>([]);
+
+const { getAllActions } = useActions();
 
 // TODO OK: Use UUID package instead of this and move this method in utils
 const getUUID = () => {
@@ -484,20 +487,29 @@ const isValidDestination = computed(
     (props.isAlerts ? formData.value.template : true),
 );
 
+const updateActionOptions = () => {
+  actionOptions.value = [];
+  store.state.organizationData.actions.forEach((action: any) => {
+    if (action.execution_details_type === "service")
+      actionOptions.value.push({
+        value: action.id,
+        label: action.name,
+        type: action.execution_details_type,
+      });
+  });
+  filteredActions.value = actionOptions.value;
+};
+
 const getActionOptions = async () => {
   try {
     isLoadingActions.value = true;
-    const actions = await actionService.list(
-      store.state.selectedOrganization.identifier,
-    );
-    actions.data.forEach((action: any) => {
-      if (action.execution_details_type === "service")
-        actionOptions.value.push({
-          value: action.id,
-          label: action.name,
-          type: action.execution_details_type,
-        });
-    });
+    // Update action options with existing actions
+    updateActionOptions();
+
+    // Get all actions from the server and update the action options
+    await getAllActions();
+    isLoadingActions.value = false;
+    updateActionOptions();
   } catch (err) {
     console.error(err);
   } finally {
