9a4ad3784aa0921bd6c50be1286a43d70ddfe7b0
ShyamOOAI <shyam@openobserve.ai>
2025-03-21 12:29:53 +0530

    Have created pages for template, folder and dashboard

---
+++ tests/api-testing/tests/create_objects.py
@@ -0,0 +1,42 @@
+import sys
+from pathlib import Path
+
+# Add the current directory to sys.path
+current_dir = Path(__file__).parent
+sys.path.append(str(current_dir))
+
+from pages.template_page import TemplatePage
+from pages.folder_page import FolderPage
+from pages.dashboard_page import DashboardPage
+
+
+# Use environment variable
+def create_objects(session, base_url, user_email, user_password, org_id, stream_name, num_objects):
+    """Create objects in the OpenObserve running instance."""
+
+
+    template_page = TemplatePage(session, base_url, org_id)
+    folder_page = FolderPage(session, base_url, org_id)
+    dashboard_page = DashboardPage(session, base_url, org_id)
+
+    for i in range(num_objects):
+
+        # Create templates
+        template_name_webhook = f"template_webhook_{template_page.Unique_value_temp}_{i}"
+        template_page.create_template_webhook(session, base_url, user_email, user_password, org_id, template_name_webhook)
+
+        template_name_email = f"template_email_{template_page.Unique_value_temp}_{i}"
+        template_page.create_template_email(session, base_url, user_email, user_password, org_id, template_name_email)
+
+        folder_name = f"folder_{folder_page.Unique_value_folder}_{i}"
+        folder_id = folder_page.create_folder(session, base_url, user_email, user_password, org_id, folder_name)
+
+        dashboard_name = f"dashboard_{dashboard_page.Unique_value_dashboard}_{i}"
+        dashboard_id = dashboard_page.create_dashboard(session, base_url, user_email, user_password, org_id, folder_id, dashboard_name)
+
+        panel_name = f"panel_{dashboard_page.Unique_value_panel}_{i}"
+        panel_id = dashboard_page.create_panel(session, base_url, user_email, user_password, org_id, stream_name, folder_id, panel_name)
+
+
+        
+       
---
+++ tests/api-testing/tests/test_workflow.py
@@ -0,0 +1,20 @@
+import os
+from create_objects import create_objects
+
+
+
+# Constants for WebSocket URL and user credentials
+ZO_BASE_URL = os.environ.get("ZO_BASE_URL")  # Use environment variable
+ZO_BASE_URL_SC = os.environ.get("ZO_BASE_URL_SC")  # Use environment variable
+ZO_ROOT_USER_EMAIL = os.environ.get("ZO_ROOT_USER_EMAIL")  # Use environment variable
+ZO_ROOT_USER_PASSWORD = os.environ.get("ZO_ROOT_USER_PASSWORD")  # Use environment variable
+ZO_BASE_URL_SC2 = os.environ.get("ZO_BASE_URL_SC2")  # Use environment variable
+
+
+Total_count = 2
+org_id = "default"
+stream_name = "default"
+
+def test_workflow(create_session, base_url):
+
+   create_objects(create_session, base_url, ZO_ROOT_USER_EMAIL, ZO_ROOT_USER_PASSWORD, org_id, stream_name, Total_count)
