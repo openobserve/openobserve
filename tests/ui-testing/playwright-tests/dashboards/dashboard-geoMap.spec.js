acecebbc8edc5ed21b4e324a7c75cca85d028f7a
ktx-akshay <akshay.patil@kiara.tech>
2025-03-28 16:59:06 +0530

    added the testes

--- tests/ui-testing/playwright-tests/dashboards/dashboard-geoMap.spec.js
+++ tests/ui-testing/playwright-tests/dashboards/dashboard-geoMap.spec.js
@@ -81,10 +81,6 @@ test.describe("dashboard filter testcases", () => {
 
     // await orgNavigation;
   });
-
-
-
-
  test("should correctly apply the filter conditions with different operators, and successfully apply them to the query", async ({
     page,
   }) => {
@@ -229,4 +225,73 @@ test.describe("dashboard filter testcases", () => {
 
     await deleteDashboard(page, randomDashboardName);
   });
+
+
+  test("Should display the correct location when manually entering latitude and longitude values", async ({
+    page,
+  }) => {
+    await page.locator('[data-test="menu-link-\\/dashboards-item"]').click();
+    await waitForDashboardPage(page);
+    await page.locator('[data-test="dashboard-add"]').click();
+    await page.locator('[data-test="add-dashboard-name"]').click();
+    await page
+      .locator('[data-test="add-dashboard-name"]')
+      .fill(randomDashboardName);
+
+    await page.locator('[data-test="dashboard-add-submit"]').click();
+    await page.waitForTimeout(3000);
+
+
+
+    const button = page.locator(
+      '[data-test="dashboard-if-no-panel-add-panel-btn"]'
+    );
+    await expect(button).toBeVisible();
+    await button.click();
+
+    await page.locator('[data-test="selected-chart-geomap-item"] img').click();
+    await page
+      .locator('[data-test="index-dropdown-stream"]')
+      .waitFor({ state: "visible" });
+    await page.locator('[data-test="index-dropdown-stream"]').click();
+
+    await page
+      .locator('[data-test="index-dropdown-stream"]')
+      .press("Control+a");
+    await page
+      .locator('[data-test="index-dropdown-stream"]')
+      .fill("geojson");
+
+    await page
+      .getByRole("option", { name: "geojson", exact: true })
+      .click();
+
+      await page.waitForTimeout(2000);
+   await page.locator('[data-test="field-list-item-logs-geojson-lat"] [data-test="dashboard-add-latitude-data"]').click();
+
+  await page.locator('[data-test="field-list-item-logs-geojson-lon"] [data-test="dashboard-add-longitude-data"]').click();
+
+  await page.locator('[data-test="field-list-item-logs-geojson-country"] [data-test="dashboard-add-weight-data"]').click();
+
+    await page.locator('[data-test="dashboard-apply"]').click();
+
+    await page.locator('[data-test="dashboard-sidebar"]').click();
+
+    await page.locator('[data-test="dashboard-config-latitude"]').click();
+    await page.locator('[data-test="dashboard-config-latitude"]').click();
+    await page.locator('[data-test="dashboard-config-latitude"]').fill('26.1206');
+
+    await page.locator('[data-test="dashboard-config-longitude"]').click();
+    await page.locator('[data-test="dashboard-config-longitude"]').fill('091.6523');
+
+    await page.locator('[data-test="dashboard-config-zoom"]').click();
+    await page.locator('[data-test="dashboard-config-zoom"]').fill('10');
+    await page.locator('[data-test="dashboard-apply"]').click();
+
+// Click on the map at the given position
+await page.locator('#chart-map canvas').click({
+  position: { x: 26.1206, y: 91.6523 } // Ensure this translates correctly to pixels
+});
+
+});
 });
