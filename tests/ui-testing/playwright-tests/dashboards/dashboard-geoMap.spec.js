d0093d8c5ac8256dc169c3d47fa6f4a79c120a00
ktx-akshay <akshay.patil@kiara.tech>
2025-03-31 15:50:55 +0530

    added the testcase and ingestion of mapdata

--- tests/ui-testing/playwright-tests/dashboards/dashboard-geoMap.spec.js
+++ tests/ui-testing/playwright-tests/dashboards/dashboard-geoMap.spec.js
@@ -2,86 +2,31 @@ import { test, expect } from "../baseFixtures.js";
 import { login } from "../utils/dashLogin.js";
 import geoMapdata from "../../../test-data/geo_map.json";
 import {
+  ingestionForMaps,
+  getAuthTokenformaps,
+  removeUTFCharacters1,
+} from "../utils/dashIngestion.js";
+
+import {
   waitForDashboardPage,
-  applyQueryButton,
   deleteDashboard,
 } from "../utils/dashCreation.js";
-
-
-// ingestion.js
-
-// Exported function to remove UTF characters
-const removeUTFCharacters = (text) => {
-
-    // console.log(text, "tex");
-   // Remove UTF characters using regular expression
- return text.replace(/[^\x00-\x7F]/g, " ");
-};
-
-// Function to retrieve authentication token (to be implemented securely)
-const getAuthToken = async () => {
- const basicAuthCredentials = Buffer.from(
-   `${process.env["ZO_ROOT_USER_EMAIL"]}:${process.env["ZO_ROOT_USER_PASSWORD"]}`
- ).toString("base64");
- return `Basic ${basicAuthCredentials}`;
-};
-
-// page is passed here to access the page object (currently not used)
-const ingestion = async (page, streamName = "geojson") => {
- if (!process.env["ORGNAME"] || !process.env["INGESTION_URL"]) {
-   throw new Error("Required environment variables are not set");
- }
-
- const orgId = process.env["ORGNAME"];
-
- try {
-   const headers = {
-     "Content-Type": "application/json",
-     Authorization: await getAuthToken(),
-   };
-
-   const fetchResponse = await fetch(
-     `${process.env.INGESTION_URL}/api/${orgId}/${streamName}/_json`,
-     {
-       method: "POST",
-       headers,
-       body: JSON.stringify(geoMapdata),
-     }
-   );
-
-   if (!fetchResponse.ok) {
-     throw new Error(`HTTP error! status: ${fetchResponse.status}, response: ${fetchResponse}`);
-   }
-
-   return await fetchResponse.json();
- } catch (error) {
-   console.error("Ingestion failed:", error);
-   throw error;
- }
-};
-
 
 const randomDashboardName =
   "Dashboard_" + Math.random().toString(36).substr(2, 9);
 
 test.describe.configure({ mode: "parallel" });
 
-test.describe("dashboard filter testcases", () => {
+test.describe("dashboard maps testcases", () => {
   test.beforeEach(async ({ page }) => {
     console.log("running before each");
     await login(page);
     await page.waitForTimeout(1000);
-    await ingestion(page);
+    await ingestionForMaps(page);
     await page.waitForTimeout(2000);
-
-    // const orgNavigation = page.goto(
-    //   `${geoMapdata.geoMapdataUrl}?org_identifier=${process.env["ORGNAME"]}`
-    // );
-
-
-    // await orgNavigation;
   });
- test("should correctly apply the filter conditions with different operators, and successfully apply them to the query", async ({
+
+  test("should correctly apply the filter conditions with different operators, and successfully apply them to the query", async ({
     page,
   }) => {
     await page.locator('[data-test="menu-link-\\/dashboards-item"]').click();
@@ -134,10 +79,7 @@ test.describe("dashboard filter testcases", () => {
 
     await page.locator('[data-test="dashboard-variable-save-btn"]').click();
 
-    // await page.waitForTimeout(3000);
-
     await page.locator('[data-test="dashboard-settings-close-btn"]').click();
-
 
     const button = page.locator(
       '[data-test="dashboard-if-no-panel-add-panel-btn"]'
@@ -154,23 +96,28 @@ test.describe("dashboard filter testcases", () => {
     await page
       .locator('[data-test="index-dropdown-stream"]')
       .press("Control+a");
-    await page
-      .locator('[data-test="index-dropdown-stream"]')
-      .fill("geojson");
+    await page.locator('[data-test="index-dropdown-stream"]').fill("geojson");
 
+    await page.getByRole("option", { name: "geojson", exact: true }).click();
+
+    await page.waitForTimeout(2000);
     await page
-      .getByRole("option", { name: "geojson", exact: true })
+      .locator(
+        '[data-test="field-list-item-logs-geojson-lat"] [data-test="dashboard-add-latitude-data"]'
+      )
       .click();
 
-      await page.waitForTimeout(2000);
-   await page.locator('[data-test="field-list-item-logs-geojson-lat"] [data-test="dashboard-add-latitude-data"]').click();
+    await page
+      .locator(
+        '[data-test="field-list-item-logs-geojson-lon"] [data-test="dashboard-add-longitude-data"]'
+      )
+      .click();
 
-  await page.locator('[data-test="field-list-item-logs-geojson-lon"] [data-test="dashboard-add-longitude-data"]').click();
-
-  await page.locator('[data-test="field-list-item-logs-geojson-country"] [data-test="dashboard-add-weight-data"]').click();
-
-    // await page.locator('[data-test="dashboard-apply"]').click();
-
+    await page
+      .locator(
+        '[data-test="field-list-item-logs-geojson-country"] [data-test="dashboard-add-weight-data"]'
+      )
+      .click();
 
     const filterButton = page.locator(
       '[data-test="field-list-item-logs-geojson-country"] [data-test="dashboard-add-filter-geomap-data"]'
@@ -180,24 +127,27 @@ test.describe("dashboard filter testcases", () => {
 
     await page.locator('[data-test="dashboard-apply"]').click();
 
-    await page.waitForTimeout(3000);
-
-
+    await page.waitForTimeout(2000);
 
     await page
-    .locator('[data-test="dashboard-variable-query-value-selector"]').click();
+      .locator('[data-test="dashboard-variable-query-value-selector"]')
+      .click();
     await page
-    .locator('[data-test="dashboard-variable-query-value-selector"]').fill("china");
-  
-  const option = await page.getByRole("option", { name: "china" });
-  await option.click();
+      .locator('[data-test="dashboard-variable-query-value-selector"]')
+      .fill("china");
 
+    const option = await page.getByRole("option", { name: "china" });
+    await option.click();
 
-
-  await page.locator('[data-test="dashboard-add-condition-label-0-country"]').click();
-  await page.locator('[data-test="dashboard-add-condition-condition-0"]').click();
-  await page.locator('[data-test="dashboard-add-condition-operator"]').click();
- 
+    await page
+      .locator('[data-test="dashboard-add-condition-label-0-country"]')
+      .click();
+    await page
+      .locator('[data-test="dashboard-add-condition-condition-0"]')
+      .click();
+    await page
+      .locator('[data-test="dashboard-add-condition-operator"]')
+      .click();
 
     await page.getByText("=", { exact: true }).click();
     await page.getByLabel("Value").click();
@@ -207,11 +157,11 @@ test.describe("dashboard filter testcases", () => {
 
     await page.waitForTimeout(3000);
 
-    await page.locator('#chart-map canvas').click({
+    await page.locator("#chart-map canvas").click({
       position: {
         x: 643,
-        y: 69
-      }
+        y: 69,
+      },
     });
 
     await page.locator('[data-test="dashboard-panel-name"]').click();
@@ -225,7 +175,6 @@ test.describe("dashboard filter testcases", () => {
 
     await deleteDashboard(page, randomDashboardName);
   });
-
 
   test("Should display the correct location when manually entering latitude and longitude values", async ({
     page,
@@ -241,8 +190,6 @@ test.describe("dashboard filter testcases", () => {
     await page.locator('[data-test="dashboard-add-submit"]').click();
     await page.waitForTimeout(3000);
 
-
-
     const button = page.locator(
       '[data-test="dashboard-if-no-panel-add-panel-btn"]'
     );
@@ -258,20 +205,28 @@ test.describe("dashboard filter testcases", () => {
     await page
       .locator('[data-test="index-dropdown-stream"]')
       .press("Control+a");
-    await page
-      .locator('[data-test="index-dropdown-stream"]')
-      .fill("geojson");
+    await page.locator('[data-test="index-dropdown-stream"]').fill("geojson");
 
+    await page.getByRole("option", { name: "geojson", exact: true }).click();
+
+    await page.waitForTimeout(2000);
     await page
-      .getByRole("option", { name: "geojson", exact: true })
+      .locator(
+        '[data-test="field-list-item-logs-geojson-lat"] [data-test="dashboard-add-latitude-data"]'
+      )
       .click();
 
-      await page.waitForTimeout(2000);
-   await page.locator('[data-test="field-list-item-logs-geojson-lat"] [data-test="dashboard-add-latitude-data"]').click();
+    await page
+      .locator(
+        '[data-test="field-list-item-logs-geojson-lon"] [data-test="dashboard-add-longitude-data"]'
+      )
+      .click();
 
-  await page.locator('[data-test="field-list-item-logs-geojson-lon"] [data-test="dashboard-add-longitude-data"]').click();
-
-  await page.locator('[data-test="field-list-item-logs-geojson-country"] [data-test="dashboard-add-weight-data"]').click();
+    await page
+      .locator(
+        '[data-test="field-list-item-logs-geojson-country"] [data-test="dashboard-add-weight-data"]'
+      )
+      .click();
 
     await page.locator('[data-test="dashboard-apply"]').click();
 
@@ -279,19 +234,33 @@ test.describe("dashboard filter testcases", () => {
 
     await page.locator('[data-test="dashboard-config-latitude"]').click();
     await page.locator('[data-test="dashboard-config-latitude"]').click();
-    await page.locator('[data-test="dashboard-config-latitude"]').fill('26.1206');
+    await page
+      .locator('[data-test="dashboard-config-latitude"]')
+      .fill("26.1206");
 
     await page.locator('[data-test="dashboard-config-longitude"]').click();
-    await page.locator('[data-test="dashboard-config-longitude"]').fill('091.6523');
+    await page
+      .locator('[data-test="dashboard-config-longitude"]')
+      .fill("091.6523");
 
     await page.locator('[data-test="dashboard-config-zoom"]').click();
-    await page.locator('[data-test="dashboard-config-zoom"]').fill('10');
+    await page.locator('[data-test="dashboard-config-zoom"]').fill("12");
     await page.locator('[data-test="dashboard-apply"]').click();
 
-// Click on the map at the given position
-await page.locator('#chart-map canvas').click({
-  position: { x: 26.1206, y: 91.6523 } // Ensure this translates correctly to pixels
-});
+    // Click on the map at the given position
+    await page.locator("#chart-map canvas").click({
+      position: { x: 26.1206, y: 91.6523 }, // Ensure this translates correctly to pixels
+    });
+    await page.waitForTimeout(5000);
+    await page.locator('[data-test="dashboard-panel-name"]').click();
+    await page
+      .locator('[data-test="dashboard-panel-name"]')
+      .fill("Dashboard_test");
+    await page.locator('[data-test="dashboard-panel-save"]').click();
 
-});
+    // Delete dashbaord
+    await page.locator('[data-test="dashboard-back-btn"]').click();
+
+    await deleteDashboard(page, randomDashboardName);
+  });
 });
