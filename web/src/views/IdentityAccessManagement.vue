7c70ec6eaf1aebca926583ecc4ea8aef1c3acaad
sai nikhil kethe <nikhil@openobserve.ai>
2025-04-01 19:14:42 +0530

    use identifier for org in usemeta org

--- web/src/views/IdentityAccessManagement.vue
+++ web/src/views/IdentityAccessManagement.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page data-test="iam-page" class="q-pa-none" style="min-height: inherit">
     <div class="flex no-wrap" style="height: calc(100vh - 50px) !important">
-      <div style="width: 160px" class="iam-tabs">
+      <div style="width: 180px" class="iam-tabs">
         <route-tabs
           ref="iamRouteTabsRef"
           dataTest="iam-tabs"
