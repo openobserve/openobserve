891b727431ac08c1e7ce3dd01a484bfd50b78047
ktx-akshay <akshay.patil@kiara.tech>
2025-04-04 12:22:32 +0530

    spell corrected

--- tests/ui-testing/pages/dashboardPages/aggregation.js
+++ tests/ui-testing/pages/dashboardPages/aggregation.js
@@ -7,7 +7,7 @@ import {
 } from "../../playwright-tests/utils/dashCreation.js";
 
 export class Aggregations{
-  constructor(page, yAxis, aggrigation_options) {
+  constructor(page, yAxis, aggregation_options) {
     this.page = page;
 
 
@@ -16,17 +16,26 @@ export class Aggregations{
     this.yAxisOption = page.locator(`[data-test="dashboard-y-item-${yAxis}"]`);
     this.yAxisDropdown = page.locator('[data-test="dashboard-y-item-dropdown"]');
 
-    //Aggrigations
-    this.yAxisDistinctOption = page.getByRole('option', { name: `${aggrigation_options}` });
+    //Aggregations
+    this.yAxisDistinctOption = page.getByRole('option', { name: `${aggregation_options}` });
 
 
   // Query Inspector Locators
      this.queryEditor = this.page.locator('[data-test="dashboard-panel-query-editor"]').getByText('distinct');
      this.queryInspectorButton = this.page.locator('[data-test="dashboard-panel-data-view-query-inspector-btn"]');
     this.queryInspectorCell = this.page.getByRole("cell", {
-        name: `SELECT histogram(_timestamp) as "x_axis_1", ${aggrigation_options === "Count (Distinct)" ? 'count(distinct' : aggrigation_options.toLowerCase()}(${yAxis})${aggrigation_options === "Count (Distinct)" ? ')' : ""} as "y_axis_1" FROM "e2e_automate" GROUP BY x_axis_1 ORDER BY x_axis_1 ASC`,
+        name: `SELECT histogram(_timestamp) as "x_axis_1", ${aggregation_options === "Count (Distinct)" ? 'count(distinct' : aggregation_options.toLowerCase()}(${yAxis})${aggregation_options === "Count (Distinct)" ? ')' : ""} as "y_axis_1" FROM "e2e_automate" GROUP BY x_axis_1 ORDER BY x_axis_1 ASC`,
         exact: true
-    }).first();    
+    }).first();  
+    
+
+    // for breakdown 
+    // this.queryInspectorCell = this.page.getByRole("cell", {
+    //   name: `SELECT histogram(_timestamp) as "x_axis_1", ${aggregation_options === "Count (Distinct)" ? 'count(distinct' : aggregation_options.toLowerCase()}(${yAxis})${aggregation_options === "Count (Distinct)" ? ')' : ""} as "y_axis_1"${breakDownOrNot ? `, ${yAxis} as "breakdown_1"` : ""} FROM "e2e_automate" GROUP BY x_axis_1 ORDER BY x_axis_1 ASC`,
+    //   exact: true
+    // }).first();
+
+
     this.queryInspectorClose = this.page.locator('[data-test="query-inspector-close-btn"]');
   }
 
