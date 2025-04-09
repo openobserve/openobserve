dcfb10e9a62c139cb188e064cfb2d1e8185763df
Loakesh Indiran <66478092+Loaki07@users.noreply.github.com>
2025-04-09 14:48:30 +0530

    feat: debug ws logs (#6498)

--- src/router/http/ws_v2/pool.rs
+++ src/router/http/ws_v2/pool.rs
@@ -44,6 +44,9 @@ impl QuerierConnectionPool {
             return if conn.is_connected().await {
                 Ok(conn.clone())
             } else {
+                log::error!(
+                    "[WS::ConnectionPool] connection to querier {querier_name} is disconnected."
+                );
                 Err(WsError::ConnectionDisconnected)
             };
         }
