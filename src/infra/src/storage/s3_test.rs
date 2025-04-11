79549225a3d843ea11deda7e53dac22691c24664
zhao.wei <weizhao029@gmail.com>
2025-04-11 09:54:15 +0800

    feat: check s3 config

---
+++ src/infra/src/storage/s3_test.rs
@@ -0,0 +1,40 @@
+// Copyright 2025 OpenObserve Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use bytes::Bytes;
+
+use crate::storage::{del, get, put};
+
+pub async fn test_s3_config() -> Result<(), anyhow::Error> {
+    let test_content = Bytes::from("Hello, S3!");
+    let test_file = "test/test2/s3_test.txt";
+
+    // Test upload
+    if let Err(e) = put(test_file, test_content.clone()).await {
+        return Err(anyhow::anyhow!("S3 upload test failed: {:?}", e));
+    }
+
+    // Test download
+    if let Err(e) = get(test_file).await {
+        return Err(anyhow::anyhow!("S3 download test failed: {:?}", e));
+    }
+
+    // Test delete
+    if let Err(e) = del(&[test_file]).await {
+        return Err(anyhow::anyhow!("S3 delete test failed: {:?}", e));
+    }
+
+    Ok(())
+}
