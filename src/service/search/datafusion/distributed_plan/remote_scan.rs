5a10cc81064c1780ca53fd7cbc01481528a31a4e
zhao.wei <weizhao029@gmail.com>
2025-04-15 19:41:58 +0800

    fix: update text

--- src/service/search/datafusion/distributed_plan/remote_scan.rs
+++ src/service/search/datafusion/distributed_plan/remote_scan.rs
@@ -531,7 +531,7 @@ impl FlightStream {
                     SearchInspectorFieldsBuilder::new()
                         .node_role(LOCAL_NODE.role.clone())
                         .node_name(self.node_name.clone())
-                        .component("remote scan end".to_string())
+                        .component("remote scan streaming".to_string())
                         .search_role("follower".to_string())
                         .duration(self.start.elapsed().as_millis() as usize)
                         .desc(format!(
