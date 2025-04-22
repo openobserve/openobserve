805fd452e69bd5d1e2cc8d3d009526e5c603ee4d
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2025-04-22 13:00:52 +0530

    comments improve

--- src/infra/src/table/migration/m20250422_000001_remove_alert_align_time.rs
+++ src/infra/src/table/migration/m20250422_000001_remove_alert_align_time.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Adds the dashboard's updated_at column
+//! Adds the alerts's align_time column
 
 use sea_orm_migration::prelude::*;
 
