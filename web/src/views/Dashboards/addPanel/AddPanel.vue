62fc6e9c66874ebdbc1d5ec9fac6ba878398aeb1
ktx-abhay <abhay.padamani@kiara.tech>
2025-04-28 17:21:58 +0530

    fix: comparison msg deep copy

--- web/src/views/Dashboards/addPanel/AddPanel.vue
+++ web/src/views/Dashboards/addPanel/AddPanel.vue
@@ -806,13 +806,15 @@ export default defineComponent({
       // console.time("add panel loadDashboard");
       await loadDashboard();
       // console.timeEnd("add panel loadDashboard");
-      
+
       // Call makeAutoSQLQuery after dashboard data is loaded
       // Only generate SQL if we're in auto query mode
-      if (!editMode.value && 
-          !dashboardPanelData.data.queries[
-            dashboardPanelData.layout.currentQueryIndex
-          ].customQuery) {
+      if (
+        !editMode.value &&
+        !dashboardPanelData.data.queries[
+          dashboardPanelData.layout.currentQueryIndex
+        ].customQuery
+      ) {
         await makeAutoSQLQuery();
       }
     });
@@ -928,8 +930,10 @@ export default defineComponent({
       if (isInitialDashboardPanelData() && !editMode.value) return false;
       //compare chartdata and dashboardpaneldata and variables data as well
       return (
-        !isEqual(chartData.value, dashboardPanelData.data) ||
-        !isEqual(variablesData, updatedVariablesData)
+        !isEqual(
+          JSON.parse(JSON.stringify(chartData.value ?? {})),
+          JSON.parse(JSON.stringify(dashboardPanelData.data ?? {})),
+        ) || !isEqual(variablesData, updatedVariablesData)
       );
     });
 
