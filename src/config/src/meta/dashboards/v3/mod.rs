0c24d6d2ec3a4afc8dae6089841639a007776700
ktx-abhay <abhay.padamani@kiara.tech>
2025-05-02 14:44:53 +0530

    add: backend changes

--- src/config/src/meta/dashboards/v3/mod.rs
+++ src/config/src/meta/dashboards/v3/mod.rs
@@ -63,6 +63,7 @@ impl From<Dashboard> for super::Dashboard {
             v3: Some(value),
             v4: None,
             v5: None,
+            v6: None,
             version,
             hash,
             updated_at,
