e9243ac4e6d83d77b107b6978cdaf1d8827b25d6
Abhay Padamani <141122205+ktx-abhay@users.noreply.github.com>
2025-05-06 13:03:31 +0530

    fix: encode search query if `ZO_UI_SQL_BASE64_ENABLED=true` (#6556)
    - #6515
    
    ---------
    
    Co-authored-by: loaki07 <rustyloaki@gmail.com>

--- web/src/composables/useLogs.ts
+++ web/src/composables/useLogs.ts
@@ -5073,6 +5073,11 @@ const useLogs = () => {
           trace_id: queryReq.traceId,
           payload: {
             query: queryReq.queryReq.query,
+            // pass encodig if enabled,
+            // make sure that `encoding: null` is not being passed, that's why used object extraction logic
+            ...(store.state.zoConfig.sql_base64_enabled
+              ? { encoding: "base64" }
+              : {}),
           } as SearchRequestPayload,
           stream_type: searchObj.data.stream.streamType,
           search_type: "ui",
