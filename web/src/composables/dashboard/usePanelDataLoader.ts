2a1e02bf7eebdd2c1cb19094b96c22130b7daf8d
Sai Nikhil <nikhil@openobserve.ai>
2025-05-07 09:46:15 +0530

    fix: 4xx error showing issue (#6732)
    Co-authored-by: ktx-abhay <abhay.padamani@kiara.tech>

--- web/src/composables/dashboard/usePanelDataLoader.ts
+++ web/src/composables/dashboard/usePanelDataLoader.ts
@@ -1503,7 +1503,11 @@ export const usePanelDataLoader = (
             ? errorDetailValue.slice(0, 300) + " ..."
             : errorDetailValue;
 
-        const errorCode = error?.response?.data?.code || error?.code || "";
+        const errorCode =
+          error?.response?.status ||
+          error?.status ||
+          error?.response?.data?.code ||
+          "";
 
         state.errorDetail = {
           message: trimmedErrorMessage,
@@ -1524,7 +1528,12 @@ export const usePanelDataLoader = (
             ? errorDetailValue.slice(0, 300) + " ..."
             : errorDetailValue;
 
-        const errorCode = error?.response?.data?.code || error?.code || "";
+        const errorCode = isWebSocketEnabled()
+          ? error?.response?.data?.code || error?.code || ""
+          : error?.response?.status ||
+            error?.status ||
+            error?.response?.data?.code ||
+            "";
 
         state.errorDetail = {
           message: trimmedErrorMessage,
