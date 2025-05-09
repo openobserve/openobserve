7baf7b119678e3914958a8bda748c9fe3e85cf9a
ktx-akshay <akshay.patil@kiara.tech>
2025-05-09 22:35:40 +0530

    added remaining test cases

--- tests/ui-testing/playwright-tests/dashboards/custom-charts.spec.js
+++ tests/ui-testing/playwright-tests/dashboards/custom-charts.spec.js
@@ -39,6 +39,10 @@ test.describe("Custom Charts Tests", () => {
   });
 
   test("Add Pictorial JSON in Monaco Editor", async ({ page }) => {
+    dashboardPanel = new DashboardPanel(page);
+    dashboardPage = new DashboardPage(page);
+    dashboardPageActions = new DashboardactionPage(page);
+
     if (!pictorialJSON) {
       console.error("Skipping test: pictorial.json not found");
       return;
@@ -61,7 +65,11 @@ test.describe("Custom Charts Tests", () => {
     ).toBeVisible();
   });
 
-  test("Add line JSON in Monaco Editor", async ({ page }) => {
+test("Add line JSON in Monaco Editor", async ({ page }) => {
+    dashboardPanel = new DashboardPanel(page);
+    dashboardPage = new DashboardPage(page);
+    dashboardPageActions = new DashboardactionPage(page);
+
     if (!lineJSON) {
       console.error("Skipping test: line.json not found");
       return;
