f3174c5a125a53a8f35380621c9213775cb1a7cd
Yashodhan Joshi <yjdoc2@gmail.com>
2025-05-12 19:48:16 +0530

    fix: ent build error
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- src/handler/http/request/ws/session.rs
+++ src/handler/http/request/ws/session.rs
@@ -454,7 +454,8 @@ async fn handle_search_event(
                     let http_response_code: u16;
                     #[cfg(feature = "enterprise")]
                     {
-                        let http_response = map_error_to_http_response(&e, trace_id.to_string());
+                        let http_response =
+                            map_error_to_http_response(&e, Some(trace_id.to_string()));
                         http_response_code = http_response.status().into();
                     }
                     // Add audit before closing
@@ -607,7 +608,8 @@ async fn handle_values_event(
                     let http_response_code: u16;
                     #[cfg(feature = "enterprise")]
                     {
-                        let http_response = map_error_to_http_response(&e, trace_id.to_string());
+                        let http_response =
+                            map_error_to_http_response(&e, Some(trace_id.to_string()));
                         http_response_code = http_response.status().into();
                     }
                     // Add audit before closing
