bb178b99e84202bfa8d17b2595d479eb4a09e711
Yashodhan Joshi <yjdoc2@gmail.com>
2025-05-14 18:30:56 +0530

    fix: add trial_ends_at col in organizations table
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

---
+++ src/infra/src/table/migration/m20250514_00001_add_trial_end_col.rs
@@ -0,0 +1,78 @@
+// Copyright 2025 OpenObserve Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Adds the dashboard's updated_at column
+
+use config::utils::time::day_micros;
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        add_updated_at_column(manager).await?;
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        // Reversing this migration is not supported.
+        Ok(())
+    }
+}
+
+// Removes the old created_at column.
+async fn add_updated_at_column(manager: &SchemaManager<'_>) -> Result<(), DbErr> {
+    let end = chrono::Utc::now().timestamp_micros() + day_micros(14);
+    if matches!(manager.get_database_backend(), sea_orm::DbBackend::MySql) {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Organizations::Table)
+                    .add_column(
+                        ColumnDef::new(Organizations::TrialEndsAt)
+                            .big_integer()
+                            .not_null()
+                            .default(end),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+    } else {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Organizations::Table)
+                    .add_column_if_not_exists(
+                        ColumnDef::new(Organizations::TrialEndsAt)
+                            .big_integer()
+                            .not_null()
+                            .default(end),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+    }
+
+    Ok(())
+}
+
+/// Identifiers used in queries on the folders table.
+#[derive(DeriveIden)]
+pub(super) enum Organizations {
+    Table,
+    TrialEndsAt,
+}
