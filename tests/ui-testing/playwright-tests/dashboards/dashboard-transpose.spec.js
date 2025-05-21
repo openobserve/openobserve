327a3bc81c9c781f84d156c682ae85cdbeb917f8
ktx-akshay <akshay.patil@kiara.tech>
2025-05-21 11:09:41 +0530

    fixed error in transpose file

--- tests/ui-testing/playwright-tests/dashboards/dashboard-transpose.spec.js
+++ tests/ui-testing/playwright-tests/dashboards/dashboard-transpose.spec.js
@@ -112,59 +112,14 @@ test.describe("dashboard UI testcases", () => {
     await dashboardList.menuItem("dashboards-item");
     await waitForDashboardPage(page);
 
-    // Create a new dashboard
-    await page.locator('[data-test="dashboard-add"]').click();
-    await page.locator('[data-test="add-dashboard-name"]').click();
-    await page
-      .locator('[data-test="add-dashboard-name"]')
-      .fill(randomDashboardName);
-    await page.locator('[data-test="dashboard-add-submit"]').click();
-
-    // Add panel to the dashboard
-    await page
-      .locator('[data-test="dashboard-if-no-panel-add-panel-btn"]')
-      .click();
-    await page
-      .locator("label")
-      .filter({ hasText: "Streamarrow_drop_down" })
-      .locator("i")
-      .click();
-    await page.getByRole("option", { name: "e2e_automate" }).click();
-
-    await page
-      .locator(
-        '[data-test="field-list-item-logs-e2e_automate-kubernetes_container_name"] [data-test="dashboard-add-y-data"]'
-      )
-      .click();
-    await page
-      .locator(
-        '[data-test="field-list-item-logs-e2e_automate-kubernetes_docker_id"] [data-test="dashboard-add-b-data"]'
-      )
-      .click();
-    await page.locator('[data-test="date-time-btn"]').click();
-    await page.locator('[data-test="date-time-relative-6-w-btn"]').click();
-    await page.locator('[data-test="dashboard-apply"]').click();
-    await page.locator('[data-test="dashboard-sidebar"]').click();
-    await page.locator('[data-test="selected-chart-table-item"] img').click();
-    await page
-      .locator('[data-test="dashboard-config-table_dynamic_columns"] div')
-      .nth(2)
-      .click();
-    await page
-      .locator('[data-test="logs-search-bar-show-query-toggle-btn"] div')
-      .nth(2)
-      .click();
-    await page
-      .locator(
-        "#fnEditor > .monaco-editor > .overflow-guard > div:nth-child(2) > .lines-content > .view-lines > .view-line"
-      )
-      .click();
-    await page
-      .locator('[data-test="dashboard-vrl-function-editor"]')
-      .locator(".inputarea")
-      .fill(".vrl=100");
-
-    await page.waitForTimeout(2000);
+    await dashboardCreate.createDashboard(randomDashboardName);
+    await dashboardCreate.addPanel();
+    await dashboardAction.addPanelName(panelName);
+    await chartTypeSelector.selectChartType("table");
+    await chartTypeSelector.selectStreamType("logs");
+    await chartTypeSelector.selectStream("e2e_automate");
+    await chartTypeSelector.searchAndAddField("kubernetes_container_name", "x");
+    await chartTypeSelector.searchAndAddField("kubernetes_pod_name", "y");
 
     await dashboardPanelConfigs.openConfigPanel();
     await dashboardPanelConfigs.selectDynamicColumns();
@@ -195,14 +150,8 @@ test.describe("dashboard UI testcases", () => {
     await chartTypeSelector.selectChartType("table");
     await chartTypeSelector.selectStreamType("logs");
     await chartTypeSelector.selectStream("e2e_automate");
-    await chartTypeSelector.searchAndAddField(
-      "kubernetes_container_name",
-      "firstcolumn"
-    );
-    await chartTypeSelector.searchAndAddField(
-      "kubernetes_pod_name",
-      "othercolumn"
-    );
+    await chartTypeSelector.searchAndAddField("kubernetes_container_name", "x");
+    await chartTypeSelector.searchAndAddField("kubernetes_pod_name", "y");
     await dashboardPanelConfigs.openConfigPanel();
     await dashboardPanelConfigs.selectTranspose();
     await dashboardPanelConfigs.selectDynamicColumns();
