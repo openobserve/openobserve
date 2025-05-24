bdc0d49ef6bfd8ae9c4ecdb1ccbc2e3b048d6ca6
sai nikhil kethe <nikhil@openobserve.ai>
2025-05-24 13:42:23 +0530

    fix: o2 ai load history in multiple instances is done

--- web/src/stores/index.ts
+++ web/src/stores/index.ts
@@ -86,6 +86,8 @@ export default createStore({
     allRoleLimitsByOrgIdByRole: {},
     modulesToDisplay: {},
     isAiChatEnabled: false,
+    currentChatTimestamp: null,
+    chatUpdated: false,
   },
   mutations: {
     login(state, payload) {
@@ -227,6 +229,12 @@ export default createStore({
     setIsAiChatEnabled(state, payload) {
       state.isAiChatEnabled = payload;
     },
+    setCurrentChatTimestamp(state, payload) {
+      state.currentChatTimestamp = payload;
+    },
+    setChatUpdated(state, payload) {
+      state.chatUpdated = payload;
+    },
   },
   actions: {
     login(context, payload) {
@@ -363,6 +371,12 @@ export default createStore({
     },
     setIsAiChatEnabled(context, payload) {
       context.commit("setIsAiChatEnabled", payload);
+    },
+    setCurrentChatTimestamp(context, payload) {
+      context.commit("setCurrentChatTimestamp", payload);
+    },
+    setChatUpdated(context, payload) {
+      context.commit("setChatUpdated", payload);
     },
   },
   modules: {},
