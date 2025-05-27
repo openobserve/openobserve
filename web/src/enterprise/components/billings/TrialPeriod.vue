047a7033346b0c72671368c73b78530b5d63975c
Bhargav <BJP232004@GMAIL.COM>
2025-05-27 10:27:55 +0530

    fix: Initial version of billing plan & trial period changes

---
+++ web/src/enterprise/components/billings/TrialPeriod.vue
@@ -0,0 +1,146 @@
+<!-- Copyright 2025 OpenObserve Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<template>
+  <div class="full-width trial-period-container q-pa-md gradient-banner" v-if="showTrialPeriodMsg == true">
+    <div class="row">
+      <div class="col">
+        <span class="o2-trial-message">{{ getTrialPeriodMessage() }}</span>
+        <br />
+        <span class="o2-trial-subtitle">Upgrade to a plan to continue enjoying the services by OpenObserve.</span>
+      </div>
+      <div class="col-2 q-mt-sm" v-if="currentPage != 'billing'">
+        <q-btn 
+          @click="redirectBilling" 
+          class="cursor-pointer text-capitalize bg-primary text-white q-px-md q-py-sm rounded-md float-right"
+          dense
+        >{{ t("billing.upgradeNow") }}</q-btn>
+      </div>
+      <div class="col-2 q-mt-sm" v-if="currentPage == 'billing'">
+        <q-btn 
+          @click="redirectContactSupport" 
+          class="cursor-pointer text-capitalize bg-primary text-white q-px-md q-py-sm rounded-md float-right"
+          dense
+        >{{ t("billing.contactSupport") }}</q-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+// @ts-ignore
+import { defineComponent, ref } from "vue";
+import { useI18n } from "vue-i18n";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import { useQuasar } from "quasar";
+import config from "@/aws-exports";
+import { siteURL } from "@/constants/config";
+
+export default defineComponent({
+  name: "TrialPeriod",
+  props: ["currentPage"],
+  methods: {
+    getTrialPeriodMessage() {
+      if(Object.hasOwn(this.store.state.organizationData.organizationSettings, "free_trial_expiry") && this.store.state.organizationData.organizationSettings.free_trial_expiry != "" && this.store.state.organizationData.organizationSettings.free_trial_expiry != null) {
+        let dueDays = this.getDueDays(this.store.state.organizationData.organizationSettings.free_trial_expiry);
+        if(dueDays >= 0) {
+          if(dueDays > 1) {
+            return `${dueDays} Days remaining in your trial account`;
+          } else {
+            return `${dueDays} Day remaining in your trial account`;
+          }
+        } else {
+          return "Your trial period has been expired.";
+        }
+      }
+    },
+  },
+  setup() {
+    const { t } = useI18n();
+    const store = useStore();
+    const q = useQuasar();
+    const router: any = useRouter();
+
+    const showTrialPeriodMsg = ref((Object.hasOwn(store.state.organizationData.organizationSettings, "free_trial_expiry") && store.state.organizationData.organizationSettings.free_trial_expiry != "" && store.state.organizationData.organizationSettings.free_trial_expiry != null) ? true : false);
+    
+    function getDueDays(microTimestamp) {
+      // Convert microseconds to milliseconds
+      const timestampMs = Math.floor(microTimestamp / 1000);
+
+      // Create date objects
+      const givenDate = new Date(timestampMs);
+      const currentDate = new Date();
+
+      // Calculate time difference in milliseconds
+      const timeDiffMs = givenDate - currentDate;
+
+      // Convert milliseconds to full days
+      const dueDays = Math.floor(timeDiffMs / (1000 * 60 * 60 * 24));
+
+      return dueDays;
+    }
+
+    const redirectBilling = () => {
+      router.push('/billings/plans/')
+    };
+
+    const redirectContactSupport = () => {
+      window.open(siteURL.contactSupport, "_blank");
+    }
+
+    return {
+      t,
+      store,
+      router,
+      config,
+      redirectBilling,
+      getDueDays,
+      showTrialPeriodMsg,
+      redirectContactSupport,
+    };
+  },
+});
+</script>
+
+<style>
+.gradient-banner {
+  background: linear-gradient(
+    to right,
+    #ffffff 70%,
+    #f7f7ff 80%,
+    #D7D7D7 90%,
+    #e7fcf2 95%,
+    #cfd9df 100%
+  );
+}
+
+.trial-period-container {
+  border: 1px solid #D7D7D7;
+}
+
+.o2-trial-message {
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 32px;
+}
+
+.o2-trial-subtitle {
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 22px;
+}
+</style>
---
+++ web/src/enterprise/components/billings/proPlan.vue
@@ -0,0 +1,138 @@
+<!-- Copyright 2023 OpenObserve Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<template>
+  <q-card class="col o2-card-wrapper">
+    <div class="row items-center justify-between q-pa-md">
+      <div>
+        <div class="o2-card-title">{{ t("billing.proPlanLabel") }}</div>
+        <div class="o2-card-subtitle">{{ t("billing.proPlanSubtitle") }}</div>
+      </div>
+      <q-chip
+        v-if="planType == planName"
+        color="indigo-1"
+        text-color="indigo-10"
+        :label="t('billing.subscribed')"
+        class="q-mt-sm text-caption"
+        dense
+      />
+    </div>
+
+    <q-separator spaced />
+
+    <div class="q-pa-md">
+      <div class="o2-page-subtitle1">{{ t("billing.features") }}</div>
+      <div class="o2-page-subtitle2 q-mb-md">{{ t("billing.included") }}</div>
+
+      <div v-for="(feature, index) in features" :key="index" class="row items-center justify-between q-mb-sm">
+        <div class="row items-center">
+          <q-icon v-if="feature.is_parent" name="check_circle_outline" color="green" size="16px" class="q-mr-sm" />
+          <q-icon v-else name="" color="green" size="16px" class="q-mr-sm" />
+          <div class="text-body2" :class="{ 'tw-font-semibold': feature.is_parent }">{{ feature.name }}</div>
+        </div>
+        <div
+          v-if="feature.price !== ''"
+          class="q-mx-sm"
+          style="flex: 1; border-top: 1px dotted #dfdfdf; height: 0;"
+        ></div>
+        <div class="text-body2 text-grey-8 text-bold">{{ feature.price }}</div>
+      </div>
+    </div>
+
+    <q-separator />
+
+    <div class="text-caption text-grey-7  q-px-md q-pt-sm">
+      {{ t("billing.unlimitedNote") }}<br />
+      {{ t("billing.paymentNote") }}
+    </div>
+
+    <div class="row justify-between q-gutter-sm  q-pa-md">
+      <q-btn
+        v-if="planType == planName"
+        :label="btnCancelSubscription"
+        text-color="black"
+        class="full-width bg-grey-4 text-bold text-capitalize text-subtitle1"
+        flat
+        @click="cancelSubscription"
+      />
+      <q-btn
+        v-else
+        :label="btnSubscribe"
+        text-color="white"
+        class="full-width bg-primary text-bold text-capitalize text-subtitle1"
+        flat
+        @click="onSubscribe"
+      />
+    </div>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useI18n } from "vue-i18n";
+
+export default defineComponent({
+  name: "proPlan",
+  props: ["planType"],
+  setup(props, { emit }) {
+    const { t } = useI18n();
+    const planName = "pay-as-you-go";
+    const btnCancelSubscription = ref(t('billing.cancelSubscription'));
+    const btnSubscribe = ref(t('billing.subscribe'));
+
+    const features = [
+      { name: 'Ingestion (Logs, Metrics, Traces)', price: '$0.30/GB' , is_parent: true},
+      { name: 'Query Volume', price: '$0.01/GB' , is_parent: true},
+      { name: 'Pipelines', price: '' , is_parent: true},
+      { name: 'Data Processed', price: '$0.10/ GB' , is_parent: false},
+      { name: 'Each additional destinations', price: '$0.30/ GB' , is_parent: false},
+      { name: 'RUM & Session Replay', price: '$1/ 1K sessions' , is_parent: true},
+      { name: 'Error Tracking', price: '$0.15/ 1K events' , is_parent: true},
+      { name: 'Action Script', price: '$1/ 1K runs' , is_parent: true},
+      { name: 'Unlimited Users', price: '' , is_parent: true},
+      { name: 'Role-Based Access Control (RBAC)', price: '' , is_parent: true},
+      { name: 'API Access', price: '' , is_parent: true},
+      { name: '15-Days Retention', price: '' , is_parent: true},
+    ];
+
+    const cancelSubscription = () => {
+      btnCancelSubscription.value = "Loading...";
+      emit("update:cancelSubscription");
+    }
+
+    const onSubscribe = () => {
+      btnSubscribe.value = "Loading...";
+      emit("update:proSubscription");
+    }
+
+    return {
+      t,
+      features,
+      cancelSubscription,
+      onSubscribe,
+      planName,
+      btnSubscribe,
+      btnCancelSubscription
+    }
+  }
+});
+</script>
+
+<style scoped>
+.full-width {
+  width: 100%;
+}
+</style>
