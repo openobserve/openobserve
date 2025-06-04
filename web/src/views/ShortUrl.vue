faac9755acba7e77d647eee84fde550aaddd77ff
udev <uddhav@openobserve.ai>
2025-06-04 18:46:06 +0530

    fix: redirect short url (#7026)
    with this POC we will let FE handle all short url redirections. We keep
    the old routes intact for backwards compatibility
    
    ---------
    
    Co-authored-by: omkark06 <omkar@zinclabs.io>

---
+++ web/src/views/ShortUrl.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="loading-container">
+    <div class="spinner"></div>
+    <div class="message">Redirecting...</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import shortURL from "@/services/short_url";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  name: "ShortUrl",
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+    const store = useStore();
+
+    const routeToHome = () => {
+      router.replace({
+        name: "home",
+      });
+    };
+
+    const handleOriginalUrl = (url: string) => {
+      const urlArray = url.split("/");
+
+      // if the url contains web, then route to the original url
+      if (urlArray.includes("web")) {
+        routeToOriginalUrl(urlArray[4]);
+      } else {
+        routeToOriginalUrl(urlArray[3]);
+      }
+    };
+
+    const routeToOriginalUrl = (url: string) => {
+      url.startsWith("/") ? router.replace(url) : router.replace("/" + url);
+    };
+
+    const fetchAndRedirect = async () => {
+      try {
+        const id = route.params.id as string;
+        const response = await shortURL.get(
+          store.state.selectedOrganization.identifier,
+          id,
+        );
+
+        if (typeof response.data === "string") {
+          handleOriginalUrl(response.data);
+        } else {
+          // Handle case where redirect URL is not found
+          routeToHome();
+        }
+      } catch (error) {
+        console.error("Error fetching short URL:", error);
+        // Redirect to home page on error
+        routeToHome();
+      }
+    };
+
+    // Execute when component is mounted
+    onMounted(() => {
+      fetchAndRedirect();
+    });
+  },
+});
+</script>
+
+<style scoped>
+.loading-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.spinner {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  border-left-color: #09f;
+  animation: spin 1s linear infinite;
+  margin-bottom: 16px;
+}
+
+.message {
+  font-size: 16px;
+  color: #666;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
