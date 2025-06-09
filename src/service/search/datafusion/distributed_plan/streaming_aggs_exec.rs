09849b2c5bb0ff4babbda832d83dfc6809e08eae
Loakesh Indiran <66478092+Loaki07@users.noreply.github.com>
2025-06-09 08:42:54 +0530

    fix: logs for streaming aggs cache (#7078)

--- src/service/search/datafusion/distributed_plan/streaming_aggs_exec.rs
+++ src/service/search/datafusion/distributed_plan/streaming_aggs_exec.rs
@@ -46,7 +46,7 @@ pub static GLOBAL_ID_CACHE: Lazy<Arc<StreamingIdCache>> =
 // init streaming cache for the id
 pub fn init_cache(id: &str, start_time: i64, end_time: i64) {
     GLOBAL_ID_CACHE.insert(id.to_string(), start_time, end_time);
-    log::debug!(
+    log::info!(
         "[StreamingAggs] init_cache: id={}, start_time={}, end_time={}",
         id,
         start_time,
@@ -58,7 +58,7 @@ pub fn init_cache(id: &str, start_time: i64, end_time: i64) {
 pub fn remove_cache(id: &str) {
     GLOBAL_CACHE.remove(id);
     GLOBAL_ID_CACHE.remove(id);
-    log::debug!("[StreamingAggs] remove_cache: id={}", id);
+    log::info!("[StreamingAggs] remove_cache: id={}", id);
 }
 
 #[derive(Debug)]
@@ -291,9 +291,14 @@ impl StreamingAggsCache {
                 self.max_entries,
                 w.len()
             );
-            if let Some(k) = w.pop_front() {
-                self.data.remove(&k);
-                GLOBAL_ID_CACHE.remove(&k);
+            if let Some(k_remove) = w.pop_front() {
+                self.data.remove(&k_remove);
+                GLOBAL_ID_CACHE.remove(&k_remove);
+                log::info!(
+                    "[StreamingAggs] [streaming_id: {}] old streaming_id removed: {}",
+                    k,
+                    k_remove
+                );
             }
         }
         w.push_back(k.clone());
