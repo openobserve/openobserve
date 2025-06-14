fa99efacb7afe122fe858352762661df6b3f260c
hengfei yang <hengfei.yang@gmail.com>
2025-06-14 14:13:59 +0800

    fix: add log when dispacth error

--- src/router/http/mod.rs
+++ src/router/http/mod.rs
@@ -169,6 +169,13 @@ async fn dispatch(
 
     let new_url = get_url(&path).await;
     if new_url.is_error {
+        log::error!(
+            "dispatch: {} to {}, get urlerror: {:?}, took: {} ms",
+            new_url.path,
+            new_url.node_addr,
+            new_url.error,
+            start.elapsed().as_millis()
+        );
         return Ok(HttpResponse::ServiceUnavailable()
             .force_close()
             .body(new_url.error.unwrap_or("internal server error".to_string())));
@@ -413,6 +420,13 @@ async fn proxy_querier_by_body(
     // get node name by consistent hash
     let Some(node_name) = cluster::get_node_from_consistent_hash(&key, &Role::Querier, None).await
     else {
+        log::error!(
+            "dispatch: {} to {}, get node from consistent hash error: {:?}, took: {} ms",
+            new_url.path,
+            new_url.node_addr,
+            "No online querier nodes",
+            start.elapsed().as_millis()
+        );
         return Ok(HttpResponse::ServiceUnavailable()
             .force_close()
             .body("No online querier nodes"));
@@ -420,6 +434,13 @@ async fn proxy_querier_by_body(
 
     // get node by name
     let Some(node) = cluster::get_cached_node_by_name(&node_name).await else {
+        log::error!(
+            "dispatch: {} to {}, get node from cache error: {:?}, took: {} ms",
+            new_url.path,
+            new_url.node_addr,
+            "No online querier nodes",
+            start.elapsed().as_millis()
+        );
         return Ok(HttpResponse::ServiceUnavailable()
             .force_close()
             .body("No online querier nodes"));
