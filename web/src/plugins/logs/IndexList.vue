1bb295df2e0c237cc08438011ef3a883ab0b0c60
Bhargav <BJP232004@GMAIL.COM>
2025-06-16 00:35:42 +0530

    fix: UI improvements

--- web/src/plugins/logs/IndexList.vue
+++ web/src/plugins/logs/IndexList.vue
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   <div
     class="column logs-index-menu full-height"
     :class="store.state.theme == 'dark' ? 'theme-dark' : 'theme-light'"
+    :key="store.state.selectedOrganization?.identifier"
   >
     <div>
       <q-select
