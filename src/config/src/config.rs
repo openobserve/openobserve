908fc8c8ea06c98084af63d7191d894df13f6b48
hengfei yang <hengfei.yang@gmail.com>
2025-06-18 09:13:23 +0800

    fix: change default value for stream stats

--- src/config/src/config.rs
+++ src/config/src/config.rs
@@ -1282,7 +1282,7 @@ pub struct Limit {
     pub job_runtime_shutdown_timeout: u64,
     #[env_config(name = "ZO_CALCULATE_STATS_INTERVAL", default = 60)] // seconds
     pub calculate_stats_interval: u64,
-    #[env_config(name = "ZO_CALCULATE_STATS_STEP_LIMIT", default = 100000)] // records
+    #[env_config(name = "ZO_CALCULATE_STATS_STEP_LIMIT", default = 1000000)] // records
     pub calculate_stats_step_limit: i64,
     #[env_config(name = "ZO_ACTIX_REQ_TIMEOUT", default = 5)] // seconds
     pub http_request_timeout: u64,
@@ -2097,7 +2097,7 @@ fn check_limit_config(cfg: &mut Config) -> Result<(), anyhow::Error> {
 
     // check for calculate stats
     if cfg.limit.calculate_stats_step_limit < 1 {
-        cfg.limit.calculate_stats_step_limit = 100000;
+        cfg.limit.calculate_stats_step_limit = 1000000;
     }
 
     Ok(())
