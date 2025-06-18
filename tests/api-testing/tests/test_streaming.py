977350195f95a8247da27d897a28f039b6f470f9
Taiming Liu <liutaiming3@gmail.com>
2025-06-17 21:09:46 -0700

    fix: match_all test case

--- tests/api-testing/tests/test_streaming.py
+++ tests/api-testing/tests/test_streaming.py
@@ -597,7 +597,7 @@ test_data_sql = [
 
     (
         "match_all query three",
-        f"SELECT * FROM \"{stream_name}\" WHERE match_all('ip-10-2-29-163.us-east-2.co*')",
+        f"SELECT * FROM \"{stream_name}\" WHERE match_all('ip-10-2-15-197.us-east-2.co*')",
         0,
         50,
         18,
