d2557f1cb491d2008f2a6a49156187b29f5367c4
ktx-akshay <akshay.patil@kiara.tech>
2025-06-19 13:00:33 +0530

    fixed the flcky test cases

--- tests/ui-testing/pages/dashboardPages/dashboard-chart.js
+++ tests/ui-testing/pages/dashboardPages/dashboard-chart.js
@@ -86,8 +86,7 @@ export default class ChartTypeSelector {
     // Click the button
     await button.click();
     //    // Clear the search input after adding the field
-    // await page.getByLabel("Clear").click();
-    await this.page.waitForTimeout(1000); // Wait for any UI updates
+    await this.page.waitForTimeout(2000); // Wait for any UI updates
     await this.page.getByLabel("Clear").click();
   }
 
