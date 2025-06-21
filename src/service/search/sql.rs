715fd5bc7362bbf90f0e8ec547c953176a6dbfbe
Huaijin <haohuaijin@gmail.com>
2025-06-21 13:41:43 +0800

    feat: secondary index str_match use tantivy index

--- src/service/search/sql.rs
+++ src/service/search/sql.rs
@@ -61,6 +61,10 @@ use super::{
     request::Request,
     utils::{conjunction, is_field, is_value, split_conjunction, trim_quotes},
 };
+use crate::service::search::{
+    datafusion::udf::{STR_MATCH_UDF_IGNORE_CASE_NAME, STR_MATCH_UDF_NAME},
+    index::get_arg_name,
+};
 
 pub static RE_ONLY_SELECT: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i)select[ ]+\*").unwrap());
 pub static RE_SELECT_FROM: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i)SELECT (.*) FROM").unwrap());
@@ -1569,9 +1573,21 @@ fn checking_inverted_index_inner(index_fields: &HashSet<&String>, expr: &Expr) -
             escape_char: _,
             any: _,
         } => checking_inverted_index_inner(index_fields, expr),
-        Expr::Function(f) => {
-            let f = f.name.to_string().to_lowercase();
-            f == MATCH_ALL_UDF_NAME || f == FUZZY_MATCH_ALL_UDF_NAME
+        Expr::Function(func) => {
+            let f = func.name.to_string().to_lowercase();
+
+            if f == MATCH_ALL_UDF_NAME || f == FUZZY_MATCH_ALL_UDF_NAME {
+                return true;
+            }
+
+            if f == STR_MATCH_UDF_NAME || f == STR_MATCH_UDF_IGNORE_CASE_NAME {
+                if let FunctionArguments::List(list) = &func.args {
+                    return list.args.len() == 2
+                        && index_fields.contains(&get_arg_name(&list.args[0]));
+                }
+            }
+
+            false
         }
         _ => false,
     }
@@ -2131,6 +2147,44 @@ mod tests {
             expected
         );
         assert_eq!(statement.to_string(), expected_sql);
+    }
+
+    // test index_visitor for str_match
+    #[test]
+    fn test_index_visitor_str_match() {
+        let sql = "SELECT * FROM t WHERE str_match(name, 'value')";
+        let mut statement = sqlparser::parser::Parser::parse_sql(&GenericDialect {}, &sql)
+            .unwrap()
+            .pop()
+            .unwrap();
+        let mut index_fields = HashSet::new();
+        index_fields.insert("name".to_string());
+        let mut index_visitor = IndexVisitor::new_from_index_fields(index_fields, true, true);
+        let _ = statement.visit(&mut index_visitor);
+        let expected = "str_match(name, 'value')";
+        assert_eq!(
+            index_visitor.index_condition.clone().unwrap().to_query(),
+            expected
+        );
+    }
+
+    // test index_visitor for str_match_ignore_case
+    #[test]
+    fn test_index_visitor_str_match_ignore_case() {
+        let sql = "SELECT * FROM t WHERE str_match_ignore_case(name, 'value')";
+        let mut statement = sqlparser::parser::Parser::parse_sql(&GenericDialect {}, &sql)
+            .unwrap()
+            .pop()
+            .unwrap();
+        let mut index_fields = HashSet::new();
+        index_fields.insert("name".to_string());
+        let mut index_visitor = IndexVisitor::new_from_index_fields(index_fields, true, true);
+        let _ = statement.visit(&mut index_visitor);
+        let expected = "str_match_ignore_case(name, 'value')";
+        assert_eq!(
+            index_visitor.index_condition.clone().unwrap().to_query(),
+            expected
+        );
     }
 
     #[test]
