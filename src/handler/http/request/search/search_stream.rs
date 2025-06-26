bcad7106e9088607cc0c00b8555e5c9c54c26f77
oasisk <ashish.j.kolhe@gmail.com>
2025-06-26 09:04:28 +0530

    fix:revert

--- src/handler/http/request/search/search_stream.rs
+++ src/handler/http/request/search/search_stream.rs
@@ -36,11 +36,12 @@ use tracing::Span;
 use crate::{
     common::{
         meta::http::HttpResponse as MetaHttpResponse,
-        utils::http::{
-            get_fallback_order_by_col_from_request, get_or_create_trace_id,
-            get_search_event_context_from_request, get_search_type_from_request,
-            get_stream_type_from_request, get_use_cache_from_request,
+        utils::{
+            get_or_create_trace_id, get_search_event_context_from_request,
+            get_search_type_from_request, get_stream_type_from_request, get_use_cache_from_request,
+            http::get_fallback_order_by_col_from_request,
         },
+        websocket::update_histogram_interval_in_query,
     },
     handler::http::request::search::{
         build_search_request_per_field, error_utils::map_error_to_http_response,
@@ -312,7 +313,39 @@ pub async fn search_http2_stream(
     }
 
     if let Some(interval) = sql.histogram_interval {
-        req.query.histogram_interval = interval;
+        // modify the sql query statement to include the histogram interval
+        if let Ok(updated_query) = update_histogram_interval_in_query(&req.query.sql, interval) {
+            req.query.sql = updated_query;
+        } else {
+            log::error!(
+                "[HTTP2_STREAM] [trace_id: {}] Failed to update query with histogram interval: {}",
+                trace_id,
+                interval
+            );
+
+            // Add audit before closing
+            #[cfg(feature = "enterprise")]
+            {
+                report_to_audit(
+                    user_id,
+                    org_id,
+                    trace_id,
+                    400,
+                    Some("Failed to update query with histogram interval".to_string()),
+                    &in_req,
+                    body_bytes,
+                )
+                .await;
+            }
+
+            return MetaHttpResponse::bad_request("Failed to update query with histogram interval");
+        }
+        log::info!(
+            "[HTTP2_STREAM] [trace_id: {}] Updated query {}; with histogram interval: {}",
+            trace_id,
+            req.query.sql,
+            interval
+        );
     }
     let req_order_by = sql.order_by.first().map(|v| v.1).unwrap_or_default();
 
