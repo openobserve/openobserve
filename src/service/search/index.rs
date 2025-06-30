95bf62bd0344cddaffd196a9ca530826fa161f02
Huaijin <haohuaijin@gmail.com>
2025-06-30 16:56:21 +0800

    chore

--- src/service/search/index.rs
+++ src/service/search/index.rs
@@ -554,7 +554,7 @@ impl Condition {
                     .trim_end_matches('*') // prefix or contains
                     .to_string();
                 let term = if get_config().common.utf8_view_enabled {
-                    Arc::new(Literal::new(ScalarValue::Utf8View(Some(value.to_string()))))
+                    Arc::new(Literal::new(ScalarValue::Utf8View(Some(format!("%{value}%")))))
                 } else {
                     Arc::new(Literal::new(ScalarValue::Utf8(Some(format!("%{value}%")))))
                 };
