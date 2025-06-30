9f676341d9e9b2fc75b8b32b7ddbc7b83bf2b2f9
sai nikhil kethe <nikhil@openobserve.ai>
2025-06-30 13:39:42 +0530

    fix: increase the wait for stream explorer

--- tests/ui-testing/playwright-tests/pipelines.spec.js
+++ tests/ui-testing/playwright-tests/pipelines.spec.js
@@ -70,7 +70,7 @@ async function exploreStreamAndNavigateToPipeline(page, streamName) {
   // Search for the stream
   await page.getByPlaceholder('Search Stream').click();
   await page.getByPlaceholder('Search Stream').fill(streamName);
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(5000);
   
   // Click on the 'Explore' button
   await page.getByRole('button', { name: 'Explore' }).first().click();
