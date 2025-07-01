74cb2418171a338dd8ae76fc91a21a5d3fc8ad3e
Subhra264 <chakrabortysubhradeep556@gmail.com>
2025-07-01 15:36:07 +0530

    feat: introduce ai agent (#7386)
    Co-authored-by: Sai Nikhil <nikhil@openobserve.ai>
    Co-authored-by: Taiming Liu <liutaiming3@gmail.com>
    Co-authored-by: udev <uddhav@openobserve.ai>
    Co-authored-by: Ashish Kolhe <35160958+oasisk@users.noreply.github.com>
    Co-authored-by: Omkar Kesarkhane <127078473+omkarK06@users.noreply.github.com>

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -15,6 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 
 <template>
+  
   <q-layout
     view="hHh Lpr lff"
     :class="[store.state.printMode === true ? 'printMode' : '']"
@@ -103,7 +104,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             @click="router.replace('/billings/plans')"
             >Upgrade to PRO Plan</q-btn
           >
-        </div>
+        </div>   
+        <q-btn
+          v-if="config.isEnterprise == 'true' && store.state.zoConfig.ai_enabled"
+          :ripple="false"
+          @click="toggleAIChat"
+          data-test="menu-link-ai-item"
+          no-caps
+          :borderless="true"
+          flat
+          dense
+          class="o2-button ai-hover-btn q-px-sm q-py-sm"
+          :class="store.state.isAiChatEnabled ? 'ai-btn-active' : ''"
+          style="border-radius: 100%;"
+          @mouseenter="isHovered = true"
+          @mouseleave="isHovered = false"
+        >
+          <div class="row items-center no-wrap tw-gap-2  ">
+            <img  :src="getBtnLogo" class="header-icon ai-icon" />
+          </div>
+        </q-btn>
         <div
           data-test="navbar-organizations-select"
           class="q-mx-sm current-organization"
@@ -157,6 +177,8 @@ class="padding-none" />
         </div> -->
 
         <ThemeSwitcher></ThemeSwitcher>
+
+
 
         <q-btn
           round
@@ -399,8 +421,10 @@ class="padding-none" />
             </q-list>
           </q-menu>
         </q-btn>
+        
       </q-toolbar>
     </q-header>
+    
 
     <q-drawer
       v-model="drawer"
@@ -419,21 +443,39 @@ class="padding-none" />
         />
       </q-list>
     </q-drawer>
-    <q-page-container
+    <div class="row full-height no-wrap">
+    <!-- Left Panel -->
+    <div
+      class="col"
+      v-show="isLoading"
+      :style="{ width: store.state.isAiChatEnabled ? '75%' : '100%' }"
       :key="store.state.selectedOrganization?.identifier"
-      v-if="isLoading"
     >
-      <router-view v-slot="{ Component }">
-        <template v-if="$route.meta.keepAlive">
-          <keep-alive>
+      <q-page-container>
+        <router-view v-slot="{ Component }">
+          <template v-if="$route.meta.keepAlive">
+            <keep-alive>
+              <component :is="Component" />
+            </keep-alive>
+          </template>
+          <template v-else>
             <component :is="Component" />
-          </keep-alive>
-        </template>
-        <template v-else>
-          <component :is="Component" />
-        </template>
-      </router-view>
-    </q-page-container>
+          </template>
+        </router-view>
+      </q-page-container>
+    </div>
+
+    <!-- Right Panel (AI Chat) -->
+
+    <div
+      class="col-auto"
+      v-show="store.state.isAiChatEnabled && isLoading"
+      style="width: 25%; max-width: 100%; min-width: 75px; z-index: 10 "
+      :class="store.state.theme == 'dark' ? 'dark-mode-chat-container' : 'light-mode-chat-container'"
+    >
+      <O2AIChat :header-height="82.5" :is-open="store.state.isAiChatEnabled" @close="closeChat" />
+    </div>
+  </div>
   </q-layout>
 </template>
 
@@ -515,6 +557,7 @@ import organizations from "@/services/organizations";
 import useStreams from "@/composables/useStreams";
 import { openobserveRum } from "@openobserve/browser-rum";
 import useSearchWebSocket from "@/composables/useSearchWebSocket";
+import O2AIChat from '@/components/O2AIChat.vue';
 
 let mainLayoutMixin: any = null;
 if (config.isCloud == "true") {
@@ -549,6 +592,7 @@ export default defineComponent({
     SlackIcon,
     ManagementIcon,
     ThemeSwitcher,
+    O2AIChat,
   },
   methods: {
     navigateToDocs() {
@@ -602,6 +646,7 @@ export default defineComponent({
     const { closeSocket } = useSearchWebSocket();
 
     const isMonacoEditorLoaded = ref(false);
+    const isHovered = ref(false);
 
     let customOrganization = router.currentRoute.value.query.hasOwnProperty(
       "org_identifier",
@@ -827,7 +872,7 @@ export default defineComponent({
         }
       }
     };
-
+    const splitterModel = ref(100);
     const selectedLanguage: any =
       langList.find((l) => l.code == getLocale()) || langList[0];
 
@@ -1217,6 +1262,28 @@ export default defineComponent({
       window.open(slackURL, "_blank");
     };
 
+    const toggleAIChat = () => {
+      const isEnabled = !store.state.isAiChatEnabled;
+      store.dispatch("setIsAiChatEnabled", isEnabled);
+      window.dispatchEvent(new Event("resize"));
+
+    };
+
+    const closeChat = () => {
+      store.dispatch("setIsAiChatEnabled", false);
+      window.dispatchEvent(new Event("resize"));
+    };
+
+    const getBtnLogo = computed(() => {
+      if (isHovered.value || store.state.isAiChatEnabled) {
+        return getImageURL('images/common/ai_icon_dark.svg')
+      }
+
+      return store.state.theme === 'dark'
+        ? getImageURL('images/common/ai_icon_dark.svg')
+        : getImageURL('images/common/ai_icon.svg')
+    })
+
     return {
       t,
       router,
@@ -1244,6 +1311,11 @@ export default defineComponent({
       openSlack,
       outlinedSettings,
       closeSocket,
+      splitterModel,
+      toggleAIChat,
+      closeChat,
+      getBtnLogo,
+      isHovered,
     };
   },
   computed: {
@@ -1563,4 +1635,47 @@ export default defineComponent({
 .header-icon {
   opacity: 0.7;
 }
+
+body.ai-chat-open {
+  .q-layout {
+    width: 75%;
+    transition: width 0.3s ease;
+  }
+}
+
+.q-layout {
+  width: 100%;
+  transition: width 0.3s ease;
+}
+
+.o2-button{
+   border-radius: 4px;
+    padding: 0px 8px;
+     color: white;
+}
+.dark-mode-chat-container{
+  border-left: 1.5px solid #232323FF ;
+}
+.light-mode-chat-container{
+  border-left: 1.5px solid #F7F7F7;
+  }
+
+  .ai-btn-active{
+    background-color: #5960b2 !important;
+  }
+  .ai-hover-btn {
+    transition: background-color 1s ease;
+  }
+
+  .ai-hover-btn:hover {
+    background-color: #5960b2; 
+  }
+
+  .ai-icon {
+    transition: transform 0.6s ease;
+  }
+
+  .ai-hover-btn:hover .ai-icon {
+    transform: rotate(-180deg);
+  }
 </style>
