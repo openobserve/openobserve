712f404bedd7daac76ff29681406a24fa16392d3
loaki07 <rustyloaki@gmail.com>
2025-07-03 14:39:21 +0530

    feat: add env to show dashboard symbol

--- src/handler/http/request/status/mod.rs
+++ src/handler/http/request/status/mod.rs
@@ -138,6 +138,7 @@ struct ConfigResponse<'a> {
     max_query_range: i64,
     histogram_enabled: bool,
     ai_enabled: bool,
+    dashboard_show_symbol_enabled: bool,
 }
 
 #[derive(Serialize)]
@@ -338,6 +339,7 @@ pub async fn zo_config() -> Result<HttpResponse, Error> {
         max_query_range: cfg.limit.default_max_query_range_days * 24,
         histogram_enabled: cfg.limit.histogram_enabled,
         ai_enabled,
+        dashboard_show_symbol_enabled: cfg.common.dashboard_show_symbol_enabled,
     }))
 }
 
