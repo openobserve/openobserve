a001e1405e5e7a2f1ec5f1b7041e3298af9ef29a
omkark06 <omkar@zinclabs.io>
2025-07-03 15:07:23 +0530

    fix: fixed error undefined store

--- web/src/plugins/logs/SearchBar.vue
+++ web/src/plugins/logs/SearchBar.vue
@@ -3808,7 +3808,7 @@ export default defineComponent({
       this.resetEditorLayout();
     },
     resetFunction(newVal) {
-      if (newVal == "" && store?.state?.savedViewFlag == false) {
+      if (newVal == "" && store && !store?.state?.savedViewFlag) {
         this.resetFunctionContent();
       }
     },
