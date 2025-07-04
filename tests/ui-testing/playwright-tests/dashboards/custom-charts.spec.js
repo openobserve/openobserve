9af5ac34f55bf84dfbf504d7f96f97703c73b5d0
ktx-akshay <akshay.patil@kiara.tech>
2025-07-04 14:11:30 +0530

    fix the failed test cases

--- tests/ui-testing/playwright-tests/dashboards/custom-charts.spec.js
+++ tests/ui-testing/playwright-tests/dashboards/custom-charts.spec.js
@@ -2,11 +2,10 @@ import { test, expect } from "../baseFixtures";
 import path from "path";
 import fs from "fs";
 import logsdata from "../../../test-data/logs_data.json";
-import { DashboardPage } from '../../pages/dashboardPage';
+import { DashboardPage } from "../../pages/dashboardPage";
 
 // Function to handle login
 async function login(page) {
-
   await page.goto(process.env["ZO_BASE_URL"]);
   if (await page.getByText("Login as internal user").isVisible()) {
     await page.getByText("Login as internal user").click();
@@ -14,8 +13,12 @@ async function login(page) {
   console.log("ZO_BASE_URL", process.env["ZO_BASE_URL"]);
   await page.waitForTimeout(1000);
 
-  await page.locator('[data-cy="login-user-id"]').fill(process.env["ZO_ROOT_USER_EMAIL"]);
-  await page.locator('[data-cy="login-password"]').fill(process.env["ZO_ROOT_USER_PASSWORD"]);
+  await page
+    .locator('[data-cy="login-user-id"]')
+    .fill(process.env["ZO_ROOT_USER_EMAIL"]);
+  await page
+    .locator('[data-cy="login-password"]')
+    .fill(process.env["ZO_ROOT_USER_PASSWORD"]);
   await page.locator('[data-cy="login-sign-in"]').click();
   await page.waitForTimeout(4000);
   await page.goto(process.env["ZO_BASE_URL"]);
@@ -29,23 +32,29 @@ async function ingestion(page) {
   ).toString("base64");
 
   const headers = {
-    "Authorization": `Basic ${basicAuthCredentials}`,
+    Authorization: `Basic ${basicAuthCredentials}`,
     "Content-Type": "application/json",
   };
-  const response = await page.evaluate(async ({ url, headers, orgId, streamName, logsdata }) => {
-    const fetchResponse = await fetch(`${url}/api/${orgId}/${streamName}/_json`, {
-      method: "POST",
+  const response = await page.evaluate(
+    async ({ url, headers, orgId, streamName, logsdata }) => {
+      const fetchResponse = await fetch(
+        `${url}/api/${orgId}/${streamName}/_json`,
+        {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify(logsdata),
+        }
+      );
+      return await fetchResponse.json();
+    },
+    {
+      url: process.env.INGESTION_URL,
       headers: headers,
-      body: JSON.stringify(logsdata)
-    });
-    return await fetchResponse.json();
-  }, {
-    url: process.env.INGESTION_URL,
-    headers: headers,
-    orgId: orgId,
-    streamName: streamName,
-    logsdata: logsdata
-  });
+      orgId: orgId,
+      streamName: streamName,
+      logsdata: logsdata,
+    }
+  );
   console.log(response);
 }
 
@@ -59,27 +68,23 @@ function readJsonFile(filename) {
   return fs.readFileSync(filePath, "utf8");
 }
 
-test.describe("Sanity Tests", () => { let dashboardPage
+test.describe("Sanity Tests", () => {
+  let dashboardPage;
   let pictorialJSON, lineJSON;
 
   test.beforeEach(async ({ page }) => {
-  
     dashboardPage = new DashboardPage(page);
-    
-});
+  });
 
   test.beforeAll(() => {
-    
     const jsonString = readJsonFile("pictorial.json");
-    pictorialJSON = jsonString
+    pictorialJSON = jsonString;
     lineJSON = readJsonFile("line.json");
   });
 
   test.beforeEach(async ({ page }) => {
-  
     await login(page);
-   
-    
+
     await page.waitForTimeout(1000);
     await ingestion(page);
   });
@@ -90,38 +95,43 @@ test.describe("Sanity Tests", () => { let dashboardPage
       return;
     }
     await dashboardPage.addCustomChart();
-  // Type the content with raw modifier to bypass autocomplete
-  await page.keyboard.insertText(pictorialJSON);
-  
-  await page.waitForTimeout(1000);
-  await page.locator('[data-test="dashboard-panel-error-bar-icon"]').click();
-  await page.locator('[data-test="dashboard-panel-query-editor"]').getByRole("textbox").fill('select * from "e2e_automate"');
-  await page.waitForTimeout(2000);
-  await page.locator('[data-test="dashboard-apply"]').click();
-  await page.waitForTimeout(3000);
-  await expect(page.getByText("Unsafe code detected: Access to 'document' is not allowed")).toBeVisible();
+    // Type the content with raw modifier to bypass autocomplete
+    await page.keyboard.insertText(pictorialJSON);
 
-  
-});
+    await page.waitForTimeout(1000);
+    // await page.locator('[data-test="dashboard-panel-error-bar-icon"]').click();
+    await page
+      .locator('[data-test="dashboard-panel-query-editor"]')
+      .getByRole("textbox")
+      .fill('select * from "e2e_automate"');
+    await page.waitForTimeout(2000);
+    await page.locator('[data-test="dashboard-apply"]').click();
+    await page.waitForTimeout(3000);
+    await expect(
+      page.getByText(
+        "Unsafe code detected: Access to 'document' is not allowed"
+      )
+    ).toBeVisible();
+  });
 
-test("Add line JSON in Monaco Editor", async ({ page }) => {
+  test("Add line JSON in Monaco Editor", async ({ page }) => {
     if (!lineJSON) {
       console.error("Skipping test: pictorial.json not found");
       return;
     }
     await dashboardPage.addCustomChart();
-  // Type the content with raw modifier to bypass autocomplete
-  await page.keyboard.insertText(lineJSON);
-  
-  await page.waitForTimeout(1000);
-  await page.locator('[data-test="dashboard-panel-error-bar-icon"]').click();
-  await page.locator('[data-test="dashboard-panel-query-editor"]').getByRole("textbox").fill('select * from "e2e_automate"');
-  await page.waitForTimeout(3000);
-  await page.locator('[data-test="dashboard-apply"]').click();
-  await page.waitForTimeout(3000);
-  // await expect(page.getByText("Unsafe code detected: Access to 'document' is not allowed")).toBeVisible();
+    // Type the content with raw modifier to bypass autocomplete
+    await page.keyboard.insertText(lineJSON);
 
-  
-});
-
+    await page.waitForTimeout(1000);
+    // await page.locator('[data-test="dashboard-panel-error-bar-icon"]').click();
+    await page
+      .locator('[data-test="dashboard-panel-query-editor"]')
+      .getByRole("textbox")
+      .fill('select * from "e2e_automate"');
+    await page.waitForTimeout(3000);
+    await page.locator('[data-test="dashboard-apply"]').click();
+    await page.waitForTimeout(3000);
+    // await expect(page.getByText("Unsafe code detected: Access to 'document' is not allowed")).toBeVisible();
+  });
 });
