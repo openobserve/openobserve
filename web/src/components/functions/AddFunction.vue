f571babcf4544367c16c2865327aebb17391ae62
sai nikhil kethe <nikhil@openobserve.ai>
2025-07-09 11:32:00 +0530

    fix: reset the input value before setting to update

--- web/src/components/functions/AddFunction.vue
+++ web/src/components/functions/AddFunction.vue
@@ -140,6 +140,7 @@ import {
   computed,
   watch,
   onUnmounted,
+  nextTick,
 } from "vue";
 
 import jsTransformService from "../../services/jstransform";
@@ -464,7 +465,12 @@ end`;
 
     const sendToAiChat = (value: any) => {
       //this is for when user in pipeline add function page and click on ai chat button
-      aiChatInputContext.value = value;
+      //here we reset the value befoere setting it because if user clears the input then again click on the same value it wont trigger the watcher that is there in the child component
+      //so to force trigger we do this
+      aiChatInputContext.value = '';
+      nextTick(() => {
+        aiChatInputContext.value = value;
+      });
       store.dispatch("setIsAiChatEnabled", true);
       //this is for when user in functions page and click on ai chat button
       emit("sendToAiChat", value);
