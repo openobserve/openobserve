0eb2fb0824d546d76308f0ad71f80831d621942c
loaki07 <rustyloaki@gmail.com>
2025-07-16 18:13:11 +0530

    fix: disable partition order for search type ui

--- src/service/search/streaming/execution.rs
+++ src/service/search/streaming/execution.rs
@@ -109,8 +109,10 @@ pub async fn do_partitioned_search(
     // unless the query is a dashboard or histogram
     let mut partition_order_by = req_order_by;
     // sort partitions in desc by _timestamp for dashboards & histograms
-    if req.search_type.expect("populate search_type") == SearchEventType::Dashboards
-        || req.query.size == -1
+    // expect if search_type is UI
+    let search_type = req.search_type.expect("populate search_type");
+    if search_type == SearchEventType::Dashboards
+        || (req.query.size == -1 && search_type != SearchEventType::UI)
     {
         partitions.sort_by(|a, b| b[0].cmp(&a[0]));
         partition_order_by = &OrderBy::Desc;
