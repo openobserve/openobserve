14894e74c22d11855208b80bfa5827da6f197357
Shrinath Rao <shnath@openobserve.ai>
2025-07-17 15:00:49 +0530

    test: Updated page manager file with correct syntax

--- tests/ui-testing/pages/page-manager.js
+++ tests/ui-testing/pages/page-manager.js
@@ -1,20 +1,20 @@
 // Centralized PageManager for dashboard-related page objects
-import { DashboardCreate } from "./dashboardPages/dashboard-create";
-import { DashboardListPage } from "./dashboardPages/dashboard-list";
-import { DashboardFolder } from "./dashboardPages/dashboard-folder";
-import { DashboardPanelActions } from "./dashboardPages/dashboard-panel-actions";
-import { DashboardPanelConfigs } from "./dashboardPages/dashboard-panel-configs";
-import { DashboardPanelEdit } from "./dashboardPages/dashboard-panel-edit";
-import { DashboardSetting } from "./dashboardPages/dashboard-settings";
-import { DashboardVariables } from "./dashboardPages/dashboard-variables";
-import { ChartTypeSelector } from "./dashboardPages/dashboard-chart";
-import { DashboardDrilldownPage } from "./dashboardPages/dashboard-drilldown";
-import { DashboardFilter } from "./dashboardPages/dashboard-filter";
-import { DashboardImport } from "./dashboardPages/dashboard-import.js";
-import { DashboardShareExportPage } from "./dashboardPages/dashboard-share-export";
-import { DashboardTimeRefresh } from "./dashboardPages/dashboard-refresh";
-import { DateTimeHelper } from "./dashboardPages/dashboard-time";
-import { LogsVisualise } from "./dashboardPages/visualise";
+import DashboardCreate from "./dashboardPages/dashboard-create";
+import DashboardListPage from "./dashboardPages/dashboard-list";
+import DashboardFolder from "./dashboardPages/dashboard-folder";
+import DashboardactionPage from "./dashboardPages/dashboard-panel-actions";
+import DashboardPanelConfigs from "./dashboardPages/dashboard-panel-configs";
+import DashboardPanel from "./dashboardPages/dashboard-panel-edit";
+import DashboardSetting from "./dashboardPages/dashboard-settings";
+import DashboardVariables from "./dashboardPages/dashboard-variables";
+import ChartTypeSelector from "./dashboardPages/dashboard-chart";
+import DashboardDrilldownPage from "./dashboardPages/dashboard-drilldown";
+import DashboardFilter from "./dashboardPages/dashboard-filter";
+import DashboardImport from "./dashboardPages/dashboard-import.js";
+import DashboardShareExportPage from "./dashboardPages/dashboard-share-export";
+import DashboardTimeRefresh from "./dashboardPages/dashboard-refresh";
+import DateTimeHelper from "./dashboardPages/dashboard-time";
+import LogsVisualise from "./dashboardPages/visualise";
 import { DashboardPage } from "./dashboardPages/dashboardPage.js";
 import { AlertsPage } from "./alertsPages/alertsPage.js";
 
@@ -56,9 +56,9 @@ class PageManager {
     this.dashboardCreate = new DashboardCreate(page);
     this.dashboardList = new DashboardListPage(page);
     this.dashboardFolder = new DashboardFolder(page);
-    this.dashboardPanelActions = new DashboardPanelActions(page);
+    this.dashboardPanelActions = new DashboardactionPage(page);
     this.dashboardPanelConfigs = new DashboardPanelConfigs(page);
-    this.dashboardPanelEdit = new DashboardPanelEdit(page);
+    this.dashboardPanelEdit = new DashboardPanel(page);
     this.dashboardSetting = new DashboardSetting(page);
     this.dashboardVariables = new DashboardVariables(page);
     this.chartTypeSelector = new ChartTypeSelector(page);
