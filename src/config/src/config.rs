ae2b3294de8326fc54125748eeceb4fd7b07b20e
hengfei yang <hengfei.yang@gmail.com>
2025-07-21 18:47:29 +0800

    fix: disable utf8view for datafusion 45

--- src/config/src/config.rs
+++ src/config/src/config.rs
@@ -1112,7 +1112,7 @@ pub struct Common {
     pub aggregation_topk_enabled: bool,
     #[env_config(name = "ZO_SEARCH_INSPECTOR_ENABLED", default = false)]
     pub search_inspector_enabled: bool,
-    #[env_config(name = "ZO_UTF8_VIEW_ENABLED", default = true)]
+    #[env_config(name = "ZO_UTF8_VIEW_ENABLED", default = false)]
     pub utf8_view_enabled: bool,
     #[env_config(
         name = "ZO_DASHBOARD_SHOW_SYMBOL_ENABLED",
