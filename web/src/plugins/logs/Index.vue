83e3564d10ec72005ac1ab1c63cba47e91afdc56
ktx-abhay <abhay.padamani@kiara.tech>
2025-07-25 16:37:01 +0530

    add: console log

--- web/src/plugins/logs/Index.vue
+++ web/src/plugins/logs/Index.vue
@@ -1468,6 +1468,8 @@ export default defineComponent({
               return;
             }
 
+            console.log("searchObj.data.queryResults", JSON.parse(JSON.stringify(searchObj.data.queryResults)));
+
             // set logs page data to searchResponseForVisualization
             if (shouldUseHistogram === true) {
               // replace hits with histogram query data
