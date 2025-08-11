4d97f07613f6566287e52d96842ee0352db862bd
ktx-akshay <akshay.patil@kiara.tech>
2025-08-11 15:57:40 +0530

    update the function

--- tests/ui-testing/pages/dashboardPages/dashboard-time.js
+++ tests/ui-testing/pages/dashboardPages/dashboard-time.js
@@ -8,11 +8,16 @@ export default class DateTimeHelper {
     this.applyQueryBtn = page.locator('[data-test="dashboard-apply"]');
     this.timePickerBtn = page.locator('[data-test="date-time-btn"]');
     this.applyTimeBtn = page.locator('[data-test="date-time-apply-btn"]');
+    this.waitForDateTimeButtonToBeEnabled = page.waitForSelector(
+      '[data-test="date-time-btn"]:not([disabled])',
+      { timeout: 15000 }
+    );
   }
   // set relative time range
   async setRelativeTimeRange(rangeCode) {
     // Minutes= m	Hours= h	Days= d	Weeks= w	Months= M
     await this.applyQueryBtn.click();
+    await this.waitForDateTimeButtonToBeEnabled();
     await this.timePickerBtn.click();
     await this.page
       .locator(`[data-test="date-time-relative-${rangeCode}-btn"]`)
