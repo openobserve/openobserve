599cb9fc753bc9f533788eb9ff983f1ea7fea019
Ashish Kolhe <ashish.j.kolhe@gmail.com>
2025-08-12 12:06:50 +0530

    fix

--- src/service/pipeline/batch_execution.rs
+++ src/service/pipeline/batch_execution.rs
@@ -764,37 +764,35 @@ async fn process_node(
                 }
                 count += 1;
             }
-            if !result_array_records.is_empty() {
-                if let Some((vrl_runtime, true)) = &vrl_runtime {
-                    let result = match apply_vrl_fn(
-                        &mut runtime,
-                        vrl_runtime,
-                        json::Value::Array(result_array_records),
-                        &org_id,
-                        &[stream_name.clone()],
-                    ) {
-                        (res, None) => res,
-                        (res, Some(error)) => {
-                            let err_msg = format!(
-                                "FunctionNode error: {}",
-                                error.get(0..500).unwrap_or(&error)
+            if !result_array_records.is_empty()
+                && let Some((vrl_runtime, true)) = &vrl_runtime
+            {
+                let result = match apply_vrl_fn(
+                    &mut runtime,
+                    vrl_runtime,
+                    json::Value::Array(result_array_records),
+                    &org_id,
+                    std::slice::from_ref(&stream_name),
+                ) {
+                    (res, None) => res,
+                    (res, Some(error)) => {
+                        let err_msg = format!(
+                            "FunctionNode error: {}",
+                            error.get(0..500).unwrap_or(&error)
+                        );  
+                        if let Err(send_err) = error_sender
+                            .send((
+                                node.id.to_string(),
+                                node.node_type(),
+                                err_msg.to_owned(),
+                                Some(func_params.name.to_owned()),
+                            ))
+                            .await
+                        {
+                            log::error!(
+                                "[Pipeline] {pipeline_name} : FunctionNode failed sending errors for collection caused by: {send_err}"
                             );
-                            if let Err(send_err) = error_sender
-                                .send((
-                                    node.id.to_string(),
-                                    node.node_type(),
-                                    err_msg.to_owned(),
-                                    Some(func_params.name.to_owned()),
-                                ))
-                                .await
-                            {
-                                log::error!(
-                                    "[Pipeline] {} : FunctionNode failed sending errors for collection caused by: {send_err}",
-                                    pipeline_name
-                                );
-                                return Ok(());
-                            }
-                            res
+                            return Ok(());
                         }
                         res
                     }
