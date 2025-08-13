abaa8c0fdd0af8913c05dff3a0d960103fc88971
Bhargav <BJP232004@GMAIL.COM>
2025-08-13 22:18:16 +0530

    fix: reverted change for live reload message

--- tests/ui-testing/playwright-tests/multiselect-stream.spec.js
+++ tests/ui-testing/playwright-tests/multiselect-stream.spec.js
@@ -173,7 +173,7 @@ await page.waitForTimeout(1000);
     });
     await page.waitForTimeout(8000);
     await expect(page.locator(".q-notification__message")).toContainText(
-      "Live mode is enabled. Only top 50 results are shown."
+      "Live mode is enabled"
     );
     await page.waitForTimeout(5000);
     await page
