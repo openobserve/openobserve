297ac8def2d9deb2cca3fb35c6a89669ffd17ff4
sai nikhil kethe <nikhil@openobserve.ai>
2025-08-13 09:37:29 +0530

    fix: show error for threshold while user provides invalid input

--- web/src/components/alerts/RealTimeAlert.vue
+++ web/src/components/alerts/RealTimeAlert.vue
@@ -113,7 +113,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                   </div>
                   <div
                     data-test="add-alert-delay-error"
-                    v-if="triggerData.silence < 0 || triggerData.silence === undefined || triggerData.silence === null"
+                    v-if="triggerData.silence < 0 || triggerData.silence === undefined || triggerData.silence === null || triggerData.silence === ''"
                     class="text-red-8 q-pt-xs q-field--error"
                     style="font-size: 11px; line-height: 12px"
                   >
