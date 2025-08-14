d0f937335446be651cb9f355703cc5516a3afe7d
sai nikhil kethe <nikhil@openobserve.ai>
2025-08-14 14:40:16 +0530

    fix: e2e failing test cases

--- tests/ui-testing/playwright-tests/Streams/multiselect-stream.spec.js
+++ tests/ui-testing/playwright-tests/Streams/multiselect-stream.spec.js
@@ -112,6 +112,8 @@ async function multistreamselect(page) {
     await page.waitForTimeout(2000);
     await page.locator('[data-test="log-search-index-list-stream-toggle-e2e_stream1"] div').nth(2).click();
     await page.waitForTimeout(4000);
+    //before we click on fn editor we need to turn on the function editor if is toggled off
+    await page.locator('[data-test="logs-search-bar-show-query-toggle-btn"] div').first().click();
     await page.locator('#fnEditor').getByRole('textbox').click()
 //   await page.locator('[data-test="log-search-index-list-stream-toggle-e2e_stream1"] div').nth(2).click({force:true});
     const cell = await page.getByRole('cell', { name: /Common Group Fields/ });
