a97d6794b1c14f710c073642508cc1400f644fe4
sai nikhil kethe <nikhil@openobserve.ai>
2025-08-18 12:38:06 +0530

    fix: alert destination table as per design

--- web/src/components/alerts/AlertsDestinationList.vue
+++ web/src/components/alerts/AlertsDestinationList.vue
@@ -15,44 +15,43 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 
 <template>
-  <q-page class="q-pa-none" style="height: 100%; min-height: inherit" >
+  <q-page class="q-pa-none" style="height: calc(100vh - 88px); min-height: inherit" >
 
-    <div v-if="!showDestinationEditor && !showImportDestination" style="height: calc(100vh - 112px); overflow-y: auto;" >
-      <div class="tw-flex tw-justify-between tw-items-center tw-px-4 tw-py-3"
-      :class="store.state.theme == 'dark' ? 'o2-table-header-dark' : 'o2-table-header-light'"
+    <div v-if="!showDestinationEditor && !showImportDestination" >
+      <div class="tw-flex tw-justify-between tw-items-center tw-px-4 tw-py-3 tw-h-[71px] tw-border-b-[1px]"
+      :class="store.state.theme == 'dark' ? 'o2-table-header-dark tw-border-gray-500' : 'o2-table-header-light tw-border-gray-200'"
       >
-        <div class="q-table__title" data-test="alert-destinations-list-title">
+        <div class="q-table__title tw-font-[600]" data-test="alert-destinations-list-title">
             {{ t("alert_destinations.header") }}
           </div>
           <div class="tw-flex tw-justify-end">
             <q-input
-            data-test="destination-list-search-input"
-            v-model="filterQuery"
-            borderless
-            filled
-            dense
-            class="q-ml-auto no-border"
-            :placeholder="t('alert_destinations.search')"
-          >
-            <template #prepend>
-              <q-icon name="search" class="cursor-pointer" />
-            </template>
-          </q-input>
+              v-model="filterQuery"
+              borderless
+              dense
+              class="q-ml-auto no-border o2-search-input"
+              :placeholder="t('function.search')"
+              :class="store.state.theme === 'dark' ? 'o2-search-input-dark' : 'o2-search-input-light'"
+            >
+              <template #prepend>
+                <q-icon class="o2-search-input-icon" :class="store.state.theme === 'dark' ? 'o2-search-input-icon-dark' : 'o2-search-input-icon-light'" name="search" />
+              </template>
+            </q-input>
           <q-btn
-            class="q-ml-md text-bold"
-            padding="sm lg"
-            outline
+            class="o2-secondary-button q-ml-md tw-h-[36px]"
+            :class="store.state.theme === 'dark' ? 'o2-secondary-button-dark' : 'o2-secondary-button-light'"
             no-caps
+            flat
             :label="t(`dashboard.import`)"
             @click="importDestination"
             data-test="destination-import"
           />
           <q-btn
             data-test="alert-destination-list-add-alert-btn"
-            class="q-ml-md text-bold no-border"
-            padding="sm lg"
-            color="secondary"
+            class="o2-primary-button q-ml-md tw-h-[36px]"
+            :class="store.state.theme === 'dark' ? 'o2-primary-button-dark' : 'o2-primary-button-light'"
             no-caps
+            flat
             :disable="!templates.length"
             :label="t(`alert_destinations.add`)"
             @click="editDestination(null)"
@@ -62,15 +61,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
       <q-table
         data-test="alert-destinations-list-table"
         ref="qTable"
-        :rows="destinations"
+        :rows="visibleRows"
         :columns="columns"
         row-key="id"
-        style="width: 100%;"
         :pagination="pagination"
-        :filter="filterQuery"
-        :filter-method="filterData"
-        class="o2-quasar-table"
-        :class="store.state.theme == 'dark' ? 'o2-quasar-table-dark' : 'o2-quasar-table-light'"
+
+        class="o2-quasar-table o2-quasar-table-header-sticky"
+        :style="hasVisibleRows
+            ? 'width: 100%; height: calc(100vh - 158px); overflow-y: auto;' 
+            : 'width: 100%'"
+        :class="store.state.theme == 'dark' ? 'o2-quasar-table-dark o2-quasar-table-header-sticky-dark o2-last-row-border-dark' : 'o2-quasar-table-light o2-quasar-table-header-sticky-light o2-last-row-border-light'"
       >
         <template #no-data>
           <div
@@ -140,19 +140,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             ></q-btn>
           </q-td>
         </template>
-        <template #top="scope">
-
-          <QTablePagination
-            :scope="scope"
-            :pageTitle="t('alert_destinations.header')"
-            :position="'top'"
-            :resultTotal="resultTotal"
-            :perPageOptions="perPageOptions"
-            @update:changeRecordPerPage="changePagination"
-          />
-        </template>
-
         <template #bottom="scope">
+          <div class="tw-flex tw-items-center tw-justify-end tw-w-full tw-h-[48px]">
+            <div class="o2-table-footer-title tw-flex tw-items-center tw-w-[200px] tw-mr-md">
+                  {{ resultTotal }} {{ t('alert_destinations.header') }}
+                </div>
           <QTablePagination
             :scope="scope"
             :position="'bottom'"
@@ -160,6 +152,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             :perPageOptions="perPageOptions"
             @update:changeRecordPerPage="changePagination"
           />
+          </div>
         </template>
         <template v-slot:header="props">
             <q-tr :props="props">
@@ -211,6 +204,7 @@ import {
   watch,
   defineComponent,
   onMounted,
+  computed,
 } from "vue";
 import type { Ref } from "vue";
 import { useI18n } from "vue-i18n";
@@ -558,6 +552,12 @@ export default defineComponent({
       });
     };
 
+    const visibleRows = computed(() => {
+      if (!filterQuery.value) return destinations.value || [];
+      return filterData(destinations.value || [], filterQuery.value);
+    });
+    const hasVisibleRows = computed(() => visibleRows.value.length > 0)
+
     return {
       t,
       qTable,
@@ -593,6 +593,8 @@ export default defineComponent({
       getDestinationByName,
       resetEditingDestination,
       selectedPerPage,
+      visibleRows,
+      hasVisibleRows,
     };
   },
 });
