c8b23a82c6909e001239f661af8353e313d8fbba
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2025-08-19 16:54:00 +0530

    fix: skip when start time > end time for delayed derived stream

--- src/config/src/meta/pipeline/components.rs
+++ src/config/src/meta/pipeline/components.rs
@@ -37,6 +37,16 @@ impl Default for PipelineSource {
     }
 }
 
+impl PipelineSource {
+    pub fn is_scheduled(&self) -> bool {
+        matches!(self, Self::Scheduled(_))
+    }
+
+    pub fn is_realtime(&self) -> bool {
+        matches!(self, Self::Realtime(_))
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
 #[serde(rename_all = "snake_case")]
 #[serde(default)]
