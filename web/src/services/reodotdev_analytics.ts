3a7f8851d07023555486b459bb4efc4d94807d20
sai nikhil kethe <nikhil@openobserve.ai>
2025-08-29 13:27:59 +0530

    fix: introduce reo dot dev

---
+++ web/src/services/reodotdev_analytics.ts
@@ -0,0 +1,61 @@
+import { ref } from "vue";
+import { loadReoScript } from "reodotdev";
+import config from "../aws-exports";
+
+interface IdentityPayload {
+  username: string;
+  type: "linkedin" | "github" | "email" | "google" | "others";
+  other_identities?: Array<{
+    username: string;
+    type: "linkedin" | "github" | "email" | "google" | "others";
+  }>;
+  firstname?: string;
+  lastname?: string;
+  company?: string;
+}
+
+const isLoaded = ref(false);
+let reoInstance: any = null;
+
+const clientID = "19d81da7206c05b";
+const source = "app";
+const enableAnalytics = config.enableAnalytics == "true";
+
+export function useReo() {
+  const reoInit = async () => {
+    if (!enableAnalytics) {
+      console.info("Analytics disabled by config.");
+      return;
+    }
+
+    try {
+    const Reo = await loadReoScript({ clientID, source });
+    Reo.init({ clientID });
+    reoInstance = Reo;
+    isLoaded.value = true;
+  } catch (error) {
+    console.error("Failed initializing Reo.Dev:", error);
+  }
+  };
+
+  const identify = (identity: IdentityPayload) => {
+    if (!enableAnalytics) {
+      console.info("Analytics disabled. Skipping identify.");
+      return;
+    }
+
+    if (!reoInstance) {
+      console.warn("Reo not initialized yet. Call init() first.");
+      return;
+    }
+
+    reoInstance.identify(identity);
+  };
+
+  return {
+    reoInit,
+    identify,
+    isLoaded,
+    reoInstance,
+  };
+}
