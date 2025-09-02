ad837e9a0afa7902258d485d7fcbab56af82e64a
omkark06 <omkar@zinclabs.io>
2025-09-02 13:05:20 +0530

    fix: set setIsInitialized to false when redirecting to logs

--- web/src/plugins/traces/TraceDetails.vue
+++ web/src/plugins/traces/TraceDetails.vue
@@ -1162,6 +1162,9 @@ export default defineComponent({
       if (!searchObj.data.traceDetails.selectedTrace) {
         return;
       }
+
+      store.dispatch("logs/setIsInitialized", false);
+
       const stream: string =
         searchObj.data.traceDetails.selectedLogStreams.join(",");
       const from =
