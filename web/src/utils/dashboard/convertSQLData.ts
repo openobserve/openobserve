8478b3ef871569d43a0f0ca77339f7aa822e59a4
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2025-09-04 17:40:05 +0530

    remove unuse code

--- web/src/utils/dashboard/convertSQLData.ts
+++ web/src/utils/dashboard/convertSQLData.ts
@@ -1281,7 +1281,6 @@ export const convertSQLData = async (
   if (options.xAxis) {
     (Array.isArray(options.xAxis) ? options.xAxis : [options.xAxis]).forEach(
       (axis) => {
-        // if (!axis.splitLine) axis.splitLine = {};
         axis.splitLine.show = showGridlines;
       },
     );
