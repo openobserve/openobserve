e86de51a97d7e50980176f831305c0f8a59f93da
Huaijin <haohuaijin@gmail.com>
2025-09-10 12:38:39 +0800

    fix: correct union query with wildcard[rc7] (#8345)

--- src/service/search/sql/mod.rs
+++ src/service/search/sql/mod.rs
@@ -65,11 +65,7 @@ use super::{
     request::Request,
     utils::{conjunction, is_field, is_value, split_conjunction, trim_quotes},
 };
-use crate::service::search::{
-    datafusion::udf::{STR_MATCH_UDF_IGNORE_CASE_NAME, STR_MATCH_UDF_NAME},
-    index::get_arg_name,
-    utils::get_field_name,
-};
+use crate::service::search::utils::get_field_name;
 
 pub static RE_ONLY_SELECT: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i)select[ ]+\*").unwrap());
 pub static RE_SELECT_FROM: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i)SELECT (.*) FROM").unwrap());
@@ -652,7 +648,6 @@ struct ColumnVisitor<'a> {
     is_wildcard: bool,
     is_distinct: bool,
     has_agg_function: bool,
-    use_inverted_index: bool,
 }
 
 impl<'a> ColumnVisitor<'a> {
@@ -668,7 +663,6 @@ impl<'a> ColumnVisitor<'a> {
             is_wildcard: false,
             is_distinct: false,
             has_agg_function: false,
-            use_inverted_index: false,
         }
     }
 }
@@ -761,34 +755,11 @@ impl VisitorMut for ColumnVisitor<'_> {
             if select.distinct.is_some() {
                 self.is_distinct = true;
             }
-            if let Some(expr) = select.selection.as_ref() {
-                // TODO: match_all only support single stream
-                if self.schemas.len() == 1 {
-                    for (_, schema) in self.schemas.iter() {
-                        let stream_settings = unwrap_stream_settings(schema.schema());
-                        let fts_fields = get_stream_setting_fts_fields(&stream_settings);
-                        let index_fields = get_stream_setting_index_fields(&stream_settings);
-                        let index_fields = itertools::chain(fts_fields.iter(), index_fields.iter())
-                            .collect::<HashSet<_>>();
-                        self.use_inverted_index =
-                            checking_inverted_index_inner(&index_fields, expr);
-                    }
-                }
-            } else if is_simple_count_query(select) || is_simple_histogram_query(select) {
-                // if there is no selection, but have histogram and fst_fields, also can use
-                // inverted index
-                if self.schemas.len() == 1 {
-                    for (_, schema) in self.schemas.iter() {
-                        let stream_settings = unwrap_stream_settings(schema.schema());
-                        let fts_fields = get_stream_setting_fts_fields(&stream_settings);
-                        let index_fields = get_stream_setting_index_fields(&stream_settings);
-                        let index_fields = itertools::chain(fts_fields.iter(), index_fields.iter())
-                            .collect::<HashSet<_>>();
-                        if !index_fields.is_empty() {
-                            self.use_inverted_index = true;
-                        }
-                    }
-                }
+        } else if let sqlparser::ast::SetExpr::SetOperation { left, right, .. } =
+            query.body.as_mut()
+        {
+            if has_wildcard(left) || has_wildcard(right) {
+                self.is_wildcard = true;
             }
         }
         if let Some(Expr::Value(Value::Number(n, _))) = query.limit.as_ref() {
@@ -804,6 +775,21 @@ impl VisitorMut for ColumnVisitor<'_> {
             }
         }
         ControlFlow::Continue(())
+    }
+}
+
+fn has_wildcard(set: &SetExpr) -> bool {
+    match set {
+        SetExpr::Select(select) => {
+            for item in select.projection.iter() {
+                if let SelectItem::Wildcard(_) = item {
+                    return true;
+                }
+            }
+            false
+        }
+        SetExpr::SetOperation { left, right, .. } => has_wildcard(left) || has_wildcard(right),
+        _ => false,
     }
 }
 
@@ -1938,55 +1924,6 @@ fn generate_table_reference(idents: &[Ident]) -> (TableReference, String) {
         let table_name = idents[1].value.clone();
         let field_name = idents[2].value.clone();
         (TableReference::partial(stream_type, table_name), field_name)
-    }
-}
-
-fn checking_inverted_index_inner(index_fields: &HashSet<&String>, expr: &Expr) -> bool {
-    match expr {
-        Expr::Identifier(Ident {
-            value,
-            quote_style: _,
-            span: _,
-        }) => index_fields.contains(value),
-        Expr::Nested(expr) => checking_inverted_index_inner(index_fields, expr),
-        Expr::BinaryOp { left, op, right } => match op {
-            BinaryOperator::And => true,
-            BinaryOperator::Or => {
-                checking_inverted_index_inner(index_fields, left)
-                    && checking_inverted_index_inner(index_fields, right)
-            }
-            BinaryOperator::Eq => checking_inverted_index_inner(index_fields, left),
-            _ => false,
-        },
-        Expr::InList {
-            expr,
-            list: _,
-            negated: _,
-        } => checking_inverted_index_inner(index_fields, expr),
-        Expr::Like {
-            negated: _,
-            expr,
-            pattern: _,
-            escape_char: _,
-            any: _,
-        } => checking_inverted_index_inner(index_fields, expr),
-        Expr::Function(func) => {
-            let f = func.name.to_string().to_lowercase();
-
-            if f == MATCH_ALL_UDF_NAME || f == FUZZY_MATCH_ALL_UDF_NAME {
-                return true;
-            }
-
-            if f == STR_MATCH_UDF_NAME || f == STR_MATCH_UDF_IGNORE_CASE_NAME {
-                if let FunctionArguments::List(list) = &func.args {
-                    return list.args.len() == 2
-                        && index_fields.contains(&get_arg_name(&list.args[0]));
-                }
-            }
-
-            false
-        }
-        _ => false,
     }
 }
 
@@ -3355,6 +3292,32 @@ mod tests {
             column_visitor.order_by,
             vec![("name".to_string(), OrderBy::Asc)]
         );
+    }
+
+    #[test]
+    fn test_column_visitor_with_wildcard() {
+        let sql = "SELECT * FROM users union select * from users";
+        let mut statement = sqlparser::parser::Parser::parse_sql(&GenericDialect {}, sql)
+            .unwrap()
+            .pop()
+            .unwrap();
+
+        let mut schemas = HashMap::new();
+        let schema = Schema::new(vec![
+            Arc::new(Field::new("name", DataType::Utf8, false)),
+            Arc::new(Field::new("age", DataType::Int32, false)),
+            Arc::new(Field::new("status", DataType::Utf8, false)),
+        ]);
+        schemas.insert(
+            TableReference::from("users"),
+            Arc::new(SchemaCache::new(schema)),
+        );
+
+        let mut column_visitor = ColumnVisitor::new(&schemas);
+        let _ = statement.visit(&mut column_visitor);
+
+        // Should extract columns, group by, order by, and detect aggregate function
+        assert!(column_visitor.is_wildcard);
     }
 
     #[test]
