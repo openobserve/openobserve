157e21e9eaec1d4f421d5bb3b6c6e9935955966f
Shrinath Rao <shnath@openobserve.ai>
2025-09-10 15:43:49 +0530

    test: Trying out testmo tool

--- tests/ui-testing/playwright.config.js
+++ tests/ui-testing/playwright.config.js
@@ -33,7 +33,10 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 5 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['junit', { outputFile: 'test-results/results.xml' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
