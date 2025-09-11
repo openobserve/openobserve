82bfd7eeef3fd53b98372b639985b9a981330233
Sai Nikhil <nikhil@openobserve.ai>
2025-09-11 17:06:18 +0530

    fix: sorting query management page rc7 (#8304)
    1. This PR fixes the issue that is related to sorting doesn't work
    properly for more number of records for execution duration and query
    range

--- web/src/components/queries/RunningQueriesList.vue
+++ web/src/components/queries/RunningQueriesList.vue
@@ -69,6 +69,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           />
         </q-td>
       </template>
+      <template #body-cell-duration="props">
+        <q-td :props="props">
+          {{ durationFormatter(props.row.duration) }}
+        </q-td>
+      </template>
+      <template #body-cell-queryRange="props">
+        <q-td :props="props">
+          {{ durationFormatter(props.row.queryRange) }}
+        </q-td>
+      </template>
 
       <template #bottom="scope">
         <q-btn
@@ -118,6 +128,7 @@ import { outlinedCancel } from "@quasar/extras/material-icons-outlined";
 import NoData from "@/components/shared/grid/NoData.vue";
 import { useStore } from "vuex";
 import QueryList from "@/components/queries/QueryList.vue";
+import { getDuration, durationFormatter } from "@/utils/zincutils";
 
 // TODO OK : Define types and interfaces for data properties.
 
@@ -289,6 +300,8 @@ export default defineComponent({
       selectedRowsModel,
       handleMultiQueryCancel,
       pagination,
+      getDuration,
+      durationFormatter,
     };
   },
 });
