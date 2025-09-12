a5488cecbcdbe833f298bdaa43602bca7e0abce8
Bhargav <BJP232004@GMAIL.COM>
2025-09-12 12:25:48 +0530

    fix: trace id added for values api

--- web/src/plugins/logs/IndexList.vue
+++ web/src/plugins/logs/IndexList.vue
@@ -1321,6 +1321,7 @@ export default defineComponent({
                     searchObj.meta.clusters.length > 0
                       ? searchObj.meta.clusters.join(",")
                       : "",
+                  traceparent: generateTraceContext().traceId,
                 });
 
                 if (res.data.hits.length) {
