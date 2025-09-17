6240bf40f76732204a01c9f805ec8ae899dc392c
sai nikhil kethe <nikhil@openobserve.ai>
2025-09-17 19:11:41 +0530

    revert: fix: add histogram error message support in logs page FE

--- web/src/plugins/logs/SearchResult.vue
+++ web/src/plugins/logs/SearchResult.vue
@@ -52,19 +52,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
               <span style="font-size: 14px">Fetching the search events</span>
             </q-tooltip>
           </span>
-          <span
-            v-else-if="!searchObj.loadingCounter && searchObj.data.histogram_function_error.length > 0"
-            class="q-ml-md"
-          >
-            <q-icon name="warning" color="warning" size="sm" />
-            <q-tooltip position="top" class="tw-text-sm tw-font-semibold tw-w-[70%]" >
-              <ul class="tw-list-disc tw-ml-4 tw-space-y-1">
-                <li v-for="(err, idx) in searchObj.data.histogram_function_error" :key="idx">
-                  {{ err }}
-                </li>
-              </ul>
-            </q-tooltip>
-          </span>
           <div
             v-else-if="
               searchObj.data.histogram.errorCode == -1 &&
