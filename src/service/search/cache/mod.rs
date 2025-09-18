9e1ea8c731f20e84cd235fbd5127eab51f3fd09d
Ashish Kolhe <ashish.j.kolhe@gmail.com>
2025-09-18 23:19:18 +0530

    fix

--- src/service/search/cache/mod.rs
+++ src/service/search/cache/mod.rs
@@ -166,7 +166,7 @@ pub async fn search(
                     limit: v.limit,
                     file_path: file_path.clone(),
                     cache_query_response,
-                    clear_cache: true && !is_streaming,
+                    clear_cache: !is_streaming,
                     ..Default::default()
                 }
             }
