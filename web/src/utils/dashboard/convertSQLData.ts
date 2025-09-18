709a14e745e89c558dead0825dc60f0bbf3c5831
Abhay Padamani <141122205+ktx-abhay@users.noreply.github.com>
2025-09-18 14:11:16 +0530

    fix: dashboard panel render issue rc7 (#8518)

--- web/src/utils/dashboard/convertSQLData.ts
+++ web/src/utils/dashboard/convertSQLData.ts
@@ -79,7 +79,7 @@ export const convertMultiSQLData = async (
         store,
         chartPanelRef,
         hoveredSeriesState,
-        [resultMetaData.value[i]],
+        [resultMetaData.value[i][0]],
         { queries: [metadata.queries[i]] },
         chartPanelStyle,
         annotations,
