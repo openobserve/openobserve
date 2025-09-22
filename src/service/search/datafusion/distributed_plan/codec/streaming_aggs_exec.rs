572803dd2f4270b0bad44d3468f097d9c98f52c8
Huaijin <haohuaijin@gmail.com>
2025-09-22 14:49:10 +0800

    fmt and add test case

--- src/service/search/datafusion/distributed_plan/codec/streaming_aggs_exec.rs
+++ src/service/search/datafusion/distributed_plan/codec/streaming_aggs_exec.rs
@@ -40,7 +40,7 @@ pub(crate) fn try_decode(
     };
     let extension_codec = super::get_physical_extension_codec("test".to_string());
     // TODO: after https://github.com/apache/datafusion/issues/17596 release
-    // we can remove the org_id 
+    // we can remove the org_id
     let mut ctx = SessionContext::new();
     register_udf(&ctx, org_id)?;
     datafusion_functions_json::register_all(&mut ctx)?;
@@ -108,12 +108,17 @@ pub(crate) fn try_encode(node: Arc<dyn ExecutionPlan>, buf: &mut Vec<u8>) -> Res
 mod tests {
     use datafusion::{
         arrow::datatypes::{DataType, Field, Schema},
+        common::config::ConfigOptions,
         functions_aggregate::count::count_udaf,
-        physical_expr::aggregate::AggregateExprBuilder,
+        physical_expr::{
+            ScalarFunctionExpr,
+            aggregate::AggregateExprBuilder,
+            expressions::{col, lit},
+        },
         physical_plan::{
             aggregates::{AggregateExec, AggregateMode, PhysicalGroupBy},
             empty::EmptyExec,
-            expressions::{col, lit},
+            filter::FilterExec,
         },
     };
     use datafusion_proto::bytes::{
@@ -122,6 +127,7 @@ mod tests {
     use o2_enterprise::enterprise::search::datafusion::distributed_plan::streaming_aggs_exec::StreamingAggsExec;
 
     use super::*;
+    use crate::service::search::datafusion::udf::str_match_udf::STR_MATCH_UDF;
 
     #[tokio::test]
     async fn test_datafusion_codec() -> Result<()> {
@@ -178,6 +184,100 @@ mod tests {
         assert_eq!(plan.target_partitions(), plan2.target_partitions());
         assert_eq!(plan.is_complete_cache_hit(), plan2.is_complete_cache_hit());
         assert_eq!(plan.cached_files(), plan2.cached_files());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_datafusion_codec_with_filter() -> Result<()> {
+        // Create schema with string field for str_match UDF
+        let schema = Arc::new(Schema::new(vec![
+            Field::new("a", DataType::Int32, false),
+            Field::new("message", DataType::Utf8, false),
+        ]));
+
+        // Create str_match UDF expression: str_match(message, "test")
+        let str_match_expr = Arc::new(ScalarFunctionExpr::try_new(
+            STR_MATCH_UDF.clone().into(),
+            vec![col("message", &schema)?, lit("test")],
+            &schema,
+            Arc::new(ConfigOptions::default()),
+        )?);
+
+        // Create FilterExec with str_match UDF
+        let filter_exec = Arc::new(FilterExec::try_new(
+            str_match_expr,
+            Arc::new(EmptyExec::new(Arc::clone(&schema))),
+        )?);
+
+        // Create grouping set for aggregation
+        let grouping_set = PhysicalGroupBy::new(
+            vec![(col("a", &schema)?, "a".to_string())],
+            vec![],
+            vec![vec![false]],
+        );
+
+        // Create aggregates
+        let aggregates = vec![Arc::new(
+            AggregateExprBuilder::new(count_udaf(), vec![lit(1i32)])
+                .schema(Arc::clone(&schema))
+                .alias("COUNT(1)")
+                .build()?,
+        )];
+
+        // Create AggregateExec with FilterExec as input
+        let agg_plan = AggregateExec::try_new(
+            AggregateMode::Partial,
+            grouping_set.clone(),
+            aggregates.clone(),
+            vec![None],
+            filter_exec.clone(),
+            Arc::clone(&schema),
+        )?;
+
+        let start_time = config::utils::time::now_micros();
+        let end_time = start_time + 1000;
+        let plan: Arc<dyn ExecutionPlan> = Arc::new(StreamingAggsExec::new(
+            "456".to_string(),
+            start_time,
+            end_time,
+            vec![],
+            Arc::new(agg_plan.clone()) as Arc<dyn ExecutionPlan>,
+            1,
+            false,
+            Arc::new(agg_plan),
+        ));
+
+        // encode
+        let proto = super::super::get_physical_extension_codec("test".to_string());
+        let plan_bytes = physical_plan_to_bytes_with_extension_codec(plan.clone(), &proto).unwrap();
+
+        // decode
+        let ctx = datafusion::prelude::SessionContext::new();
+        // Register UDFs in the context
+        ctx.register_udf(STR_MATCH_UDF.clone());
+        let plan2 = physical_plan_from_bytes_with_extension_codec(&plan_bytes, &ctx, &proto)?;
+        let plan2 = plan2.as_any().downcast_ref::<StreamingAggsExec>().unwrap();
+        let plan = plan.as_any().downcast_ref::<StreamingAggsExec>().unwrap();
+
+        // check
+        assert_eq!(plan.id(), plan2.id());
+        assert_eq!(plan.start_time(), plan2.start_time());
+        assert_eq!(plan.end_time(), plan2.end_time());
+        assert_eq!(plan.target_partitions(), plan2.target_partitions());
+        assert_eq!(plan.is_complete_cache_hit(), plan2.is_complete_cache_hit());
+        assert_eq!(plan.cached_files(), plan2.cached_files());
+
+        // Verify that the aggregate plan contains a FilterExec
+        let agg_plan_decoded = plan2.aggregate_plan();
+        let agg_exec = agg_plan_decoded
+            .as_any()
+            .downcast_ref::<AggregateExec>()
+            .unwrap();
+        let input_plan = agg_exec.input();
+
+        // The input should be a FilterExec
+        assert!(input_plan.as_any().downcast_ref::<FilterExec>().is_some());
 
         Ok(())
     }
