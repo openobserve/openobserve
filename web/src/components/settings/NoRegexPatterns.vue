3eebe3fb95316e3358253d96efa650947270569d
sai nikhil kethe <nikhil@openobserve.ai>
2025-09-29 16:49:07 +0530

    fix: rename regex patterns to sensitive data redaction

--- web/src/components/settings/NoRegexPatterns.vue
+++ web/src/components/settings/NoRegexPatterns.vue
@@ -25,10 +25,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         style="width: 125px; margin: 20vh auto 1rem"
       />
       <span class="title-text">{{ t("regex_patterns.no_data") }}</span>
-        <span class="subtitle-text">Import Regex patterns from your Library or <span class="create-new-text" @click="createNewRegexPattern">Create New</span></span>
+        <span class="subtitle-text">Import patterns from your Library or <span class="create-new-text" @click="createNewRegexPattern">Create New</span></span>
         <div class="import-button-container">
             <q-btn no-caps class="q-mt-sm" @click="importRegexPattern">
-              <span class="import-button-text">Import Regex Pattern</span>
+              <span class="import-button-text">Import Pattern</span>
             </q-btn>
         </div>    
     </div>
