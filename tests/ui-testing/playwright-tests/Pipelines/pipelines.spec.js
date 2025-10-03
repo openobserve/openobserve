0ca36d37ad99f5781aca38b4f782977236e4036e
sai nikhil kethe <nikhil@openobserve.ai>
2025-10-03 10:49:44 +0530

    fix: add pipeline back btn locator removed instead use cancel

--- tests/ui-testing/playwright-tests/Pipelines/pipelines.spec.js
+++ tests/ui-testing/playwright-tests/Pipelines/pipelines.spec.js
@@ -117,7 +117,7 @@ async function exploreStreamAndInteractWithLogDetails(page, streamName) {
 
 async function deletePipeline(page, randomPipelineName) {
   // Click the back button
-  await page.locator('[data-test="add-pipeline-back-btn"]').click();
+  await page.locator('[data-test="add-pipeline-cancel-btn"]').click();
   await page.locator('[data-test="confirm-button"]').click();
   await page.waitForTimeout(2000);
 
