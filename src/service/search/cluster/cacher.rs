cc724258cc7ad6e9d39ba7e11212b4ab567e939d
Loakesh Indiran <66478092+Loaki07@users.noreply.github.com>
2025-10-07 20:23:22 +0530

    feat: cache hist query with non ts col order by#8731 (#8758)
    ref: https://github.com/openobserve/openobserve/pull/8731
    
    ---------
    
    Signed-off-by: loaki07 <rustyloaki@gmail.com>

--- src/service/search/cluster/cacher.rs
+++ src/service/search/cluster/cacher.rs
@@ -214,6 +214,7 @@ pub async fn get_cached_results(
             ts_column,
             discard_interval: cache_req.discard_interval,
             is_descending: cache_req.is_descending,
+            is_histogram_non_ts_order: cache_req.is_histogram_non_ts_order, // Forward from original request
         },
     )
     .await
