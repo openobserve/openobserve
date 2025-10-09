bb3c136e7c1e79ba29fd1180f9cb55058afbc677
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2025-10-09 11:37:43 +0530

    refactor: streamline annotation fetching logic

--- web/src/composables/dashboard/usePanelDataLoader.ts
+++ web/src/composables/dashboard/usePanelDataLoader.ts
@@ -1250,14 +1250,6 @@ export const usePanelDataLoader = (
                   },
                   error: handleSearchError,
                   complete: async (payload: any) => {
-                    // get annotations
-                    const annotationList = shouldFetchAnnotations()
-                      ? await refreshAnnotations(
-                          startISOTimestamp,
-                          endISOTimestamp,
-                        )
-                      : [];
-                    state.annotations = annotationList;
                     state.loading = false;
                     saveCurrentStateToCache();
                     removeTraceId(traceId);
