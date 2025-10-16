b3cf6defc6d3126297f9ea44f687c3caf11ba2af
Bhargav <BJP232004@GMAIL.COM>
2025-10-16 16:44:20 +0530

    fix: header menu items

--- web/src/components/ThemeSwitcher.vue
+++ web/src/components/ThemeSwitcher.vue
@@ -15,7 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 
 <template>
-  <q-btn class="round-button" round flat dense :ripple="false" @click="toggleDarkMode">
+  <q-btn class="round-button" flat dense :ripple="false" @click="toggleDarkMode">
     <q-icon :name="DarkModeIcon" class="header-icon"></q-icon>
     <q-tooltip anchor="top middle" self="bottom middle">
       Switch to {{ darkMode ? "Light Mode" : "Dark Mode" }}
