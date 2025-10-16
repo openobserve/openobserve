0a9b06966383b883e5093fe271b405208b073bed
sai nikhil kethe <nikhil@openobserve.ai>
2025-10-16 13:49:17 +0530

    fix: add group page modified

--- web/src/components/iam/groups/AddGroup.vue
+++ web/src/components/iam/groups/AddGroup.vue
@@ -14,13 +14,15 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
-  <q-card class="column full-height">
-    <q-card-section class="tw-w-full q-py-md">
-      <div data-test="add-group-section">
-        <div class="flex justify-between items-center q-py-sm">
+  <q-card class="o2-side-dialog column full-height">
+    <q-card-section class="q-py-md tw-w-full">
+      <div class="row items-center no-wrap q-py-sm">
+        <div class="col">
           <div data-test="add-group-section-title" style="font-size: 18px">
             {{ t("iam.addGroup") }}
           </div>
+        </div>
+        <div class="col-auto">
           <q-icon
             data-test="add-group-close-dialog-btn"
             name="cancel"
@@ -29,55 +31,54 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             @click="emits('cancel:hideform')"
           />
         </div>
-        <q-separator class="tw-w-full" />
-        <div class=" q-mt-md o2-input">
-          <div data-test="add-group-groupname-input-btn">
-            <q-input
-              v-model.trim="name"
-              :label="t('common.name') + ' *'"
-              color="input-border"
-              bg-color="input-bg"
-              class="q-py-md showLabelOnTop"
-              stack-label
-              outlined
-              filled
-              dense
-              :rules="[
-                (val: any, rules: any) =>
-                  !!val
-                    ? isValidGroupName ||
-                      `Use alphanumeric and '_' characters only, without spaces.`
-                    : t('common.nameRequired'),
-              ]"
-              maxlength="100"
-            >
-              <template v-slot:hint>
-                Use alphanumeric and '_' characters only, without spaces.
-              </template>
-            </q-input>
-          </div>
+      </div>
 
-          <div class="flex justify-start q-mt-sm">
-            <q-btn
-              v-close-popup
-              class="q-mr-md o2-secondary-button tw-h-[36px]"
-              :label="t('alerts.cancel')"
-              no-caps
-              flat
-              :class="store.state.theme === 'dark' ? 'o2-secondary-button-dark' : 'o2-secondary-button-light'"
-              @click="$emit('cancel:hideform')"
-              data-test="add-group-cancel-btn"
-            />
-            <q-btn
-              class="o2-primary-button no-border tw-h-[36px]"
-              :label="t('alerts.save')"
-              no-caps
-              flat
-              :class="store.state.theme === 'dark' ? 'o2-primary-button-dark' : 'o2-primary-button-light'"
-              @click="saveGroup"
-              data-test="add-group-submit-btn"
-            />
-          </div>
+      <q-separator />
+      <div data-test="add-group-section">
+        <q-input
+          v-model.trim="name"
+          :label="t('common.name') + ' *'"
+          class="showLabelOnTop tw-mt-2"
+          stack-label
+          hide-bottom-space
+          borderless
+          dense
+          :rules="[
+            (val: any, rules: any) =>
+              !!val
+                ? isValidGroupName ||
+                  `Use alphanumeric and '_' characters only, without spaces.`
+                : t('common.nameRequired'),
+          ]"
+          maxlength="100"
+          data-test="add-group-groupname-input-btn"
+        >
+          <template v-slot:hint>
+            Use alphanumeric and '_' characters only, without spaces.
+          </template>
+        </q-input>
+
+        <div class="flex justify-start tw-mt-6">
+          <q-btn
+            v-close-popup
+            class="q-mr-md o2-secondary-button tw-h-[36px]"
+            :label="t('alerts.cancel')"
+            no-caps
+            flat
+            :class="store.state.theme === 'dark' ? 'o2-secondary-button-dark' : 'o2-secondary-button-light'"
+            @click="$emit('cancel:hideform')"
+            data-test="add-group-cancel-btn"
+          />
+          <q-btn
+            :disable="!name || !isValidGroupName"
+            class="o2-primary-button no-border tw-h-[36px]"
+            :label="t('alerts.save')"
+            no-caps
+            flat
+            :class="store.state.theme === 'dark' ? 'o2-primary-button-dark' : 'o2-primary-button-light'"
+            @click="saveGroup"
+            data-test="add-group-submit-btn"
+          />
         </div>
       </div>
     </q-card-section>
@@ -94,10 +95,6 @@ import { useReo } from "@/services/reodotdev_analytics";
 
 const { t } = useI18n();
 const props = defineProps({
-  width: {
-    type: String,
-    default: "30vw",
-  },
   group: {
     type: Object,
     default: () => null,
@@ -156,4 +153,3 @@ const saveGroup = () => {
 };
 </script>
 
-<style scoped></style>
