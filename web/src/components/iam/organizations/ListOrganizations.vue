9e5d6edbe2481ea014903458200b4cbe470019e0
sai nikhil kethe <nikhil@openobserve.ai>
2025-10-16 14:57:14 +0530

    fix: iam list tables modified as per new UI

--- web/src/components/iam/organizations/ListOrganizations.vue
+++ web/src/components/iam/organizations/ListOrganizations.vue
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <!-- eslint-disable vue/v-on-event-hyphenation -->
 <!-- eslint-disable vue/attribute-hyphenation -->
 <template>
-  <q-page class="q-pa-none" style="min-height: inherit; height: calc(100vh - 57px);">
+  <q-page class="q-pa-none card-container" style="min-height: inherit; height: calc(100vh - 42px);">
       <div class="tw-flex tw-justify-between tw-items-center tw-px-4 tw-py-3 tw-h-[71px] tw-border-b-[1px]"
       :class="store.state.theme =='dark' ? 'o2-table-header-dark tw-border-gray-500' : 'o2-table-header-light tw-border-gray-200'"
       style="position: sticky; top: 0; z-index: 1000 ;"
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             </q-input>
           
             <q-btn
-              class="q-ml-md o2-primary-button tw-h-[36px]"
+              class="q-ml-sm o2-primary-button tw-h-[36px]"
               flat
               :class="store.state.theme === 'dark' ? 'o2-primary-button-dark' : 'o2-primary-button-light'"
               no-caps
