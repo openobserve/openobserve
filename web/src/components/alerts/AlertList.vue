5bf2ab4fc54aeea5a83fad241def2b5c9b48b1b6
sai nikhil kethe <nikhil@openobserve.ai>
2025-10-24 10:53:59 +0530

    fix: icons changed in logs page

--- web/src/components/alerts/AlertList.vue
+++ web/src/components/alerts/AlertList.vue
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   <div
     data-test="alert-list-page"
     class="q-pa-none flex flex-col"
-    style="height: calc(100vh - 60px);"
   >
     <div class="tw-w-full tw-px-[0.625rem] tw-mb-[0.625rem]" v-if="!showAddAlertDialog && !showImportAlertDialog">
       <div class="card-container">
