c2a730bd41929c504a908051ebef2a1e566efc1f
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2025-10-27 15:15:54 +0530

    fix: update layout by adding card-container class to editors and panels for html, markdown and custom chart

--- web/src/components/dashboards/addPanel/CustomChartEditor.vue
+++ web/src/components/dashboards/addPanel/CustomChartEditor.vue
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 <template>
   <div
-    class="markdown-editor"
+    class="markdown-editor card-container"
     style="width: 100%; height: 100%; overflow: auto"
   >
     <div style="width: 100%; height: calc(100% - 1px)">
