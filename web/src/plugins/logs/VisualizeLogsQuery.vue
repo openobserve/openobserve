0c7ddc03b4b16b018cf16a1a5f0a4ea8e11f9503
Bhargav <BJP232004@GMAIL.COM>
2025-10-28 18:24:01 +0530

    fix: splitter issue resize fix

--- web/src/plugins/logs/VisualizeLogsQuery.vue
+++ web/src/plugins/logs/VisualizeLogsQuery.vue
@@ -105,7 +105,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           </template>
           <template #after>
             <div
-              class="row card-container tw-ml-[0.625rem]"
+              class="row card-container"
               :style="{
                 height: '100%',
                 width: dashboardPanelData.layout.showFieldList
