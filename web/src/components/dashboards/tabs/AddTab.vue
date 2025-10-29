c9708bd324522b736da9de5dc45d6dde71849366
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2025-10-29 16:04:26 +0530

    fix: update button styles and add hover effects for AddTab and TabsSettings components

--- web/src/components/dashboards/tabs/AddTab.vue
+++ web/src/components/dashboards/tabs/AddTab.vue
@@ -63,20 +63,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         <div class="flex justify-start">
           <q-btn
             v-close-popup="true"
-            class="o2-secondary-button tw-h-[36px]"
             :label="t('dashboard.cancel')"
-            text-color="light-text"
-            no-caps
+            class="o2-secondary-button tw-h-[36px]"
+            :class="store.state.theme === 'dark' ? 'o2-secondary-button-dark' : 'o2-secondary-button-light'"
+            flat
             data-test="dashboard-add-cancel"
           />
           <q-btn
             :disable="tabData.name.trim() === ''"
             :loading="onSubmit.isLoading.value"
             :label="t('dashboard.save')"
-            class="o2-primary-button q-ml-md tw-h-[36px]"
-            type="submit"
-            no-caps
+            class="o2-primary-button tw-h-[36px] q-ml-md"
+            :class="store.state.theme === 'dark' ? 'o2-primary-button-dark' : 'o2-primary-button-light'"
+            flat
             borderless
+            type="submit"
             data-test="dashboard-add-tab-submit"
           />
         </div>
