b6d70c7487b7920541c03670f452c415eda3b766
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2025-10-30 14:54:32 +0530

    fix: update button styles in ScheduledDashboards and ViewDashboard components

--- web/src/views/Dashboards/ScheduledDashboards.vue
+++ web/src/views/Dashboards/ScheduledDashboards.vue
@@ -76,9 +76,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
             <q-btn
               data-test="alert-list-add-alert-btn"
-              class="q-ml-md text-bold no-border"
-              color="secondary"
-              no-caps
+              class="o2-primary-button tw-h-[36px] q-ml-md"
+              :class="store.state.theme === 'dark' ? 'o2-primary-button-dark' : 'o2-primary-button-light'"
+              flat
               :label="t(`dashboard.newReport`)"
               @click="createNewReport"
             />
