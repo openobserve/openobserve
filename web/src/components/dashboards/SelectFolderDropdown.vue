ad0b49026524e8beeb8a09f188a1f7d9d604e9b0
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2025-10-31 18:17:23 +0530

    fix: add custom class to SelectFolderDropdown and adjust margin-top in app.scss

--- web/src/components/dashboards/SelectFolderDropdown.vue
+++ web/src/components/dashboards/SelectFolderDropdown.vue
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
       behavior="menu"
       borderless
       dense
-      class="showLabelOnTop no-case tw-mr-1 tw-mt-[1px]"
+      class="showLabelOnTop no-case tw-mr-1 tw-mt-[1px] o2-custom-select-dashboard"
       style="width: calc(100% - 44px)"
     >
       <template #no-option>
