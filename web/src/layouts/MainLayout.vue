f84a760a636f2d830a19a059eb87797ed3409df7
Omkar Kesarkhane <127078473+omkarK06@users.noreply.github.com>
2025-11-05 13:10:29 +0530

    fix: Service Graph UI improvements (#8958)

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -633,7 +633,6 @@ import {
   outlinedDescription,
   outlinedCode,
   outlinedDevices,
-  outlinedBubbleChart,
 } from "@quasar/extras/material-icons-outlined";
 import SlackIcon from "@/components/icons/SlackIcon.vue";
 import ManagementIcon from "@/components/icons/ManagementIcon.vue";
@@ -816,12 +815,6 @@ export default defineComponent({
           icon: outlinedAccountTree,
           link: "/traces",
           name: "traces",
-        },
-        {
-          title: "Service Graph",
-          icon: outlinedBubbleChart,
-          link: "/service-graph",
-          name: "serviceGraph",
         },
         {
           title: t("menu.rum"),
