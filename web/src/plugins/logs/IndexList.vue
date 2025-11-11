6c82adfd8b675a03139f074954027fe349720c5d
sai nikhil kethe <nikhil@openobserve.ai>
2025-11-11 07:22:57 +0530

    fix: remove resetting pagination everytime we toggle

--- web/src/plugins/logs/IndexList.vue
+++ web/src/plugins/logs/IndexList.vue
@@ -991,8 +991,6 @@ export default defineComponent({
     watch(
       () => searchObj.meta.quickMode,
       (isActive) => {
-        //reset pagintion whenever user toggles on / off quick mode 
-        resetPagination();
 
         if (isActive) {
           // check if its present in the array dont add it again
