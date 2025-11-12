0690bbd6b2591d44e6896f7b1600b45a67219c88
omkark06 <omkar@zinclabs.io>
2025-11-12 17:59:20 +0530

    fix: session UI improvements

--- web/src/components/common/sidebar/FieldList.vue
+++ web/src/components/common/sidebar/FieldList.vue
@@ -411,7 +411,7 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .traces-field-table {
-  height: calc(100vh - 200px) !important;
+  height: calc(100vh - 212px) !important;
 }
 .q-menu {
   box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.1);
