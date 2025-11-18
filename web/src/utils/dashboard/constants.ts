b36166a8045aa1c9b930c7d180ca14015d25f915
ktx-abhay <abhay.padamani@kiara.tech>
2025-11-18 12:40:30 +0530

    fix: code commenting

--- web/src/utils/dashboard/constants.ts
+++ web/src/utils/dashboard/constants.ts
@@ -1,6 +1,7 @@
+// variables constans
 export const SELECT_ALL_VALUE = "_o2_all_";
 
 export const CUSTOM_VALUE = "::_o2_custom";
 
-
+// add panel general constants
 export const MAX_FIELD_LABEL_CHARS = 50;
