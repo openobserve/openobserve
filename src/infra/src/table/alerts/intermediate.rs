8927ec31960caee03593957da4bd8c6466fc5d2d
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2025-11-19 23:51:40 +0530

    fix reviews

--- src/infra/src/table/alerts/intermediate.rs
+++ src/infra/src/table/alerts/intermediate.rs
@@ -619,32 +619,32 @@ impl From<StreamType> for MetaStreamType {
 }
 
 /// Row template type. Stored in the DB as a 16-bit integer.
-pub enum RowTemplateType {
+pub enum RowTemplateTypeDb {
     String,
     Json,
 }
 
-impl RowTemplateType {
+impl RowTemplateTypeDb {
     const STRING: i16 = 0;
     const JSON: i16 = 1;
 }
 
-impl From<RowTemplateType> for i16 {
-    fn from(value: RowTemplateType) -> Self {
+impl From<RowTemplateTypeDb> for i16 {
+    fn from(value: RowTemplateTypeDb) -> Self {
         match value {
-            RowTemplateType::String => RowTemplateType::STRING,
-            RowTemplateType::Json => RowTemplateType::JSON,
+            RowTemplateTypeDb::String => RowTemplateTypeDb::STRING,
+            RowTemplateTypeDb::Json => RowTemplateTypeDb::JSON,
         }
     }
 }
 
-impl TryFrom<i16> for RowTemplateType {
+impl TryFrom<i16> for RowTemplateTypeDb {
     type Error = FromI16Error;
 
     fn try_from(value: i16) -> Result<Self, Self::Error> {
         match value {
-            Self::STRING => Ok(RowTemplateType::String),
-            Self::JSON => Ok(RowTemplateType::Json),
+            Self::STRING => Ok(RowTemplateTypeDb::String),
+            Self::JSON => Ok(RowTemplateTypeDb::Json),
             _ => Err(FromI16Error {
                 value,
                 ty: "RowTemplateType".to_owned(),
@@ -653,20 +653,20 @@ impl TryFrom<i16> for RowTemplateType {
     }
 }
 
-impl From<config::meta::alerts::alert::RowTemplateType> for RowTemplateType {
+impl From<config::meta::alerts::alert::RowTemplateType> for RowTemplateTypeDb {
     fn from(value: config::meta::alerts::alert::RowTemplateType) -> Self {
         match value {
-            config::meta::alerts::alert::RowTemplateType::String => RowTemplateType::String,
-            config::meta::alerts::alert::RowTemplateType::Json => RowTemplateType::Json,
+            config::meta::alerts::alert::RowTemplateType::String => RowTemplateTypeDb::String,
+            config::meta::alerts::alert::RowTemplateType::Json => RowTemplateTypeDb::Json,
         }
     }
 }
 
-impl From<RowTemplateType> for config::meta::alerts::alert::RowTemplateType {
-    fn from(value: RowTemplateType) -> Self {
+impl From<RowTemplateTypeDb> for config::meta::alerts::alert::RowTemplateType {
+    fn from(value: RowTemplateTypeDb) -> Self {
         match value {
-            RowTemplateType::String => config::meta::alerts::alert::RowTemplateType::String,
-            RowTemplateType::Json => config::meta::alerts::alert::RowTemplateType::Json,
+            RowTemplateTypeDb::String => config::meta::alerts::alert::RowTemplateType::String,
+            RowTemplateTypeDb::Json => config::meta::alerts::alert::RowTemplateType::Json,
         }
     }
 }
--- src/infra/src/table/alerts/mod.rs
+++ src/infra/src/table/alerts/mod.rs
@@ -67,7 +67,7 @@ impl TryFrom<alerts::Model> for MetaAlert {
             .transpose()?;
         let trigger_frequency_type: intermediate::TriggerFrequencyType =
             value.trigger_frequency_type.try_into()?;
-        let row_template_type: intermediate::RowTemplateType =
+        let row_template_type: intermediate::RowTemplateTypeDb =
             value.row_template_type.try_into()?;
 
         // Transform database JSON values into intermediate types which can be
@@ -602,7 +602,7 @@ fn update_mutable_fields(
         .transpose()?;
     let row_template = Some(alert.row_template).filter(|s| !s.is_empty());
     let row_template_type: i16 =
-        intermediate::RowTemplateType::from(alert.row_template_type).into();
+        intermediate::RowTemplateTypeDb::from(alert.row_template_type).into();
     let description = Some(alert.description).filter(|s| !s.is_empty());
     let enabled = alert.enabled;
     let tz_offset = alert.tz_offset;
