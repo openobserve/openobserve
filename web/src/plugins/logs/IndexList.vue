a2e995c12fe514092b2ed49464c3390fa5f856a0
sai nikhil kethe <nikhil@openobserve.ai>
2025-12-04 12:05:02 +0530

    fix: issue 9285

--- web/src/plugins/logs/IndexList.vue
+++ web/src/plugins/logs/IndexList.vue
@@ -64,6 +64,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                 :model-value="selected"
                 size="20px"
                 @update:model-value="toggleOption(opt.value)"
+                @click.stop
               />
             </q-item-section>
           </q-item>
