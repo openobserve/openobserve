59389dcd6c9a43c47e6e8c095b5e3e90b6ee7d37
omkark06 <omkar@zinclabs.io>
2025-12-10 16:16:31 +0530

    fix: added startTimeUs and endTimeUs

--- web/src/plugins/traces/TraceDetails.vue
+++ web/src/plugins/traces/TraceDetails.vue
@@ -427,6 +427,7 @@ import {
   formatTimeWithSuffix,
   getImageURL,
   convertTimeFromNsToMs,
+  convertTimeFromNsToUs,
 } from "@/utils/zincutils";
 import TraceTimelineIcon from "@/components/icons/TraceTimelineIcon.vue";
 import ServiceMapIcon from "@/components/icons/ServiceMapIcon.vue";
@@ -1130,8 +1131,10 @@ export default defineComponent({
       return {
         [store.state.zoConfig.timestamp_column]:
           span[store.state.zoConfig.timestamp_column],
+        startTimeUs: convertTimeFromNsToUs(span.start_time),
         startTimeMs: convertTimeFromNsToMs(span.start_time),
         endTimeMs: convertTimeFromNsToMs(span.end_time),
+        endTimeUs: convertTimeFromNsToUs(span.end_time),
         durationMs: span?.duration ? Number((span?.duration / 1000).toFixed(4)) : 0, // This key is standard, we use for calculating width of span block. This should always be in ms
         durationUs: span?.duration ? Number(span?.duration?.toFixed(4)) : 0, // This key is used for displaying duration in span block. We convert this us to ms, s in span block
         idleMs: span.idle_ns ? convertTime(span.idle_ns) : 0,
