2869bab5b27600f0faab68334637c895cea7fbdf
Ashish Kolhe <35160958+oasisk@users.noreply.github.com>
2025-12-17 17:14:25 +0530

    fix: organizations ep permisions issue (#9667)
    ### **PR Type**
    Bug fix
    
    
    ___
    
    ### **Description**
    - Prefer META_ORG_ID user in organizations endpoint
    
    - Fallback to first user when META membership missing
    
    - Applied changes to both validate_credentials functions
    
    
    ___
    
    
    
    <details> <summary><h3> File Walkthrough</h3></summary>
    
    <table><thead><tr><th></th><th align="left">Relevant
    files</th></tr></thead><tbody><tr><td><strong>Bug
    fix</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>validator.rs</strong><dd><code>Prefer META org user in
    validator</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    src/handler/http/auth/validator.rs
    
    <ul><li>Added logic to find user in <code>config::META_ORG_ID</code>
    within <code>all_users</code><br> <li> Fallback to first user if META
    org user absent<br> <li> Added explanatory comments for permission
    rationale<br> <li> Applied changes in both
    <code>validate_credentials</code> and
    <br><code>validate_credentials_ext</code></ul>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/openobserve/openobserve/pull/9667/files#diff-f4d4b51574b378a8c372345ea48875834a4eadacfac33fa4d0ceca1e0ef96b35">+14/-2</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    </table></td></tr></tr></tbody></table>
    
    </details>
    
    ___

--- src/handler/http/auth/validator.rs
+++ src/handler/http/auth/validator.rs
@@ -170,7 +170,13 @@ pub async fn validate_credentials(
                 if all_users.is_empty() {
                     None
                 } else {
-                    all_users.first().cloned()
+                    // For organizations endpoint, specifically look for user in _meta org
+                    // since permission check at line 966 expects the user to be in _meta
+                    all_users
+                        .iter()
+                        .find(|u| u.org == config::META_ORG_ID)
+                        .cloned()
+                        .or_else(|| all_users.first().cloned())
                 }
             }
             Err(e) => {
@@ -351,7 +357,13 @@ pub async fn validate_credentials_ext(
                 if all_users.is_empty() {
                     None
                 } else {
-                    all_users.first().cloned()
+                    // For organizations endpoint, specifically look for user in _meta org
+                    // since permission check at line 966 expects the user to be in _meta
+                    all_users
+                        .iter()
+                        .find(|u| u.org == config::META_ORG_ID)
+                        .cloned()
+                        .or_else(|| all_users.first().cloned())
                 }
             }
             Err(_) => None,
