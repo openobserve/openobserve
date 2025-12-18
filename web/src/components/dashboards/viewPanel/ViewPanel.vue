591314d1c8d5e074f6ffb6c636dbed1da4496600
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2025-12-18 18:42:27 +0530

    same as main

--- web/src/components/dashboards/viewPanel/ViewPanel.vue
+++ web/src/components/dashboards/viewPanel/ViewPanel.vue
@@ -114,7 +114,7 @@
           <div class="col" style="height: 100%">
             <div class="layout-panel-container col" style="height: 100%">
               <VariablesValueSelector
-                :variablesConfig="filteredVariablesConfig"
+                :variablesConfig="currentDashboardData.data?.variables"
                 :showDynamicFilters="
                   currentDashboardData.data?.variables?.showDynamicFilters
                 "
@@ -276,7 +276,6 @@ import { isEqual } from "lodash-es";
 import { processQueryMetadataErrors } from "@/utils/zincutils";
 import { outlinedWarning } from "@quasar/extras/material-icons-outlined";
 import { symOutlinedDataInfoAlert } from "@quasar/extras/material-symbols-outlined";
-import { getScopeType } from "@/utils/dashboard/variables/variablesScopeUtils";
 import { useVariablesManager } from "@/composables/dashboard/useVariablesManager";
 import { defineAsyncComponent } from "vue";
 
@@ -618,51 +617,6 @@ export default defineComponent({
       }
     };
 
-    // Computed property to filter variables based on current panel and tab context
-    const filteredVariablesConfig = computed(() => {
-      // Get current panel ID and tab ID from route query parameters
-      const currentPanelId = (route.query.panelId as string) || props.panelId;
-      const currentTabId = route.query.tab as string;
-
-      // Return early if no variables are configured
-      if (
-        !currentDashboardData.data?.variables ||
-        !currentDashboardData.data.variables.list
-      ) {
-        return currentDashboardData.data?.variables || { list: [] };
-      }
-
-      // Filter variables based on scope
-      const filteredList = currentDashboardData.data.variables.list.filter(
-        (variable: any) => {
-          const scopeType = getScopeType(variable);
-
-          // Always include global variables
-          if (scopeType === "global") {
-            return true;
-          }
-
-          // Include tab-level variables if they belong to the current tab
-          if (scopeType === "tabs" && currentTabId) {
-            return variable.tabs.includes(currentTabId);
-          }
-
-          // Include panel-level variables if they belong to the current panel
-          if (scopeType === "panels" && currentPanelId) {
-            return variable.panels.includes(currentPanelId);
-          }
-
-          return false;
-        },
-      );
-
-      // Return the filtered variables config
-      return {
-        ...currentDashboardData.data.variables,
-        list: filteredList,
-      };
-    });
-
     watch(selectedDate, () => {
       updateDateTime(selectedDate.value);
     });
@@ -835,7 +789,6 @@ export default defineComponent({
       errorMessage,
       outlinedWarning,
       symOutlinedDataInfoAlert,
-      filteredVariablesConfig,
       showLegendsDialog,
       currentPanelData,
       panelSchemaRendererRef,
