75095b0f618ad6b20ecf26436d0c543c8c5060cb
Yashodhan Joshi <yjdoc2@gmail.com>
2025-12-24 11:06:59 +0530

    fix: remove arduino protoc action
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

---
+++ .github/protoc.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+
+PLATFORM=$(uanme -p)
+
+if [[ $PLATFORM -eq "x86_64" ]]; then
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protoc-21.12-linux-x86_64.zip
+  unzip protoc-21.12-linux-x86_64.zip -d protoc
+elif [[ $PLATFORM -eq "aarch64" ]]
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protoc-21.12-linux-aarch_64.zip
+  unzip protoc-21.12-linux-aarch_64.zip -d protoc
+else
+  echo "unsupported platform $PLATFORM"
+fi
+
+sudo cp protoc/bin/protoc /usr/local/bin/
+sudo cp -r protoc/include/google /usr/local/include/
+
+echo "successfully installed protoc"
