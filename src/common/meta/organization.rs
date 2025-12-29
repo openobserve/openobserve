4e164e844cbdf94ffcf9509b1739f830962e8a30
Ashish Kolhe <ashish.j.kolhe@gmail.com>
2025-12-29 11:57:52 +0530

    fixes

--- src/common/meta/organization.rs
+++ src/common/meta/organization.rs
@@ -63,8 +63,13 @@ pub struct OrganizationCreationResponse {
 #[derive(Serialize, Deserialize, ToSchema, Clone, Debug)]
 pub struct ServiceAccountTokenInfo {
     pub email: String,
+    /// Token is no longer returned directly for security reasons
+    /// Use the assume_role API to obtain temporary session tokens
+    #[serde(skip_serializing)]
     pub token: String,
     pub role: String,
+    /// Instructions for obtaining a temporary session token
+    pub message: String,
 }
 
 #[derive(Serialize, Deserialize, ToSchema, Clone, Debug)]
---
+++ src/infra/src/table/migration/m20251226_000001_add_expires_at_to_sessions.rs
@@ -0,0 +1,50 @@
+// Copyright 2025 OpenObserve Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Sessions::Table)
+                    .add_column(ColumnDef::new(Sessions::ExpiresAt).big_integer().null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Sessions::Table)
+                    .drop_column(Sessions::ExpiresAt)
+                    .to_owned(),
+            )
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Sessions {
+    Table,
+    ExpiresAt,
+}
