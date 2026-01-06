9e736c6bac08a407a10d39b731a2b996cd52c61d
Uddhav Dave <uddhav@openobserve.ai>
2026-01-06 17:39:50 +0530

    org selection

---
+++ web/src/services/awsMarketplace.ts
@@ -0,0 +1,64 @@
+// Copyright 2025 OpenObserve Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import http from "./http";
+
+export interface LinkSubscriptionRequest {
+  token: string;
+}
+
+export interface LinkSubscriptionResponse {
+  success: boolean;
+  customer_identifier: string;
+  message?: string;
+}
+
+export interface ActivationStatusResponse {
+  status: "pending_activation" | "active" | "payment_failed" | "cancelled";
+  customer_identifier: string;
+  org_id: string;
+  message?: string;
+  activated_at?: string;
+}
+
+const awsMarketplace = {
+  /**
+   * Link an AWS Marketplace subscription to an organization
+   * Called after user is redirected from AWS Marketplace with a token
+   */
+  linkSubscription: (
+    org_identifier: string,
+    token: string
+  ): Promise<{ data: LinkSubscriptionResponse }> => {
+    return http().post(`/api/${org_identifier}/aws-marketplace/link-subscription`, {
+      token,
+    });
+  },
+
+  /**
+   * Check the activation status of an AWS Marketplace subscription
+   * Poll this endpoint to wait for subscribe-success SNS notification
+   */
+  getActivationStatus: (
+    org_identifier: string,
+    customer_id: string
+  ): Promise<{ data: ActivationStatusResponse }> => {
+    return http().get(
+      `/api/${org_identifier}/aws-marketplace/activation-status?customer_id=${customer_id}`
+    );
+  },
+};
+
+export default awsMarketplace;
