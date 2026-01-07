5fa7b0678cfae60e6de009a5baace33212a57dce
hengfeiyang <hengfei.yang@gmail.com>
2026-01-07 23:29:35 +0800

    docs: update license header

--- src/handler/http/request/search/saved_view.rs
+++ src/handler/http/request/search/saved_view.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 OpenObserve Inc.
+// Copyright 2026 OpenObserve Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
