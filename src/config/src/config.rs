24641413e338e6465992e6f18ed2a69e79614d3d
Huaijin <haohuaijin@gmail.com>
2026-01-08 16:00:12 +0800

    update

--- src/config/src/config.rs
+++ src/config/src/config.rs
@@ -1385,7 +1385,7 @@ pub struct Limit {
     pub metrics_max_points_per_series: usize,
     #[env_config(name = "ZO_METRICS_CACHE_MAX_ENTRIES", default = 10000)]
     pub metrics_cache_max_entries: usize,
-    #[env_config(name = "ZO_METRICS_INLIST_FILTER_ENABLED", default = false)]
+    #[env_config(name = "ZO_METRICS_INLIST_FILTER_ENABLED", default = true)]
     pub metrics_inlist_filter_enabled: bool,
     #[env_config(name = "ZO_COLS_PER_RECORD_LIMIT", default = 1000)]
     pub req_cols_per_record_limit: usize,
