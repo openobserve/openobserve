d9e9ea49ad67ba58f2b80b2640509e56ef99bd47
sai nikhil kethe <nikhil@openobserve.ai>
2026-01-20 23:36:05 +0530

    fix: remove router-view from the incidentList

--- web/src/components/alerts/IncidentList.vue
+++ web/src/components/alerts/IncidentList.vue
@@ -16,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 <template>
   <div data-test="incident-list" class="flex q-mt-xs">
-    <router-view />
     <div class="tw:w-full tw:h-full tw:px-[0.625rem] tw:pb-[0.625rem]">
       <!-- Header with title and search -->
       <div class="card-container tw:mb-[0.625rem]">
