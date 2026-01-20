06691a38e5f1560ef8c84790f05f4ea0b521179b
omkark06 <omkar@zinclabs.io>
2026-01-20 12:55:51 +0530

    removed headers

--- src/handler/http/router/mod.rs
+++ src/handler/http/router/mod.rs
@@ -85,10 +85,6 @@ pub fn cors_layer() -> CorsLayer {
             header::HeaderName::from_lowercase(b"x-openobserve-span-id").unwrap(),
             header::HeaderName::from_lowercase(b"x-openobserve-trace-id").unwrap(),
             header::HeaderName::from_lowercase(b"x-openobserve-sampled").unwrap(),
-            header::HeaderName::from_lowercase(b"x-datadog-parent-id").unwrap(),
-            header::HeaderName::from_lowercase(b"x-datadog-trace-id").unwrap(),
-            header::HeaderName::from_lowercase(b"x-datadog-sampling-priority").unwrap(),
-            header::HeaderName::from_lowercase(b"x-datadog-origin").unwrap(),
         ])
         .allow_origin(AllowOrigin::mirror_request())
         .allow_credentials(true)
