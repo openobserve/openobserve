8dd10c8805bb7e83a897c9328bc2ffa2b1f53ea2
Harsh Mahajan <007harshmahajan@gmail.com>
2026-02-05 22:23:50 +0530

    fix

--- src/handler/http/request/traces/mod.rs
+++ src/handler/http/request/traces/mod.rs
@@ -312,11 +312,11 @@ pub async fn get_latest_traces(
         sum(_o2_llm_usage_details_total) as llm_usage_details_total, \
         sum(_o2_llm_cost_details_total) as llm_cost_details_total, \
         FIRST_VALUE(_o2_llm_input ORDER BY {TIMESTAMP_COL_NAME} ASC) as llm_input \
-        FROM {stream_name}"
+        FROM \"{stream_name}\""
         )
     } else {
         format!(
-            "SELECT trace_id, min({TIMESTAMP_COL_NAME}) as zo_sql_timestamp, min(start_time) as trace_start_time, max(end_time) as trace_end_time FROM {stream_name}"
+            "SELECT trace_id, min({TIMESTAMP_COL_NAME}) as zo_sql_timestamp, min(start_time) as trace_start_time, max(end_time) as trace_end_time FROM \"{stream_name}\""
         )
     };
     let query_sql = if filter.is_empty() {
@@ -453,7 +453,7 @@ pub async fn get_latest_traces(
         .collect::<Vec<String>>()
         .join("','");
     let query_sql = format!(
-        "SELECT {TIMESTAMP_COL_NAME}, trace_id, start_time, end_time, duration, service_name, operation_name, span_status FROM {stream_name} WHERE trace_id IN ('{trace_ids}') ORDER BY {TIMESTAMP_COL_NAME} ASC"
+        "SELECT {TIMESTAMP_COL_NAME}, trace_id, start_time, end_time, duration, service_name, operation_name, span_status FROM \"{stream_name}\" WHERE trace_id IN ('{trace_ids}') ORDER BY {TIMESTAMP_COL_NAME} ASC"
     );
     req.query.from = 0;
     req.query.size = 9999;
