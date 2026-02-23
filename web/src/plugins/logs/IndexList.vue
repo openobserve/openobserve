b4d679ece7230af970baa7f9f291a001f76468de
omkark06 <omkar@zinclabs.io>
2026-02-23 16:52:37 +0530

    fix: reverted change to fix test cases for now

--- web/src/plugins/logs/IndexList.vue
+++ web/src/plugins/logs/IndexList.vue
@@ -1226,6 +1226,16 @@ export default defineComponent({
           };
         }
 
+        // Initialize stream-specific values if not exists
+        if (!streamFieldValues.value[fieldName]) {
+          streamFieldValues.value[fieldName] = {};
+        }
+
+        if (!streamFieldValues.value[fieldName][streamName])
+          streamFieldValues.value[fieldName][streamName] = {
+            values: [],
+          };
+
         // Process the results
         if (response.content.results.hits.length) {
           // Store stream-specific values
