214c08b5bda6e9c4f14dce4b050a90c180aea879
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2026-03-09 12:26:30 +0530

    fix: scheduled pipeline test run query should use ui search type and rbac fixes for alert history and generate_sql apis

--- web/src/components/pipeline/NodeForm/ScheduledPipeline.vue
+++ web/src/components/pipeline/NodeForm/ScheduledPipeline.vue
@@ -2340,7 +2340,7 @@ const runQuery = async () => {
           page_type: selectedStreamType.value,
           validate: true,
         },
-        "derived_stream",
+        "ui",
       )
       .then((res: any) => {
         if (res.data.hits.length > 0) {
