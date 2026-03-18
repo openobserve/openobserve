5218f0184fdf5ea572126171a56472b20ca84a7b
loaki07 <rustyloaki@gmail.com>
2026-03-18 10:13:23 +0530

    fix: allow missing destinations field for anomaly detection alert creation

--- src/handler/http/models/alerts/mod.rs
+++ src/handler/http/models/alerts/mod.rs
@@ -72,6 +72,7 @@ pub struct Alert {
 
     /// List of destination names to notify when alert fires.
     /// Destinations must be pre-configured in the system.
+    #[serde(default)]
     #[schema(example = json!(["slack-alerts", "pagerduty"]))]
     pub destinations: Vec<String>,
 
