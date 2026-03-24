8e0d61744661ae4c67e3f1cf51b506cac19748b3
Yashodhan Joshi <yjdoc2@gmail.com>
2026-03-24 16:40:21 +0530

    fix: run db and migration init for sorcemap tests
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- src/service/sourcemaps/mod.rs
+++ src/service/sourcemaps/mod.rs
@@ -528,9 +528,19 @@ mod tests {
 
     use std::collections::HashSet;
 
-    use super::{super::db::sourcemaps::*, *};
+    use super::{
+        super::{super::migration, db::sourcemaps::*},
+        *,
+    };
 
     async fn upload_zip(svc: &str, env: &str, version: &str) {
+        migration::init_db().await.unwrap();
+        // ensure database tables are created
+        infra::db::create_table().await.unwrap();
+        // db migration steps, since it's separated out
+        infra::table::migrate().await.unwrap();
+        infra::init().await.unwrap();
+
         delete_group(
             "default",
             Some(svc.into()),
