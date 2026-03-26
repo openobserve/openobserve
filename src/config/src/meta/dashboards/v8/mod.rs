951eaf516efb4c5e55b96ff0b77ba02261454d7e
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2026-03-26 13:53:16 +0530

    pr suggestion

--- src/config/src/meta/dashboards/v8/mod.rs
+++ src/config/src/meta/dashboards/v8/mod.rs
@@ -309,9 +309,10 @@ pub struct AxisArgValue {
     pub stream_alias: Option<String>,
 }
 
-#[derive(Debug, Clone, PartialEq, Hash, Serialize, Deserialize, ToSchema)]
+#[derive(Debug, Clone, PartialEq, Hash, Serialize, Deserialize, ToSchema, Default)]
 #[serde(rename_all = "lowercase")]
 pub enum JoinType {
+    #[default]
     Inner,
     Left,
     Right,
@@ -405,15 +406,18 @@ pub struct StreamFieldObj {
     pub stream_alias: Option<String>,
 }
 
-#[derive(Debug, Clone, PartialEq, Hash, Serialize, Deserialize, ToSchema)]
+#[derive(Debug, Clone, PartialEq, Hash, Serialize, Deserialize, ToSchema, Default)]
 #[serde(rename_all = "lowercase")]
 pub enum FilterConditionType {
+    #[default]
     List,
     Condition,
+    #[serde(other)]
+    Unknown,
 }
 
-#[derive(Debug, Clone, PartialEq, Hash, Serialize, Deserialize, ToSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Hash, Serialize, Deserialize, ToSchema, Default)]
+#[serde(default, rename_all = "camelCase")]
 pub struct FilterCondition {
     #[serde(rename = "type")]
     pub typ: FilterConditionType,
