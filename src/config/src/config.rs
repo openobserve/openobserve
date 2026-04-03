87cca93cf634058439b31c3aa3f38904e3f7ff59
Huaijin <haohuaijin@gmail.com>
2026-04-03 19:29:11 +0800

    feat: support s3 infrequence acces storage class

--- src/config/src/config.rs
+++ src/config/src/config.rs
@@ -2061,6 +2061,12 @@ pub struct S3 {
     pub feature_bulk_delete: bool,
     #[env_config(name = "ZO_S3_ALLOW_INVALID_CERTIFICATES", default = false)]
     pub allow_invalid_certificates: bool,
+    #[env_config(
+        name = "ZO_S3_FEATURE_FORCE_INFREQUENT_ACCESS",
+        default = false,
+        help = "Use STANDARD_IA storage class for compliance storage type"
+    )]
+    pub feature_force_infrequent_access: bool,
     #[env_config(name = "ZO_S3_SYNC_TO_CACHE_INTERVAL", default = 600)] // seconds
     pub sync_to_cache_interval: u64,
     #[env_config(name = "ZO_S3_MAX_RETRIES", default = 10)]
