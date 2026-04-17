8366353287a0964d34ef13c932dcd9f82dd1b68e
Bhargav <BJP232004@GMAIL.COM>
2026-04-17 16:13:21 +0530

    fix: alert list page scroll fix

--- web/src/components/alerts/AlertList.vue
+++ web/src/components/alerts/AlertList.vue
@@ -194,7 +194,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                 style="width: 100%"
                 :style="
                   filteredResults?.length
-                    ? 'width: 100%; height: calc(100vh  - var(--navbar-height) - 77px)'
+                    ? 'width: 100%; height: calc(100vh  - var(--navbar-height) - 87px)'
                     : 'width: 100%'
                 "
                 class="o2-quasar-table o2-row-md o2-quasar-table-header-sticky"
