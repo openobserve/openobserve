7c8c6af80250ba263061268cd4d0cda537f49fc3
Bhargav <BJP232004@GMAIL.COM>
2026-04-17 19:08:34 +0530

    fix: ui fixes (#11314)

--- web/src/components/alerts/AlertList.vue
+++ web/src/components/alerts/AlertList.vue
@@ -194,7 +194,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                 style="width: 100%"
                 :style="
                   filteredResults?.length
-                    ? 'width: 100%; height: calc(100vh  - var(--navbar-height) - 77px)'
+                    ? 'width: 100%; height: calc(100vh  - var(--navbar-height) - 87px)'
                     : 'width: 100%'
                 "
                 class="o2-quasar-table o2-row-md o2-quasar-table-header-sticky"
@@ -311,7 +311,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                                     ? 'warning'
                                     : props.row.status === 'disabled'
                                       ? 'grey'
-                                      : 'info'
+                                      : 'positive'
                             "
                             :label="props.row.status"
                             style="text-transform: capitalize; cursor: default"
