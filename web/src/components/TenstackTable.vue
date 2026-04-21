d8534c26bc677512aa6fcb1a6c4d9aba7a435b9e
omkark06 <omkar@zinclabs.io>
2026-04-21 15:45:44 +0530

    fix: updated table row border color

--- web/src/components/TenstackTable.vue
+++ web/src/components/TenstackTable.vue
@@ -585,7 +585,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                 formattedRows?.[virtualRow.index]?.original?.isExpandedRow
               "
               :ref="(node: any) => node && rowVirtualizer.measureElement(node)"
-              class="tw:absolute tw:flex tw:w-max tw:items-center tw:justify-start tw:border-b tw:cursor-pointer hover:tw:bg-[var(--o2-hover-gray)]"
+              class="tw:absolute tw:flex tw:w-max tw:items-center tw:justify-start tw:border-b tw:border-b-[var(--o2-tag-grey-1)] tw:cursor-pointer hover:tw:bg-[var(--o2-hover-gray)]"
               :class="[
                 defaultColumns &&
                 !wrap &&
