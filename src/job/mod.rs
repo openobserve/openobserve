a383c19142efd6c8e68bcb701afcb1536ea4eb02
Yashodhan Joshi <yjdoc2@gmail.com>
2026-05-07 16:49:51 +0530

    fix: actually watch the storage providers
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- src/job/mod.rs
+++ src/job/mod.rs
@@ -891,6 +891,7 @@ pub async fn init() -> Result<(), anyhow::Error> {
         tokio::task::spawn(cipher::run());
         tokio::task::spawn(db::keys::watch());
         tokio::task::spawn(org_storage::run());
+        tokio::task::spawn(db::org_storage_providers::watch());
     }
 
     #[cfg(feature = "vectorscan")]
