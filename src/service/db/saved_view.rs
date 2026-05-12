5339c5e9065731d89a2887a99343d0696a4d216d
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2026-05-12 17:18:29 +0530

    fix: cargo clippy

--- src/service/db/saved_view.rs
+++ src/service/db/saved_view.rs
@@ -26,7 +26,10 @@ use crate::{
 pub const SAVED_VIEWS_KEY_PREFIX: &str = "/organization/savedviews";
 
 pub async fn set_view(org_id: &str, view: &CreateViewRequest) -> Result<View, Error> {
-    if view_exists_with_name(org_id, &view.view_name).await.is_some() {
+    if view_exists_with_name(org_id, &view.view_name)
+        .await
+        .is_some()
+    {
         return Err(Error::Message(format!(
             "Saved view with name '{}' already exists in this organization",
             view.view_name
@@ -56,13 +59,13 @@ pub async fn update_view(
     view_id: &str,
     view: &UpdateViewRequest,
 ) -> Result<View, Error> {
-    if let Some(existing_id) = view_exists_with_name(org_id, &view.view_name).await {
-        if existing_id != view_id {
-            return Err(Error::Message(format!(
-                "Saved view with name '{}' already exists in this organization",
-                view.view_name
-            )));
-        }
+    if let Some(existing_id) = view_exists_with_name(org_id, &view.view_name).await
+        && existing_id != view_id
+    {
+        return Err(Error::Message(format!(
+            "Saved view with name '{}' already exists in this organization",
+            view.view_name
+        )));
     }
     let key = format!("{SAVED_VIEWS_KEY_PREFIX}/{org_id}/{view_id}");
     let updated_view = match get_view(org_id, view_id).await {
