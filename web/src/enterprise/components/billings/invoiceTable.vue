ff71b1f3631bbb853415e953c4efc03c6a3c7fe5
Kirtan Thakkar <kirtan@kiara.tech>
2026-05-15 11:33:16 +0530

    refactor: update icon imports and replace Download component with OIcon in invoiceTable

--- web/src/enterprise/components/billings/invoiceTable.vue
+++ web/src/enterprise/components/billings/invoiceTable.vue
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           size="icon-sm"
           class="q-ml-xs"
         >
-          <Download class="tw:size-4" />
+          <OIcon name="download" size="sm" />
         </OButton>
       </q-td>
     </template>
@@ -80,6 +80,7 @@ import QTablePagination from "@/components/shared/grid/Pagination.vue";
 import BillingService from "@/services/billings";
 import { getImageURL } from "@/utils/zincutils";
 import OButton from '@/lib/core/Button/OButton.vue';
+import OIcon from '@/lib/core/Icon/OIcon.vue';
 
 export default defineComponent({
   name: "InvoiceHistory",
@@ -87,7 +88,7 @@ export default defineComponent({
     NoData,
     QTablePagination,
     OButton,
-    Download,
+    OIcon,
   },
   props: [],
   setup(props) {
