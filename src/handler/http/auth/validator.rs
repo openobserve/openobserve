ccb2f925990e65b19ae25340420e1fa1b2801871
Subhra264 <chakrabortysubhradeep556@gmail.com>
2026-05-18 17:55:39 +0530

    fix: check if user password is empty in the authentication (#11906)

--- src/handler/http/auth/validator.rs
+++ src/handler/http/auth/validator.rs
@@ -369,6 +369,17 @@ pub async fn validate_credentials(
     if (path_columns.len() == 1 || INGESTION_EP.iter().any(|s| path_columns.contains(s)))
         && user.token.eq(&user_password)
     {
+        if user_password.is_empty() {
+            return Ok(TokenValidationResponse {
+                is_valid: false,
+                user_email: "".to_string(),
+                is_internal_user: false,
+                user_role: None,
+                user_name: "".to_string(),
+                family_name: "".to_string(),
+                given_name: "".to_string(),
+            });
+        }
         return Ok(build_token_validation_response(&user));
     }
 
