1570f2a953fe3f80b7a3f1a80d57a3bb77d48ca5
Huaijin <haohuaijin@gmail.com>
2026-05-18 20:00:15 +0800

    feat: impl tantivy footer cache (#11895)

--- src/infra/src/cache/mod.rs
+++ src/infra/src/cache/mod.rs
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+pub mod bytes_cache;
 pub mod file_data;
 pub mod meta;
 pub mod stats;
