cce933405002d8f10973f91a1664854befe911c7
omkark06 <omkar@zinclabs.io>
2026-05-19 15:38:34 +0530

    fix: updated logs sidebar collapse style

--- web/src/plugins/logs/Index.vue
+++ web/src/plugins/logs/Index.vue
@@ -119,7 +119,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                 </OButton>
               </template>
               <template #after>
-                <div class="tw:pr-[0.625rem] tw:pb-[0.625rem] tw:h-full">
+                <div class="tw:px-[0.625rem] tw:pb-[0.625rem] tw:h-full">
                   <div
                     class="card-container tw:h-full tw:w-full relative-position"
                   >
