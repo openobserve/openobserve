3e1253cae918272e83039bcee46bbdf0e8ef8c3d
omkark06 <omkar@zinclabs.io>
2026-05-19 16:28:50 +0530

    fix: updated qsplitter

--- web/src/lib/core/Splitter/OSplitter.vue
+++ web/src/lib/core/Splitter/OSplitter.vue
@@ -7,7 +7,7 @@
       'o-splitter',
       horizontal ? 'o-splitter--horizontal' : 'o-splitter--vertical',
       'tw:flex',
-      horizontal ? 'tw:flex-row' : 'tw:flex-col'
+      horizontal ? 'tw:flex-col' : 'tw:flex-row'
     ]"
   >
     <!-- Before slot -->
@@ -15,7 +15,7 @@
       :class="[
         'o-splitter__before',
         'tw:overflow-hidden',
-        horizontal ? 'tw:h-full' : 'tw:w-full'
+        horizontal ? 'tw:w-full' : 'tw:h-full'
       ]"
       :style="beforeStyle"
     >
@@ -34,7 +34,7 @@
         'tw:absolute',
         'tw:z-10',
         disable ? 'tw:cursor-not-allowed tw:opacity-50' : '',
-        horizontal ? 'tw:w-[4px] tw:h-full tw:cursor-col-resize!' : 'tw:h-[4px] tw:w-full tw:cursor-row-resize!',
+        horizontal ? 'tw:h-[4px] tw:w-full tw:cursor-row-resize!' : 'tw:w-[4px] tw:h-full tw:cursor-col-resize!',
         separatorClass
       ]"
       :style="[separatorStyle, separatorPosition]"
@@ -56,7 +56,7 @@
       :class="[
         'o-splitter__after',
         'tw:overflow-hidden',
-        horizontal ? 'tw:h-full' : 'tw:w-full'
+        horizontal ? 'tw:w-full' : 'tw:h-full'
       ]"
       :style="afterStyle"
     >
@@ -72,7 +72,7 @@ import type { OSplitterProps, OSplitterEmits } from './OSplitter.types'
 
 const props = withDefaults(defineProps<OSplitterProps>(), {
   horizontal: false,
-  limits: () => [0, 100],
+  limits: () => [0, 0],
   unit: '%',
   disable: false,
   separator: true,
@@ -85,12 +85,12 @@ const emit = defineEmits<OSplitterEmits>()
 const containerRef = ref<HTMLElement | null>(null)
 
 // Determine min/max values from limits prop
-const minValue = computed(() => props.limits?.[0] || 0)
-const maxValue = computed(() => props.limits?.[1] || 100)
+const minValue = computed(() => props.limits?.[0] ||  (props.unit ==='%' ? 0 : 0))
+const maxValue = computed(() => props.limits?.[1] || (props.unit ==='%' ? 100 : 1000))
 
 // Setup resizer composable with faster throttling for smoother movement
 const { value: currentValue, isResizing, onMouseDown } = useResizer({
-  direction: props.horizontal ? 'horizontal' : 'vertical',
+  direction: !props.horizontal ? 'horizontal' : 'vertical',
   initialValue: props.modelValue,
   minValue: minValue.value,
   maxValue: maxValue.value,
@@ -107,8 +107,8 @@ const { value: currentValue, isResizing, onMouseDown } = useResizer({
 const beforeStyle = computed(() => {
   const size = `${currentValue.value}${props.unit}`
   return props.horizontal
-    ? { width: size }
-    : { height: size }
+    ? { height: size }
+    : { width: size }
 })
 
 const afterStyle = computed(() => {
@@ -117,16 +117,16 @@ const afterStyle = computed(() => {
     : `calc(100% - ${currentValue.value}px)`
 
   return props.horizontal
-    ? { width: remainingSize }
-    : { height: remainingSize }
+    ? { height: remainingSize }
+    : { width: remainingSize }
 })
 
 // Separator absolute positioning
 const separatorPosition = computed(() => {
   const position = `${currentValue.value}${props.unit}`
   return props.horizontal
-    ? { left: position, top: '0' }
-    : { top: position, left: '0' }
+    ? { top: position, left: '0' }
+    : { left: position, top: '0' }
 })
 
 // Keyboard navigation support
