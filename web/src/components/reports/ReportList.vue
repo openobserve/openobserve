a969d7ce703cb4ebe240926fc866753e9788bbfb
omkark06 <omkar@zinclabs.io>
2026-05-21 10:52:04 +0530

    fix: filter scheduled reports

--- web/src/components/reports/ReportList.vue
+++ web/src/components/reports/ReportList.vue
@@ -495,7 +495,7 @@ const loadReports = async (folderId: string, nameQuery?: string) => {
       store.state.selectedOrganization.identifier,
       folder,
       undefined,
-      isCache || undefined,
+      isCache,
       nameQuery || undefined,
     );
 
