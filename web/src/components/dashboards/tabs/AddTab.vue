af315c4ad91815eb5952d818a1b418d4113d1922
ktx-riya <riya.kukadiya@kiara.tech>
2026-05-24 16:08:14 +0530

    fix: ux revamp layout issues (#12176)

--- web/src/components/dashboards/tabs/AddTab.vue
+++ web/src/components/dashboards/tabs/AddTab.vue
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <template>
   <ODrawer data-test="add-tab-dialog"
     :open="open"
-    size="md"
+    :width="20"
     :title="editMode ? 'Edit Tab' : 'Add Tab'"
     secondary-button-label="Cancel"
     primary-button-label="Save"
