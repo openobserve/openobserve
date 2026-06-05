d7bc6b6bbd85ad268242d280e56ee3f395e3fef6
ktx-riya <riya.kukadiya@kiara.tech>
2026-06-05 18:07:31 +0530

    fix: disable frame for dashboard charts in TracesMetricsDashboard and added bg for the footer seciton of fieldlist selection section

--- web/src/plugins/traces/metrics/TracesMetricsDashboard.vue
+++ web/src/plugins/traces/metrics/TracesMetricsDashboard.vue
@@ -27,6 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             v-if="show"
             ref="dashboardChartsRef"
             :viewOnly="true"
+            :frame="false"
             :dashboardData="dashboardData || {}"
             :currentTimeObj="currentTimeObj"
             :allowAlertCreation="false"
