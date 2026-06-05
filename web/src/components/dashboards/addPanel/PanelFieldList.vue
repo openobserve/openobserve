5dd5d6f73c7a0c4c624ae00f08f060fa3f64068c
ktx-riya <riya.kukadiya@kiara.tech>
2026-06-05 17:55:59 +0530

    fix: added border for metrics and appPanle

--- web/src/components/dashboards/addPanel/PanelFieldList.vue
+++ web/src/components/dashboards/addPanel/PanelFieldList.vue
@@ -1,7 +1,7 @@
 <!-- Copyright 2026 OpenObserve Inc. -->
 
 <template>
-  <div class="tw:w-full tw:h-full tw:flex tw:flex-col tw:px-3 tw:bg-surface-panel">
+  <div class="tw:w-full tw:h-full tw:flex tw:flex-col tw:px-3 tw:bg-surface-panel tw:border-r tw:border-border-default">
     <div class="tw:text-base tw:font-bold tw:flex-shrink-0 tw:my-3">
       {{ t("panel.fields") }}
     </div>
