5be5a302d3008bac9c98dbf5786e59bf7f6a9449
Huaijin <haohuaijin@gmail.com>
2026-06-17 11:49:37 +0800

    perf: improve approx_distinct performance (#12714)
    - close https://github.com/openobserve/o2-enterprise/issues/1862
    
    ## todo
    - [x] fix access plan
    - [x] use forked datafusion

--- src/service/search/datafusion/table_provider/listing_adapter.rs
+++ src/service/search/datafusion/table_provider/listing_adapter.rs
@@ -244,9 +244,7 @@ fn handler_tantivy_index(
                     .into_inner()
                     .into_iter()
                     .map(|mut file| {
-                        if let Some(access_plan) = generate_access_plan(&file) {
-                            file = file.with_extension(access_plan);
-                        }
+                        generate_access_plan(&mut file);
                         file
                     })
                     .collect();
