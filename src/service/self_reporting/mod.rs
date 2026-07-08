b5d67b3de7e69a806d18752a09bdcd83651908dd
Subhra264 <chakrabortysubhradeep556@gmail.com>
2026-07-08 13:57:08 +0530

    fix: self_reporting_timeout_errors and self_reporting_dropped_triggers metrics label mismatch (#13086)
    The two metrics were using an empty string as the label name even though
    no label names were registered in the metrics. Hence, whenever this
    metric is emitted, there is a runtime panic. This pr fixes that.

--- src/service/self_reporting/mod.rs
+++ src/service/self_reporting/mod.rs
@@ -279,7 +279,7 @@ pub fn publish_triggers_usage(trigger: TriggerData) {
 
             // Increment dropped triggers metric
             metrics::SELF_REPORTING_DROPPED_TRIGGERS
-                .with_label_values(&[""])
+                .with_label_values(&["usage"])
                 .inc();
         }
         Err(tokio::sync::mpsc::error::TrySendError::Closed(_)) => {
@@ -345,7 +345,7 @@ pub async fn publish_error(error_data: ErrorData) {
                 error_source
             );
             metrics::SELF_REPORTING_TIMEOUT_ERRORS
-                .with_label_values(&[""])
+                .with_label_values(&["error"])
                 .inc();
         }
         Err(EnqueueError::SendFailed(e)) => {
