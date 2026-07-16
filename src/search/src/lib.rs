0250b9c067eb4db1e17e28b05ae9b8d015dcdbe9
haohuaijin <haohuaijin@gmail.com>
2026-07-16 22:38:46 +0800

    refactor: move search engine into search crate

--- src/search/src/lib.rs
+++ src/search/src/lib.rs
@@ -13,11 +13,18 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+pub mod bloom_pruner;
 #[cfg(feature = "enterprise")]
 pub mod cipher;
+pub mod datafusion;
 pub mod enrichment;
 pub mod file_cache;
+pub mod index;
+pub mod inspector;
+pub mod sql;
+pub mod tantivy;
 pub mod types;
+pub mod utils;
 
 use config::meta::{
     search::{Response, ScanStats},
