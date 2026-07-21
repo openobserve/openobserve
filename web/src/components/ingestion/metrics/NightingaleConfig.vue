2be4fc627c86eabd1373f517ec3e69b873e4827b
IluckySi <ilucky@apache.org>
2026-07-21 16:39:52 +0800

    feat(web): add nightingale option in custom metric ingestion (#13264)
    This PR adds nightingale as a explicit custom metric ingestion.
    github: https://github.com/ccfos/nightingale
    
    The configuration as follows：
    
    > [[Pushgw.Writers]]
    > Url = "http://localhost:5080/api/default/prometheus/api/v1/write"
    > BasicAuthUser = "root@example.com"
    > BasicAuthPass = "o2oi_XUppAyGUwBxTvGnjXcg5ybFnVE4WnjOC"
    
    <img width="2940" height="1004" alt="image"
    src="https://github.com/user-attachments/assets/1683255f-1591-4a40-bfe0-ea52c9484746"
    />
    <img width="2926" height="1614" alt="image"
    src="https://github.com/user-attachments/assets/c3c3b181-5b6a-4104-b7e4-8857597dfd8e"
    />
    
    Co-authored-by: Hengfei Yang <hengfei.yang@gmail.com>

---
+++ web/src/components/ingestion/metrics/NightingaleConfig.vue
@@ -0,0 +1,69 @@
+<!-- Copyright 2026 OpenObserve Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<template>
+  <IngestionContent>
+    <CopyContent class="copy-content-container-cls" :content="content" />
+    <IngestionDocLink href="https://github.com/ccfos/nightingale">
+      to learn how to ingest metrics using Nightingale
+    </IngestionDocLink>
+  </IngestionContent>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useStore } from "vuex";
+import { getEndPoint, getIngestionURL } from "../../../utils/zincutils";
+import CopyContent from "@/components/CopyContent.vue";
+import IngestionContent from "@/components/ingestion/IngestionContent.vue";
+import IngestionDocLink from "@/components/ingestion/IngestionDocLink.vue";
+
+export default defineComponent({
+  name: "nightingale-config",
+  props: {
+    currOrgIdentifier: {
+      type: String,
+    },
+    currUserEmail: {
+      type: String,
+    },
+  },
+  components: { CopyContent, IngestionContent, IngestionDocLink },
+  setup() {
+    const store = useStore();
+    const endpoint: any = ref({
+      url: "",
+      host: "",
+      port: "",
+      protocol: "",
+      tls: "",
+    });
+
+    const ingestionURL = getIngestionURL();
+    endpoint.value = getEndPoint(ingestionURL);
+
+    const content = `[[Pushgw.Writers]]
+Url = "${endpoint.value.url}/api/${store.state.selectedOrganization.identifier}/prometheus/api/v1/write"
+BasicAuthUser = "[EMAIL]"
+BasicAuthPass = "[PASSCODE]"`;
+
+    return {
+      content,
+    };
+  },
+});
+</script>
+
