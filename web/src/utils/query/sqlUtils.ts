35733709dab910649666a83768d63efd6eb19e6d
ktx-abhay <abhay.padamani@kiara.tech>
2026-07-28 16:05:29 +0530

    fix: specify type for streamName variable in getStreamNameFromQuery function

--- web/src/utils/query/sqlUtils.ts
+++ web/src/utils/query/sqlUtils.ts
@@ -935,7 +935,7 @@ export const convertQueryIntoSingleLine = async (query: any) => {
 };
 
 export const getStreamNameFromQuery = async (query: any) => {
-  let streamName = null;
+  let streamName: string | null = null;
   try {
     await importSqlParser();
     try {
