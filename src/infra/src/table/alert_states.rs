7b682044520a993a6a7e90bf8c3afc5f68ad7a09
ktx-abhay <abhay.padamani@kiara.tech>
2026-08-20 13:59:51 +0530

    feat: add icon field to folder creation in alert states

--- src/infra/src/table/alert_states.rs
+++ src/infra/src/table/alert_states.rs
@@ -1072,6 +1072,7 @@ mod tests {
             name: Set("default".to_string()),
             description: Set(None),
             r#type: Set(0),
+            icon: Set(None),
         }
         .insert(conn)
         .await
