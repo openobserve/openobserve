fea81150bcd0b054f1f8cb846441c7a3440ee088
hengfeiyang <hengfei.yang@gmail.com>
2026-08-24 22:36:37 +0800

    fix: panic on timestamp get

--- src/core/src/logs/otlp.rs
+++ src/core/src/logs/otlp.rs
@@ -411,11 +411,9 @@ pub async fn handle_request(
                         }
 
                         for (idx, mut res) in stream_pl_results {
+                            let timestamp = timestamps.get(idx).copied().unwrap_or_default();
                             if let Err(e) = super::handle_timestamp_for_value(
-                                &mut res,
-                                timestamps[idx],
-                                timestamps[idx],
-                                timestamps[idx],
+                                &mut res, timestamp, timestamp, timestamp,
                             ) {
                                 stream_status.status.failed += 1;
                                 stream_status.status.error = e.to_string();
@@ -540,12 +538,10 @@ pub async fn handle_request(
             .any(|p| p.kind == config::meta::pipeline::PipelineKind::User);
         if !has_user_pipeline && !json_data_by_stream.contains_key(&stream_name) {
             for (idx, mut res) in pipeline_inputs.iter().cloned().enumerate() {
-                if let Err(e) = super::handle_timestamp_for_value(
-                    &mut res,
-                    timestamps[idx],
-                    timestamps[idx],
-                    timestamps[idx],
-                ) {
+                let timestamp = timestamps.get(idx).copied().unwrap_or_default();
+                if let Err(e) =
+                    super::handle_timestamp_for_value(&mut res, timestamp, timestamp, timestamp)
+                {
                     stream_status.status.failed += 1;
                     stream_status.status.error = e.to_string();
                     metrics::INGEST_ERRORS
