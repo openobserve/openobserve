17e7016a8092df71f53dedc66e7e7ebe5844b0be
hengfeiyang <hengfei.yang@gmail.com>
2026-09-09 16:30:39 +0800

    fix: use client rw for get stats

--- src/infra/src/file_list/postgres.rs
+++ src/infra/src/file_list/postgres.rs
@@ -1107,7 +1107,9 @@ WHERE stream = $1 {time_filter}
 GROUP BY stream;
             "#
         );
-        let pool = CLIENT_RO.clone();
+        // we use RW here to avoid error: canceling statement due to conflict with recovery
+        // PG SQLSTATE 40001
+        let pool = CLIENT_RW.clone();
         DB_QUERY_NUMS
             .with_label_values(&["stats_by_date_range", "file_list"])
             .inc();
