041e3e75e739eb5fee21d9676daf23330a1e8615
Huaijin <haohuaijin@gmail.com>
2026-09-10 12:59:13 +0000

    refactor(promql): remove redundant aggregation wrappers (#14390)

--- src/promql/src/aggregations/group.rs
+++ src/promql/src/aggregations/group.rs
@@ -13,30 +13,12 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use config::meta::promql::value::{EvalContext, Sample, Value};
-use datafusion::error::Result;
+use config::meta::promql::value::Sample;
 use hashbrown::HashSet;
-use promql_parser::parser::LabelModifier;
 
 use crate::aggregations::{Accumulate, AggFunc};
 
 /// https://prometheus.io/docs/prometheus/latest/querying/operators/#aggregation-operators
-pub fn group(param: &Option<LabelModifier>, data: Value, eval_ctx: &EvalContext) -> Result<Value> {
-    let start = std::time::Instant::now();
-    log::info!(
-        "[trace_id: {}] [PromQL Timing] group() started",
-        eval_ctx.trace_id,
-    );
-
-    let result = super::eval_aggregate(param, data, Group, eval_ctx);
-    log::info!(
-        "[trace_id: {}] [PromQL Timing] group() execution took: {:?}",
-        eval_ctx.trace_id,
-        start.elapsed()
-    );
-    result
-}
-
 pub struct Group;
 
 impl AggFunc for Group {
@@ -88,15 +70,16 @@ impl Accumulate for GroupAccumulate {
 mod tests {
     use std::sync::Arc;
 
-    use config::meta::promql::value::{Label, RangeValue, Sample, Value};
+    use config::meta::promql::value::{EvalContext, Label, RangeValue, Sample, Value};
 
     use super::*;
+    use crate::aggregations::eval_aggregate;
 
     #[test]
     fn test_group_value_none_input() {
         let timestamp = 1640995200;
         let eval_ctx = EvalContext::new(timestamp, timestamp + 1, 1, "test".to_string());
-        let result = group(&None, Value::None, &eval_ctx).unwrap();
+        let result = eval_aggregate(&None, Value::None, Group, &eval_ctx).unwrap();
         assert!(matches!(result, Value::None));
     }
 
@@ -104,7 +87,7 @@ mod tests {
     fn test_group_invalid_input_returns_err() {
         let timestamp = 1640995200;
         let eval_ctx = EvalContext::new(timestamp, timestamp + 1, 1, "test".to_string());
-        let result = group(&None, Value::Float(1.0), &eval_ctx);
+        let result = eval_aggregate(&None, Value::Float(1.0), Group, &eval_ctx);
         assert!(result.is_err());
     }
 
@@ -112,7 +95,7 @@ mod tests {
     fn test_group_empty_matrix_returns_none() {
         let timestamp = 1640995200;
         let eval_ctx = EvalContext::new(timestamp, timestamp + 1, 1, "test".to_string());
-        let result = group(&None, Value::Matrix(vec![]), &eval_ctx).unwrap();
+        let result = eval_aggregate(&None, Value::Matrix(vec![]), Group, &eval_ctx).unwrap();
         assert!(matches!(result, Value::None));
     }
 
@@ -155,7 +138,7 @@ mod tests {
         let eval_ctx = EvalContext::new(timestamp, timestamp + 1, 1, "test".to_string());
 
         // Test group without label grouping - should return 1.0 for each group
-        let result = group(&None, data.clone(), &eval_ctx).unwrap();
+        let result = eval_aggregate(&None, data.clone(), Group, &eval_ctx).unwrap();
 
         match result {
             Value::Matrix(matrix) => {
