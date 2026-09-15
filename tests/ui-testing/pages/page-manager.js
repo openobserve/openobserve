3391a8d0427ebdf625e69e5270c2671d09402046
Shrinath-O2 <214651540+Shrinath-O2@users.noreply.github.com>
2026-09-15 14:10:43 +0000

    test(autoe2e): heal generated tests + strip hand-off before PR

--- tests/ui-testing/pages/page-manager.js
+++ tests/ui-testing/pages/page-manager.js
@@ -91,6 +91,7 @@ import { CrossLinkPage } from "./generalPages/crossLinkPage.js";
 import { ModelPricingPage } from "./generalPages/modelPricingPage.js";
 import { EditionFeaturesPage } from "./generalPages/editionFeaturesPage.js";
 import { OrganizationManagementPage } from "./generalPages/organizationManagementPage.js";
+import { LogoManagementPage } from "./generalPages/logoManagementPage.js";
 import { StatusPagesPage } from "./generalPages/statusPagesPage.js";
 import { RegexPatternsFormValidationPage } from "./generalPages/regexPatternsFormValidationPage.js";
 import { CipherKeysFormValidationPage } from "./generalPages/cipherKeysFormValidationPage.js";
@@ -228,6 +229,7 @@ class PageManager {
     this.modelPricingPage = new ModelPricingPage(page);
     this.editionFeaturesPage = new EditionFeaturesPage(page);
     this.organizationManagementPage = new OrganizationManagementPage(page);
+    this.logoManagementPage = new LogoManagementPage(page);
     this.statusPagesPage = new StatusPagesPage(page);
     this.regexPatternsFormValidation = new RegexPatternsFormValidationPage(page);
     this.sharedComponentsFormValidation = new SharedComponentsFormValidationPage(page);
