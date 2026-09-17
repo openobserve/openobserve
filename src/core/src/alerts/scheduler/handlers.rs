f0c1b0126ff0bfd29b82b3f52b1b08a8915baa98
Yashodhan Joshi <yashodhan@openobserve.ai>
2026-09-17 18:36:50 +0530

    fix: correctly handle silence for pending period (#14563)
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>
    Co-authored-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- src/core/src/alerts/scheduler/handlers.rs
+++ src/core/src/alerts/scheduler/handlers.rs
@@ -2219,6 +2219,16 @@ async fn handle_alert_triggers(
                         } else {
                             None
                         };
+                        // reset the next run time without silence, because this was never
+                        // delivered, simply pending
+                        new_trigger.next_run_at = alert.trigger_condition.get_next_trigger_time(
+                            true,
+                            alert.tz_offset,
+                            false,
+                            None,
+                        )?;
+                        new_trigger.is_silenced = false;
+                        trigger_data_stream.next_run_at = new_trigger.next_run_at;
                         new_trigger.data = json::to_string(&trigger_data).unwrap();
                         db::scheduler::update_trigger(new_trigger, true, &query_trace_id).await?;
                         // Condition matched; only the notification was
@@ -2247,6 +2257,13 @@ async fn handle_alert_triggers(
                             } else {
                                 None
                             };
+                            // reset the next run time without silence, because this was never
+                            // delivered, simply pending
+                            new_trigger.next_run_at = alert
+                                .trigger_condition
+                                .get_next_trigger_time(true, alert.tz_offset, false, None)?;
+                            new_trigger.is_silenced = false;
+                            trigger_data_stream.next_run_at = new_trigger.next_run_at;
                             new_trigger.data = json::to_string(&trigger_data).unwrap();
                             db::scheduler::update_trigger(new_trigger, true, &query_trace_id)
                                 .await?;
@@ -2277,6 +2294,16 @@ async fn handle_alert_triggers(
                 } else {
                     None
                 };
+                // reset the next run time without silence, because this was never delivered,
+                // simply pending
+                new_trigger.next_run_at = alert.trigger_condition.get_next_trigger_time(
+                    true,
+                    alert.tz_offset,
+                    false,
+                    None,
+                )?;
+                new_trigger.is_silenced = false;
+                trigger_data_stream.next_run_at = new_trigger.next_run_at;
                 new_trigger.data = json::to_string(&trigger_data).unwrap();
                 db::scheduler::update_trigger(new_trigger, true, &query_trace_id).await?;
                 // Condition matched; only the notification was
