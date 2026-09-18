ae3946fddb8812f3fea96428257eb46f04c545d3
hengfeiyang <hengfei.yang@gmail.com>
2026-09-18 15:33:02 +0800

    chore(config): default query partition strategy to file_hash

--- src/config/src/config.rs
+++ src/config/src/config.rs
@@ -1858,7 +1858,7 @@ pub struct Common {
     #[env_config(
         name = "ZO_FEATURE_QUERY_PARTITION_STRATEGY",
         parse,
-        default = "file_num"
+        default = "file_hash"
     )]
     pub feature_query_partition_strategy: QueryPartitionStrategy,
     #[env_config(
