0dd4f683478604762743ba4b2cb511ae9c625b1a
Yashodhan Joshi <yjdoc2@gmail.com>
2026-09-21 11:50:51 +0530

    feat: add BE for org_domain mapping
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- src/api/management/src/request/organization/settings.rs
+++ src/api/management/src/request/organization/settings.rs
@@ -19,6 +19,7 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use config::META_ORG_ID;
 use db::organization::{get_org_setting, set_org_setting};
 use infra::errors::{DbError, Error};
 #[cfg(feature = "enterprise")]
@@ -149,6 +150,59 @@ pub async fn create(
         }
         field_found = true;
         data.cross_links = cross_links;
+    }
+
+    // ignore this for all non _meta orgs
+    #[cfg(feature = "cloud")]
+    if org_id == META_ORG_ID
+        && let Some(mappings) = settings.domain_org_mappings
+    {
+        field_found = true;
+        for mapping in &mappings {
+            use o2_openfga::authorizer::groups::get_all_groups;
+
+            if openobserve_core::organization::get_org(&mapping.org_id)
+                .await
+                .is_none()
+            {
+                return MetaHttpResponse::bad_request(format!(
+                    "No org with org id {} found",
+                    mapping.org_id
+                ));
+            }
+
+            if !matches!(
+                mapping.base_role.as_str(),
+                "admin" | "editor" | "viewer" | "allowed_user"
+            ) {
+                return MetaHttpResponse::bad_request(format!(
+                    "base role {} for org {} is not a valid base role - only admin, editor, viewer, allowed_user are supported",
+                    mapping.base_role, mapping.org_id
+                ));
+            }
+            if let Some(group) = mapping.user_group.as_ref() {
+                let all_groups = match get_all_groups(&mapping.org_id, None).await {
+                    Ok(v) => v,
+                    Err(e) => {
+                        log::error!(
+                            "error getting all groups for {} when updating domain org mappings : {e}",
+                            mapping.org_id
+                        );
+                        return MetaHttpResponse::bad_request(format!(
+                            "error getting groups for org {} : {e}",
+                            mapping.org_id
+                        ));
+                    }
+                };
+                if !all_groups.contains(&group) {
+                    return MetaHttpResponse::bad_request(format!(
+                        "custom group {group} not found in org {}",
+                        mapping.org_id
+                    ));
+                }
+            }
+        }
+        data.domain_org_mappings = mappings;
     }
 
     if !field_found {
